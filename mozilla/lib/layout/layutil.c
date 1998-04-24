fb85da5998da2ddad542a7884c01e6a7016d67ef
mlm <>
1998-04-24 23:43:28 +0000

    Branch layout for OPS new functionality for upcoming ns->moz renaming

--- mozilla/lib/layout/layutil.c
+++ mozilla/lib/layout/layutil.c
@@ -2933,7 +2933,7 @@ lo_ConvertMochaEntities(MWContext * context, lo_DocState *state, PA_Tag * tag)
 	    return TRUE;
 
 	/* attempt to lock JS */
-	if (!LM_AttemptLockJS(lo_JSLockReleased, context))
+	if (!LM_AttemptLockJS(context, lo_JSLockReleased, context))
 	    return FALSE;
 
     /* 
@@ -3008,7 +3008,7 @@ lo_ConvertMochaEntities(MWContext * context, lo_DocState *state, PA_Tag * tag)
 	tag->data_len = XP_STRLEN((char *) tag->data);
 	PA_UNLOCK(tag->data);
 
-	LM_UnlockJS();
+	LM_UnlockJS(context);
 
 	return TRUE;
 
