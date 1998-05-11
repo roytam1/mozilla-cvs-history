e606d211537c2dd9196bfce9c516a061d72d27f3
hardts <>
1998-05-11 23:16:45 +0000

    temporary hack to disable loading RDF images

--- mozilla/cmd/winfe/cxicon.cpp
+++ mozilla/cmd/winfe/cxicon.cpp
@@ -208,7 +208,7 @@ void NSNavCenterImage::ProcessIcon()
 		//  We may be in the call stack of the image lib, and doing
 		//  lots of fast small get urls causes it to barf due
 		//  to list management not being reentrant.
-		FEU_ClientPull(iconContext->GetContext(), 0, NET_CreateURLStruct(pUrl, NET_DONT_RELOAD), FO_CACHE_AND_PRESENT, FALSE);
+//		FEU_ClientPull(iconContext->GetContext(), 0, NET_CreateURLStruct(pUrl, NET_DONT_RELOAD), FO_CACHE_AND_PRESENT, FALSE);
 	}
 	else 
 	{ // handle window internal format BMP
