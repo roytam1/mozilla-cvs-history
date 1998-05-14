7c51cb390bdbc0d4f2387d7045ccf83cda4c7d01
tao <>
1998-05-14 18:08:07 +0000

    Merge: Nova->Normandy, new files for Nova Addressbook features.

---
+++ mozilla/cmd/xfe/src/ViewDashBDlg.h
@@ -0,0 +1,70 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+/* -*- Mode: C++; tab-width: 4 -*-
+   ViewDashBDlg.h -- View dialog with a dashboard.
+   Created: Tao Cheng <tao@netscape.com>, 27-apr-98.
+ */
+
+
+#ifndef _VIEWDASHBDLG_H_
+#define _VIEWDASHBDLG_H_
+
+#include "ViewDialog.h"
+#include "Dashboard.h"
+
+class XFE_ViewDashBDlg : public XFE_ViewDialog
+{
+public:
+	XFE_ViewDashBDlg(Widget     parent, 
+					 char      *name, 
+					 MWContext *context,
+					 Boolean    ok     = True, 
+					 Boolean    cancel = True,
+					 Boolean    help   = False, 
+					 Boolean    apply  = False, 
+					 Boolean    modal  = False);
+	
+	virtual ~XFE_ViewDashBDlg();
+
+
+protected:
+	static Widget create_chrome_widget(Widget   parent, 
+									   char    *name,
+									   Boolean  ok, 
+									   Boolean  cancel,
+									   Boolean  help, 
+									   Boolean  apply,
+									   Boolean  separator, 
+									   Boolean  modal);
+
+	Widget createButtonArea(Widget parent,
+							Boolean ok, Boolean cancel,
+							Boolean help, Boolean apply);
+
+	virtual void attachView();
+
+	XFE_Dashboard *m_dashboard;
+	Widget         m_aboveButtonArea;
+	Widget         m_okBtn;
+	
+private:
+	Widget         m_buttonArea;
+};
+
+#endif /* _VIEWDASHBDLG_H_ */
