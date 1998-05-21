6918fbd8965ef92e92b82ab06f5c19230b9eb12e
pierre <>
1998-05-21 01:00:04 +0000

    Added prototype to fix Mac build

--- mozilla/lib/libnet/mkpop3.h
+++ mozilla/lib/libnet/mkpop3.h
@@ -21,6 +21,7 @@
 extern void NET_InitPop3Protocol(void);
 
 MODULE_PRIVATE XP_Bool NET_GetAllowAtSignInMailUserName();
+MODULE_PRIVATE int32 net_ProcessPop3 (ActiveEntry *ce);
 MODULE_PRIVATE void NET_SetAllowAtSignInMailUserName(XP_Bool allow);
 
 #endif /* not MKPOP3_H */
