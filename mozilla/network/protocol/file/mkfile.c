e07df67e4530ca28eb81f93d68b83ac9c39077d3
montulli <>
1998-06-05 23:41:37 +0000

    more stream fixes

--- mozilla/network/protocol/file/mkfile.c
+++ mozilla/network/protocol/file/mkfile.c
@@ -1696,14 +1696,14 @@ NET_HTTPIndexFormatToHTMLConverter(int         format_out,
 
 #define SANE_BUFLEN	1024
 
-NET_StreamClass *
+NET_VoidStreamClass *
 net_CloneWysiwygLocalFile(MWContext *window_id, URL_Struct *URL_s,
 			  uint32 nbytes, const char * wysiwyg_url,
 			  const char * base_href)
 {
 	char *filename;
 	XP_File fromfp;
-	NET_StreamClass *stream;
+	NET_VoidStreamClass *stream;
 	int32 buflen, len;
 	char *buf;
 
@@ -1721,7 +1721,7 @@ net_CloneWysiwygLocalFile(MWContext *window_id, URL_Struct *URL_s,
 		XP_FileClose(fromfp);
 		return 0;
 	  }
-	buflen = stream->is_write_ready(stream);
+	buflen = NET_StreamIsWriteReady(stream);
 	if (buflen > SANE_BUFLEN)
 		buflen = SANE_BUFLEN;
 	buf = (char *)PR_Malloc(buflen * sizeof(char));
@@ -1738,7 +1738,7 @@ net_CloneWysiwygLocalFile(MWContext *window_id, URL_Struct *URL_s,
 		len = XP_FileRead(buf, len, fromfp);
 		if (len <= 0)
 			break;
-		if (stream->put_block(stream, buf, len) < 0)
+		if (NET_StreamPutBlock(stream, buf, len) < 0)
 			break;
 		nbytes -= len;
 	  }
@@ -1747,8 +1747,8 @@ net_CloneWysiwygLocalFile(MWContext *window_id, URL_Struct *URL_s,
 	if (nbytes != 0)
 	  {
 		/* NB: Our caller must clear top_state->mocha_write_stream. */
-		stream->abort(stream, MK_UNABLE_TO_CONVERT);
-		PR_Free(stream);
+		NET_StreamAbort(stream, MK_UNABLE_TO_CONVERT);
+		NET_StreamFree(stream);
 		return 0;
 	  }
 	return stream;
