916d2293354912d2ac1d26c509a5ce293802b0da
danda <>
1998-06-12 02:39:17 +0000

    modifications to test application submitted by Patrick McCormick (pmccormi@mit.edu)

--- mozilla/modules/rdf/src/test.cpp
+++ mozilla/modules/rdf/src/test.cpp
@@ -34,14 +34,159 @@
  * 
  */
 
+/* $Id$ 
+ */
+
 #include "stdlib.h"
 #include "rdf-int.h"
 #include "rdf.h"
 #include "rdfparse.h"
+#include "mcf.h"
 
 const char    *dataSources[] = { 
    "rdf:remoteStore", NULL 
 }; 
+
+/* prototypes */
+void printDB(RDF rdf);
+void printRDFFile(RDFFile file);
+void printRDFNotif(RDF_Notification notif);
+
+/* fns */
+
+void printDB(RDF rdf)
+{
+    int i;
+    assert(rdf);
+
+    assert(rdf->translators);
+    for (i = 0; i < rdf->numTranslators; i++) {
+        RDFT trans = rdf->translators[i];
+        assert(trans);        
+        RDFL list = trans->rdf;
+        assert(list);
+    
+        printf("Translator #%d: %s\n", i, trans->url);
+
+        if (list->rdf != rdf) { /* the list DB should be the same as the current DB */
+            printf("Error: list->rdf = %p, rdf = %p, should be equal!\n", list->rdf, rdf);
+        }
+
+    }    
+
+    RDFFile file = rdf->files;    
+    if (file == NULL) {
+        printf("RDFDB->files is NULL\n");
+    }
+    else {
+        printRDFFile(file);
+    }
+
+    RDF_Notification notif = rdf->notifs;
+
+    if (notif == NULL) {
+        printf("RDFDB->notif is NULL\n");
+    }
+    else {
+        printRDFNotif(notif);
+    }
+}
+
+void printRDFFile(RDFFile file) 
+{
+    int i;
+
+    assert(file);
+
+    printf("1. List of All Assertions.\n");
+
+    assert(file->assertionList);
+    for (i = 0; i < file->assertionCount; i++) {
+        Assertion a = file->assertionList[i];
+        printf("Assertion Chain #%d:\n", i);
+        assert(a);
+
+        while (a != null) {
+            printf("Assertion: [u: %s, s: %s, ", a->u->url, a->s->url);
+
+            if (a->s == gCoreVocab->RDF_parent) {
+                printf("parent name: %s]\n", ((RDF_Resource)(a->value))->url);
+            }
+            else {
+                printf("value: %s]\n", a->value);
+            }
+            a = a->next;
+        }
+    }
+
+    printf("\n2. List of All Resources.\n");
+
+    printf("Resource Top: [u: %s]\n", file->top->url);
+    printf("Resource RTop: [u: %s]\n", file->rtop->url);
+
+    for (i = 0; i < file->resourceCount; i++) {
+        RDF_Resource r = file->resourceList[i];
+        printf("Resource #%d: [u: %s]\n", i, r->url);
+    }
+}
+
+void printRDFNotif(RDF_Notification notif)
+{
+    while (notif != NULL) {
+        printf("\tNotification: pdata = %s, event = ", notif->pdata);
+
+        RDF_EventType eventType = notif->theEvent->eventType;
+        switch(eventType) {
+        case RDF_ASSERT_NOTIFY:
+            printf("Assert\n"); break;
+        case RDF_DELETE_NOTIFY:
+            printf("Delete\n"); break;
+        case RDF_KILL_NOTIFY:
+            printf("Kill\n"); break;
+        case RDF_CREATE_NOTIFY:
+            printf("Create\n"); break;
+        case RDF_RESOURCE_GC_NOTIFY:
+            printf("ResourceGC\n"); break;
+        case RDF_INSERT_NOTIFY:
+            printf("Insert\n"); break;
+        default:
+            printf("Unknown\n"); break;
+        }
+
+        notif = notif->next;
+    }
+}
+
+void printChildren(RDF rdf, RDF_Resource root, int depth)
+{
+      RDF_Cursor c;
+      RDF_Resource u;
+      int i;
+      char spaces[40];
+    
+      for (i = 0; i < depth; i++) {
+         spaces[i] = ' ';
+      }
+      spaces[i] = 0;
+
+      i = 0;
+
+      c = RDF_GetSources(rdf, root, gCoreVocab->RDF_parent, RDF_RESOURCE_TYPE, true);
+      if (c)
+      {
+         printf("Listing children of resource %s:\n", root->url);
+
+         u = (RDF_Resource)RDF_NextValue(c);
+         while (u)
+         {
+            printf("%s%i: %s\n", spaces, ++i, u->url);
+            printChildren(rdf, u, depth+1);
+            u = (RDF_Resource)RDF_NextValue(c);
+         }
+
+         RDF_DisposeCursor(c);
+      }
+}
 
 void 
 main(int argc, char** argv) 
@@ -49,15 +194,22 @@ main(int argc, char** argv)
    RDF                     rdf; 
    RDF_Error               err; 
    RDF_InitParamsStruct    initParams = {0}; 
-   RDF_Resource  u, s, root; 
+   RDF_Resource  u, s, root;
    void   *v; 
    RDFFile file;
-#ifdef XP_WIN
-   char* fileURL = (argc > 1) ? argv[1] : "file:///test.rdf";
-#else
-   char* fileURL = (argc > 1) ? argv[1] : "file://test.rdf";
-#endif
-   char* rootURL = (char*)getMem(200);
+
+   char *fileURL = argv[1];
+   char *rootURL = (char*)getMem(200);
+
+   /* If no URL specified, print out usage info. */
+
+   if (argc < 2) {
+     printf("Mozilla RDF Standalone Test Utility\n");
+     printf("Usage: %s <URL>\n", argv[0]);
+     exit(1);
+   }
+
+   /* first init the RDF engine */
 
    err = RDF_Init(&initParams); 
    if (err)
@@ -67,8 +219,8 @@ main(int argc, char** argv)
    }
    else
    {
-      printf("RDF Init success\n"); 
-   } 
+      printf("RDF_Init: success\n"); 
+   }
 
   /* 
    * Try and get a reference to the remote store DB 
@@ -81,8 +233,9 @@ main(int argc, char** argv)
    }
    else
    {
-      printf("RDF_GetDB success\n"); 
-   } 
+      printf("RDF_GetDB: success\n"); 
+   }
+
    sprintf(rootURL, "%s#root", fileURL);
    root = (RDF_Resource)RDF_GetResource(NULL, rootURL, PR_TRUE);
    setResourceType(root, RDF_RT);
@@ -92,18 +245,22 @@ main(int argc, char** argv)
    s = gCoreVocab->RDF_name; 
    v = "Dan Libby";  
 
+   printf("\nTEST: Adding assertion to RDF DB.\n");
+
    /* make an assertion into RDF's graph */
-   RDF_Assert(rdf, u,s,v, RDF_STRING_TYPE);  
+   RDF_Assert(rdf, u, s, v, RDF_STRING_TYPE);
 
    /* check to see if assertion exists */
-   if (!RDF_HasAssertion(rdf,u,s,v, RDF_STRING_TYPE, true))
+   if (!RDF_HasAssertion(rdf, u, s, v, RDF_STRING_TYPE, true))
    {
-      printf("Assertion failure.\n"); 
+      printf("Assertion does not exist!\n\n"); 
    }
    else
    {
-      printf("Assertion success.\n"); 
+      printf("Assertion [%s,%s,%s] exists.\n\n", u, s, v); 
    } 
+
+   printDB(rdf);
 
    /* Import an RDF file */
    printf("Reading \"%s\"\n", fileURL);
@@ -115,26 +272,22 @@ main(int argc, char** argv)
       printf("\"%s\" read in successfully. (%i assertions)\n", fileURL, file->assertionCount);
       fflush(stdout);
 
+      printf("\nTEST: Listing all Assertions and Resources in File Structure.\n");
+
+      printRDFFile(file);
+
       PRFileDesc *fd = PR_GetSpecialFD(PR_StandardOutput);
-      outputRDFTree (rdf, fd, root);
+ 
+      /* in order to get the top of the tree, I use this "0-th element" hack.  Isn't there
+         a more elegant way to find the root node of an RDF file?  */
 
-#if 0
-      RDF_Cursor c;
-      int i = 0;
+      printf("\nTEST: Print out file from DB.\n");
 
-      c = RDF_GetSources(rdf, root, gCoreVocab->RDF_parent, RDF_RESOURCE_TYPE, true);
-      if (c)
-      {
-         u = (RDF_Resource)RDF_NextValue(c);
-         while (u)
-         {
-            printf("%i: %s\n", ++i, u->url);
-            u = (RDF_Resource)RDF_NextValue(c);
-         }
+      outputRDFTree (rdf, fd, file->resourceList[0]); 
 
-         RDF_DisposeCursor(c);
-      }
-#endif
+      printf("\nTEST: Print Tree (homemade version).\n");
+
+      printChildren(rdf, file->resourceList[0], 0);
    }
    else
    {
