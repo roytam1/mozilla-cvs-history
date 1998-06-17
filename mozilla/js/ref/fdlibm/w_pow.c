1707e827c76543da3581e33b6aed14768072ef29
henrit <>
1998-06-17 14:29:21 +0000

    ECMA compliant Math object

---
+++ mozilla/js/ref/fdlibm/w_pow.c
@@ -0,0 +1,68 @@
+
+
+/* @(#)w_pow.c 1.3 95/01/18 */
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunSoft, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice 
+ * is preserved.
+ * ====================================================
+ */
+
+/* 
+ * wrapper pow(x,y) return x**y
+ */
+
+#include "fdlibm.h"
+
+
+#ifdef __STDC__
+	double fd_pow(double x, double y)	/* wrapper pow */
+#else
+	double fd_pow(x,y)			/* wrapper pow */
+	double x,y;
+#endif
+{
+#ifdef _IEEE_LIBM
+	return  __ieee754_pow(x,y);
+#else
+	double z;
+	z=__ieee754_pow(x,y);
+	if(_LIB_VERSION == _IEEE_|| fd_isnan(y)) return z;
+	if(fd_isnan(x)) {
+	    if(y==0.0) 
+	        return __kernel_standard(x,y,42); /* pow(NaN,0.0) */
+	    else 
+		return z;
+	}
+	if(x==0.0){ 
+#ifdef HPUX
+		if ((-1==fd_copysign(1, x))&&((y+1)/2==fd_floor((y+1)/2))) {
+			if (y>0)
+			  return x;
+			else
+			  return 1/x;
+		}
+#endif
+	    if(y==0.0)
+	        return __kernel_standard(x,y,20); /* pow(0.0,0.0) */
+	    if(fd_finite(y)&&y<0.0)
+	        return __kernel_standard(x,y,23); /* pow(0.0,negative) */
+	    return z;
+	}
+	if(!fd_finite(z)) {
+	    if(fd_finite(x)&&fd_finite(y)) {
+	        if(fd_isnan(z))
+	            return __kernel_standard(x,y,24); /* pow neg**non-int */
+	        else 
+	            return __kernel_standard(x,y,21); /* pow overflow */
+	    }
+	} 
+	if(z==0.0&&fd_finite(x)&&fd_finite(y))
+	    return __kernel_standard(x,y,22); /* pow underflow */
+	return z;
+#endif
+}
