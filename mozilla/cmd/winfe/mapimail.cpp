3c987be1c94351508d76cf56e69f6f3f3b8ba531
ricardob <>
1998-06-19 21:27:26 +0000

    followed jsw xxx suggestion

--- mozilla/cmd/winfe/mapimail.cpp
+++ mozilla/cmd/winfe/mapimail.cpp
@@ -1,4 +1,21 @@
-/* Insert copyright and license here 1997 */
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Mozilla Communicator client code.
+ *
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation.  Portions created by Netscape are Copyright (C) 1997
+ * Netscape Communications Corporation.  All Rights Reserved.
+ */
 //
 //  More MAPI Hooks for Communicator
 //  Written by: Rich Pizzarro (rhp@netscape.com)
@@ -738,11 +755,10 @@ BOOL                    winShowFlag = FALSE;
     lstrcat(ptr, (LPSTR) tempString);
   }
 
-  BOOL    bEncrypt = FALSE;
-  BOOL    bSign    = FALSE;
+  BOOL    bxxx = FALSE;
+  BOOL    bxxx2    = FALSE;
 
-  PREF_GetBoolPref("mail.crypto_sign_outgoing_mail", &bSign);
-  PREF_GetBoolPref("mail.encrypt_outgoing_mail", &bEncrypt);
+  
   MSG_CompositionFields *fields =
       MSG_CreateCompositionFields(real_addr, real_addr, 
                   toString, 
@@ -752,8 +768,8 @@ BOOL                    winShowFlag = FALSE;
 									"", subject, "",
 									"", "", "",
 									"", 
-                  bEncrypt,
-                  bSign);
+                  bxxx,
+                  bxxx2);
   if (!fields)
   {
     return(MAPI_E_FAILURE);
