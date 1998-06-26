c2114dc687bd23a989344fc2718222430da8e21d
beard <>
1998-06-26 23:59:29 +0000

    Macified.

--- mozilla/sun-java/stubs/include/jni_md.h
+++ mozilla/sun-java/stubs/include/jni_md.h
@@ -1,22 +1,49 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/*
+ * @(#)jni_md.h	1.2 96/10/24
  *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
- * http://www.mozilla.org/NPL/
+ * Copyright (c) 1993-1996 Sun Microsystems, Inc. All Rights Reserved.
  *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
+ * Permission to use, copy, modify, and distribute this software
+ * and its documentation for NON-COMMERCIAL purposes and without
+ * fee is hereby granted provided that this copyright notice
+ * appears in all copies.
  *
- * The Initial Developer of this code under the NPL is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+ * The Java source code is the confidential and proprietary information
+ * of Sun Microsystems, Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ *
+ * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
  */
 #ifndef JNI_MD_H
 #define JNI_MD_H
+
+#ifdef XP_MAC
+
+#define JNIEXPORT 
+#define JNICALL
+
+/*
+	Types.h is part of the MacOS Universal Interfaces release.
+	It defines SInt32, wide, and SInt8.
+	
+	We cannot use SInt64 for jlong because it maps to long long
+	when the compiler supports it, but MRJ 2.0 was built assumming
+	jlong was a struct which uses different calling conventions.
+*/
+#include <Types.h>
+
+typedef SInt32 		jint;
+typedef wide 		jlong;
+typedef UInt8 		jbyte;
+
+#else
 
 /* Get the rest of the stuff from jri_md.h */
 #include "jri_md.h"
@@ -24,5 +51,7 @@
 #ifndef FAR		/* for non-Win16 */
 #define FAR
 #endif
+
+#endif /* !XP_MAC */
 
 #endif /* JNI_MD_H */
