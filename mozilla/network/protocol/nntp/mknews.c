5c3f0f5d82381153dd36c373479d726c7192d6c2
morse <>
1998-07-12 11:51:34 +0000

    added single signon

--- mozilla/network/protocol/nntp/mknews.c
+++ mozilla/network/protocol/nntp/mknews.c
@@ -1812,9 +1812,17 @@ net_news_begin_authorize(ActiveEntry * ce)
 
 
 	if (!username) {
+#if defined(SingleSignon)
+	  username = SI_Prompt(ce->window_id,
+			       XP_GetString(XP_PROMPT_ENTER_USERNAME),
+			       "",
+			       cd->control_con->hostname);
+
+#else
 	  username = FE_Prompt(ce->window_id,
-						   XP_GetString(XP_PROMPT_ENTER_USERNAME),
-						   username ? username : "");
+			       XP_GetString(XP_PROMPT_ENTER_USERNAME),
+			       username ? username : "");
+#endif
 	  
 	  /* reset net_news_last_username_probably_valid to false */
 	  net_news_last_username_probably_valid = FALSE;
@@ -1933,10 +1941,22 @@ net_news_authorize_response(ActiveEntry * ce)
             *cp = '@';
     
           }
-		if (!password) {
-		  password = 
-			FE_PromptPassword(ce->window_id, XP_GetString( 
-			           XP_PLEASE_ENTER_A_PASSWORD_FOR_NEWS_SERVER_ACCESS ) );
+	if (!password) {
+#if defined(SingleSignon)
+	    password = 
+		SI_PromptPassword
+		    (ce->window_id,
+		    XP_GetString
+			(XP_PLEASE_ENTER_A_PASSWORD_FOR_NEWS_SERVER_ACCESS),
+		    cd->control_con->hostname,
+		    TRUE);
+#else
+	    password =
+		FE_PromptPassword
+		    (ce->window_id,
+		    XP_GetString
+			(XP_PLEASE_ENTER_A_PASSWORD_FOR_NEWS_SERVER_ACCESS));
+#endif
 		  net_news_last_username_probably_valid = FALSE;
 		}
 		  
