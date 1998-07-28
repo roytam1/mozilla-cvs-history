4cea0b12954e3499d824b00b918864bf7460a3d6
warren%netscape.com <>
1998-07-28 08:31:29 +0000

    Unix changes to build from OJI_19980618_TIP_MERGE1.

--- mozilla/cmd/xfe/src/plugin.cpp
+++ mozilla/cmd/xfe/src/plugin.cpp
@@ -948,7 +948,7 @@ FE_LoadPlugin(void *pdesc, NPNetscapeFuncs *funcs, np_handle* handle)
 
 	    if ((err != NS_OK)
                 || (userPlugin == NULL)
-                || (userPlugin->Initialize(thePluginManager) != nsPluginError_NoError)) {
+                || (userPlugin->Initialize(thePluginManager) != NS_OK)) {
 		PR_UnloadLibrary(plugin->dlopen_obj);
 		plugin->dlopen_obj = NULL;
 		return NULL;
