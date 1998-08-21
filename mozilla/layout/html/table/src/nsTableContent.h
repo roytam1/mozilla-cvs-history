456a10ab28bc09dca1e521021aab317d0819da46
joki%netscape.com <>
1998-08-21 21:38:26 +0000

    Modifying nsIContent's AppendChild -> AppendChildTo to stop conflict with DOM interfaces.

--- mozilla/layout/html/table/src/nsTableContent.h
+++ mozilla/layout/html/table/src/nsTableContent.h
@@ -100,7 +100,7 @@ public:
 
   NS_IMETHOD InsertChildAt(nsIContent* aKid, PRInt32 aIndex, PRBool aNotify);
   NS_IMETHOD ReplaceChildAt(nsIContent* aKid, PRInt32 aIndex, PRBool aNotify);
-  NS_IMETHOD AppendChild(nsIContent* aKid, PRBool aNotify);
+  NS_IMETHOD AppendChildTo(nsIContent* aKid, PRBool aNotify);
   NS_IMETHOD RemoveChildAt(PRInt32 aIndex, PRBool aNotify);
 
 
