c5279ded7f7c5437a50ec6261d691561f9e9da44
coop%netscape.com <>
1998-10-02 15:21:41 +0000

    Added NPL for all classes.

--- mozilla/js/src/liveconnect/classes/netscape/javascript/JSUtil.java
+++ mozilla/js/src/liveconnect/classes/netscape/javascript/JSUtil.java
@@ -1,4 +1,4 @@
-/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- Mode: Java; tab-width: 8; c-basic-offset: 4 -*-
  *
  * The contents of this file are subject to the Netscape Public License
  * Version 1.0 (the "NPL"); you may not use this file except in
