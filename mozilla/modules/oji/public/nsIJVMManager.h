f09fd0e384db9ac547a622949b7d87d4912b89ac
beard%netscape.com <>
1998-11-13 22:49:27 +0000

    Changed comment for CreateProxyJNI, added GetProxyJNI.

--- mozilla/modules/oji/public/nsIJVMManager.h
+++ mozilla/modules/oji/public/nsIJVMManager.h
@@ -54,10 +54,20 @@ class nsISecureJNI2;
 class nsIJVMManager : public nsISupports {
 public:
     /**
-     * Creates a proxy JNI for a given secure environment.
+     * Creates a proxy JNI with an optional secure environment (which can be NULL).
+     * There is a one-to-one correspondence between proxy JNIs and threads, so
+     * calling this method multiple times from the same thread will return
+     * the same proxy JNI.
      */
 	NS_IMETHOD
 	CreateProxyJNI(nsISecureJNI2* inSecureEnv, JNIEnv** outProxyEnv) = 0;
+	
+	/**
+	 * Returns the proxy JNI associated with the current thread, or NULL if no
+	 * such association exists.
+	 */
+	NS_IMETHOD
+	GetProxyJNI(JNIEnv** outProxyEnv) = 0;
 };
 
 #define NS_IJVMMANAGER_IID                           \
