5a6558bfe055ede6e8546e5b8b777111185cf37d
waterson%netscape.com <>
1999-02-17 08:38:17 +0000

    Changes for new nsCOMPtr.

--- mozilla/rdf/content/src/nsRDFResourceElement.cpp
+++ mozilla/rdf/content/src/nsRDFResourceElement.cpp
@@ -1339,7 +1339,7 @@ RDFResourceElementImpl::SetAttribute(PRInt32 aNameSpaceID,
                     if (xulListener->mAttribute.EqualsIgnoreCase(aString))
                         {
                             // Set the attribute in the broadcast listener.
-                            nsCOMPtr<nsIContent> contentNode(xulListener->mListener);
+                            nsCOMPtr<nsIContent> contentNode(do_QueryInterface(xulListener->mListener));
                             if (contentNode)
                                 {
                                     contentNode->SetAttribute(aNameSpaceID, aName, aValue, aNotify);
@@ -1474,7 +1474,7 @@ RDFResourceElementImpl::UnsetAttribute(PRInt32 aNameSpaceID, nsIAtom* aName, PRB
                     if (xulListener->mAttribute.EqualsIgnoreCase(aString))
                         {
                             // Unset the attribute in the broadcast listener.
-                            nsCOMPtr<nsIContent> contentNode(xulListener->mListener);
+                            nsCOMPtr<nsIContent> contentNode(do_QueryInterface(xulListener->mListener));
                             if (contentNode)
                                 {
                                     contentNode->UnsetAttribute(aNameSpaceID, aName, aNotify);
