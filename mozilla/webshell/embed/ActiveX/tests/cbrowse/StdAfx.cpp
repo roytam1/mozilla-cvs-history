281d3cda7fa7c305b648ce7c76a318e6b1e0dc3d
cvs2git <>
1999-04-13 01:00:19 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.7.32'.

--- mozilla/webshell/embed/ActiveX/tests/cbrowse/StdAfx.cpp
+++
@@ -1,9 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	cbrowse.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-#include <atlimpl.cpp>
-#include <atlctl.cpp>
-#include <atlwin.cpp>
