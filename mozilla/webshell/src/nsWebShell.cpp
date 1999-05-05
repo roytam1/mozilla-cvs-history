9b1bf93877bee1c62632d346514373b36e44418b
leaf%mozilla.org <>
1999-05-05 05:34:19 +0000

    backing myself out; nisheeth's real fix is in

--- mozilla/webshell/src/nsWebShell.cpp
+++ mozilla/webshell/src/nsWebShell.cpp
@@ -171,7 +171,6 @@ public:
 	NS_IMETHOD GetParentEvenIfChrome(nsIWebShell*& aParent);
   NS_IMETHOD GetChildCount(PRInt32& aResult);
   NS_IMETHOD AddChild(nsIWebShell* aChild);
-  NS_IMETHOD RemoveChild(nsIWebShell* aChild);
   NS_IMETHOD ChildAt(PRInt32 aIndex, nsIWebShell*& aResult);
   NS_IMETHOD GetName(const PRUnichar** aName);
   NS_IMETHOD SetName(const PRUnichar* aName);
@@ -892,11 +891,6 @@ nsWebShell::Destroy()
   SetObserver(nsnull);
   SetDocLoaderObserver(nsnull);
 
-  // Remove this webshell from its parent's child list
-  if (nsnull != mParent) {
-    mParent->RemoveChild(this);
-  }
-
   if (nsnull != mDocLoader) {
     mDocLoader->SetContainer(nsnull);
   }
@@ -1208,7 +1202,6 @@ nsWebShell::GetChildCount(PRInt32& aResult)
   return NS_OK;
 }
 
-
 NS_IMETHODIMP
 nsWebShell::AddChild(nsIWebShell* aChild)
 {
@@ -1220,20 +1213,6 @@ nsWebShell::AddChild(nsIWebShell* aChild)
   aChild->SetParent(this);
   aChild->SetDefaultCharacterSet(mDefaultCharacterSet); 
   NS_ADDREF(aChild);
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsWebShell::RemoveChild(nsIWebShell* aChild)
-{
-  NS_PRECONDITION(nsnull != aChild, "nsWebShell::RemoveChild(): null ptr");
-  if (nsnull == aChild) {
-    return NS_ERROR_NULL_POINTER;
-  }
-  mChildren.RemoveElement(aChild);
-  aChild->SetParent(nsnull);
-  NS_RELEASE(aChild);
 
   return NS_OK;
 }
