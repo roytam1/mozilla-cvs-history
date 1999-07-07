9865884aa55c30b592f0f68d62b6da2851cff3f7
cvs2git <>
1999-07-07 04:38:32 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.7.8'.

--- mozilla/intl/uconv/src/nsUNIXCharset.cpp
+++ mozilla/intl/uconv/src/nsUNIXCharset.cpp
@@ -64,7 +64,7 @@ nsUNIXCharset::nsUNIXCharset()
           if(NS_FAILED(res)) {
               nsAutoString localeKey("locale.all.");
               localeKey.Append(locale);
-              res = info->Get(localeKey, mCharset);
+              nsresult res = info->Get(localeKey, mCharset);
               if(NS_SUCCEEDED(res))  {
                   delete info;
                   return; // succeeded
