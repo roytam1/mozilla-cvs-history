e3cb9b4138b2994074261ce8b6a5f23204e5af5d
mscott%netscape.com <>
1999-07-08 21:49:54 +0000

    Sych up with necko change to nsIStreamObserver. OnStopRequest, OnStartRequest and OnDataAvailable all now take in a nsIChannel as the first argument. Since channels don't make sense here we're passing in nsnull everywhere. I'm still trying to get a reversal on the interface change so these changes may get backed out.

--- mozilla/mailnews/mime/src/nsMimeEmitter2.cpp
+++ mozilla/mailnews/mime/src/nsMimeEmitter2.cpp
@@ -436,7 +436,7 @@ nsMimeEmitter2::Write(const char *buf, PRUint32 size, PRUint32 *amountWritten)
     mBufferMgr->ReduceBuffer(written);
     nsCOMPtr<nsIInputStream> inputStream = do_QueryInterface(mOutStream); 
     if (inputStream)
-      mOutListener->OnDataAvailable(mURL, inputStream, written);
+      mOutListener->OnDataAvailable(nsnull, mURL, inputStream, written);
     *amountWritten = written;
 
     // if we couldn't write all the old data, buffer the new data
@@ -460,7 +460,7 @@ nsMimeEmitter2::Write(const char *buf, PRUint32 size, PRUint32 *amountWritten)
     mBufferMgr->IncreaseBuffer(buf+written, (size-written));
     nsCOMPtr<nsIInputStream> inputStream = do_QueryInterface(mOutStream); 
     if (inputStream)
-      mOutListener->OnDataAvailable(mURL, inputStream, written);
+      mOutListener->OnDataAvailable(nsnull, mURL, inputStream, written);
   }
 
   return rc;
