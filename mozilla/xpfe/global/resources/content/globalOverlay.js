ece22ba9a5e723b336218fa7aa31982f8f599d75
cvs2git <>
1999-07-19 03:29:57 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.102.2'.

--- mozilla/xpfe/global/resources/content/globalOverlay.js
+++
@@ -1,8 +0,0 @@
-function goQuitApplication()
-{
-	var editorShell = Components.classes["component://netscape/editor/editorshell"].createInstance();
-	editorShell = editorShell.QueryInterface(Components.interfaces.nsIEditorShell);
-	
-	if ( editorShell )
-		editorShell.Exit();
-}
