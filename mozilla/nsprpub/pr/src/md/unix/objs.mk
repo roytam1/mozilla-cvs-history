06bbdf1ccae172d401816822d24be27fe44bdf6d
cls%seawood.org <>
1999-08-20 16:21:42 +0000

    Merged with HEAD from 8/18 or so.

--- mozilla/nsprpub/pr/src/md/unix/objs.mk
+++ mozilla/nsprpub/pr/src/md/unix/objs.mk
@@ -124,6 +124,14 @@ QNX_CSRCS = \
 	qnx.c \
 	$(NULL)
 
+NTO_CSRCS = \
+	nto.c \
+	$(NULL)
+
+ifeq ($(PTHREADS_USER),1)
+CSRCS += $(PTH_USER_CSRCS)
+endif
+
 ifeq ($(OS_ARCH),IRIX)
 CSRCS += $(IRIX_CSRCS)
 endif
@@ -191,7 +199,11 @@ ifeq ($(OS_ARCH),DGUX)
 CSRCS += $(DGUX_CSRCS)
 endif
 ifeq ($(OS_ARCH),QNX)
+ifeq ($(OS_TARGET),NTO)
+CSRCS += $(NTO_CSRCS)
+else
 CSRCS += $(QNX_CSRCS)
+endif
 endif
  
 #
