7b19a154e8f6ebaaf054facbdc77cabd93618d90
leif%netscape.com <>
1999-10-04 20:20:33 +0000

    Added support for a "-t <separator>" option.

--- mozilla/directory/perldap/examples/tabdump.pl
+++ mozilla/directory/perldap/examples/tabdump.pl
@@ -35,18 +35,19 @@ use Mozilla::LDAP::Utils;		# LULU, utilities.
 # Constants, shouldn't have to edit these...
 #
 $APPNAM	= "tabdump";
-$USAGE	= "$APPNAM [-nv] -b base -h host -D bind -w pswd -P cert attr1,attr2,.. srch";
+$USAGE	= "$APPNAM [-nv] -b base -h host -D bind -w pswd -P [-t <sep>] cert attr1,attr2,.. srch";
 
 
 #################################################################################
 # Check arguments, and configure some parameters accordingly..
 #
-if (!getopts('nvb:h:D:p:s:w:P:'))
+if (!getopts('nvb:h:D:p:s:w:P:t:'))
 {
    print "usage: $APPNAM $USAGE\n";
    exit;
 }
 %ld = Mozilla::LDAP::Utils::ldapArgs();
+$separator = (defined($opt_t) ? $opt_t : "\t");
 
 $attributes = $ARGV[$[];
 $search = $ARGV[$[ + 1];
@@ -68,7 +69,7 @@ while ($entry)
 {
   foreach (@attr)
     {
-      print $entry->{$_}[0], "\t";
+      print $entry->{$_}[0], $separator;
     }
   print "\n";
   $entry = $conn->nextEntry;
