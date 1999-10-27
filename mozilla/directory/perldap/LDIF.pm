c84f42ce174e35c3fc8771f1c90cf85060706d36
dmose%mozilla.org <>
1999-10-27 21:02:40 +0000

    updating license boilerplate in all files; checkin approved by leif@netscape.com

--- mozilla/directory/perldap/LDIF.pm
+++ mozilla/directory/perldap/LDIF.pm
@@ -1,23 +1,28 @@
 #############################################################################
 # $Id$
 #
-# The contents of this file are subject to the Netscape Public License
-# Version 1.0 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://www.mozilla.org/NPL/
-#
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-# License for the specific language governing rights and limitations
-# under the License.
-#
-# The Original Code was released March, 1999.  The Initial Developer of
-# the Original Code is Netscape Communications Corporation.  Portions
-# created by Netscape are Copyright (C) 1999 Netscape Communications
-# Corporation.  All Rights Reserved.
-#
-# Contributor(s): Leif Hedstrom, John M. Kristian
-#
+# 
+# The contents of this file are subject to the Netscape Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/NPL/
+#  
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#  
+# The Original Code is LDIF.pm, released
+# March, 1999.
+# 
+# The Initial Developer of the Original Code is Netscape
+# Communications Corporation. Portions created by Netscape are
+# Copyright (C) 1999 Netscape Communications Corporation. All
+# Rights Reserved.
+# 
+# Contributor(s): Leif Hedstrom <leif@netscape.com>
+#                 John M. Kristian <kristian@netscape.com>
+# 
 #############################################################################
 
 package Mozilla::LDAP::LDIF;
