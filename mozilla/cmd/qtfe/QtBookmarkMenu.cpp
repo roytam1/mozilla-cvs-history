2d5aa754501fc866f97a674182c91104579c4b63
dmose%mozilla.org <>
1999-10-31 02:18:58 +0000

    updating license from version 1.0 to version 1.1

--- mozilla/cmd/qtfe/QtBookmarkMenu.cpp
+++ mozilla/cmd/qtfe/QtBookmarkMenu.cpp
@@ -1,21 +1,29 @@
 /* $Id$
  *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
- * http://www.mozilla.org/NPL/
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The Initial Developer of this code under the NPL is Eirik Eng.
- * Further developed by Warwick Allison, Kalle Dalheimer, Eirik Eng,
- * Matthias Ettrich, Arnt Gulbrandsen, Haavard Nord and Paul Olav
- * Tvete.  Copyright (C) 1998 Warwick Allison, Kalle Dalheimer, Eirik
- * Eng, Matthias Ettrich, Arnt Gulbrandsen, Haavard Nord and Paul Olav
- * Tvete.  All Rights Reserved.
+ * The Initial Developer of this code under the NPL is Eirik
+ * Eng.  Portions created by Warwick Allison, Kalle Dalheimer, 
+ * Eirik Eng, Matthias Ettrich, Arnt Gulbrandsen, Haavard Nord and 
+ * Paul Olav Tvete are Copyright (C) 1998 Warwick Allison, Kalle Dalheimer,
+ * Eirik Eng, Matthias Ettrich, Arnt Gulbrandsen, Haavard Nord, and
+ * Paul Olav Tvete. All Rights Reserved.
+ *
+ * Contributors: Warwick Allison
+ *               Kalle Dalheimer
+ *               Eirik Eng
+ *               Matthias Ettrich
+ *               Arnt Gulbrandsen
+ *               Haavard Nord
+ *               Paul Olav Tvete
  */
 
 #include "QtBookmarkMenu.h"
