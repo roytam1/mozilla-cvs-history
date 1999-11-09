65cf6991384fcce6b8ee8659740e2509e6e956b4
fur%netscape.com <>
1999-11-09 17:36:48 +0000

    Sync with trunk

--- mozilla/xpcom/build/dlldeps.cpp
+++ mozilla/xpcom/build/dlldeps.cpp
@@ -1,19 +1,23 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.0 (the "NPL"); you may not use this file except in
- * compliance with the NPL.  You may obtain a copy of the NPL at
- * http://www.mozilla.org/NPL/
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
  *
- * Software distributed under the NPL is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
- * for the specific language governing rights and limitations under the
- * NPL.
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
  *
- * The Initial Developer of this code under the NPL is Netscape
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
  * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
- * Reserved.
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
  */
 
 // Force references to all of the symbols that we want exported from
@@ -51,6 +55,9 @@
 #include "nsTextFormater.h"
 #include "nsStorageStream.h"
 #include "nsIBinaryInputStream.h"
+#ifdef DEBUG
+#include "pure.h"
+#endif
 
 class dummyComparitor: public nsAVLNodeComparitor {
 public:
@@ -98,6 +105,7 @@ void XXXNeverCalled()
     info->GetName(NULL);
 #ifdef DEBUG
     info->print(NULL);
+    PurePrintf(0);
 #endif
     XPTC_InvokeByIndex(NULL, 0, 0, NULL);
     NS_NewFileSpec(NULL);
