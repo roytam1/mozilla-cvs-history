12908f50f43508ddeec54a4daaf9e1ca7c1b1a3b
rogerl%netscape.com <>
1999-12-02 00:15:51 +0000

    Merge Linux/X86 fix from tip

--- mozilla/js/src/config/Linux_All.mk
+++ mozilla/js/src/config/Linux_All.mk
@@ -22,6 +22,7 @@
 CC = /usr/bin/gcc
 CCC = /usr/bin/g++
 CFLAGS +=  -Wall -Wno-format
+OS_CFLAGS = -DXP_UNIX -DSVR4 -DSYSV -D_BSD_SOURCE -DPOSIX_SOURCE
 
 RANLIB = echo
 MKSHLIB = $(LD) -shared $(XMKSHLIBOPTS)
@@ -32,10 +33,10 @@ MKSHLIB = $(LD) -shared $(XMKSHLIBOPTS)
 CPU_ARCH = $(shell uname -m)
 ifeq (86,$(findstring 86,$(CPU_ARCH)))
 CPU_ARCH = x86
+OS_CFLAGS+= -DX86_LINUX
 endif
 GFX_ARCH = x
 
-OS_CFLAGS = -DXP_UNIX -DSVR4 -DSYSV -D_BSD_SOURCE -DPOSIX_SOURCE
 OS_LIBS = -lm -lc
 
 ASFLAGS += -x assembler-with-cpp
