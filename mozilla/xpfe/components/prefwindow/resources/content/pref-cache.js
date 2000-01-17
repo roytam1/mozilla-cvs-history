0d906d39c9370b61df49b357780496197bb2214b
cvs2git <>
2000-01-17 21:28:24 +0000

    This commit was manufactured by cvs2svn to create branch 'WH_OFFLINE_BRANCH'.

--- mozilla/xpfe/components/prefwindow/resources/content/pref-cache.js
+++
@@ -1,9 +0,0 @@
-
-function prefCacheSelectFolder()
-{
-  bundle = srGetStrBundle("chrome://pref/locale/prefutilities.properties");
-  var folderField = document.getElementById("browserCacheDirectory");
-  var url = getFileOrFolderURL( bundle.GetStringFromName("cachefolder"), true );
-  if( url != -1 )
-    folderField.value = url;
-}
