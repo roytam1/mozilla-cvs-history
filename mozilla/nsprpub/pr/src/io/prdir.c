f7478bd63ea5ef6d0abd27d5b1ffa3c1d78953a3
wtc%netscape.com <>
2000-01-27 19:13:55 +0000

    PR_MakeDir should call nspr initialization (needed on Windows, at least).

--- mozilla/nsprpub/pr/src/io/prdir.c
+++ mozilla/nsprpub/pr/src/io/prdir.c
@@ -74,6 +74,7 @@ PR_IMPLEMENT(PRStatus) PR_MakeDir(const char *name, PRIntn mode)
 {
 PRInt32 rv;
 
+	if (!_pr_initialized) _PR_ImplicitInitialization();
 	rv = _PR_MD_MAKE_DIR(name, mode);
 	if (rv < 0) {
 		return PR_FAILURE;
