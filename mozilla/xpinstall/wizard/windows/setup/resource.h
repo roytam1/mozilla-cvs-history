54ad09c168b1eba1c097ca3b6c50ca429b8782df
ssu%netscape.com <>
2000-02-08 00:00:30 +0000

    fixes for removing MFC dependencies.

--- mozilla/xpinstall/wizard/windows/setup/resource.h
+++ mozilla/xpinstall/wizard/windows/setup/resource.h
@@ -3,29 +3,7 @@
 // Used by setup.rc
 //
 
-/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/*
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- * The Original Code is Mozilla Communicator client code,
- * released March 31, 1998.
- *
- * The Initial Developer of the Original Code is Netscape Communications
- * Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
- *
- * Contributor(s): 
- *     Sean Su <ssu@netscape.com>
- */
+#include "winnt.rh"
 
 #define IDS_ERROR_DLL_LOAD              1
 #define IDS_ERROR_STRING_LOAD           2
@@ -36,6 +14,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
