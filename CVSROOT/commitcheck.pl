1b4ea23a6a8ec90f4304160a07104e964b4e1373
despotdaemon%netscape.com <>
2000-05-08 17:58:16 +0000

    Pseudo-automatic update of changes made by kmcclusk@netscape.com.

--- CVSROOT/commitcheck.pl
+++ CVSROOT/commitcheck.pl
@@ -309,6 +309,7 @@ if (m:^mozilla/xpcom/.*$:) {return '89';}
 if (m:^xpcom/io$:) {return '90';}
 if (m:^mozilla/modules/zlib/.*$:) {return '93';}
 if (m:^mozilla/gfx/public/.*$:) {return '95';}
+if (m:^mozilla/gfx/src/windows/.*$:) {return '95';}
 if (m:^mozilla/view/.*$:) {return '96';}
 if (m:^mozilla/layout/.*$:) {return '98';}
 if (m:^mozilla/webshell/.*$:) {return '101';}
