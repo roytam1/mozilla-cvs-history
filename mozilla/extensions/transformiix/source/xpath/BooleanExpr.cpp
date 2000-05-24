e0eb35aa4daa977ee4e77a244341c40ee36d11d9
kvisco%ziplink.net <>
2000-05-24 05:00:25 +0000

    Added fix from Marc Schefer regarding OR expressions

--- mozilla/extensions/transformiix/source/xpath/BooleanExpr.cpp
+++ mozilla/extensions/transformiix/source/xpath/BooleanExpr.cpp
@@ -3,24 +3,29 @@
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
  * the License at http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
- * 
+ *
  * The Original Code is TransforMiiX XSLT processor.
- * 
+ *
  * The Initial Developer of the Original Code is The MITRE Corporation.
  * Portions created by MITRE are Copyright (C) 1999 The MITRE Corporation.
  *
  * Portions created by Keith Visco as a Non MITRE employee,
  * (C) 1999 Keith Visco. All Rights Reserved.
- * 
- * Contributor(s): 
+ *
+ * Contributor(s):
  * Keith Visco, kvisco@ziplink.net
  *   -- original author.
- *    
+ *
+ * Marc Schefer, schefer@xo3.com
+ *   -- fixed a bug regarding Or expressions
+ *      - If the left hand was false, the right hand expression
+ *        was not getting checked.
+ *
  * $Id$
  */
 
@@ -92,8 +97,7 @@ ExprResult* BooleanExpr::evaluate(Node* context, ContextState* cs) {
 
     //-- check left expression for early decision
     if (( op == OR ) && (lval)) return new BooleanResult(MB_TRUE);
-    else if (!lval) return new BooleanResult(MB_FALSE);
-
+    else if ((op == AND) && (!lval)) return new BooleanResult(MB_FALSE);
 
     MBool rval = MB_FALSE;
     if ( rightExpr ) {
