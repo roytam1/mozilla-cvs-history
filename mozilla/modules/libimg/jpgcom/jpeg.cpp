b844384c5bb1ebe8f770598eb5e4aa1ed0418bb7
jband%netscape.com <>
2000-09-22 10:42:54 +0000

    backing out check in from jin_nance that broke Mac

--- mozilla/modules/libimg/jpgcom/jpeg.cpp
+++ mozilla/modules/libimg/jpgcom/jpeg.cpp
@@ -64,6 +64,9 @@ PR_END_EXTERN_C
 /* Normal JFIF markers can't have more bytes than this. */
 #define MAX_JPEG_MARKER_LENGTH  (((PRUint32)1 << 16) - 1)
 
+int il_debug;
+PRLogModuleInfo *il_log_module = NULL;
+
 /*
  * States that the jpeg decoder might be in
  */
