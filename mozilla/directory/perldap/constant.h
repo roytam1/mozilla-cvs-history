fc860100c702e93188d75e2160d60540d74cec70
leif%netscape.com <>
2000-10-05 22:22:16 +0000

    - Merge with v1.4.1.

--- mozilla/directory/perldap/constant.h
+++ mozilla/directory/perldap/constant.h
@@ -1,9 +1,9 @@
 /*
- *******************************************************************************
+ ******************************************************************************
  * $Id$
  *
  * The contents of this file are subject to the Mozilla Public License
- * Version 1.0 (the "License"); you may not use this file except in
+ * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
@@ -23,7 +23,7 @@
  * DESCRIPTION
  *    Constants.
  *
- *******************************************************************************/
+ *****************************************************************************/
 
 #ifdef __cplusplus
 extern "C" {
