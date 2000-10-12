b28c977a71011caf58583920c42ef3090cad904b
cvs2git <>
2000-10-12 16:02:33 +0000

    This commit was manufactured by cvs2svn to create tag 'SeaMonkey_M18_RELEASE'.

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -52,10 +52,10 @@
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_BRANCH = SeaMonkey_M18_BRANCH
-NSPR_CO_TAG = SeaMonkey_M18_BRANCH
-PSM_CO_TAG = SeaMonkey_M18_BRANCH
-LDAPCSDK_CO_TAG = SeaMonkey_M18_BRANCH
+MOZ_CO_BRANCH = SeaMonkey_M18_RELEASE
+NSPR_CO_TAG = SeaMonkey_M18_RELEASE
+PSM_CO_TAG = SeaMonkey_M18_RELEASE
+LDAPCSDK_CO_TAG = SeaMonkey_M18_RELEASE
 BUILD_MODULES = all
 
 #######################################################################
