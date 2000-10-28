0a64e278bff036695f79799574571d4f4509f596
rjc%netscape.com <>
2000-10-28 05:06:00 +0000

    Modify client.mk to pull Netscape_20001027_BRANCH

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -52,10 +52,10 @@
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG = Netscape_20000922_BRANCH
-NSPR_CO_TAG = Netscape_20000922_BRANCH
-PSM_CO_TAG = Netscape_20000922_BRANCH
-LDAPCSDK_CO_TAG = Netscape_20000922_BRANCH
+MOZ_CO_TAG = Netscape_20001027_BRANCH
+NSPR_CO_TAG = Netscape_20001027_BRANCH
+PSM_CO_TAG = Netscape_20001027_BRANCH
+LDAPCSDK_CO_TAG = Netscape_20001027_BRANCH
 BUILD_MODULES = all
 
 #######################################################################
