c78528fb8c557e69668a4bd64c50671cbac02f6a
mscott%netscape.com <>
2000-10-30 23:56:10 +0000

    Bug #56938 --> Implement stand alone method for setting up a refresh URI in the docshell given just a channel. sr=rpotts r=sspitzer

--- mozilla/docshell/base/nsDocShell.cpp
+++ mozilla/docshell/base/nsDocShell.cpp
@@ -3777,6 +3777,17 @@ nsDocShell::OnNewURI(nsIURI *aURI, nsIChannel *aChannel, PRUint32 aLoadType)
   }
 
    SetCurrentURI(aURI);
+   // if there's a refresh header in the channel, this method
+   // will set it up for us. 
+   SetupRefreshURI(aChannel);
+   
+   mInitialPageLoad = PR_FALSE;
+   return NS_OK;
+}
+
+NS_IMETHODIMP 
+nsDocShell::SetupRefreshURI(nsIChannel * aChannel)
+{
    nsCOMPtr<nsIHTTPChannel> httpChannel(do_QueryInterface(aChannel));
    if(httpChannel)
    {
@@ -3859,7 +3870,6 @@ nsDocShell::OnNewURI(nsIURI *aURI, nsIChannel *aChannel, PRUint32 aLoadType)
       }
    }
 
-   mInitialPageLoad = PR_FALSE;
    return NS_OK;
 }
 
