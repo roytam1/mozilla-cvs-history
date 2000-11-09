aea78ef645cadfcead7c2573967bf8ca90a29b1e
kestes%staff.mail.com <>
2000-11-09 19:17:39 +0000

    add a statement:    use lib '#tinder_libdir#';
    ensure that all filnames are taint free, they can come from globs

--- mozilla/webtools/tinderbox2/src/lib/TinderDB/BasicTxtDB.pm
+++ mozilla/webtools/tinderbox2/src/lib/TinderDB/BasicTxtDB.pm
@@ -41,6 +41,8 @@ package TinderDB::BasicTxtDB;
 
 # Tinderbox libraries
 
+use lib '#tinder_libdir#';
+
 use FileStructure;
 use Persistence;
 
@@ -103,14 +105,18 @@ sub unlink_files {
   my ($self, $dir, @files) = @_;
 
   foreach $file (@files) {
+    $full_file = "$dir/$file";
 
-    unlink ("$dir/$file") ||
-      die("Could not remove filename: '$dir/$file': $!\n");
+    # This may be the output of a glob, make it taint safe.
+    $full_file = main::extract_filename_chars($full_file);
+
+    unlink ("$full_file") ||
+      die("Could not remove filename: '$full_file': $!\n");
 
     # since we have 'required' these files we need to forget that they
     # were loaded.
 
-    delete $INC{"$dir/$file"};
+    delete $INC{"$full_file"};
   }
 
   return ;  
@@ -197,8 +203,10 @@ sub loadtree_db {
 
   # ignore unlink errors, cleaning up the directory is not important. 
   
-  (scalar(@sorted_files)) &&
-    unlink(@sorted_files);
+  foreach $file (@sorted_files) {
+    $file = main::extract_filename_chars($file);
+    unlink($file);
+  }
 
   return ;
 }
