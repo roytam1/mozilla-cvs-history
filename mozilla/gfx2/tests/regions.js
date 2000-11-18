b4b6307c0f70b02e89d7b85c574123e6cc8b35dd
pavlov%netscape.com <>
2000-11-18 05:04:09 +0000

    make the test actually work :-)

--- mozilla/gfx2/tests/regions.js
+++ mozilla/gfx2/tests/regions.js
@@ -2,11 +2,17 @@
 
 const nsIRegion = Components.interfaces.nsIRegion;
 
-r1 = Components.classes["mozilla.gfx.region.2"].createInstance(nsIRegion)
+function newRegion()
+{
+ var region = Components.classes["@mozilla.org/gfx/region;2"].createInstance(nsIRegion);
+ return region;
+}
+
+r1 = newRegion();
 r1.setToRect(0, 0, 400, 400);
 dump("r1.isEmpty() == " + r1.isEmpty() + "\n");
 
 
-r2 = Components.classes["mozilla.gfx.region.2"].createInstance(nsIRegion)
+r2 = newRegion();
 dump("r2.isEmpty() == " + r2.isEmpty() + "\n");
 
