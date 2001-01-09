dc33c71162cf28b0e8e790d7a612b0417e9ed98e
wtc%netscape.com <>
2001-01-09 01:35:40 +0000

    Bugzilla bug #64666: using a minus (-) in a scan set is not portable. Thanks to Michael Kaply <mkaply@us.ibm.com> for the bug report and patch. (NSPRPUB_CLIENT_BRANCH)

--- mozilla/nsprpub/pr/src/io/prlog.c
+++ mozilla/nsprpub/pr/src/io/prlog.c
@@ -181,7 +181,7 @@ void _PR_InitLog(void)
         PRInt32 bufSize = DEFAULT_BUF_SIZE;
         while (pos < evlen) {
             PRIntn level = 1, count = 0, delta = 0;
-            count = sscanf(&ev[pos], "%64[A-Za-z0-9]%n:%d%n",
+            count = sscanf(&ev[pos], "%64[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]%n:%d%n",
                            module, &delta, &level, &delta);
             pos += delta;
             if (count == 0) break;
@@ -266,7 +266,7 @@ static void _PR_SetLogModuleLevel( PRLogModuleInfo *lm )
         while (pos < evlen) {
             PRIntn level = 1, count = 0, delta = 0;
 
-            count = sscanf(&ev[pos], "%64[A-Za-z0-9]%n:%d%n",
+            count = sscanf(&ev[pos], "%64[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]%n:%d%n",
                            module, &delta, &level, &delta);
             pos += delta;
             if (count == 0) break;
