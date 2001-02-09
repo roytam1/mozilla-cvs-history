0c896bf2cfeac58a78b5d7e657765bfeb397d4df
sspitzer%netscape.com <>
2001-02-09 18:24:33 +0000

    more changes for the test harness.  now to go work on the code...

--- mozilla/mailnews/base/resources/content/dbviewtest.js
+++ mozilla/mailnews/base/resources/content/dbviewtest.js
@@ -28,6 +28,46 @@ function sortThreadedDescending()
     dbview.sort(nsMsgViewSortType.byThread,nsMsgViewSortOrder.descending);
 }
 
+function sortPriorityAscending()
+{
+    dbview.sort(nsMsgViewSortType.byPriority,nsMsgViewSortOrder.ascending);
+}
+
+function sortPriorityDescending()
+{
+    dbview.sort(nsMsgViewSortType.byPriority,nsMsgViewSortOrder.descending);
+}
+
+function sortSenderAscending()
+{
+    dbview.sort(nsMsgViewSortType.bySender,nsMsgViewSortOrder.ascending);
+}
+
+function sortSenderDescending()
+{
+    dbview.sort(nsMsgViewSortType.bySender,nsMsgViewSortOrder.descending);
+}
+
+function sortSubjectAscending()
+{
+    dbview.sort(nsMsgViewSortType.bySubject,nsMsgViewSortOrder.ascending);
+}
+
+function sortSubjectDescending()
+{
+    dbview.sort(nsMsgViewSortType.bySubject,nsMsgViewSortOrder.descending);
+}
+
+function sortSizeAscending()
+{
+    dbview.sort(nsMsgViewSortType.bySize,nsMsgViewSortOrder.ascending);
+}
+
+function sortSizeDescending()
+{
+    dbview.sort(nsMsgViewSortType.bySize,nsMsgViewSortOrder.descending);
+}
+
 function dumpView()
 {   
     dbview.dumpView();
