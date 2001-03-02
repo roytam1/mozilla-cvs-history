f6d0f2cf91b3fde2128f1c3f67fac4436d70c776
bienvenu%netscape.com <>
2001-03-02 00:02:49 +0000

    fix crash when outliner asks about cell at index -1

--- mozilla/mailnews/base/src/nsMsgDBView.cpp
+++ mozilla/mailnews/base/src/nsMsgDBView.cpp
@@ -503,6 +503,9 @@ NS_IMETHODIMP nsMsgDBView::GetCellProperties(PRInt32 aRow, const PRUnichar *colI
       return rv;
   }
 
+  if (!msgHdr)
+    return NS_MSG_INVALID_DBVIEW_INDEX;
+
   char    flags = m_flags.GetAt(aRow);
   if (!(flags & MSG_FLAG_READ))
     properties->AppendElement(kUnreadMsgAtom);  
