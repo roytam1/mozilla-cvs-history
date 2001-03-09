c59f407f88ad8327b9ae0fa3c39d42b4dcd80d16
pavlov%netscape.com <>
2001-03-09 04:22:40 +0000

    new branch.. prepairing to land

--- mozilla/gfx/public/nsIRenderingContext.h
+++ mozilla/gfx/public/nsIRenderingContext.h
@@ -44,6 +44,13 @@ struct nsRect;
 struct nsBoundingMetrics;
 #endif
 
+
+#ifdef USE_IMG2
+/* gfx2 */
+#include "gfxtypes.h"
+class gfxIImageContainer;
+#endif
+
 //cliprect/region combination methods
 
 typedef enum
@@ -755,6 +762,21 @@ public:
                      PRUint32           aLength,
                      nsBoundingMetrics& aBoundingMetrics,
                      PRInt32*           aFontID = nsnull) = 0;
+#endif
+
+
+#ifdef USE_IMG2
+  /* [noscript] void drawImage (in gfxIImageContainer aImage, [const] in nsRect aSrcRect, [const] in nsPoint aDestPoint); */
+  NS_IMETHOD DrawImage(gfxIImageContainer *aImage, const nsRect * aSrcRect, const nsPoint * aDestPoint) = 0;
+
+  /* [noscript] void drawScaledImage (in gfxIImageContainer aImage, [const] in nsRect aSrcRect, [const] in nsRect aDestRect); */
+  NS_IMETHOD DrawScaledImage(gfxIImageContainer *aImage, const nsRect * aSrcRect, const nsRect * aDestRect) = 0;
+
+  /* [noscript] void drawTile (in gfxIImageContainer aImage, in nscoord aXOffset, in nscoord aYOffset, [const] in nsRect aTargetRect); */
+  NS_IMETHOD DrawTile(gfxIImageContainer *aImage, nscoord aXOffset, nscoord aYOffset, const nsRect * aTargetRect) = 0;
+
+  /* [noscript] void drawScaledTile (in gfxIImageContainer aImage, in nscoord aXOffset, in nscoord aYOffset, in nscoord aTileWidth, in nscoord aTileHeight, [const] in nsRect aTargetRect); */
+  NS_IMETHOD DrawScaledTile(gfxIImageContainer *aImage, nscoord aXOffset, nscoord aYOffset, nscoord aTileWidth, nscoord aTileHeight, const nsRect * aTargetRect) = 0;
 #endif
 };
 
