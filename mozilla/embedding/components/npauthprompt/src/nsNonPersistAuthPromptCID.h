8fba19a6faf1eec600d8a0b2eda1c18fbf91b78b
ccarlen%netscape.com <>
2001-04-05 20:53:34 +0000

    First checkin to branch.

---
+++ mozilla/embedding/components/npauthprompt/src/nsNonPersistAuthPromptCID.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2001 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+#ifndef __nsNonPersistAuthPromptCID_h__
+#define __nsNonPersistAuthPromptCID_h__
+
+// {2f977d46-5485-11d4-87e2-0010a4e75ef2}
+#define NS_NONPERSISTAUTHPROMPT_CID \
+ {0x2f977d46, 0x5485, 0x11d4, {0x87, 0xe2, 0x00, 0x10, 0xa4, 0xe7, 0x5e, 0xf2}}
+#define NS_NONPERSISTAUTHPROMPT_CONTRACTID \
+ "@mozilla.org/embedcomp/non-persist-auth-prompt;1"
+
+#endif // __nsNonPersistAuthPromptCID_h__
