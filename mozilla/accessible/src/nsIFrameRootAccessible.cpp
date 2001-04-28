df25f17460a13ea393a606feefce7bb9ae357ba9
rods%netscape.com <>
2001-04-28 00:19:54 +0000

    for IFrame support

---
+++ mozilla/accessible/src/nsIFrameRootAccessible.cpp
@@ -0,0 +1,123 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s): 
+ */
+
+#include "nsIFrameRootAccessible.h"
+#include "nsCOMPtr.h"
+#include "nsIDocument.h"
+#include "nsIPresShell.h"
+#include "nsIPresContext.h"
+#include "nsIContent.h"
+#include "nsIFrame.h"
+#include "nsIDocShell.h"
+#include "nsIWebShell.h"
+#include "nsIDocShellTreeItem.h"
+
+//-----------------------------------------------------
+// construction 
+//-----------------------------------------------------
+nsIFrameRootAccessible::nsIFrameRootAccessible(nsIWeakReference* aShell, nsIFrame* aFrame):
+  nsRootAccessible(aShell, aFrame)
+{
+}
+
+//-----------------------------------------------------
+// destruction
+//-----------------------------------------------------
+nsIFrameRootAccessible::~nsIFrameRootAccessible()
+{
+}
+
+  /* readonly attribute nsIAccessible accParent; */
+NS_IMETHODIMP nsIFrameRootAccessible::GetAccParent(nsIAccessible * *aAccParent) 
+{ 
+  // Start by finding our PresShell and from that
+  // we get our nsIDocShell in order to walk the DocShell tree
+  nsCOMPtr<nsIPresShell> presShell = do_QueryReferent(mPresShell);
+  nsCOMPtr<nsIDocShell> docShell;
+  if (NS_SUCCEEDED(GetDocShellFromPS(presShell, getter_AddRefs(docShell)))) {
+    // Now that we have the DocShell QI 
+    // it to a tree item to find it's parent
+    nsCOMPtr<nsIDocShellTreeItem> item(do_QueryInterface(docShell));
+    if (item) {
+      nsCOMPtr<nsIDocShellTreeItem> itemParent;
+      item->GetParent(getter_AddRefs(itemParent));
+      // QI to get the WebShell for the parent document
+      nsCOMPtr<nsIDocShell> parentDocShell(do_QueryInterface(itemParent));
+      if (parentDocShell) {
+        // Get the PresShell/Content and 
+        // Root Content Node of the parent document
+        nsCOMPtr<nsIPresShell> parentPresShell;
+        nsCOMPtr<nsIPresContext> parentPresContext;
+        nsCOMPtr<nsIContent> rootContent;
+        if (NS_SUCCEEDED(GetDocShellObjects(parentDocShell,
+                                            getter_AddRefs(parentPresShell), 
+                                            getter_AddRefs(parentPresContext),
+                                            getter_AddRefs(rootContent)))) {
+          // QI the DocShell (of this sub-doc) to a webshell
+          nsCOMPtr<nsIWebShell> webShell(do_QueryInterface(docShell));
+          if (webShell && parentPresShell && parentPresContext && rootContent) {
+            // Now, find the Content in the parent document
+            // that represents this sub-doc, 
+            // we do that matching webshells
+            nsCOMPtr<nsIContent> content;
+            if (FindContentForWebShell(parentPresShell, 
+                                       rootContent, 
+                                       webShell,
+                                       getter_AddRefs(content))) {
+              // OK, we found the content node in the parent doc
+              // that corresponds to this sub-doc
+              // Get the frame for that content
+              nsCOMPtr<nsIWeakReference> wr = getter_AddRefs(NS_GetWeakReference(parentPresShell));
+              nsIFrame* frame = nsnull;
+              parentPresShell->GetPrimaryFrameFor(content, &frame);
+#ifdef NS_DEBUG_X
+              printf("** Found: Con:%p  Fr:%p", content, frame);
+              char * name;
+              if (GetNameForFrame(frame, &name)) {
+                printf(" Name:[%s]", name);
+                nsMemory::Free(name);
+              }
+              printf("\n");
+#endif
+              // Now, go find the parent accessible for this frame.
+              return nsAccessible::GetAccParent(parentPresContext, wr, frame, aAccParent);
+            }
+          }
+        }
+      }
+    }
+  }
+  *aAccParent = nsnull;
+  return NS_OK;
+}
+
+  /* nsIAccessible getAccNextSibling (); */
+NS_IMETHODIMP nsIFrameRootAccessible::GetAccNextSibling(nsIAccessible **_retval) 
+{
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+  /* nsIAccessible getAccPreviousSibling (); */
+NS_IMETHODIMP nsIFrameRootAccessible::GetAccPreviousSibling(nsIAccessible **_retval) 
+{
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
