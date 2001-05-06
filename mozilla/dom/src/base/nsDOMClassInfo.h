c978bba9f0045e871478941f9a397ef28147f011
jst%netscape.com <>
2001-05-06 12:36:38 +0000

    Updating license header.

--- mozilla/dom/src/base/nsDOMClassInfo.h
+++ mozilla/dom/src/base/nsDOMClassInfo.h
@@ -1,5 +1,5 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- *
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
  * The contents of this file are subject to the Netscape Public
  * License Version 1.1 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
@@ -10,16 +10,17 @@
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
  *
- * The Original Code is Mozilla Communicator client code.
+ * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is Netscape Communications
- * Corporation.  Portions created by Netscape are
- * Copyright (C) 2001 Netscape Communications Corporation. All
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 2000 Netscape Communications Corporation. All
  * Rights Reserved.
  *
- * Contributor(s): 
+ * Contributor(s):
  *   Johnny Stenback <jst@netscape.com> (original author)
  */
+
 #ifndef nsDOMClassInfo_h___
 #define nsDOMClassInfo_h___
 
