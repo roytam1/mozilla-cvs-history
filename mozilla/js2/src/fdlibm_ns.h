497d17f5730b2d2c2edf2180e344195085a13fa2
beard%netscape.com <>
2001-06-02 02:45:09 +0000

    Added  mozilla boilerplate, got working on the Mac.

--- mozilla/js2/src/fdlibm_ns.h
+++ mozilla/js2/src/fdlibm_ns.h
@@ -1,41 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Netscape Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express oqr
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is the JavaScript 2 Prototype.
+ *
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation. All
+ * Rights Reserved.
+ *
+ * Contributor(s):  
+ *   Roger Lawrence <rogerl@netscape.com>
+ *   Patrick Beard <beard@netscape.com>
+ *
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU Public License (the "GPL"), in which case the
+ * provisions of the GPL are applicable instead of those above.
+ * If you wish to allow use of your version of this file only
+ * under the terms of the GPL and not to allow others to use your
+ * version of this file under the NPL, indicate your decision by
+ * deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL.  If you do not delete
+ * the provisions above, a recipient may use your version of this
+ * file under either the NPL or the GPL.
+ */
 
 #include <math.h>
 
 #if defined(_WIN32) && !defined(__MWERKS__)
-#define JS_USE_FDLIBM_MATH 1
 #define __STDC__
-
-#elif defined(linux)
-#define JS_USE_FDLIBM_MATH 1
-
-#else
-#define JS_USE_FDLIBM_MATH 0
 #endif
-
-#if !JS_USE_FDLIBM_MATH
-
-/*
- * Use system provided math routines.
- */
-
-#define fd_acos acos
-#define fd_asin asin
-#define fd_atan atan
-#define fd_atan2 atan2
-#define fd_ceil ceil
-#define fd_copysign copysign
-#define fd_cos cos
-#define fd_exp exp
-#define fd_fabs fabs
-#define fd_floor floor
-#define fd_fmod fmod
-#define fd_log log
-#define fd_pow pow
-#define fd_sin sin
-#define fd_sqrt sqrt
-#define fd_tan tan
-
-#else
 
 /*
  * Use math routines in fdlibm.
@@ -113,8 +115,26 @@ namespace fd {
     inline double pow(double x, double y)        { return fd_pow(x, y); }
 }
 
+#elif defined(macintosh)
+
+// the macintosh MSL provides acceptable implementations for all of these.
+namespace fd {
+    inline double atan(double x)               { return ::atan(x); }
+    inline double atan2(double x, double y)    { return ::atan2(x, y); }
+    inline double ceil(double x)               { return ::ceil(x); }
+    inline double cos(double x)                { return ::cos(x); }
+    inline double fabs(double x)               { return ::fabs(x); }
+    inline double floor(double x)              { return ::floor(x); }
+    inline double fmod(double x, double y)     { return ::fmod(x, y); }
+    inline double sin(double x)                { return ::sin(x); }
+    inline double sqrt(double x)               { return ::sqrt(x); }
+    inline double tan(double x)                { return ::tan(x); }
+    inline double copysign(double x, double y) { return ::copysign(x, y); }
+    inline double asin(double x)               { return ::asin(x); }
+    inline double acos(double x)               { return ::acos(x); }
+    inline double exp(double x)                { return ::exp(x); }
+    inline double log(double x)                { return ::log(x); }
+    inline double pow(double x, double y)      { return ::pow(x, y); }
+}
+
 #endif
-
-
-#endif
-
