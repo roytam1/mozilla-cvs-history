ba417919a6ebad6b0552593f1766f9707d42b302
beard%netscape.com <>
2001-06-02 01:53:59 +0000

    Now linking against fdlibm.

--- mozilla/js2/common.mk
+++ mozilla/js2/common.mk
@@ -5,6 +5,9 @@ LIBBOEHM = $(BOEHM_DIR)/gc.a
 JS2_DIR = $(top_srcdir)/src/
 LIBJS2 = $(JS2_DIR)/libjs2.a
 
+FDLIBM_DIR = $(top_srcdir)/../js/src/fdlibm/Linux_All_DBG.OBJ
+LIBFDLIBM = $(FDLIBM_DIR)/libfdm.a
+
 WFLAGS = -Wmissing-prototypes -Wstrict-prototypes -Wunused \
          -Wswitch
 
