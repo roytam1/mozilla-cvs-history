263eb8e6ea080b0d543a2117c49f4f482eac1bd5
pavlov%netscape.com <>
2001-06-20 07:33:40 +0000

    pull accessible from the branch too

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -52,14 +52,14 @@
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-#MOZ_CO_TAG = <tag>
+MOZ_CO_TAG = IMG2_20010618_BRANCH
 NSPR_CO_TAG = NSPRPUB_CLIENT_BRANCH
 PSM_CO_TAG = #We will now build PSM from the tip instead of a branch.
 NSS_CO_TAG = NSS_CLIENT_TAG
 LDAPCSDK_CO_TAG = LDAPCSDK_40_BRANCH
-ACCESSIBLE_CO_TAG = 
-GFX2_CO_TAG = 
-IMGLIB2_CO_TAG = 
+ACCESSIBLE_CO_TAG = IMG2_20010618_BRANCH
+GFX2_CO_TAG = IMG2_20010618_BRANCH
+IMGLIB2_CO_TAG = IMG2_20010618_BRANCH
 BUILD_MODULES = all
 
 #######################################################################
