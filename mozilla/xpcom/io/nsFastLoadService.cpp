bc184a85b3c9bc5b3c1ab123d69eded90ff7f015
brendan%mozilla.org <>
2001-06-29 16:57:20 +0000

    Avoid dumping the stream's buffer with gratuitous seeks -- nsFastLoadFile::ReadObject handles sequential and random access just fine without nsFastLoadService::GetFastLoadReferent having to tell and re-seek.

--- mozilla/xpcom/io/nsFastLoadService.cpp
+++ mozilla/xpcom/io/nsFastLoadService.cpp
@@ -56,10 +56,12 @@ nsFastLoadService::nsFastLoadService()
 nsFastLoadService::~nsFastLoadService()
 {
     gFastLoadService_ = nsnull;
+
     if (mObjectInputStream)
         mObjectInputStream->Close();
     if (mObjectOutputStream)
         mObjectOutputStream->Close();
+
     if (mFastLoadPtrMap)
         PL_DHashTableDestroy(mFastLoadPtrMap);
     if (mLock)
@@ -238,22 +240,11 @@ nsFastLoadService::GetFastLoadReferent(nsISupports* *aPtrAddr)
     nsresult rv;
     nsCOMPtr<nsISeekableStream> seekable(do_QueryInterface(mObjectInputStream));
 
-    PRUint32 saveOffset;
-    rv = seekable->Tell(&saveOffset);
+    rv = seekable->Seek(nsISeekableStream::NS_SEEK_SET, entry->mOffset);
     if (NS_FAILED(rv)) return rv;
-
-    if (saveOffset != entry->mOffset) {
-        rv = seekable->Seek(nsISeekableStream::NS_SEEK_SET, entry->mOffset);
-        if (NS_FAILED(rv)) return rv;
-    }
 
     rv = mObjectInputStream->ReadObject(PR_TRUE, aPtrAddr);
     if (NS_FAILED(rv)) return rv;
-
-    if (saveOffset != entry->mOffset) {
-        rv = seekable->Seek(nsISeekableStream::NS_SEEK_SET, saveOffset);
-        if (NS_FAILED(rv)) return rv;
-    }
 
     // Shrink the table if half the entries are removed sentinels.
     PRUint32 size = PR_BIT(mFastLoadPtrMap->sizeLog2);
