dbf4c646e9a1fad76629ad8cb26ead02dc6a8e24
waldemar%netscape.com <>
2001-07-04 03:57:39 +0000

    Merged with trunk

--- mozilla/js2/src/formatter.h
+++ mozilla/js2/src/formatter.h
@@ -17,7 +17,7 @@
  * Copyright (C) 1998 Netscape Communications Corporation. All
  * Rights Reserved.
  *
- * Contributor(s): 
+ * Contributor(s):
  *
  * Alternatively, the contents of this file may be used under the
  * terms of the GNU Public License (the "GPL"), in which case the
@@ -43,7 +43,7 @@
 #include "utilities.h"
 #include "strings.h"
 
-namespace JavaScript 
+namespace JavaScript
 {
 //
 // Output
@@ -60,6 +60,7 @@ namespace JavaScript
     }
 #endif
 
+
 // A Formatter is an abstract base class representing a simplified output stream.
 // One can print text to a Formatter by using << and the various global
 // print... methods below. Formatters accept both char and char16 text and
@@ -70,40 +71,27 @@ namespace JavaScript
         virtual void printChar16(char16 ch);
         virtual void printZStr8(const char *str);
         virtual void printStr8(const char *strBegin, const char *strEnd) = 0;
-        virtual void printStr16(const char16 *strBegin,
-                                const char16 *strEnd) = 0;
+        virtual void printStr16(const char16 *strBegin, const char16 *strEnd) = 0;
         virtual void printString16(const String &s);
         virtual void printVFormat8(const char *format, va_list args);
       public:
-        
+
         Formatter &operator<<(char ch) {printChar8(ch); return *this;}
         Formatter &operator<<(char16 ch) {printChar16(ch); return *this;}
         Formatter &operator<<(const char *str) {printZStr8(str); return *this;}
         Formatter &operator<<(const String &s) {printString16(s); return *this;}
-        Formatter &operator<<(uint32 i) {
-            printFormat(*this, "%u", i);
-            return *this;
-        }
-        Formatter &operator<<(int32 i) {
-            printFormat(*this, "%d", i);
-            return *this;
-        }
-        Formatter &operator<<(uint8 i) {
-            printFormat(*this, "%u", i);
-            return *this;
-        }
+        Formatter &operator<<(uint32 i) {printFormat(*this, "%u", i); return *this;}
+        Formatter &operator<<(int32 i) {printFormat(*this, "%d", i); return *this;}
+        Formatter &operator<<(uint8 i) {printFormat(*this, "%u", i); return *this;}
 
-
-        friend void printString(Formatter &f, const char *strBegin,
-                                const char *strEnd) {
+        friend void printString(Formatter &f, const char *strBegin, const char *strEnd) {
             f.printStr8(strBegin, strEnd);
         }
 
-        friend void printString(Formatter &f, const char16 *strBegin,
-                                const char16 *strEnd) {
+        friend void printString(Formatter &f, const char16 *strBegin, const char16 *strEnd) {
             f.printStr16(strBegin, strEnd);
         }
-        
+
         friend void printFormat(Formatter &f, const char *format, ...) {
             va_list args;
             va_start(args, format);
@@ -111,32 +99,14 @@ namespace JavaScript
             va_end(args);
         }
     };
-        
-    void printNum(Formatter &f, uint32 i, int nDigits, char pad,
-                  const char *format);
+
+    void printNum(Formatter &f, uint32 i, int nDigits, char pad, const char *format);
     void printChar(Formatter &f, char ch, int count);
     void printChar(Formatter &f, char16 ch, int count);
-
-    inline void printDec(Formatter &f, int32 i, int nDigits = 0,
-                         char pad = ' ') {
-        printNum(f, (uint32)i, nDigits, pad, "%i");
-    }
-    
-    inline void printDec(Formatter &f, uint32 i, int nDigits = 0,
-                         char pad = ' ') {
-        printNum(f, i, nDigits, pad, "%u");
-    }
-    
-    inline void printHex(Formatter &f, int32 i, int nDigits = 0,
-                         char pad = '0') {
-        printNum(f, (uint32)i, nDigits, pad, "%X");
-    }
-    
-    inline void printHex(Formatter &f, uint32 i, int nDigits = 0,
-                         char pad = '0') {
-        printNum(f, i, nDigits, pad, "%X");
-    }
-    
+    inline void printDec(Formatter &f, int32 i, int nDigits = 0, char pad = ' ') {printNum(f, (uint32)i, nDigits, pad, "%i");}
+    inline void printDec(Formatter &f, uint32 i, int nDigits = 0, char pad = ' ') {printNum(f, i, nDigits, pad, "%u");}
+    inline void printHex(Formatter &f, int32 i, int nDigits = 0, char pad = '0') {printNum(f, (uint32)i, nDigits, pad, "%X");}
+    inline void printHex(Formatter &f, uint32 i, int nDigits = 0, char pad = '0') {printNum(f, i, nDigits, pad, "%X");}
     void printPtr(Formatter &f, void *p);
 
 
@@ -147,13 +117,11 @@ namespace JavaScript
     // BitSet passed to the constructor.
     class AsciiFileFormatter: public Formatter {
         FILE *file;
-        BitSet<256> filter; // Set of first 256 characters that are to be
-                            // converted to escape sequences
+        BitSet<256> filter; // Set of first 256 characters that are to be converted to escape sequences
         bool filterEmpty;   // True if filter passes all 256 characters
       public:
-        static BitSet<256> defaultFilter;  // Default value of filter when not
-                                           // given in the constructor
-        
+        static BitSet<256> defaultFilter;  // Default value of filter when not given in the constructor
+
         explicit AsciiFileFormatter(FILE *file, BitSet<256> *filter = 0);
 
       private:
@@ -161,7 +129,7 @@ namespace JavaScript
         bool filterChar(char16 ch) {
             return char16Value(ch) >= 0x100 || filter[char16Value(ch)];
         }
-        
+
       protected:
         void printChar8(char ch);
         void printChar16(char16 ch);
@@ -204,100 +172,73 @@ namespace JavaScript
 
       private:
         STATIC_CONST(uint32, infiniteLength = 0x80000000);
-        const uint32 lineWidth; // Current maximum desired line width
+        const uint32 lineWidth;         // Current maximum desired line width
 
         struct BlockInfo {
-            uint32 margin;      // Saved margin before this block's beginning
-            uint32 lastBreak;   // Saved lastBreak before this block's beginning
-            bool fits;          // True if this entire block fits on one line
+            uint32 margin;              // Saved margin before this block's beginning
+            uint32 lastBreak;           // Saved lastBreak before this block's beginning
+            bool fits;                  // True if this entire block fits on one line
         };
+
         // Variables for the back end that prints to the destination
-        Formatter &outputFormatter;   // Destination formatter on which the
-                                      // result should be printed
-        uint32 outputPos;             // Number of characters printed on current
-                                      // output line
-        uint32 lineNum;               // Serial number of current line
-        uint32 lastBreak;             // Number of line just after the last
-                                      // break that occurred in this block
-        uint32 margin;                // Current left margin in spaces
-        ArrayBuffer<BlockInfo, 20> savedBlocks; // Stack of saved information
-                                                // about partially printed blocks
+        Formatter &outputFormatter;     // Destination formatter on which the result should be printed
+        uint32 outputPos;               // Number of characters printed on current output line
+        uint32 lineNum;                 // Serial number of current line
+        uint32 lastBreak;               // Number of line just after the last break that occurred in this block
+        uint32 margin;                  // Current left margin in spaces
+        ArrayBuffer<BlockInfo, 20> savedBlocks; // Stack of saved information about partially printed blocks
 
         // Variables for the front end that calculates block sizes
         struct Item: ListQueueEntry {
-            enum Kind {text, blockBegin, indentBlockBegin, blockEnd, indent,
-                       linearBreak, fillBreak};
+            enum Kind {text, blockBegin, indentBlockBegin, blockEnd, indent, linearBreak, fillBreak};
 
-            const Kind kind;   // The kind of this text sequence
-            bool lengthKnown;  // True if totalLength is known; always true for
-                               // text, blockEnd, and indent Items
-            uint32 length;     // Length of this text sequence, number of spaces
-                               // for this break, or delta for indent or
-                               // indentBlockBegin
-            uint32 totalLength;  // Total length of this block (for blockBegin)
-                                 // or length of this break plus following clump
-                                 // (for breaks)
-                                 // If lengthKnown is false, this is the
-                                 // serialPos of this Item instead of a length
+            const Kind kind;            // The kind of this text sequence
+            bool lengthKnown;           // True if totalLength is known; always true for text, blockEnd, and indent Items
+            uint32 length;              // Length of this text sequence, number of spaces for this break, or delta for indent or indentBlockBegin
+            uint32 totalLength;         // Total length of this block (for blockBegin) or length of this break plus following clump (for breaks);
+                                        // If lengthKnown is false, this is the serialPos of this Item instead of a length
             bool hasKind(Kind k) const {return kind == k;}
-            
-            explicit Item(Kind kind) :
-                    kind(kind), lengthKnown(true) {}
-            Item(Kind kind, uint32 length) :
-                    kind(kind), lengthKnown(true), length(length) {}
+
+            explicit Item(Kind kind): kind(kind), lengthKnown(true) {}
+            Item(Kind kind, uint32 length): kind(kind), lengthKnown(true), length(length) {}
             Item(Kind kind, uint32 length, uint32 beginSerialPos):
-                    kind(kind), lengthKnown(false), length(length),
-                    totalLength(beginSerialPos) {}
-            
+                    kind(kind), lengthKnown(false), length(length), totalLength(beginSerialPos) {}
+
             void computeTotalLength(uint32 endSerialPos) {
                 ASSERT(!lengthKnown);
                 lengthKnown = true;
                 totalLength = endSerialPos - totalLength;
             }
-            
+
         };
 
 #ifdef DEBUG
         Region *topRegion;              // Most deeply nested Region
 #endif
         uint32 nNestedBlocks;           // Number of nested Blocks
-        
+
         uint32 leftSerialPos;           // The difference rightSerialPos-
-        uint32 rightSerialPos;          // leftSerialPos is always the number of
-                                        // characters that would be output by
-                                        // printing activeItems if they all fit
-                                        // on one line; only the difference
-                                        // matters -- the absolute values are
-                                        // irrelevant and may wrap around 2^32.
-        
-        ArrayQueue<Item *, 20> itemStack; // Stack of enclosing nested Items
-                                          // whose lengths have not yet been
-                                          // determined itemStack always has
-                                          // room for at least nNestedBlocks
-                                          // extra entries so that end Items
-                                          // may be added without throwing an
-                                          // exception.
-        
-        Pool<Item> itemPool;              // Pool from which to allocate
-                                          // activeItems
-        
-        ListQueue<Item> activeItems;      // Queue of items left to be printed
-        
-        ArrayQueue<char16, 256> itemText; // Text of text items in activeItems,
-                                          // in the same order as in activeItems
+        uint32 rightSerialPos;          // leftSerialPos is always the number of characters that would be output by
+                                        // printing activeItems if they all fit on one line; only the difference
+                                        // matters -- the absolute values are irrelevant and may wrap around 2^32.
+
+        ArrayQueue<Item *, 20> itemStack; // Stack of enclosing nested Items whose lengths have not yet been determined;
+                                        // itemStack always has room for at least nNestedBlocks extra entries so that end Items
+                                        // may be added without throwing an exception.
+        Pool<Item> itemPool;            // Pool from which to allocate activeItems
+        ListQueue<Item> activeItems;    // Queue of items left to be printed
+        ArrayQueue<char16, 256> itemText; // Text of text items in activeItems, in the same order as in activeItems
 
       public:
-        static uint32 defaultLineWidth;   // Default for lineWidth if not given
-                                          // to the constructor
+        static uint32 defaultLineWidth; // Default for lineWidth if not given to the constructor
 
-        explicit PrettyPrinter(Formatter &f,
-                               uint32 lineWidth = defaultLineWidth);
+        explicit PrettyPrinter(Formatter &f, uint32 lineWidth = defaultLineWidth);
       private:
         PrettyPrinter(const PrettyPrinter&);    // No copy constructor
         void operator=(const PrettyPrinter&);   // No assignment operator
       public:
         ~PrettyPrinter();
-        
+
       private:
         void outputBreak(bool sameLine, uint32 nSpaces);
         bool reduceLeftActiveItems(uint32 rightOffset);
@@ -317,13 +258,10 @@ namespace JavaScript
       public:
 
         void requiredBreak();
-        void linearBreak(uint32 nSpaces) {conditionalBreak(nSpaces,
-                                                           Item::linearBreak);}
+        void linearBreak(uint32 nSpaces) {conditionalBreak(nSpaces, Item::linearBreak);}
         void linearBreak(uint32 nSpaces, bool required);
-        void fillBreak(uint32 nSpaces) {
-            conditionalBreak(nSpaces, Item::fillBreak);
-        }
-        
+        void fillBreak(uint32 nSpaces) {conditionalBreak(nSpaces, Item::fillBreak);}
+
         void end();
 
         friend class Region;
@@ -337,9 +275,7 @@ namespace JavaScript
           protected:
             PrettyPrinter &pp;
 
-            Region(PrettyPrinter &pp) : pp(pp) {
-                DEBUG_ONLY(next = pp.topRegion; pp.topRegion = this;);
-            }
+            Region(PrettyPrinter &pp): pp(pp) {DEBUG_ONLY(next = pp.topRegion; pp.topRegion = this;);}
           private:
             Region(const Region&);          // No copy constructor
             void operator=(const Region&);  // No assignment operator
@@ -348,7 +284,7 @@ namespace JavaScript
             ~Region() {pp.topRegion = next;}
 #endif
         };
-        
+
         // Use an Indent object to temporarily indent a PrettyPrinter by the
         // offset given to the Indent's constructor. The PrettyPrinter's margin
         // is set back to its original value when the Indent object is destroyed.
@@ -358,11 +294,10 @@ namespace JavaScript
         class Indent: public Region {
             Item &endItem;              // The Item returned by beginIndent
           public:
-            Indent(PrettyPrinter &pp, int32 offset) :
-                    Region(pp), endItem(pp.beginIndent(offset)) {}
+            Indent(PrettyPrinter &pp, int32 offset): Region(pp), endItem(pp.beginIndent(offset)) {}
             ~Indent() {pp.endIndent(endItem);}
         };
-        
+
         // Use a Block object to temporarily enter a PrettyPrinter block.  If an
         // offset is provided, line breaks inside the block are indented by that
         // offset relative to the existing indent; otherwise, line breaks inside
@@ -372,20 +307,14 @@ namespace JavaScript
         class Block: public Region {
             Item &endItem;              // The Item returned by beginBlock
           public:
-            explicit Block(PrettyPrinter &pp) :
-                    Region(pp), endItem(pp.beginBlock(Item::blockBegin, 0)) {}
-            Block(PrettyPrinter &pp, int32 offset) :
-                    Region(pp), endItem(pp.beginBlock(Item::indentBlockBegin,
-                                                      offset)) {}
+            explicit Block(PrettyPrinter &pp): Region(pp), endItem(pp.beginBlock(Item::blockBegin, 0)) {}
+            Block(PrettyPrinter &pp, int32 offset): Region(pp), endItem(pp.beginBlock(Item::indentBlockBegin, offset)) {}
             ~Block() {pp.endBlock(endItem);}
         };
     };
 
-    
-    void escapeString(Formatter &f, const char16 *begin, const char16 *end,
-                      char16 quote);
-    void quoteString(Formatter &f, const String &s, char16 quote);
-    
-}
 
+    void escapeString(Formatter &f, const char16 *begin, const char16 *end, char16 quote);
+    void quoteString(Formatter &f, const String &s, char16 quote);
+}
 #endif /* formatter_h___ */
