ce39f6e07aa16382197940b5479373de62451355
pschwartau%netscape.com <>
2001-07-14 02:04:25 +0000

    add warnings, locate correct fdlibm for opt builds

--- mozilla/js2/common.mk
+++ mozilla/js2/common.mk
@@ -5,15 +5,16 @@ LIBBOEHM = $(BOEHM_DIR)/gc.a
 JS2_DIR = $(top_srcdir)/src/
 LIBJS2 = $(JS2_DIR)/libjs2.a
 
-FDLIBM_DIR = $(top_srcdir)/../js/src/fdlibm/Linux_All_DBG.OBJ
-LIBFDLIBM = $(FDLIBM_DIR)/libfdm.a
-
 WFLAGS = -Wmissing-prototypes -Wstrict-prototypes -Wunused \
-         -Wswitch
-
+         -Wswitch -Wall -Wconversion
 
 if DEBUG
 CXXFLAGS = -DXP_UNIX -g -DDEBUG $(WFLAGS)
+JS1x_BINDIR = Linux_All_DBG.OBJ
 else
 CXXFLAGS = -DXP_UNIX -O2 -Wuninitialized $(WFLAGS)
+JS1x_BINDIR = Linux_All_OPT.OBJ
 endif
+
+FDLIBM_DIR = $(top_srcdir)/../js/src/fdlibm/$(JS1x_BINDIR)
+LIBFDLIBM = $(FDLIBM_DIR)/libfdm.a
