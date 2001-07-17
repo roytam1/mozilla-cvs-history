1a53bd9213d326ce63a8c0042a1279d62f7ed63f
mcafee%netscape.com <>
2001-07-17 17:56:32 +0000

    Switching to return value for PostMozilla status.

--- mozilla/tools/tinderbox/build-seamonkey-util.pl
+++ mozilla/tools/tinderbox/build-seamonkey-util.pl
@@ -688,12 +688,12 @@ sub BuildIt {
 		#
 		my $external_build = "$Settings::BaseDir/post-mozilla.pl";
 		if (-e $external_build and $build_status eq 'success') {
-		  PostMozilla::main();
-		} 
+		  $build_status = PostMozilla::main();
+		}
 
         close LOG;
         chdir $build_dir;
-        
+		
         mail_build_finished_message($start_time, $build_status, $logfile)
           if $Settings::ReportStatus;
 
