bfac530106446c241b3e1cdd02b1bf2e61ad2514
rogerl%netscape.com <>
2001-07-18 22:35:28 +0000

    Moving stuff around so as not to break other platform builds. d'oh

--- mozilla/js2/src/tracer.cpp
+++ mozilla/js2/src/tracer.cpp
@@ -132,3 +132,36 @@ void trace_dump(JavaScript::Formatter& f)
     }
 
 }
+
+
+#include "formatter.h"
+
+namespace JavaScript {
+namespace Shell {
+
+
+void do_dikdik(Formatter &f)
+{
+
+    
+f << "   \\       /                          \n";
+f << "     \\ __/     _______                \n";
+f << "     /   \\    /       \\               \n";
+f << "   / +   + \\/           \\             \n";
+f << "  |         |             \\------*    \n";
+f << "   \\       /               |          \n";
+f << "     \\   /\\               /           \n";
+f << "      | |   \\           /             \n";
+f << "      \\_/     \\  ___  /||             \n";
+f << "               ||      ||             \n";
+f << "               ||      ||             \n";
+f << "               ||      ||             \n";
+f << "               ||                     \n";
+f << "\n";
+
+}
+
+
+}
+}
+
