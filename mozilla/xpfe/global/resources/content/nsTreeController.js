db06181e992273dae82013d080e1e90380c1019c
hewitt%netscape.com <>
2001-07-19 07:25:25 +0000

    fixing typo on behalf of blake

--- mozilla/xpfe/global/resources/content/nsTreeController.js
+++ mozilla/xpfe/global/resources/content/nsTreeController.js
@@ -95,7 +95,7 @@ function nsTreeController_copy(tree)
         // the #URL property for anonymous resources (see bookmarks.js)
         //var ID = getAbsoluteID("bookmarksTree", node);
         var ID = node.id;
-        var RDFC = Components.classes[rdfc_contractid].getService(nsIRDFContainer);
+        var RDF = Components.classes[rdf_contractid].getService(Components.interfaces.nsIRDFService);
         var IDRes = RDF.GetResource(ID);
 
         var nameNode = datasource.GetTarget(IDRes, nameResource, true);
