156ca8547146f60a2e735e828bee438dbeaa1b93
bbaetz%cs.mcgill.ca <>
2001-08-03 05:43:47 +0000

    merge

--- mozilla/layout/html/style/src/nsCSSFrameConstructor.cpp
+++ mozilla/layout/html/style/src/nsCSSFrameConstructor.cpp
@@ -5521,7 +5521,6 @@ nsCSSFrameConstructor::ConstructXULFrame(nsIPresShell*            aPresShell,
       } // End of RESIZER CONSTRUCTION logic
 
       else if (aTag == nsXULAtoms::image) {
-        processChildren = PR_TRUE;
         isReplaced = PR_TRUE;
         rv = NS_NewImageBoxFrame(aPresShell, &newFrame);
       }
