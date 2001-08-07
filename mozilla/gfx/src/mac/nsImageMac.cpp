daf5bd99e33f668809753fb1ad2528838bde963b
pavlov%netscape.com <>
2001-08-07 12:50:55 +0000

    do some more pr_mins so we tile the right stuff

--- mozilla/gfx/src/mac/nsImageMac.cpp
+++ mozilla/gfx/src/mac/nsImageMac.cpp
@@ -849,10 +849,9 @@ NS_IMETHODIMP nsImageMac::DrawTile(nsIRenderingContext &aContext,
 
   for (PRInt32 y = aY0; y < aY1; y += mHeight)
     for (PRInt32 x = aX0; x < aX1; x += mHeight)
-      Draw(aContext,aSurface, x,y,
-           PR_MIN(validWidth, aX1-x),
-           PR_MIN(validHeight, aY1-y));
-
+      Draw(aContext, aSurface,
+           0, 0, PR_MIN(validWidth, aX1-x), PR_MIN(validHeight, aY1-y),
+           x, y, PR_MIN(validWidth, aX1-x), PR_MIN(validHeight, aY1-y));
 
   return PR_TRUE;
 }
