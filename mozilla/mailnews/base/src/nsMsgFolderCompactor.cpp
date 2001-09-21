6d6b3d34d279d382ae1a6ff05bca58799d9e6878
naving%netscape.com <>
2001-09-21 21:45:24 +0000

    100326 r=bienvenu sr=sspitzer Abort compact if writing to folder fails. 90702 r=bienvenu sr=sspitzer Update the expunged bytes for offline stores after compacting.

--- mozilla/mailnews/base/src/nsMsgFolderCompactor.cpp
+++ mozilla/mailnews/base/src/nsMsgFolderCompactor.cpp
@@ -484,6 +484,7 @@ nsFolderCompactState::OnStopRequest(nsIRequest *request, nsISupports *ctxt,
     // because the compact did not successfully complete.
     if (NS_SUCCEEDED(status))
     {
+      m_folder->NotifyCompactCompleted();
       CleanupTempFilesAfterError();
       ReleaseFolderLock();
       Release();
@@ -494,6 +495,7 @@ done:
   if (NS_FAILED(rv)) {
     m_status = rv; // set the status to rv so the destructor can remove the
                    // temp folder and database
+    m_folder->NotifyCompactCompleted();
     ReleaseFolderLock();
     Release(); // kill self
     return rv;
@@ -521,6 +523,8 @@ nsFolderCompactState::OnDataAvailable(nsIRequest *request, nsISupports *ctxt,
       count -= readCount;
       NS_ASSERTION (writeCount == readCount, 
                     "Oops, write fail, folder can be corrupted!\n");
+      if (writeCount != readCount)
+        return NS_MSG_ERROR_WRITING_MAIL_FOLDER;
     }
   }
   return rv;
@@ -633,6 +637,11 @@ nsOfflineStoreCompactState::FinishCompact()
   m_fileStream = nsnull;
 
     // make sure the new database is valid
+  nsCOMPtr <nsIDBFolderInfo> dbFolderInfo;
+  m_db->GetDBFolderInfo(getter_AddRefs(dbFolderInfo));
+  if (dbFolderInfo)
+    dbFolderInfo->SetExpungedBytes(0);
+  m_folder->UpdateSummaryTotals(PR_TRUE);
   m_db->SetSummaryValid(PR_TRUE);
   m_db->Commit(nsMsgDBCommitType::kLargeCommit);
 
