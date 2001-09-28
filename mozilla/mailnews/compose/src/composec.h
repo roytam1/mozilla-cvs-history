b5d1fa65578d3510b871fc1ef2780380379a2996
ddrinan%netscape.com <>
2001-09-28 05:56:54 +0000

    More work on composition

--- mozilla/mailnews/compose/src/composec.h
+++ mozilla/mailnews/compose/src/composec.h
@@ -3,17 +3,13 @@
    composec.h --- the crypto interface to MIME generation (see compose.c.)
  */
 
-#include "xp.h"
-
-XP_BEGIN_PROTOS
-
-extern int mime_begin_crypto_encapsulation (MWContext *context,
-											XP_File file,
+#include "nspr.h"
+#include "msgCore.h"
+#include "nsFileStream.h"
+extern int mime_begin_crypto_encapsulation (nsOutputFileStream * file,
 											void **closure_return,
 											PRBool encrypt_p, PRBool sign_p,
 											const char *recipients,
 											PRBool saveAsDraft);
 extern int mime_finish_crypto_encapsulation (void *closure, PRBool abort_p);
 extern int mime_crypto_write_block (void *closure, char *buf, PRInt32 size);
-
-XP_END_PROTOS
