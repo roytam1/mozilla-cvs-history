4b258423eeb45e4d843a41a1056537b02398c9ee
attinasi%netscape.com <>
2001-10-05 05:28:29 +0000

    Sanity checking params to avoid topcrash. b=100298 sr=kin r=kmcclusk a=PDT

--- mozilla/layout/html/base/src/nsBlockFrame.cpp
+++ mozilla/layout/html/base/src/nsBlockFrame.cpp
@@ -1066,6 +1066,7 @@ HaveAutoWidth(const nsHTMLReflowState& aReflowState)
 static PRBool
 IsPercentageAwareChild(const nsIFrame* aFrame)
 {
+  NS_ASSERTION(aFrame, "null frame is not allowed");
   nsresult rv;
 
   const nsStyleMargin* margin;
@@ -3449,6 +3450,8 @@ nsBlockFrame::ReflowInlineFrame(nsBlockReflowState& aState,
                                 nsIFrame* aFrame,
                                 PRUint8* aLineReflowStatus)
 {
+  NS_ENSURE_ARG_POINTER(aFrame);
+  
   *aLineReflowStatus = LINE_REFLOW_OK;
 
   // If it's currently ok to be reflowing in first-letter style then
