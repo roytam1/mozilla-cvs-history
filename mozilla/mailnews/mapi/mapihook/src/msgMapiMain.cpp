dc655439192c27c89e926ef538767751c0829837
mscott%netscape.com <>
2001-10-10 00:51:19 +0000

    Bug #102821 --> back out part of my fix from yesterday which attempted to keep mozilla.exe running until the last mapi session had logged off. We found some holes were the mozilla process wasn't getting terminated. Now we'll start up and terminate mozilla for each mapi transaction (unless it's already running of course).
    no r or sr since this is just a back out of part of yesterday's checkin.

--- mozilla/mailnews/mapi/mapihook/src/msgMapiMain.cpp
+++ mozilla/mailnews/mapi/mapihook/src/msgMapiMain.cpp
@@ -35,7 +35,6 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include "msgMapiMain.h"
-#include "nsIAppShellService.h"
 #include "nsIServiceManager.h"
 #include "nsCOMPtr.h"
 
@@ -139,14 +138,6 @@ PRInt16 nsMAPIConfiguration::RegisterSession(PRUint32 aHwnd,
 
     PR_Lock(m_Lock);
 
-    // make sure the app doesn't exit until all mapi sessions have been logged off....
-    if (!sessionCount)
-    {
-      nsCOMPtr<nsIAppShellService> appShell(do_GetService("@mozilla.org/appshell/appShellService;1"));
-      if (appShell)
-        appShell->SetQuitOnLastWindowClosing(PR_FALSE);
-    }
-
     // Check whether max sessions is exceeded
 
     if (sessionCount >= m_nMaxSessions)
@@ -234,15 +225,6 @@ PRBool nsMAPIConfiguration::UnRegisterSession(PRUint32 aSessionID)
                 bResult = PR_TRUE;
             }
         }
-    }
-
-        // if we are back to 0 sessions again....
-    if (!sessionCount)
-    {
-      // ...then allow mozilla to exit after hte last window is closed...
-      nsCOMPtr<nsIAppShellService> appShell(do_GetService("@mozilla.org/appshell/appShellService;1"));
-      if (appShell)
-        appShell->SetQuitOnLastWindowClosing(PR_TRUE);
     }
 
     PR_Unlock(m_Lock);
