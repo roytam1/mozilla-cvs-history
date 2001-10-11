b60a9336833af6725480cf75977b371659359bd8
srilatha%netscape.com <>
2001-10-11 10:43:32 +0000

    Fix for bug# 103908. Checking the mapidll, changed to getSytemDirectory and also fixed getShowDialog() r=ssu, sr=dveditz, a=pdt.

--- mozilla/mailnews/mapi/registry/src/nsMapiRegistryUtils.h
+++ mozilla/mailnews/mapi/registry/src/nsMapiRegistryUtils.h
@@ -45,15 +45,6 @@
 #include "Registry.h"
 #include "nsString.h"
 
-// This function returns the (fully-qualified) name of this executable.
-static nsCString thisApplication();
-
-// returns the brandName of the current application
-static nsCString brandName();
-
-// returns TRUE if the Mapi32.dll is smart dll.
-static PRBool isSmartDll();
-
 //Sets the registry key for basekey/keyname valuename.
 nsresult SetRegistryKey(HKEY baseKey, const char * keyName, 
                         const char * valueName, char * value);
