de23115d7de5b30e130d4c713e0ad9016d7d2799
rginda%netscape.com <>
2001-11-11 07:56:53 +0000

    rev

--- mozilla/extensions/irc/xul/content/static.js
+++ mozilla/extensions/irc/xul/content/static.js
@@ -36,7 +36,7 @@ const MSG_UNKNOWN   = getMsg ("unknown");
 
 client.defaultNick = getMsg( "defaultNick" );
 
-client.version = "0.8.5-pre9";
+client.version = "0.8.5-pre10";
 
 client.TYPE = "IRCClient";
 client.COMMAND_CHAR = "/";
