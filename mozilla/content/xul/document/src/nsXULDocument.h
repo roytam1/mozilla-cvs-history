d8de7f7ab2c9586cd62d28ec694dbfe21ccb18ce
waterson%netscape.com <>
2001-11-15 02:06:04 +0000

    Bug 108775. r=jag, sr=hyatt, a=blizzard.

--- mozilla/content/xul/document/src/nsXULDocument.h
+++ mozilla/content/xul/document/src/nsXULDocument.h
@@ -584,6 +584,7 @@ protected:
     nsVoidArray                mSubDocuments;     // [OWNER] of subelements
     PRPackedBool               mIsPopup;
     PRPackedBool               mIsFastLoad;
+    PRPackedBool               mApplyingPersistedAttrs;
     nsXULDocument*             mNextFastLoad;
     nsCOMPtr<nsIDOMXULCommandDispatcher>     mCommandDispatcher; // [OWNER] of the focus tracker
 
