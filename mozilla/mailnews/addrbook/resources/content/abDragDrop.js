f35bfd4d42a2432e980b712c7eca58ea6e853a92
sspitzer%netscape.com <>
2001-11-20 17:00:26 +0000

    more work for dnd.

--- mozilla/mailnews/addrbook/resources/content/abDragDrop.js
+++ mozilla/mailnews/addrbook/resources/content/abDragDrop.js
@@ -36,21 +36,17 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var abResultPaneObserver = {
+var abResultsPaneObserver = {
   onDragStart: function (aEvent, aXferData, aDragAction)
     {
       aXferData.data = new TransferData();
+      var selectedCards = GetSelectedAbCards();
+      var selectedAddresses = GetSelectedAddresses();
 
-      var selArray = GetSelectedAbCards();
-      var count = selArray.length;
-      dump("selArray.length = " + count + "\n");
-      for (i = 0; i < count; i++ ) {
-        var address = GenerateAddressFromCard(selArray[i]);
-        dump("address #" + i + " = " + address + "\n");
-
-        aXferData.data.addDataForFlavour("moz/abcard", selArray[i]);
-        aXferData.data.addDataForFlavour("text/x-moz-address", address);
-      }
+      dump("XXX " + selectedAddresses + "\n");
+ 
+      aXferData.data.addDataForFlavour("moz/abcard", selectedCards);
+      aXferData.data.addDataForFlavour("text/x-moz-address", selectedAddresses);
     },
 
   onDrop: function (aEvent, aXferData, aDragSession)
@@ -67,147 +63,39 @@ var abResultPaneObserver = {
 
   getSupportedFlavours: function ()
     {
+     dump("XXX getSupportedFlavours\n");
+     return null;
+    }
+};
+
+var abDirTreeObserver = {
+  onDragStart: function (aEvent, aXferData, aDragAction)
+    {
+    },
+
+  onDrop: function (aEvent, aXferData, aDragSession)
+    {
+	    var xferData = aXferData.data.split("\n");
+      dump("XXX = " + xferData[0] + "\n");
+    },
+
+  onDragExit: function (aEvent, aDragSession)
+    {
+    },
+
+  onDragOver: function (aEvent, aFlavour, aDragSession)
+    {
+      var id = aEvent.target.id;
+      var uri = aEvent.target.getAttribute("uri");
+      dump("over = " + id + "," + uri + "\n");
+      return true;
+    },
+
+  getSupportedFlavours: function ()
+    {
       var flavourSet = new FlavourSet();
       flavourSet.appendFlavour("moz/abcard");
-      flavourSet.appendFlavour("text/x-moz-address");
       return flavourSet;
     }
 };
 
-function debugDump(msg)
-{
-  // uncomment for noise
-  dump(msg+"\n");
-}
-
-function GetDragService()
-{
-  var dragService = Components.classes["@mozilla.org/widget/dragservice;1"].getService();
-  if (dragService) 
-    dragService = dragService.QueryInterface(nsIDragService);
-
-  return dragService;
-}
-
-function DragOverTree(event)
-{
-	var validFlavor = false;
-	var dragSession = null;
-	var retVal = true;
-
-	var dragService = GetDragService();
-	if ( !dragService )	return(false);
-
-	dragSession = dragService.getCurrentSession();
-	if ( !dragSession )	return(false);
-
-	if ( dragSession.isDataFlavorSupported("text/x-moz-address"))	
-    validFlavor = true;
-
-	// touch the attribute on the rowgroup to trigger the repaint with the drop feedback.
-	if ( validFlavor )
-	{
-		//XXX this is really slow and likes to refresh N times per second.
-		var treeItem = event.target.parentNode.parentNode;
-		treeItem.setAttribute ( "dd-triggerrepaint", 0 );
-		dragSession.canDrop = true;
-		// necessary??
-		retVal = false; // do not propagate message
-	}
-	return(retVal);
-}
-
-function DropOnDirectoryTree(event)
-{
-	debugDump("DropOnTree\n");
-
-    if (event.target.localName != "treecell" &&
-        event.target.localName != "treeitem")
-        return false;
-
-  var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService().QueryInterface(Components.interfaces.nsIRDFService);
-
-	var treeRoot = dirTree;
-	if (!treeRoot)	return(false);
-	var treeDatabase = treeRoot.database;
-	if (!treeDatabase)
-    return(false);
-
-	// target is the <treecell>, and "id" is on the <treeitem> two levels above
-	var treeItem = event.target.parentNode.parentNode;
-	if (!treeItem)
-    return(false);
-
-	// drop action is always "on" not "before" or "after"
-	// get drop hint attributes
-	var dropBefore = treeItem.getAttribute("dd-droplocation");
-	var dropOn = treeItem.getAttribute("dd-dropon");
-
-	var dropAction;
-	if (dropOn == "true") 
-		dropAction = "on";
-	else
-		return(false);
-
-	var targetID = treeItem.getAttribute("id");
-	if (!targetID)
-    return(false);
-
-	debugDump("***targetID = " + targetID + "\n");
-
-	var dragService = GetDragService();
-	if ( !dragService )	
-    return(false);
-	
-	var dragSession = dragService.getCurrentSession();
-	if ( !dragSession )	
-    return(false);
-
-	var trans = Components.classes["@mozilla.org/widget/transferable;1"].createInstance(Components.interfaces.nsITransferable);
-	if ( !trans ) 
-    return(false);
-	trans.addDataFlavor("text/x-moz-address");
-
-	for ( var i = 0; i < dragSession.numDropItems; ++i )
-	{
-		dragSession.getData ( trans, i );
-		var dataObj = new Object();
-		var bestFlavor = new Object();
-		var len = new Object();
-		trans.getAnyTransferData ( bestFlavor, dataObj, len );
-		if ( dataObj )
-      dataObj = dataObj.value.QueryInterface(Components.interfaces.nsISupportsWString);
-		if ( !dataObj )	
-      continue;
-
-		var address = dataObj.data.substring(0, len.value);
-		if (!address)	
-      continue;
-
-		debugDump("drop address #" + i + ": drop '" + address + "' " + dropAction + " '" + targetID + "'\n");
-
-/*
-		var sourceNode = rdf.GetResource(sourceID, true);
-		if (!sourceNode)
-			continue;
-		
-		var targetNode = rdf.GetResource(targetID, true);
-		if (!targetNode) 
-			continue;
-
-		// Prevent dropping of a node before, after, or on itself
-		if (sourceNode == targetNode)	
-      continue;
-
-		if (sourceID.substring(0,targetID.length) != targetID)
-		{
-			var cardResource = rdf.GetResource(sourceID);
-			var card = cardResource.QueryInterface(Components.interfaces.nsIAbCard);
-			if (!card.isMailList)
-				card.dropCardToDatabase(targetID);
-		}
-*/
-	}
-
-	return(false);
-}
