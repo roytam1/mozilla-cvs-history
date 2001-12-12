928d61b5a2ded712797f688e4edd4f7eb3153d02
shannond%netscape.com <>
2001-12-12 21:51:09 +0000

    Bug 113818.  Additional changes and error checking in engine.cpp

--- mozilla/cck/ib/engine.cpp
+++ mozilla/cck/ib/engine.cpp
@@ -20,54 +20,72 @@ int main(int argc, char *argv[])
   CString che_path;
   CString che_file;
   
-  if(!strcmp(argv[1], "-c"))
+
+  if(argc == 3)
   {
-    //The option "-c" means that ibengine.exe was called from wizardmachine.exe
     configPath = argv[2];
     FillGlobalWidgetArray(configPath);
-  }
-  else if(!strcmp(argv[1], "-u"))
-  {
-    //The option "-u" means that the user is running ibengine.exe via command line
-    configPath = argv[2];
-    FillGlobalWidgetArray(configPath);
-    rootPath = GetModulePath();
-    templateDir = rootPath + "WSTemplate";
-    configname = GetGlobal("_NewConfigName");
-    configpath = rootPath + "Configs\\" + configname;
 
-    //Grab exact name of the .che file from configPath
-    che_file = configPath;
-    int extractposition = che_file.ReverseFind('\\');
-    extractposition++;
-    extractposition = (che_file.GetLength()) - extractposition;
-    che_file = che_file.Right(extractposition);
+    if(!strcmp(argv[1], "-c"))
+    {
+      //The option "-c" means that ibengine.exe 
+      //was called from wizardmachine.exe
 
-    //These are some commands that we only want to run if ibengine.exe is run at command line
+      StartIB();
+    }
+    else if(!strcmp(argv[1], "-u"))
+    {
+      //The option "-u" means that the user is 
+      //running ibengine.exe via command line
 
-    //Create the config path
-    _mkdir(configpath);
+      rootPath = GetModulePath();
+      templateDir = rootPath + "WSTemplate";
+      configname = GetGlobal("_NewConfigName");
+      configpath = rootPath + "Configs\\" + configname;
 
-    //Copy files and directories from WSTemplate to new config directory
-    CopyDir(templateDir, configpath, NULL, FALSE);
+      //Grab exact name of the .che file from configPath
+      che_file = configPath;
+      int extractposition = che_file.ReverseFind('\\');
+      extractposition++;
+      extractposition = (che_file.GetLength()) - extractposition;
+      che_file = che_file.Right(extractposition);
 
-    //Copy the .che file given on command line to the appropriate directory
-    che_path = configpath + "\\" + che_file;
-    CopyFile(configPath, che_path, FALSE);
+      //These are some commands that we only want to run if 
+      //ibengine.exe is run at command line
+
+      //Create the config path
+      _mkdir(configpath);
+
+      //Copy files and directories from WSTemplate 
+      //to new config directory
+      CopyDir(templateDir, configpath, NULL, FALSE);
+
+      //Copy the .che file given on command line to the 
+      //appropriate directory
+      che_path = configpath + "\\" + che_file;
+      CopyFile(configPath, che_path, FALSE);
+
+      StartIB();
+
+      printf("\nInstaller creation is complete.  The build is "
+        "in %sConfigs\\%s\\Output\n", rootPath, configname); 
+
+    }
+    else
+    {
+      printf("\nYou have supplied incorrect command line options. \n"
+             "Please, run either \"WizardMachine.exe\" "
+             "or \"ibengine.exe -u <your_config_file_path>\"\n");
+      return 1;
+    } 
   }
   else
   {
     printf("\nYou have supplied incorrect command line options. \n"
-           "Please, run either \"WizardMachine.exe\" or \"ibengine.exe -u <your_config_file_path>\"\n");
+           "Please, run either \"WizardMachine.exe\" "
+           "or \"ibengine.exe -u <your_config_file_path>\"\n");
     return 1;
-  } 
+  }
       
-
-  StartIB();
-
-  CString root = GetGlobal("Root");
-  CString configName = GetGlobal("_NewConfigName");
-
-  printf("\nInstaller creation is complete. The build is in %sConfigs\\%s\\Output\n", root, configName);
   return 0;
 }
