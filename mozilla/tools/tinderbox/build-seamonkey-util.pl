c7fc9af0d1fa07c453c781cb5d3b982f6242ecc6
cltbld%netscape.com <>
2001-12-14 07:30:22 +0000

    Adding auto=1 to layout test url

--- mozilla/tools/tinderbox/build-seamonkey-util.pl
+++ mozilla/tools/tinderbox/build-seamonkey-util.pl
@@ -970,7 +970,7 @@ sub run_all_tests {
 	  # load tests instead of hard-coding to localhost.
 	  $test_result =
 		FileBasedTest("LayoutPerformanceTest", $build_dir, $binary_dir,
-					  $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=30000\"",
+					  $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=30000&auto=1\"",
 					  $Settings::LayoutPerformanceTestTimeout,
 					  "_x_x_mozilla_page_load", 1, 0);
     }
