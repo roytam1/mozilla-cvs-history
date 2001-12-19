8f3d7dba6b05dd3d5e397959ae7df8fb1ed5d393
peterlubczynski%netscape.com <>
2001-12-19 16:32:08 +0000

    Fix for propigating stream destruction reason code to NPP_DestroyStream bug 115119 r=av sr=attinasi

--- mozilla/modules/plugin/base/src/ns4xPluginStreamListener.h
+++ mozilla/modules/plugin/base/src/ns4xPluginStreamListener.h
@@ -48,7 +48,7 @@ public:
   ns4xPluginStreamListener(nsIPluginInstance* inst, void* notifyData);
   virtual ~ns4xPluginStreamListener();
   PRBool IsStarted();
-  nsresult CleanUpStream();
+  nsresult CleanUpStream(NPReason reason);
 
 protected:
   void* mNotifyData;
