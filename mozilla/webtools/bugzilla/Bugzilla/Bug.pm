8ff6c43e494bc18a4b7aefdabe25697532f0bbcb
dkl%redhat.com <>
2001-12-27 05:09:16 +0000

    Merge with HEAD on 2001/12/26

--- mozilla/webtools/bugzilla/Bugzilla/Bug.pm
+++ mozilla/webtools/bugzilla/Bugzilla/Bug.pm
@@ -105,9 +105,7 @@ sub initBug  {
   if (!$usergroupset) { $usergroupset = '0' }
   $self->{'usergroupset'} = $usergroupset;
 
-    my $query = "";
-    if ($::driver eq 'mysql') {
-        $query = "
+  my $query = "
     select
       bugs.bug_id, product, version, rep_platform, op_sys, bug_status,
       resolution, priority, bug_severity, component, assigned_to, reporter,
@@ -117,22 +115,6 @@ sub initBug  {
     from bugs left join votes using(bug_id)
     where bugs.bug_id = $bug_id
     group by bugs.bug_id";
-    } elsif ($::driver eq 'Pg') {
-        $query = "
-    select
-      bugs.bug_id, product, version, rep_platform, op_sys, bug_status,
-      resolution, priority, bug_severity, component, assigned_to, reporter,
-      bug_file_loc, short_desc, target_milestone, qa_contact,
-      status_whiteboard, creation_ts,
-      groupset, delta_ts, sum(votes.count)
-    from bugs left join votes using(bug_id)
-    where bugs.bug_id = $bug_id
-    and (bugs.groupset & int8($usergroupset)) = bugs.groupset
-    group by bugs.bug_id, product, version, rep_platform, op_sys, bug_status,
-         resolution, priority, bug_severity, component, assigned_to, reporter,
-         bug_file_loc, short_desc, target_milestone, qa_contact, status_whiteboard,
-         creation_ts, groupset, delta_ts";
-    }
 
   &::SendSQL(&::SelectVisible($query, $user_id, $usergroupset));
   my @row;
@@ -370,7 +352,7 @@ sub UserInGroup {
         return 0;
     }
     &::ConnectToDatabase();
-    &::SendSQL("select (group_bit & int8($self->{'usergroupset'})) != 0 from groups where name = " 
+    &::SendSQL("select (bit & $self->{'usergroupset'}) != 0 from groups where name = " 
            . &::SqlQuote($groupname));
     my $bit = &::FetchOneColumn();
     if ($bit) {
@@ -454,18 +436,14 @@ sub Collision {
     my $self = shift();
     my $write = "WRITE";        # Might want to make a param to control
                                 # whether we do LOW_PRIORITY ...
-    if ($::driver eq 'mysql') {
-        &::SendSQL("LOCK TABLES bugs $write, bugs_activity $write, cc $write, " .
-                "cc AS selectVisible_cc $write, " .
-                "profiles $write, dependencies $write, votes $write, " .
-                "keywords $write, longdescs $write, fielddefs $write, " .
-                "keyworddefs READ, groups READ, attachments READ, products READ");
-    } 
+    &::SendSQL("LOCK TABLES bugs $write, bugs_activity $write, cc $write, " .
+               "cc AS selectVisible_cc $write, " .
+            "profiles $write, dependencies $write, votes $write, " .
+            "keywords $write, longdescs $write, fielddefs $write, " .
+            "keyworddefs READ, groups READ, attachments READ, products READ");
     &::SendSQL("SELECT delta_ts FROM bugs where bug_id=$self->{'bug_id'}");
     my $delta_ts = &::FetchOneColumn();
-    if ($::driver eq 'mysql') {
-        &::SendSQL("unlock tables");
-    }
+    &::SendSQL("unlock tables");
     if ($self->{'delta_ts'} ne $delta_ts) {
        return 1;
     }
