400288eef0ac7ab4fbbb68902e5c3a4b47f8757f
mcafee%netscape.com <>
2002-01-16 02:18:27 +0000

    parameterizing per-page pageloader timeout value LayoutPerformanceTestPageTimeout.

--- mozilla/tools/tinderbox/build-seamonkey-util.pl
+++ mozilla/tools/tinderbox/build-seamonkey-util.pl
@@ -1024,7 +1024,7 @@ sub run_all_tests {
       # load tests instead of hard-coding to localhost.
       $test_result =
         FileBasedTest("LayoutPerformanceTest", $build_dir, $binary_dir,
-                      $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=30000&auto=1\"",
+                      $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=15000&auto=1\"",
                       $Settings::LayoutPerformanceTestTimeout,
                       "_x_x_mozilla_page_load", 1, 0);
       
@@ -1035,7 +1035,7 @@ sub run_all_tests {
         print_log "TinderboxPrint:Tp:[CRASH]\n";
         $test_result = 
           FileBasedTest("LayoutPerformanceTest", $build_dir, $binary_dir,
-                        $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=30000&auto=1\"",
+                        $binary . " -P $Settings::MozProfileName \"http://localhost/page-loader/loader.pl?delay=1000&nocache=0&maxcyc=4&timeout=$Settings::LayoutPerformanceTestPageTimeout&auto=1\"",
                         $Settings::LayoutPerformanceTestTimeout,
                         "_x_x_mozilla_page_load", 1, 0);
       }
