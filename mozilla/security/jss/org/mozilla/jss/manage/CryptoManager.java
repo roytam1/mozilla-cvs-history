ad15fbab0409738a10f5974b67da14f30b4e7023
nicolson%netscape.com <>
2002-01-17 22:54:05 +0000

    add secret decoder ring for JSS 3.1.2, special release for CMS.

--- mozilla/security/jss/org/mozilla/jss/manage/CryptoManager.java
+++ mozilla/security/jss/org/mozilla/jss/manage/CryptoManager.java
@@ -1240,15 +1240,15 @@ public final class CryptoManager implements TokenSupplier
     /********************************************************************/
 
     public static final String
-    JAR_JSS_VERSION     = "JSS_VERSION = JSS_3_0";
+    JAR_JSS_VERSION     = "JSS_VERSION = JSS_3_1_2";
     public static final String
     JAR_JDK_VERSION     = "JDK_VERSION = JDK 1.2.2";
     public static final String
-    JAR_NSS_VERSION     = "NSS_VERSION = NSS_3_2_RTM";
+    JAR_NSS_VERSION     = "NSS_VERSION = NSS_3_3_1_RTM";
     public static final String
-    JAR_DBM_VERSION     = "DBM_VERSION = NSS_3_1_1_RTM";
+    JAR_DBM_VERSION     = "DBM_VERSION = NSS_3_3_1_RTM";
     public static final String
-    JAR_NSPR_VERSION    = "NSPR_VERSION = NSPRPUB_RELEASE_4_1";
+    JAR_NSPR_VERSION    = "NSPR_VERSION = NSPRPUB_RELEASE_4_1_2";
 
     /**
      * Loads the JSS dynamic library if necessary.
