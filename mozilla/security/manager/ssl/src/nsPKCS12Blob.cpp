7170b6ce39df30a9f91ab76028889003b3e67534
javi%netscape.com <>
2002-01-31 19:29:56 +0000

    Check in another patch required to get Mac w/ NSS 3.4 working.

--- mozilla/security/manager/ssl/src/nsPKCS12Blob.cpp
+++ mozilla/security/manager/ssl/src/nsPKCS12Blob.cpp
@@ -495,6 +495,29 @@ nsPKCS12Blob::inputToDecoder(SEC_PKCS12DecoderContext *dcx, nsILocalFile *file)
   return NS_OK;
 }
 
+#ifdef XP_MAC
+
+OSErr ConvertMacPathToUnixPath(const char *macPath, char **unixPath)
+{
+  PRIntn len;
+  char *cursor;
+  
+  len = PL_strlen(macPath);
+  cursor = (char*)PR_Malloc(len+2);
+  if (!cursor)
+    return memFullErr;
+    
+  memcpy(cursor+1, macPath, len+1);
+  *unixPath = cursor;
+  *cursor = '/';
+  while ((cursor = PL_strchr(cursor, ':')) != NULL) {
+    *cursor = '/';
+    cursor++;
+  }
+  return noErr;
+}
+#endif
+
 //
 // C callback methods
 //
