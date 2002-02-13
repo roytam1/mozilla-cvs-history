6b14245bf4391669bbb6c50c548028913074a8fa
margaret.chan%sun.com <>
2002-02-13 20:23:33 +0000

    Bug #104253 - [094] Fields in the account manager are now editable when they shouldn't be (like the server name, user name).  Ported by jeff.qiu@sun.com.  a=jdunn@netscape.

--- mozilla/mailnews/base/prefs/resources/content/am-server.js
+++ mozilla/mailnews/base/prefs/resources/content/am-server.js
@@ -30,13 +30,6 @@ function onInit()
 
     setupBiffUI();
     setupMailOnServerUI();
-
-    // disable for bug #14295.  Also see bug 104253 and 103181 for why the elements
-    // are being disabled here.
-    var disabledElem = document.getElementById("server.username");
-    disabledElem.setAttribute("disabled","true");
-    disabledElem = document.getElementById("server.hostName");
-    disabledElem.setAttribute("disabled","true");
 }
 
 function onPreInit(account, accountValues)
