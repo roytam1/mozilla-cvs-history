197993288bcc1b8165c2288f207fd76f28594008
joe.chou%sun.com <>
2002-03-04 23:16:28 +0000

    Bugzilla Bug 92132:  Typing in a path to the URL bar loads via a file;                      adds to the Netscape history as HTTP                      Igor Nekrestyanov  nis@sparc.spb.su                      Andrew Brygin      bae@sparc.spb.su                      Babak Mahbod       Babak.Mahbod@sun.com

--- mozilla/xpfe/browser/resources/content/sessionHistoryUI.js
+++ mozilla/xpfe/browser/resources/content/sessionHistoryUI.js
@@ -134,7 +134,9 @@ function addToUrlbarHistory()
        // create the nsIURI objects for comparing the 2 urls
        var ioService = Components.classes["@mozilla.org/network/io-service;1"]
                      .getService(Components.interfaces.nsIIOService);
-       
+
+       var entryToAdd = rdf.GetLiteral(urlToAdd);
+
        try {
          var unused = { };
          var scheme = ioService.extractScheme(urlToAdd, unused, unused);
@@ -192,11 +194,10 @@ function addToUrlbarHistory()
           }
        }   // while
 
-       var entry = rdf.GetLiteral(urlToAdd);
        // Otherwise, we've got a new URL in town. Add it!
        // Put the value as it was typed by the user in to RDF
        // Insert it to the beginning of the list.
-       entries.InsertElementAt(entry, 1, true);
+       entries.InsertElementAt(entryToAdd, 1, true);
 
        // Remove any expired history items so that we don't let
        // this grow without bound.
