7e7012cf5347704785437d2cb6d72c81a9bafb3a
rjesup%wgate.com <>
2002-03-12 19:41:06 +0000

    Added license block, removed spurious inlines, correct indent

--- mozilla/layout/html/base/src/nsReflowTree.h
+++ mozilla/layout/html/base/src/nsReflowTree.h
@@ -1,5 +1,44 @@
-#ifndef NSREFLOWTREE_H
-#define NSREFLOWTREE_H
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is 
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2002
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *    Mike Shaver <shaver@mozilla.org>
+ *    Randell Jesup <rjesup@wgate.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the NPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the NPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef nsReflowTree_h__
+#define nsReflowTree_h__
 ////// Iteration:
 // nsReflowTree::Iterator iter(reflowcmd->GetCurrentTarget());
 // PRBool amTarget = reflowIterator.IsTarget();
@@ -41,7 +80,7 @@ public:
         class ChildChunk {
             friend class nsReflowTree::Node;
             friend class nsReflowTree::Node::Iterator;
-            static const int KIDS_CHUNK_SIZE = 10;
+            static const int KIDS_CHUNK_SIZE = 300;
 
             Node       *mKids[KIDS_CHUNK_SIZE];
             ChildChunk *mNext;
@@ -63,12 +102,12 @@ public:
             ~Iterator() { }
             Node *NextChild();
             Node *NextChild(nsIFrame **aChildIFrame);
-	    // Set the mPos to the node with this nsIFrame, or null
+            // Set the mPos to the node with this nsIFrame, or null
             Node *SelectChild(nsIFrame *aChildIFrame);
-            inline Node *CurrentChild() { return mPos ? *mPos : nsnull; }
-            inline Node *CurrentNode()  { return mNode; }
-	    inline PRBool IsTarget()
-	      { return mNode ? mNode->IsTarget() : PR_FALSE; }
+            Node *CurrentChild() { return mPos ? *mPos : nsnull; }
+            Node *CurrentNode()  { return mNode; }
+            PRBool IsTarget()
+                { return mNode ? mNode->IsTarget() : PR_FALSE; }
         private:
             Node *mNode;
             Node **mPos;
