d29269bd83c5b9d699de7dda30d271b8fac7774c
kestes%walrus.com <>
2002-03-19 18:47:48 +0000

    some of my own personal debug code got checked in by accident, remove it. It will cause problems for others.
    Add a 1; at the bottom of the file.

--- mozilla/webtools/tinderbox2/src/lib/TinderDB/VC_Perforce.pm
+++ mozilla/webtools/tinderbox2/src/lib/TinderDB/VC_Perforce.pm
@@ -123,12 +123,7 @@ use Time::Local;
 
 # Load Tinderbox libraries
 
-use lib '/tmp/tinderbox2/bin/local_conf',
-   '/tmp/tinderbox2/bin/default_conf',
-   '/tmp/tinderbox2/bin/lib',
-   '/tmp/t/tinderbox2/./build/local_conf',
-   '/tmp/t/tinderbox2/./build/default_conf',
-   '/tmp/t/tinderbox2/./build/lib';
+use lib '#tinder_libdir#';
 
 
 use TinderDB::BasicTxtDB;
@@ -600,7 +595,6 @@ sub apply_db_updates {
   while (1) {
 
         my $change_num = $METADATA{$tree}{'next_change_num'};
-        $ENV{'PATH'}=$ENV{'PATH'}.':/usr/src/perforce';
         my (@cmd) = ('p4', 'describe', '-s', $change_num);
 
         store_cmd_output(@cmd);        
@@ -1012,3 +1006,6 @@ sub list2table_row {
     
     return $row;
 }
+
+
+1;
