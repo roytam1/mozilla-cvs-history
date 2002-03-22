11f503866526baa31e8c0e1f662108e8c8472222
margaret.chan%sun.com <>
2002-03-22 23:39:14 +0000

    Bug 104498 - optimize SetReferences() / ParseReferences().  Ported by henry.jia@sun.com.  a=jdunn@netscape.com

--- mozilla/mailnews/db/msgdb/src/nsMsgHdr.cpp
+++ mozilla/mailnews/db/msgdb/src/nsMsgHdr.cpp
@@ -292,9 +292,9 @@ NS_IMETHODIMP nsMsgHdr::GetNumReferences(PRUint16 *result)
 	return NS_OK;
 }
 
-nsresult nsMsgHdr::ParseReferences(nsCString &references)
+nsresult nsMsgHdr::ParseReferences(const char *references)
 {
-	const char *startNextRef = references.get();
+	const char *startNextRef = references;
 	nsCAutoString resultReference;
 
 	while (startNextRef && *startNextRef)
@@ -316,7 +316,7 @@ NS_IMETHODIMP nsMsgHdr::GetStringReference(PRInt32 refNum, nsCString &resultRefe
 		
 		if(NS_SUCCEEDED(err))
 		{
-			ParseReferences(references);
+			ParseReferences(references.get());
 			m_initedValues |= REFERENCES_INITED;
 		}
 	}
@@ -351,13 +351,16 @@ NS_IMETHODIMP nsMsgHdr::SetAuthor(const char *author)
 
 NS_IMETHODIMP nsMsgHdr::SetReferences(const char *references)
 {
-	nsCAutoString CStrReference(references);
-	ParseReferences(CStrReference);
+        if( *references == '\0' ) {
+            m_numReferences = 0;
+        }
+        else {
+            ParseReferences(references);
+            m_numReferences = m_references.Count();
+        }
 
-	m_numReferences = m_references.Count();
 	SetUInt32Column(m_numReferences, m_mdb->m_numReferencesColumnToken);
-
-	m_initedValues |= REFERENCES_INITED;
+        m_initedValues |= REFERENCES_INITED;
 
 	return SetStringColumn(references, m_mdb->m_referencesColumnToken);
 }
