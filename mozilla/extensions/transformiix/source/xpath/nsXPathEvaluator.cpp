7db18eb116699b4c7db12c0819f6ebcb7f062569
axel%pike.org <>
2002-04-05 10:54:25 +0000

    merging 131899, 110156, 110155 (outliner->tree), 134608

--- mozilla/extensions/transformiix/source/xpath/nsXPathEvaluator.cpp
+++ mozilla/extensions/transformiix/source/xpath/nsXPathEvaluator.cpp
@@ -65,7 +65,7 @@ nsXPathEvaluator::~nsXPathEvaluator()
 }
 
 NS_IMETHODIMP
-nsXPathEvaluator::CreateExpression(const nsAReadableString & aExpression,
+nsXPathEvaluator::CreateExpression(const nsAString & aExpression,
                                    nsIDOMXPathNSResolver *aResolver,
                                    nsIDOMXPathExpression **aResult)
 {
