abc92d3d19618fc66d21d5ecd733fc24306b55c9
bzbarsky%mit.edu <>
2002-05-05 22:43:59 +0000

    140262 - context menu for input type=password should include paste. r=biesi sr=blake, a=asa

--- mozilla/xpfe/communicator/resources/content/nsContextMenu.js
+++ mozilla/xpfe/communicator/resources/content/nsContextMenu.js
@@ -721,7 +721,6 @@ nsContextMenu.prototype = {
           attrib = type.toUpperCase();
 
         return( (attrib != "IMAGE") &&
-                (attrib != "PASSWORD") &&
                 (attrib != "CHECKBOX") &&
                 (attrib != "RADIO") &&
                 (attrib != "SUBMIT") &&
