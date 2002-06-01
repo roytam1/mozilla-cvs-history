bf5b031db7ef21ca115ef5062a953a04d8347c3d
kaie%netscape.com <>
2002-06-01 11:48:54 +0000

    b=139561 Implement S/Mime "on demand verification" for IMAP messages r=javi sr=bienvenu

--- mozilla/mailnews/base/resources/content/msgMail3PaneWindow.js
+++ mozilla/mailnews/base/resources/content/msgMail3PaneWindow.js
@@ -1209,6 +1209,11 @@ function SelectMessage(messageUri)
     gDBView.selectMsgByKey(msgHdr.messageKey);
 }
 
+function ReloadWithAllParts()
+{
+  gDBView.reloadMessageWithAllParts();
+}
+
 function ReloadMessage()
 {
   gDBView.reloadMessage();
