4ae1435a8f3e2ba04e634044e1e3811809dd1778
ian%hixie.ch <>
2002-06-08 20:41:11 +0000

    Download manager pref panel. b=132440, r=ben, sr=blake

--- mozilla/modules/libpref/src/init/all.js
+++ mozilla/modules/libpref/src/init/all.js
@@ -711,6 +711,11 @@ pref("update_notifications.enabled", true);
 pref("update_notifications.provider.0.frequency", 7); // number of days
 pref("update_notifications.provider.0.datasource", "chrome://communicator-region/locale/region.properties");
 
+// 0 opens the download manager
+// 1 opens a progress dialog
+// 2 and other values, no download manager, no progress dialog. 
+pref("browser.downloadmanager.behavior", 1);
+
 // if true, allow plug-ins to override internal imglib decoder mime types in full-page mode
 pref("plugin.override_internal_types", false);
 
