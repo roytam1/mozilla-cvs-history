79536a10ec83c590595ec986fb3208ddc1e8d446
blakeross%telocity.com <>
2002-06-15 21:55:00 +0000

    This didn't get adt+'d yet, my mistake.

--- mozilla/xpfe/communicator/resources/content/tasksOverlay.js
+++ mozilla/xpfe/communicator/resources/content/tasksOverlay.js
@@ -46,19 +46,9 @@ function toDownloadManager()
 {
   var dlmgr = Components.classes['@mozilla.org/download-manager;1'].getService();
   dlmgr = dlmgr.QueryInterface(Components.interfaces.nsIDownloadManager);
-
-  var windowMediator = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService();
-  windowMediator = windowMediator.QueryInterface(Components.interfaces.nsIWindowMediator);
-
-  var dlmgrWindow = windowMediator.getMostRecentWindow("Download:Manager");
-  if (dlmgrWindow) {
-    dlmgrWindow.focus();
-  }
-  else {
-    dlmgr.open(window);
-  }
+  dlmgr.open(window);
 }
-
+  
 function toJavaScriptConsole()
 {
     toOpenWindowByType("global:console", "chrome://global/content/console.xul");
