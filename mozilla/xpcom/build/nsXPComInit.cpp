3cfa07e8dd86732494a769a3c1042338fda6ecad
darin%netscape.com <>
2002-06-21 23:55:49 +0000

    fixes bug 147333 "Cannot load local files whose names contain Japanese/Chinese characters" r=bstell sr=brendan a=valeski adt+

--- mozilla/xpcom/build/nsXPComInit.cpp
+++ mozilla/xpcom/build/nsXPComInit.cpp
@@ -83,6 +83,9 @@
 
 #include "nsILocalFile.h"
 #include "nsLocalFile.h"
+#ifdef XP_UNIX
+#include "nsNativeCharsetUtils.h"
+#endif
 #include "nsDirectoryService.h"
 #include "nsDirectoryServiceDefs.h"
 #include "nsICategoryManager.h"
@@ -335,6 +338,9 @@ nsresult NS_COM NS_InitXPCOM2(nsIServiceManager* *result,
     rv = nsMemoryImpl::Startup();
     if (NS_FAILED(rv)) return rv;
 
+#ifdef XP_UNIX
+    NS_StartupNativeCharsetUtils();
+#endif
     NS_StartupLocalFile();
 
     StartupSpecialSystemDirectory();
@@ -573,6 +579,9 @@ nsresult NS_COM NS_ShutdownXPCOM(nsIServiceManager* servMgr)
 
     // Shutdown nsLocalFile string conversion
     NS_ShutdownLocalFile();
+#ifdef XP_UNIX
+    NS_ShutdownNativeCharsetUtils();
+#endif
 
     // Shutdown the timer thread and all timers that might still be alive before
     // shutting down the component manager
