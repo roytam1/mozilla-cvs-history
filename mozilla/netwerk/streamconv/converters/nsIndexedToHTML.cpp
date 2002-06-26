c180e939faa66d490a4070ab7d85c9b6bea56163
bbaetz%student.usyd.edu.au <>
2002-06-26 12:09:44 +0000

    Oops, didn't notice tree was closed - backed out previous patch.

--- mozilla/netwerk/streamconv/converters/nsIndexedToHTML.cpp
+++ mozilla/netwerk/streamconv/converters/nsIndexedToHTML.cpp
@@ -211,9 +211,12 @@ nsIndexedToHTML::OnStartRequest(nsIRequest* request, nsISupports *aContext) {
     // Anything but a gopher url needs to end in a /,
     // otherwise we end up linking to file:///foo/dirfile
 
+    char* spec = nsCRT::strdup(titleUri.get());
+
     buffer.Append(NS_LITERAL_STRING("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head><title>"));
 
     nsXPIDLString title;
+    nsString uniSpec; 
     nsXPIDLCString encoding;
     rv = mParser->GetEncoding(getter_Copies(encoding));
     if (NS_FAILED(rv)) return rv;
@@ -223,17 +226,14 @@ nsIndexedToHTML::OnStartRequest(nsIRequest* request, nsISupports *aContext) {
         if (NS_FAILED(rv)) return rv;
     }
 
-    nsXPIDLString unEscapeSpec;
-    rv = mTextToSubURI->UnEscapeAndConvert(encoding, titleUri.get(),
-                                           getter_Copies(unEscapeSpec));
+    PRUnichar   *unEscapeSpec = nsnull;
+    rv = mTextToSubURI->UnEscapeAndConvert(encoding, spec, &unEscapeSpec);
     if (NS_FAILED(rv)) return rv;
     
-    nsXPIDLString htmlEscSpec;
-    htmlEscSpec.Adopt(nsEscapeHTML2(unEscapeSpec.get(),
-                                    unEscapeSpec.Length()));
+    uniSpec.Adopt(unEscapeSpec);
 
     const PRUnichar* formatTitle[] = {
-        htmlEscSpec.get()
+        uniSpec.get()
     };
 
     rv = mBundle->FormatStringFromName(NS_LITERAL_STRING("DirTitle").get(),
@@ -243,7 +243,7 @@ nsIndexedToHTML::OnStartRequest(nsIRequest* request, nsISupports *aContext) {
     if (NS_FAILED(rv)) return rv;
     buffer.Append(title);
 
-    buffer.Append(NS_LITERAL_STRING("</title><base href=\""));    
+    buffer.Append(NS_LITERAL_STRING("</title><base href=\""));
     buffer.Append(NS_ConvertASCIItoUCS2(baseUri));
     buffer.Append(NS_LITERAL_STRING("\"/>\n"));
 
@@ -258,7 +258,7 @@ nsIndexedToHTML::OnStartRequest(nsIRequest* request, nsISupports *aContext) {
     buffer.Append(NS_LITERAL_STRING("</head>\n<body>\n<h1>"));
     
     const PRUnichar* formatHeading[] = {
-        htmlEscSpec.get()
+        uniSpec.get()
     };
 
     rv = mBundle->FormatStringFromName(NS_LITERAL_STRING("DirTitle").get(),
