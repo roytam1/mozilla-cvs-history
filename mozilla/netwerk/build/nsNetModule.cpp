e9a521648261142352f6348f4cc4044da64608f1
sspitzer%netscape.com <>
2002-07-14 06:16:02 +0000

    back again, not ready for prime time.

--- mozilla/netwerk/build/nsNetModule.cpp
+++ mozilla/netwerk/build/nsNetModule.cpp
@@ -918,7 +918,6 @@ static const nsModuleComponentInfo gNetModuleInfo[] = {
     },
 
 #ifdef USE_DEFAULT_BROWSER
-#else
     { "HTTP Handler",
       NS_HTTPPROTOCOLHANDLER_CID,
       NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "http",
@@ -928,7 +927,7 @@ static const nsModuleComponentInfo gNetModuleInfo[] = {
       NS_HTTPPROTOCOLHANDLER_CID,
       NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "https",
       nsHttpHandler::Create },
-
+#else
     { "absync (really HTTP) Handler",
       NS_HTTPPROTOCOLHANDLER_CID,
       NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "absync",
