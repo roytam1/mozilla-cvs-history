98e1e3844d4051b8ad9dfae3f04cb97c6ba3f1ad
leaf%mozilla.org <>
2002-08-05 22:20:29 +0000

    pull the branch.

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -52,14 +52,14 @@
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-#MOZ_CO_TAG = <tag>
-NSPR_CO_TAG = NSPRPUB_PRE_4_2_CLIENT_BRANCH
-PSM_CO_TAG = #We will now build PSM from the tip instead of a branch.
-NSS_CO_TAG = NSS_CLIENT_TAG
-LDAPCSDK_CO_TAG = ldapcsdk_50_client_branch
-ACCESSIBLE_CO_TAG = 
-GFX2_CO_TAG = 
-IMGLIB2_CO_TAG = 
+MOZ_CO_TAG = MOZILLA_1_1_BRANCH
+NSPR_CO_TAG = MOZILLA_1_1_BRANCH
+PSM_CO_TAG = MOZILLA_1_1_BRANCH
+NSS_CO_TAG = MOZILLA_1_1_BRANCH
+LDAPCSDK_CO_TAG = MOZILLA_1_1_BRANCH
+ACCESSIBLE_CO_TAG = MOZILLA_1_1_BRANCH
+GFX2_CO_TAG = MOZILLA_1_1_BRANCH
+IMGLIB2_CO_TAG = MOZILLA_1_1_BRANCH
 BUILD_MODULES = all
 
 #######################################################################
