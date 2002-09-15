65a695013ccc085bda09f06966e48290043c5975
dmose%netscape.com <>
2002-09-15 03:52:52 +0000

    First cut at prefs file.

---
+++ mozilla/mailnews/extensions/junkmail/junkmailPrefs.js
@@ -0,0 +1,6 @@
+pref("mail.server.default.junkmail.threshhold", 255.0);
+
+// should messages that have been identified as junk be moved to another 
+// folder?
+//
+pref("mail.server.default.junkmail.move", true);
