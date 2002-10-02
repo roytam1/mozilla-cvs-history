aa3847de11bbada1f04dbbfa0a5b72cded90c1bd
pinkerton%netscape.com <>
2002-10-02 17:09:32 +0000

    Add way to edit cookie sites that are blocked or allowed (bug 160123)

--- mozilla/camino/PreferencePanes/Privacy/PrivacyPane.mm
+++ mozilla/camino/PreferencePanes/Privacy/PrivacyPane.mm
@@ -4,6 +4,11 @@
 #include "nsIServiceManagerUtils.h"
 #include "nsIPref.h"
 #include "nsCCookieManager.h"
+#include "nsIPermissionManager.h"
+#include "nsISimpleEnumerator.h"
+#include "nsIPermission.h"
+#include "nsISupportsArray.h"
+#include "nsXPIDLString.h"
 
 
 // prefs for keychain password autofill
@@ -14,6 +19,7 @@ static const char* const gAutoFillEnabledPref = "chimera.keychain_passwords_auto
 
 - (void) dealloc
 {
+  NS_IF_RELEASE(mManager);
   [super dealloc];
 }
 
@@ -35,6 +41,11 @@ static const char* const gAutoFillEnabledPref = "chimera.keychain_passwords_auto
   PRBool warnAboutCookies = PR_TRUE;
   mPrefService->GetBoolPref("network.cookie.warnAboutCookies", &warnAboutCookies);
   [mPromptForCookie setState:(warnAboutCookies ? NSOnState : NSOffState)];
+  
+  // store permission manager service and cache the enumerator.
+  nsCOMPtr<nsIPermissionManager> pm ( do_GetService(NS_PERMISSIONMANAGER_CONTRACTID) );
+  mManager = pm.get();
+  NS_IF_ADDREF(mManager);
   
   // Keychain checkboxes	
 
@@ -82,6 +93,119 @@ static const char* const gAutoFillEnabledPref = "chimera.keychain_passwords_auto
     return;
   mPrefService->SetIntPref("network.cookie.cookieBehavior", [[mCookies selectedCell] tag]);
 }
+
+
+-(IBAction) editCookieSites:(id)aSender
+{
+  nsCOMPtr<nsISimpleEnumerator> permEnum;
+  if ( mManager ) 
+    mManager->GetEnumerator(getter_AddRefs(permEnum));
+
+  // build parallel permission list for speed with a lot of blocked sites
+  NS_NewISupportsArray(&mCachedPermissions);     // ADDREFs
+  if ( mCachedPermissions && permEnum ) {
+    PRBool hasMoreElements = PR_FALSE;
+    permEnum->HasMoreElements(&hasMoreElements);
+    while ( hasMoreElements ) {
+      nsCOMPtr<nsISupports> curr;
+      permEnum->GetNext(getter_AddRefs(curr));
+      mCachedPermissions->AppendElement(curr);
+      
+      permEnum->HasMoreElements(&hasMoreElements);
+    }
+  }
+  
+	[NSApp beginSheet:mCookieSitePanel
+        modalForWindow:[mCookies window]		// any old window accessor
+        modalDelegate:self
+        didEndSelector:@selector(editCookieSitesSheetDidEnd:returnCode:contextInfo:)
+        contextInfo:NULL];
+        
+  // ensure a row is selected (cocoa doesn't do this for us, but will keep
+  // us from unselecting a row once one is set; go figure).
+  [mSiteTable selectRow:0 byExtendingSelection:NO];
+}
+
+-(IBAction) editCookieSitesDone:(id)aSender
+{
+  // save stuff
+  
+  [mCookieSitePanel orderOut:self];
+  [NSApp endSheet:mCookieSitePanel];
+  
+	NS_IF_RELEASE(mCachedPermissions);
+}
+
+- (void)editCookieSitesSheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void  *)contextInfo
+{
+}
+
+
+-(IBAction) removeCookieSite:(id)aSender
+{
+  if ( mCachedPermissions && mManager ) {
+    // remove from parallel array and cookie permissions list
+    int row = [mSiteTable selectedRow];
+    
+    // remove from permission manager (which is done by host, not by row), then 
+    // remove it from our parallel array (which is done by row). Since we keep a
+    // parallel array, removing multiple items by row is very difficult since after 
+    // deleting, the array is out of sync with the next cocoa row we're told to remove. Punt!
+    nsCOMPtr<nsISupports> rowItem = dont_AddRef(mCachedPermissions->ElementAt(row));
+    nsCOMPtr<nsIPermission> perm ( do_QueryInterface(rowItem) );
+    if ( perm ) {
+      nsXPIDLCString host;
+      perm->GetHost(getter_Copies(host));
+      mManager->Remove(host.get(), 0);           // could this api _be_ any worse? Come on!
+      
+      mCachedPermissions->RemoveElementAt(row);
+    }
+    [mSiteTable reloadData];
+  }
+}
+
+
+//
+// NSTableDataSource protocol methods
+//
+
+- (int)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+  PRUint32 numRows = 0;
+  if ( mCachedPermissions )
+    mCachedPermissions->Count(&numRows);
+
+  return (int) numRows;
+}
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(int)rowIndex
+{
+  NSString* retVal = nil;
+  if ( mCachedPermissions ) {
+    nsCOMPtr<nsISupports> rowItem = dont_AddRef(mCachedPermissions->ElementAt(rowIndex));
+    nsCOMPtr<nsIPermission> perm ( do_QueryInterface(rowItem) );
+    if ( perm ) {
+      if ( [[aTableColumn identifier] isEqualToString:@"Website"] ) {
+        // website url column
+        nsXPIDLCString host;
+        perm->GetHost(getter_Copies(host));
+        retVal = [NSString stringWithCString:host];
+      }
+      else {
+        // allow/deny column
+        PRBool capability = PR_FALSE;			// false = deny, true = accept;
+        perm->GetCapability(&capability);
+        if ( capability )
+          retVal = [self getLocalizedString:@"Allow"];
+        else
+          retVal = [self getLocalizedString:@"Deny"];
+      }
+    }
+  }
+  
+  return retVal;
+}
+
 
 //
 // clickStorePasswords
--- mozilla/chimera/PreferencePanes/Privacy/PrivacyPane.h
+++ mozilla/chimera/PreferencePanes/Privacy/PrivacyPane.h
@@ -3,17 +3,33 @@
 #import "PreferencePaneBase.h"
 
 class nsIPref;
+class nsIPermissionManager;
+class nsISimpleEnumerator;
+class nsISupportsArray;
 
 @interface OrgMozillaChimeraPreferencePrivacy : PreferencePaneBase
 {
   IBOutlet id mCookies;
+  IBOutlet id mCookieSitePanel;
   IBOutlet NSButton* mPromptForCookie;
   
   IBOutlet NSButton* mStorePasswords;
   IBOutlet NSButton* mAutoFillPasswords;
+  
+  IBOutlet NSTableView* mSiteTable;
+  nsIPermissionManager* mManager;         // STRONG (should be nsCOMPtr)  
+  nsISupportsArray* mCachedPermissions;		// parallel list of permissions for speed, STRONG (should be nsCOMPtr)
 }
 
 -(IBAction) clearCookies:(id)aSender;
+-(IBAction) editCookieSites:(id)aSender;
+-(IBAction) editCookieSitesDone:(id)aSender;
+-(IBAction) removeCookieSite:(id)aSender;
+- (void) editCookieSitesSheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void  *)contextInfo;
+
+// data source informal protocol (NSTableDataSource)
+- (int)numberOfRowsInTableView:(NSTableView *)aTableView;
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(int)rowIndex;
 
 -(IBAction) clickPromptForCookie:(id)sender;
 -(IBAction) clickEnableCookies:(id)sender;
