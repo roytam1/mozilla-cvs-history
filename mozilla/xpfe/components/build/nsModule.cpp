3bc331e62bde72e3040484e685c4275b18ae1773
mscott%netscape.com <>
2002-10-09 21:26:25 +0000

    don't build various xpfe components like search, urlbar history, internet search, related search, session history and bookmarks if we are building stand alone mail

--- mozilla/xpfe/components/build/nsModule.cpp
+++ mozilla/xpfe/components/build/nsModule.cpp
@@ -37,17 +37,21 @@
 #include "nsIGenericFactory.h"
 #include "nsICategoryManager.h"
 #include "nsAutoComplete.h"
+
+#ifndef MOZ_MINOTAUR
 #include "nsBookmarksService.h"
 #include "nsDirectoryViewer.h"
-#include "nsDownloadManager.h"
-#include "nsDownloadProxy.h"
 #include "nsGlobalHistory.h"
-#include "rdf.h"
-#include "nsTimeBomb.h"
 #include "nsLocalSearchService.h"
 #include "nsInternetSearchService.h"
 #include "nsRelatedLinksHandlerImpl.h"
 #include "nsUrlbarHistory.h"
+#endif
+
+#include "nsDownloadManager.h"
+#include "nsDownloadProxy.h"
+#include "rdf.h"
+#include "nsTimeBomb.h"
 #include "nsXPIDLString.h"
 #include "nsCharsetMenu.h"
 #include "nsFontPackageHandler.h"
@@ -63,17 +67,21 @@
 // Factory constructors
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAutoCompleteItem)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAutoCompleteResults)
+
+#ifndef MOZ_MINOTAUR
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsBookmarksService, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsHTTPIndex, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsDirectoryViewerFactory)
-NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsDownloadManager, Init)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsDownloadProxy)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGlobalHistory, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(LocalSearchDataSource, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(InternetSearchDataSource, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(RelatedLinksHandlerImpl, Init)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsTimeBomb)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsUrlbarHistory)
+#endif
+
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsDownloadManager, Init)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsDownloadProxy)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsTimeBomb)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsFontPackageHandler)
 #if defined(XP_WIN)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAlertsService)
@@ -91,13 +99,14 @@ RegisterProc(nsIComponentManager *aCompMgr,
              const char *componentType,
              const nsModuleComponentInfo *info)
 {
-    nsresult rv;
+    nsresult rv = NS_OK;
     nsCOMPtr<nsICategoryManager> catman = do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv);
     if (NS_FAILED(rv)) return rv;
 
     // add the MIME types layotu can handle to the handlers category.
     // this allows users of layout's viewers (the docshell for example)
     // to query the types of viewers layout can create.
+#ifndef MOZ_MINOTAUR
     nsXPIDLCString previous;
     rv = catman->AddCategoryEntry("Gecko-Content-Viewers", "application/http-index-format",
                                    NS_DOCUMENT_LOADER_FACTORY_CONTRACTID_PREFIX "view;1?type=application/http-index-format",
@@ -111,6 +120,7 @@ RegisterProc(nsIComponentManager *aCompMgr,
                                   PR_TRUE,
                                   PR_TRUE,
                                   getter_Copies(previous));
+#endif
 
     return rv;
 }
@@ -139,6 +149,7 @@ static const nsModuleComponentInfo components[] = {
       nsAutoCompleteResultsConstructor},
     { "AutoComplete Search Item", NS_AUTOCOMPLETEITEM_CID, NS_AUTOCOMPLETEITEM_CONTRACTID,
       nsAutoCompleteItemConstructor},
+#ifndef MOZ_MINOTAUR
     { "Bookmarks", NS_BOOKMARKS_SERVICE_CID, NS_BOOKMARKS_SERVICE_CONTRACTID,
       nsBookmarksServiceConstructor },
     { "Bookmarks", NS_BOOKMARKS_SERVICE_CID, NS_BOOKMARKS_DATASOURCE_CONTRACTID,
@@ -153,10 +164,6 @@ static const nsModuleComponentInfo components[] = {
       nsHTTPIndexConstructor },
     { "Directory Viewer", NS_HTTPINDEX_SERVICE_CID, NS_HTTPINDEX_DATASOURCE_CONTRACTID,
       nsHTTPIndexConstructor },
-    { "Download Manager", NS_DOWNLOADMANAGER_CID, NS_DOWNLOADMANAGER_CONTRACTID,
-      nsDownloadManagerConstructor },
-    { "Download", NS_DOWNLOAD_CID, NS_DOWNLOAD_CONTRACTID,
-      nsDownloadProxyConstructor },
     { "Global History", NS_GLOBALHISTORY_CID, NS_GLOBALHISTORY_CONTRACTID,
       nsGlobalHistoryConstructor },
     { "Global History", NS_GLOBALHISTORY_CID, NS_GLOBALHISTORY_DATASOURCE_CONTRACTID,
@@ -173,11 +180,16 @@ static const nsModuleComponentInfo components[] = {
       NS_INTERNETSEARCH_DATASOURCE_CONTRACTID, InternetSearchDataSourceConstructor },
     { "Related Links Handler", NS_RELATEDLINKSHANDLER_CID, NS_RELATEDLINKSHANDLER_CONTRACTID,
 	  RelatedLinksHandlerImplConstructor},
-    { "Netscape TimeBomb", NS_TIMEBOMB_CID, NS_TIMEBOMB_CONTRACTID, nsTimeBombConstructor},
     { "nsUrlbarHistory", NS_URLBARHISTORY_CID,
       NS_URLBARHISTORY_CONTRACTID, nsUrlbarHistoryConstructor },
     { "nsUrlbarHistory", NS_URLBARHISTORY_CID,
       NS_URLBARAUTOCOMPLETE_CONTRACTID, nsUrlbarHistoryConstructor },
+#endif
+    { "Download Manager", NS_DOWNLOADMANAGER_CID, NS_DOWNLOADMANAGER_CONTRACTID,
+      nsDownloadManagerConstructor },
+    { "Download", NS_DOWNLOAD_CID, NS_DOWNLOAD_CONTRACTID,
+      nsDownloadProxyConstructor },
+    { "Netscape TimeBomb", NS_TIMEBOMB_CID, NS_TIMEBOMB_CONTRACTID, nsTimeBombConstructor},
     { "nsCharsetMenu", NS_CHARSETMENU_CID,
       NS_RDF_DATASOURCE_CONTRACTID_PREFIX NS_CHARSETMENU_PID,
       NS_NewCharsetMenu },
