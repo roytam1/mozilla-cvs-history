6646b847e78fdf6457e5582445ffbeaa763f392e
kestes%walrus.com <>
2002-12-10 19:24:37 +0000

    add referesh times to summary pages, this variable was empty before this fix.

--- mozilla/webtools/tinderbox2/src/lib/Summaries.pm
+++ mozilla/webtools/tinderbox2/src/lib/Summaries.pm
@@ -58,7 +58,7 @@ sub summary_pages {
 # Look! we use 'my' here but we wish to change $summary_ref and pass
 # it back to the caller, see the return statement.
 
-  my ($tree, $summary_ref) = @_;
+  my ($tree, $summary_ref, $refresh_time) = @_;
 
   # Build all the summary pages and save data into the $symmary_ref
   # also take the time to build an index page for this tree pointing
@@ -86,6 +86,8 @@ sub summary_pages {
     $TREE = $tree;
     $TREE_STATE = TinderHeader::gettree_header('TreeState', $tree);
     $HTML_TIME = HTMLPopUp::timeHTML($main::TIME);
+
+    $REFRESH_TIME = $refresh_time;
 
   };
 
