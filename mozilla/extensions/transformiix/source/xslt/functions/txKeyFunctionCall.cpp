cf2c207c6dd0d0ed1b80ea5dce7eb179afe9c19e
sicking%bigfoot.com <>
2003-03-04 14:20:22 +0000

    sync with tip

--- mozilla/extensions/transformiix/source/xslt/functions/txKeyFunctionCall.cpp
+++ mozilla/extensions/transformiix/source/xslt/functions/txKeyFunctionCall.cpp
@@ -262,10 +262,7 @@ txKeyHash::init()
     nsresult rv = mKeyValues.Init(8);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    rv = mIndexedKeys.Init(1);
-    NS_ENSURE_SUCCESS(rv, rv);
-
-    return NS_OK;
+    return mIndexedKeys.Init(1);
 }
 
 /**
