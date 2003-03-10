cf49ef05fd33881b6e5bd010b8cd0f36b3eba0c5
cvs2git <>
2003-03-10 15:26:42 +0000

    This commit was manufactured by cvs2svn to create tag 'BOOKMARKS_20030310_BASE'.

---
+++ mozilla/webtools/bugzilla/runtests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TEST_VERBOSE=0
+PART1='use Test::Harness qw(&runtests $verbose); $verbose='
+PART2='; runtests @ARGV;'
+for f in $*; do
+  if [ $f == "--verbose" ] ; then
+    TEST_VERBOSE=1
+  fi
+done
+
+/usr/bonsaitools/bin/perl -e "${PART1}${TEST_VERBOSE}${PART2}" t/*.t
