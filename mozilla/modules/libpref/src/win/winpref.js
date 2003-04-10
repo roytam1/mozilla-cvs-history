c5a9fe6d4f0a4ff314de274c3bfce7c8897ad698
taka%netscape.com <>
2003-04-10 20:41:50 +0000

    change japanese font assignment back to the original - fix for #619087

--- mozilla/modules/libpref/src/win/winpref.js
+++ mozilla/modules/libpref/src/win/winpref.js
@@ -52,8 +52,8 @@ pref("font.name.sans-serif.he", "Arial");
 pref("font.name.monospace.he", "Courier New");
 pref("font.name.cursive.he", "Comic Sans MS");
 
-pref("font.name.serif.ja", "MS UI Gothic");
-pref("font.name.sans-serif.ja", "ＭＳ Ｐ明朝"); // "MS PMincho"
+pref("font.name.serif.ja", "ＭＳ Ｐ明朝"); // "MS PMincho"
+pref("font.name.sans-serif.ja", "MS UI Gothic");
 pref("font.name.monospace.ja", "ＭＳ ゴシック"); // "MS Gothic"
 pref("font.name-list.serif.ja", "MS PMincho, MS PGothic, MS Mincho, MS Gothic");
 pref("font.name-list.sans-serif.ja", "MS PGothic, MS PMincho, MS Gothic, MS Mincho");
