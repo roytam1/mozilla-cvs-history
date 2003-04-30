864031f0b5d2960c82845a3f78e88ef909b6455e
alex.fritze%crocodile-clips.com <>
2003-04-30 16:02:06 +0000

    merge from trunk

--- mozilla/dom/src/base/nsDOMClassInfo.cpp
+++ mozilla/dom/src/base/nsDOMClassInfo.cpp
@@ -913,6 +913,7 @@ jsval nsDOMClassInfo::sDocument_id        = JSVAL_VOID;
 jsval nsDOMClassInfo::sWindow_id          = JSVAL_VOID;
 jsval nsDOMClassInfo::sFrames_id          = JSVAL_VOID;
 jsval nsDOMClassInfo::sSelf_id            = JSVAL_VOID;
+jsval nsDOMClassInfo::sSubmit_id          = JSVAL_VOID;
 
 const JSClass *nsDOMClassInfo::sObjectClass   = nsnull;
 
@@ -1015,6 +1016,7 @@ nsDOMClassInfo::DefineStaticJSVals(JSContext *cx)
   SET_JSVAL_TO_STRING(sWindow_id,          cx, "window");
   SET_JSVAL_TO_STRING(sFrames_id,          cx, "frames");
   SET_JSVAL_TO_STRING(sSelf_id,            cx, "self");
+  SET_JSVAL_TO_STRING(sSubmit_id,          cx, "submit");
 
   return NS_OK;
 }
@@ -2886,6 +2888,7 @@ nsDOMClassInfo::ShutDown()
   sWindow_id          = JSVAL_VOID;
   sFrames_id          = JSVAL_VOID;
   sSelf_id            = JSVAL_VOID;
+  sSubmit_id          = JSVAL_VOID;
 
   NS_IF_RELEASE(sXPConnect);
   NS_IF_RELEASE(sSecMan);
@@ -5336,7 +5339,11 @@ nsHTMLFormElementSH::NewResolve(nsIXPConnectWrappedNative *wrapper,
                                 PRUint32 flags, JSObject **objp,
                                 PRBool *_retval)
 {
-  if ((!(JSRESOLVE_ASSIGNING & flags)) && JSVAL_IS_STRING(id)) {
+  // If we're resolving a string other than "submit", and we're not
+  // resolving for assignment, check if the form knows about the
+  // resolved string.
+  if ((!(JSRESOLVE_ASSIGNING & flags)) && JSVAL_IS_STRING(id) &&
+      id != sSubmit_id) {
     nsCOMPtr<nsISupports> native;
 
     wrapper->GetNative(getter_AddRefs(native));
