01ce155c1afae2807d6562d727e6d4ae7d4d2e82
ccarlen%netscape.com <>
2003-06-02 15:40:39 +0000

    Bug 621837 - Need URILoader fix on 1.4 branch

--- mozilla/uriloader/base/nsURILoader.cpp
+++ mozilla/uriloader/base/nsURILoader.cpp
@@ -595,7 +595,12 @@ NS_IMETHODIMP nsURILoader::OpenURI(nsIChannel *channel,
     channel->GetURI(getter_AddRefs(uri));
     if (uri) {
       PRBool doAbort = PR_FALSE;
-      winContextListener->OnStartURIOpen(uri, &doAbort);
+      
+      if (aIsContentPreferred) {
+        // Only propagate the notification if the load was
+        // initiated from a link click.
+        winContextListener->OnStartURIOpen(uri, &doAbort);
+      }
          
       if (doAbort) {
         return NS_OK;
