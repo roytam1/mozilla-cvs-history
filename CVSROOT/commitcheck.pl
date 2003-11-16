3f5985dea212bc3a3cb56e3de3d73f7384b47878
despotdaemon%netscape.com <>
2003-11-16 23:20:16 +0000

    Pseudo-automatic update of changes made by silver@warwickcompsoc.co.uk.

--- CVSROOT/commitcheck.pl
+++ CVSROOT/commitcheck.pl
@@ -302,6 +302,18 @@ $fullname{'93'} = 'Zlib';
 sub GetT {
 ($b,$_) = (@_);
 if ($b eq 'HEAD') {
+if (m:^mozilla/java/webclient/.*$:) {return '127';}
+if (m:^mozilla/java/dom/.*$:) {return '130';}
+if (m:^mozilla/java/util/.*$:) {return '133';}
+if (m:^CVSROOT/commitcheck\.pl$:) {return '3';}
+if (m:^CVSROOT/passwd$:) {return '3';}
+if (m:^mozilla/extensions/xmlterm/.*$:) {return '141';}
+if (m:^mozilla/security/coreconf/.*$:) {return '145';}
+if (m:^mozilla/security/jss/.*$:) {return '145';}
+if (m:^mozilla/security/nss/.*$:) {return '145';}
+if (m:^xpcom/remote$:) {return '143';}
+if (m:^composer$:) {return '173';}
+if (m:^embedding/browser/gtk$:) {return '144';}
 if (m:^mozilla/[^/]*$:) {return '19';}
 if (m:^mozilla/mstone/.*$:) {return '147';}
 if (m:^mozilla/gfx/src/xlib/.*$:) {return '121';}
@@ -419,18 +431,6 @@ if (m:^mozilla/mailnews/[^/]*$:) {return '123';}
 if (m:^mozilla/mailnews/mime/[^/]*$:) {return '124';}
 if (m:^mozilla/java/xpcom/.*$:) {return '128';}
 if (m:^mozilla/java/plugins/.*$:) {return '129';}
-if (m:^mozilla/java/webclient/.*$:) {return '127';}
-if (m:^mozilla/java/dom/.*$:) {return '130';}
-if (m:^mozilla/java/util/.*$:) {return '133';}
-if (m:^CVSROOT/commitcheck\.pl$:) {return '3';}
-if (m:^CVSROOT/passwd$:) {return '3';}
-if (m:^mozilla/extensions/xmlterm/.*$:) {return '141';}
-if (m:^mozilla/security/coreconf/.*$:) {return '145';}
-if (m:^mozilla/security/jss/.*$:) {return '145';}
-if (m:^mozilla/security/nss/.*$:) {return '145';}
-if (m:^xpcom/remote$:) {return '143';}
-if (m:^composer$:) {return '173';}
-if (m:^embedding/browser/gtk$:) {return '144';}
 }
 if ($b eq 'NSPRPUB_PRE_4_2_CLIENT_BRANCH') {
 if (m:^mozilla/nsprpub/.*$:) {return '146';}
