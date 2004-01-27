312e0f36321aa3f564c2218c63e52301c788d35c
darin%meer.net <>
2004-01-27 00:27:41 +0000

    fixing licenses and removing dead header files

--- mozilla/xpcom/string/public/nsDependentConcatenation.h
+++ mozilla/xpcom/string/public/nsDependentConcatenation.h
@@ -1,28 +1,3 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/*
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- * 
- * The Original Code is Mozilla.
- * 
- * The Initial Developer of the Original Code is Netscape
- * Communications.  Portions created by Netscape Communications are
- * Copyright (C) 2001 by Netscape Communications.  All
- * Rights Reserved.
- * 
- * Contributor(s): 
- *   Scott Collins <scc@mozilla.org> (original author)
- */
-
-/* nsDependentConcatenation.h --- string concatenation machinery lives here, but don't include this file
-    directly, always get it by including either "nsAString.h" or one of the compatibility headers */
 
 #ifndef nsDependentConcatenation_h___
 #define nsDependentConcatenation_h___
