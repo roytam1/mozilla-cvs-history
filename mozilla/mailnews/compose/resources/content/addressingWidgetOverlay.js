af7ba318746352e9b5d184705965a14bd66899e1
scott%scott-macgregor.org <>
2004-02-03 22:10:50 +0000

    Bug #230577 --> mail address autocompletion chooses three addresses from address book entries of a certain form
    modify the header parser to not break on spaces anymore (hopefully there are not any MUAs out there that are still
    generating addresses that way).

--- mozilla/mailnews/compose/resources/content/addressingWidgetOverlay.js
+++ mozilla/mailnews/compose/resources/content/addressingWidgetOverlay.js
@@ -1028,19 +1028,13 @@ var gAutomatedAutoCompleteListener = null;
 
 function parseAndAddAddresses(addressText, recipientType)
 {
-  var fullNames = [];
+  var strippedAddresses = addressText.replace(/.* >> (.*)/, "$1"); // strip any leading >> characters inserted by the autocomplete widget
 
-  var unquotedCommaExpr = /("[^>]*>|[^",]+)(?:, *)?/g; 
-  while (m = unquotedCommaExpr(addressText)) 
-    fullNames.push(m[1]); 
-
-  numAddresses = fullNames.length;
-
-  for (index in fullNames)
-  {
-    // we want to eat leading and trailing white space... 
-    fullNames[index] = fullNames[index].replace(/^\s+|\s+$/g, "");
-  }
+  var hdrParser = Components.classes["@mozilla.org/messenger/headerparser;1"].getService(Components.interfaces.nsIMsgHeaderParser);
+  var addresses = {};
+  var names = {};
+  var fullNames = {};
+  var numAddresses = hdrParser.parseHeadersWithArray(strippedAddresses, addresses, names, fullNames);
 
   if (numAddresses > 0)
   {
@@ -1050,7 +1044,7 @@ function parseAndAddAddresses(addressText, recipientType)
     if (!gAutomatedAutoCompleteListener)
       gAutomatedAutoCompleteListener = new AutomatedAutoCompleteHandler();
 
-    gAutomatedAutoCompleteListener.init(fullNames, numAddresses, recipientType);
+    gAutomatedAutoCompleteListener.init(fullNames.value, numAddresses, recipientType);
   }
 }
 
