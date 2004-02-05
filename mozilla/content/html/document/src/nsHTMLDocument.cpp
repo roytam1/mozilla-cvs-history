ce18d66860312c86874b5681ff8b15339b6cbfce
darin%meer.net <>
2004-02-05 03:08:38 +0000

    using nsAutoString instead of nsString

--- mozilla/content/html/document/src/nsHTMLDocument.cpp
+++ mozilla/content/html/document/src/nsHTMLDocument.cpp
@@ -2517,7 +2517,7 @@ nsHTMLDocument::WriteCommon(const nsAString& aText,
   static NS_NAMED_LITERAL_STRING(empty, "");
   const nsAString  *term = aNewlineTerminate ? &new_line : &empty;
 
-  const nsString text = aText + *term;
+  const nsAutoString text = aText + *term;
 
   // Save the data in cache
   if (mWyciwygChannel) {
