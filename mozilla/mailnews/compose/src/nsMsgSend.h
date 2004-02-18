c78ebff3a300e01aef7d75dd2415f560c4afc0ee
bienvenu%nventure.com <>
2004-02-18 18:58:05 +0000

    fix 234546 attaching file that we don't have access to at msg send time spins forever, sr=mscott

--- mozilla/mailnews/compose/src/nsMsgSend.h
+++ mozilla/mailnews/compose/src/nsMsgSend.h
@@ -268,7 +268,7 @@ public:
   //
   // Attachment processing...
   //
-  int         HackAttachments(const struct nsMsgAttachmentData *attachments,
+  nsresult    HackAttachments(const struct nsMsgAttachmentData *attachments,
 					                    const struct nsMsgAttachedFile *preloaded_attachments);
   nsresult    CountCompFieldAttachments();
   nsresult    AddCompFieldLocalAttachments();
