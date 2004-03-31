12ef8d00b0fdbe86a4ec16435b70d6a65380267e
leaf%mozilla.org <>
2004-03-31 23:54:26 +0000

    enable profiling for release builds, to get debugging symbols for talkback. disable pdbfile generation for now, until talkback understands pdb files.

--- mozilla/tools/tinderbox/build-seamonkey-util.pl
+++ mozilla/tools/tinderbox/build-seamonkey-util.pl
@@ -360,6 +360,10 @@ sub SetupEnv {
     if ($Settings::ReleaseBuild) {
         $ENV{BUILD_OFFICIAL}   = 1;
         $ENV{MOZILLA_OFFICIAL} = 1;
+      if ($Settings::OS =~ /^WIN/) {
+          $ENV{MOZ_PROFILE}      = 1;
+          $ENV{PDBFILE}      = "NONE";
+      }
     }
 
     if ($Settings::ObjDir ne '') {
