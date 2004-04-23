100b341cfeb50a6fb05af63efeb3d9b444f783cb
cbiesinger%web.de <>
2004-04-23 20:14:47 +0000

    bug 240367, also filter out nsITransport::STATUS_READING and STATUS_WRITING messages r+sr=darin a=mkaply

--- mozilla/embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
+++ mozilla/embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
@@ -82,6 +82,7 @@
 #include "nsIDOMHTMLDocument.h"
 
 #include "ftpCore.h"
+#include "nsITransport.h"
 #include "nsISocketTransport.h"
 #include "nsIStringBundle.h"
 
@@ -928,6 +929,8 @@ NS_IMETHODIMP nsWebBrowserPersist::OnStatus(
         case NS_NET_STATUS_SENDING_TO:
         case NS_NET_STATUS_RECEIVING_FROM:
         case NS_NET_STATUS_WAITING_FOR:
+        case nsITransport::STATUS_READING:
+        case nsITransport::STATUS_WRITING:
             break;
 
         default:
