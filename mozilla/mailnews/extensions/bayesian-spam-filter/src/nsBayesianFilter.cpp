1837dbf9b12877e49df637b531ef06f746d31f2c
scott%scott-macgregor.org <>
2004-06-01 03:25:07 +0000

    Bug #245176 --> several unsigned integers in the core bayesian algorithm should be signed integers because they can be negative. Slightly improves our false negative rate on the benchmark.

--- mozilla/mailnews/extensions/bayesian-spam-filter/src/nsBayesianFilter.cpp
+++ mozilla/mailnews/extensions/bayesian-spam-filter/src/nsBayesianFilter.cpp
@@ -992,7 +992,7 @@ void nsBayesianFilter::classifyMessage(Tokenizer& tokenizer, const char* message
         first = 0;
 
     double H = 1.0, S = 1.0;
-    PRUint32 Hexp = 0, Sexp = 0;
+    PRInt32 Hexp = 0, Sexp = 0;
     goodclues=0;
     int e;
 
