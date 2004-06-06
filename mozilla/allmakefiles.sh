9b0852477a1a2a7ab7c7e25d0c13ea539e2f8979
mconnor%myrealbox.com <>
2004-06-06 14:40:52 +0000

    bustage fix from 245433

--- mozilla/allmakefiles.sh
+++ mozilla/allmakefiles.sh
@@ -884,6 +884,7 @@ browser/components/shell/public/Makefile
 browser/installer/Makefile
 browser/installer/unix/Makefile
 browser/installer/windows/Makefile
+browser/base/content/contents-region.rdf
 "
 
 MAKEFILES_xulapp="
