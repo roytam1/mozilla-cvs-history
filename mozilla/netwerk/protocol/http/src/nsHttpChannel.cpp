0642b124b3e315b80aaa64be036b2c98f8ec43a1
darin%meer.net <>
2004-06-09 22:24:46 +0000

    fixes bug 246106 "Defensive (anti-phishing) auth prompts are displayed when they should not be displayed" r+sr=dveditz a=asa

--- mozilla/netwerk/protocol/http/src/nsHttpChannel.cpp
+++ mozilla/netwerk/protocol/http/src/nsHttpChannel.cpp
@@ -2464,6 +2464,9 @@ nsHttpChannel::ConfirmAuth(const nsString &bundleKey, PRBool doYesNoPrompt)
     if (!prompt)
         return PR_TRUE;
 
+    // do not prompt again
+    mSuppressDefensiveAuth = PR_TRUE;
+
     PRBool confirmed;
     if (doYesNoPrompt) {
         PRInt32 choice;
