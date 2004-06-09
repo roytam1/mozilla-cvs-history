72d85c96589b698abb7e5758c558c201290b280c
cbiesinger%web.de <>
2004-06-09 22:02:35 +0000

    fixing DEBUG_CAPS_HACKER bustage, by porting caillon's patch from the 1.7 branch

--- mozilla/caps/src/nsScriptSecurityManager.cpp
+++ mozilla/caps/src/nsScriptSecurityManager.cpp
@@ -3233,13 +3233,13 @@ PrintPropertyPolicy(PLDHashTable *table, PLDHashEntryHdr *entry,
     prop.AppendWithConversion((PRUnichar*)JSValIDToString(cx, pp->key));
     prop += ": Get=";
     if (SECURITY_ACCESS_LEVEL_FLAG(pp->mGet))
-        prop.AppendInt(pp->mGet.level);
+        prop.AppendInt((PRInt32)pp->mGet.level);
     else
         prop += pp->mGet.capability;
 
     prop += " Set=";
     if (SECURITY_ACCESS_LEVEL_FLAG(pp->mSet))
-        prop.AppendInt(pp->mSet.level);
+        prop.AppendInt((PRInt32)pp->mSet.level);
     else
         prop += pp->mSet.capability;
         
