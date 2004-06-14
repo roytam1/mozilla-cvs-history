4ca487140d25426a893c67dbde8a08682a9e4483
scott%scott-macgregor.org <>
2004-06-14 08:32:26 +0000

    revert the biff idi to 101...changing it didn't help

--- mozilla/mailnews/base/src/nsMessengerWinIntegration.cpp
+++ mozilla/mailnews/base/src/nsMessengerWinIntegration.cpp
@@ -79,7 +79,7 @@
 #define MAIL_COMMANDLINE_ARG " -mail"
 #define TIMER_INTERVAL_PREF "mail.windows_xp_integration.unread_count_interval"
 
-#define IDI_MAILBIFF 115
+#define IDI_MAILBIFF 101
 
 #define NEW_MAIL_ALERT_ICON "chrome://messenger/skin/icons/new-mail-alert.png"
 #define SHOW_ALERT_PREF "mail.biff.show_alert"
