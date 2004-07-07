2f8be53f6312a3bf415f71f94d8858c40995c04d
bienvenu%nventure.com <>
2004-07-07 18:42:06 +0000

    port rss stuff to branch

--- mozilla/mailnews/local/src/nsNoIncomingServer.h
+++ mozilla/mailnews/local/src/nsNoIncomingServer.h
@@ -59,7 +59,6 @@ public:
     virtual ~nsNoIncomingServer();
     
     NS_IMETHOD GetLocalStoreType(char * *type);
-    NS_IMETHOD GetFilterList(nsIMsgWindow *aMsgWindow, nsIMsgFilterList **aResult);
     NS_IMETHOD GetCanSearchMessages(PRBool *canSearchMessages);
     NS_IMETHOD GetServerRequiresPasswordForBiff(PRBool *aServerRequiresPasswordForBiff);
 };
