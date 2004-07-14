7812d9ed85779db7128e181001d6ddd147e638d9
cvs2git <>
2004-07-14 13:39:40 +0000

    This commit was manufactured by cvs2svn to create tag 'MOZILLA_1_8a_RELEASE'.

--- mozilla/security/nss/lib/crmf/crmfdec.c
+++ mozilla/security/nss/lib/crmf/crmfdec.c
@@ -1,39 +1,36 @@
 /* -*- Mode: C; tab-width: 8 -*-*/
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
  * The Original Code is the Netscape security libraries.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1994-2000
- * the Initial Developer. All Rights Reserved.
- *
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are 
+ * Copyright (C) 1994-2000 Netscape Communications Corporation.  All
+ * Rights Reserved.
+ * 
  * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+ * 
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU General Public License Version 2 or later (the
+ * "GPL"), in which case the provisions of the GPL are applicable 
+ * instead of those above.  If you wish to allow use of your 
+ * version of this file only under the terms of the GPL and not to
+ * allow others to use your version of this file under the MPL,
+ * indicate your decision by deleting the provisions above and
+ * replace them with the notice and other provisions required by
+ * the GPL.  If you do not delete the provisions above, a recipient
+ * may use your version of this file under either the MPL or the
+ * GPL.
+ */
 
 
 #include "crmf.h"
@@ -81,11 +78,6 @@ crmf_decode_process_raverified(CRMFCertReqMsg *inCertReqMsg)
 static SECStatus
 crmf_decode_process_signature(CRMFCertReqMsg *inCertReqMsg)
 {
-    PORT_Assert(inCertReqMsg->poolp);
-    if (!inCertReqMsg->poolp) {
-    	PORT_SetError(SEC_ERROR_INVALID_ARGS);
-	return SECFailure;
-    }
     return SEC_ASN1Decode(inCertReqMsg->poolp,
 			  &inCertReqMsg->pop->popChoice.signature,
 			  CRMFPOPOSigningKeyTemplate, 
@@ -256,11 +248,6 @@ crmf_decode_process_single_control(PRArenaPool *poolp,
     asn1Template = crmf_get_pkiarchiveoptions_subtemplate(inControl);
 
     PORT_Assert (asn1Template != NULL);
-    PORT_Assert (poolp != NULL);
-    if (!asn1Template || !poolp) {
-    	PORT_SetError(SEC_ERROR_INVALID_ARGS);
-	return SECFailure;
-    }
     /* We've got a union, so passing a pointer to one element of the
      * union is the same as passing a pointer to any of the other
      * members of the union.
@@ -359,7 +346,7 @@ CRMF_CreateCertReqMessagesFromDER(const char *buf, long len)
     /* Wanna make sure the arena is big enough to store all of the requests
      * coming in.  We'll guestimate according to the length of the buffer.
      */
-    arenaSize = len + len/2;
+    arenaSize = len * 1.5;
     poolp = PORT_NewArena(arenaSize);
     if (poolp == NULL) {
         return NULL;
--- mozilla/security/nss/lib/util/secasn1t.h
+++ mozilla/security/nss/lib/util/secasn1t.h
@@ -1,38 +1,35 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
  * The Original Code is the Netscape security libraries.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1994-2000
- * the Initial Developer. All Rights Reserved.
- *
+ * 
+ * The Initial Developer of the Original Code is Netscape
+ * Communications Corporation.  Portions created by Netscape are 
+ * Copyright (C) 1994-2000 Netscape Communications Corporation.  All
+ * Rights Reserved.
+ * 
  * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+ * 
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU General Public License Version 2 or later (the
+ * "GPL"), in which case the provisions of the GPL are applicable 
+ * instead of those above.  If you wish to allow use of your 
+ * version of this file only under the terms of the GPL and not to
+ * allow others to use your version of this file under the MPL,
+ * indicate your decision by deleting the provisions above and
+ * replace them with the notice and other provisions required by
+ * the GPL.  If you do not delete the provisions above, a recipient
+ * may use your version of this file under either the MPL or the
+ * GPL.
+ */
 
 /*
  * Types for encoding/decoding of ASN.1 using BER/DER (Basic/Distinguished
@@ -119,14 +116,12 @@ typedef struct sec_ASN1Template_struct {
 #define SEC_ASN1_ENUMERATED		0x0a
 #define SEC_ASN1_EMBEDDED_PDV           0x0b
 #define SEC_ASN1_UTF8_STRING		0x0c
-/*                                      0x0d */
-/*                                      0x0e */
-/*                                      0x0f */
 #define SEC_ASN1_SEQUENCE		0x10
 #define SEC_ASN1_SET			0x11
 #define SEC_ASN1_NUMERIC_STRING         0x12
 #define SEC_ASN1_PRINTABLE_STRING	0x13
 #define SEC_ASN1_T61_STRING		0x14
+#define SEC_ASN1_TELETEX_STRING SEC_ASN1_T61_STRING
 #define SEC_ASN1_VIDEOTEX_STRING        0x15
 #define SEC_ASN1_IA5_STRING		0x16
 #define SEC_ASN1_UTC_TIME		0x17
@@ -138,7 +133,6 @@ typedef struct sec_ASN1Template_struct {
 /*                                      0x1d */
 #define SEC_ASN1_BMP_STRING		0x1e
 #define SEC_ASN1_HIGH_TAG_NUMBER	0x1f
-#define SEC_ASN1_TELETEX_STRING 	SEC_ASN1_T61_STRING
 
 /*
 ** Modifiers to type tags.  These are also specified by a/the
