b04f36b177db14a1276de47a77a769e17903e479
caillon%redhat.com <>
2004-07-30 05:44:05 +0000

    Fixing bustage.

--- mozilla/xpfe/components/history/src/nsGlobalHistory.cpp
+++ mozilla/xpfe/components/history/src/nsGlobalHistory.cpp
@@ -1031,7 +1031,7 @@ nsGlobalHistory::RemovePagesFromHost(const char *aHost, PRBool aEntireDomain)
   nsresult rv = RemoveMatchingRows(matchHostCallback, (void *)&hostInfo, PR_TRUE);
   if (NS_FAILED(rv)) return rv;
 
-  return Commit(kCompressCommit)
+  return Commit(kCompressCommit);
 }
 
 PRBool
