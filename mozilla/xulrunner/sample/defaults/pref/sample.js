e7b885efc292ab5bcfb9186350e64abc4c4899a6
darin%meer.net <>
2004-08-09 22:42:47 +0000

    adding xulrunner sample application

---
+++ mozilla/xulrunner/sample/defaults/pref/sample.js
@@ -0,0 +1,10 @@
+// This is this application's unique identifier used by the Extension System to
+// identify this application as an extension target, and by the SmartUpdate
+// system to identify this application to the Update server.
+pref("app.id", "{0ac57cfd-1996-489b-beb6-93a1f417daf1}");
+pref("app.version", "0.1");
+pref("app.build_id", "2004080914");
+pref("app.extensions.version", "0.1");
+
+// start with this chrome URL
+pref("browser.chromeURL", "chrome://sample/content/");
