3e7e8de8e020a205b3ac346ab78908511b3083ff
brendan%mozilla.org <>
2004-08-10 22:22:32 +0000

    Fix for bug 252892, a=mkaply.

--- mozilla/js/src/jsparse.c
+++ mozilla/js/src/jsparse.c
@@ -1506,9 +1506,13 @@ Statement(JSContext *cx, JSTokenStream *ts, JSTreeContext *tc)
             }
 
             if (pn1->pn_type == TOK_VAR) {
-                /* Tell js_EmitTree(TOK_VAR) to generate a final POP. */
-                pn1->pn_extra = JS_TRUE;
+                /* Tell js_EmitTree(TOK_VAR) that pn1 is part of a for/in. */
+                pn1->pn_extra |= PNX_FORINVAR;
+
+                /* Generate a final POP only if the var has an initializer. */
                 pn2 = pn1->pn_head;
+                if (pn2->pn_expr)
+                    pn1->pn_extra |= PNX_POPVAR;
             } else {
                 pn2 = pn1;
             }
@@ -1828,7 +1832,7 @@ Statement(JSContext *cx, JSTokenStream *ts, JSTreeContext *tc)
             return NULL;
 
         /* Tell js_EmitTree to generate a final POP. */
-        pn->pn_extra = JS_TRUE;
+        pn->pn_extra |= PNX_POPVAR;
         break;
 
       case TOK_RETURN:
@@ -1994,7 +1998,7 @@ Variables(JSContext *cx, JSTokenStream *ts, JSTreeContext *tc)
     if (!pn)
         return NULL;
     pn->pn_op = CURRENT_TOKEN(ts).t_op;
-    pn->pn_extra = JS_FALSE;            /* assume no JSOP_POP needed */
+    pn->pn_extra = 0;                   /* assume no JSOP_POP needed */
     PN_INIT_LIST(pn);
 
     /*
@@ -2834,7 +2838,7 @@ PrimaryExpr(JSContext *cx, JSTokenStream *ts, JSTreeContext *tc)
         if (!pn)
             return NULL;
         pn->pn_type = TOK_RB;
-        pn->pn_extra = JS_FALSE;
+        pn->pn_extra = 0;
 
 #if JS_HAS_SHARP_VARS
         if (defsharp) {
@@ -2853,7 +2857,7 @@ PrimaryExpr(JSContext *cx, JSTokenStream *ts, JSTreeContext *tc)
                 tt = js_PeekToken(cx, ts);
                 ts->flags &= ~TSF_REGEXP;
                 if (tt == TOK_RB) {
-                    pn->pn_extra = JS_TRUE;
+                    pn->pn_extra |= PNX_ENDCOMMA;
                     break;
                 }
 
