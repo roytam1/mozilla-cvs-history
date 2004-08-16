2b197ab5d8a7e84b7ae109b607c091851ef6fa31
scott%scott-macgregor.org <>
2004-08-16 17:15:07 +0000

    Bug #255733--> Fix the regression introduced by Bug #240723, unable to export address book data.

--- mozilla/mailnews/addrbook/src/nsAddressBook.cpp
+++ mozilla/mailnews/addrbook/src/nsAddressBook.cpp
@@ -168,7 +168,6 @@ const ExportAttributesTableStruct EXPORT_ATTRIBUTES_TABLE[EXPORT_ATTRIBUTES_TABL
 //
 nsAddressBook::nsAddressBook()
 {
-  mDocShell = nsnull;
 }
 
 nsAddressBook::~nsAddressBook()
@@ -260,25 +259,6 @@ nsresult nsAddressBook::DoCommand(nsIRDFDataSource* db,
   }
 
   return rv;
-}
-
-NS_IMETHODIMP nsAddressBook::SetDocShellWindow(nsIDOMWindowInternal *aWin)
-{
-  NS_PRECONDITION(aWin != nsnull, "null ptr");
-  if (!aWin)
-    return NS_ERROR_NULL_POINTER;
- 
-  nsCOMPtr<nsIScriptGlobalObject> globalObj( do_QueryInterface(aWin) );
-  if (!globalObj) {
-    return NS_ERROR_FAILURE;
-  }
-
-  // mDocShell is a weak reference
-  mDocShell = globalObj->GetDocShell();
-  if (!mDocShell)
-    return NS_ERROR_NOT_INITIALIZED;
-
-  return NS_OK;
 }
 
 NS_IMETHODIMP nsAddressBook::GetAbDatabaseFromURI(const char *aURI, nsIAddrDatabase **aDB)
@@ -1352,8 +1332,9 @@ enum ADDRESSBOOK_EXPORT_FILE_TYPE
  TAB_EXPORT_TYPE = 2
 };
 
-NS_IMETHODIMP nsAddressBook::ExportAddressBook(nsIAbDirectory *aDirectory)
+NS_IMETHODIMP nsAddressBook::ExportAddressBook(nsIDOMWindowInternal *aParentWin, nsIAbDirectory *aDirectory)
 {
+  NS_ENSURE_ARG_POINTER(aParentWin);
   nsresult rv;
 
   nsCOMPtr<nsIFilePicker> filePicker = do_CreateInstance("@mozilla.org/filepicker;1", &rv);
@@ -1365,7 +1346,16 @@ NS_IMETHODIMP nsAddressBook::ExportAddressBook(nsIAbDirectory *aDirectory)
   rv = bundleService->CreateBundle("chrome://messenger/locale/addressbook/addressBook.properties", getter_AddRefs(bundle));
   NS_ENSURE_SUCCESS(rv, rv);
 
-  nsCOMPtr<nsIDOMWindow> parentWindow = do_GetInterface(mDocShell);
+  nsCOMPtr<nsIScriptGlobalObject> globalObj = do_QueryInterface(aParentWin, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIDocShell> docShell = globalObj->GetDocShell();
+  if (!docShell)
+    return NS_ERROR_NULL_POINTER;
+
+  nsCOMPtr<nsIDOMWindow> parentWindow = do_GetInterface(docShell);
+  if (!parentWindow)
+    return NS_NOINTERFACE;
 
   nsXPIDLString title;
   rv = bundle->GetStringFromName(NS_LITERAL_STRING("ExportAddressBookTitle").get(), getter_Copies(title));
