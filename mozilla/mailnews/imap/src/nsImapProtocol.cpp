6ebebf74528cb2139c6528aabb1f01fa50b17b32
bienvenu%nventure.com <>
2004-08-20 20:35:24 +0000

    fix top crasher in imap hdr download, 255581

--- mozilla/mailnews/imap/src/nsImapProtocol.cpp
+++ mozilla/mailnews/imap/src/nsImapProtocol.cpp
@@ -269,6 +269,11 @@ NS_IMETHODIMP nsMsgImapLineDownloadCache::GetMsgUid(nsMsgKey *aMsgUid)
     *aMsgUid = fLineInfo->uidOfMessage;
     return NS_OK;
 }
+NS_IMETHODIMP nsMsgImapLineDownloadCache::SetMsgUid(nsMsgKey aMsgUid)
+{
+    fLineInfo->uidOfMessage = aMsgUid;
+    return NS_OK;
+}
 
 /* attribute long msgSize; */
 NS_IMETHODIMP nsMsgImapLineDownloadCache::GetMsgSize(PRInt32 *aMsgSize)
@@ -2492,6 +2497,15 @@ nsresult nsImapProtocol::BeginMessageDownLoad(
     m_fromHeaderSeen = PR_FALSE;
     if (GetServerStateParser().GetDownloadingHeaders())
     {
+      // if we get multiple calls to BeginMessageDownload w/o intervening
+      // calls to NormalEndMessageDownload or Abort, then we're just
+      // going to fake a NormalMessageEndDownload. This will most likely 
+      // cause an empty header to get written to the db, and the user
+      // will have to delete the empty header themselves, which
+      // should remove the message from the server as well.
+      if (m_curHdrInfo)
+        NormalMessageEndDownload();
+      if (!m_curHdrInfo)
       m_hdrDownloadCache.StartNewHdr(getter_AddRefs(m_curHdrInfo));
       if (m_curHdrInfo)
         m_curHdrInfo->SetMsgSize(total_message_size);
@@ -3318,6 +3332,7 @@ void nsImapProtocol::NormalMessageEndDownload()
   if (m_imapMailFolderSink && GetServerStateParser().GetDownloadingHeaders())
   {
     m_curHdrInfo->SetMsgSize(GetServerStateParser().SizeOfMostRecentMessage());
+    m_curHdrInfo->SetMsgUid(GetServerStateParser().CurrentResponseUID());
     m_hdrDownloadCache.FinishCurrentHdr();
     PRInt32 numHdrsCached;
     m_hdrDownloadCache.GetNumHeaders(&numHdrsCached);
@@ -3363,6 +3378,7 @@ void nsImapProtocol::NormalMessageEndDownload()
       }
     }
   }
+  m_curHdrInfo = nsnull;
 }
 
 void nsImapProtocol::AbortMessageDownLoad()
@@ -3386,6 +3402,7 @@ void nsImapProtocol::AbortMessageDownLoad()
   else if (m_imapMessageSink)
         m_imapMessageSink->AbortMsgWriteStream();
 
+  m_curHdrInfo = nsnull;
 }
 
 
