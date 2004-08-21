fe481c58bb3a4f5cb386f8fc66e81b35a503e788
neil%parkwaycc.co.uk <>
2004-08-21 17:17:14 +0000

    Bug 242046 Make the JS status handler a property of the XUL window, should speed up calls to set window.status r=jag sr=jst moa=mscott a=asa

--- mozilla/browser/base/content/browser.js
+++ mozilla/browser/base/content/browser.js
@@ -353,6 +353,12 @@ function prepareForStartup()
 
   // initialize observers and listeners
   window.XULBrowserWindow = new nsBrowserStatusHandler();
+  window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+        .getInterface(Components.interfaces.nsIWebNavigation)
+        .QueryInterface(Components.interfaces.nsIDocShellTreeItem).treeOwner
+        .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+        .getInterface(Components.interfaces.nsIXULWindow)
+        .XULBrowserWindow = window.XULBrowserWindow;
   window.browserContentListener =
     new nsBrowserContentListener(window, gBrowser);
 
@@ -606,6 +612,12 @@ function Shutdown()
 
   window.XULBrowserWindow.destroy();
   window.XULBrowserWindow = null;
+  window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+        .getInterface(Components.interfaces.nsIWebNavigation)
+        .QueryInterface(Components.interfaces.nsIDocShellTreeItem).treeOwner
+        .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+        .getInterface(Components.interfaces.nsIXULWindow)
+        .XULBrowserWindow = null;
 
   window.browserContentListener.close();
   // Close the app core.
