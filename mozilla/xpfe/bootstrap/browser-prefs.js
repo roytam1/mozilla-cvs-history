46b09f3a219b1689ac08f6658a36edf019019818
ben%bengoodger.com <>
2004-08-29 07:30:01 +0000

    255394 - non-default search query parameter generation. r+sr=brendan@mozilla.org, a=me

--- mozilla/xpfe/bootstrap/browser-prefs.js
+++ mozilla/xpfe/bootstrap/browser-prefs.js
@@ -98,6 +98,11 @@ pref("browser.search.opensidebarsearchpanel", true);
 pref("browser.search.last_search_category", "NC:SearchCategory?category=urn:search:category:1");
 pref("browser.search.mode", 0);
 pref("browser.search.powermode", 0);
+
+pref("browser.search.param.Google.1.name", "chrome://navigator/content/searchconfig.properties");
+pref("browser.search.param.Google.1.custom", "chrome://navigator/content/searchconfig.properties");
+pref("browser.search.param.Google.1.default", "chrome://navigator/content/searchconfig.properties");
+
 // basic search popup constraint: minimum sherlock plugin version displayed
 // (note: must be a string representation of a float or it'll default to 0.0)
 pref("browser.search.basic.min_ver", "0.0");
