18a35fb3f092c04f46013966e401868eeda806ca
mostafah%oeone.com <>
2004-09-07 16:12:29 +0000

    Fixed bug 254356: incorrect line endings Undefined ICAL_UNIX_NEWLINE which doesn't comply with rfc2445 Imported fix from libical.cvs to icalpropery.c

--- mozilla/other-licenses/libical/src/libical/icalproperty.c
+++ mozilla/other-licenses/libical/src/libical/icalproperty.c
@@ -187,13 +187,19 @@ icalproperty* icalproperty_new_from_string(const char* str)
     icalcomponent *comp;
     int errors  = 0;
 
+#ifdef ICAL_UNIX_NEWLINE
+    char newline[] = "\n";
+#else
+    char newline[] = "\r\n";
+#endif
+
     icalerror_check_arg_rz( (str!=0),"str");
 
     /* Is this a HACK or a crafty reuse of code? */
 
     icalmemory_append_string(&buf, &buf_ptr, &buf_size, "BEGIN:VCALENDAR\n");
     icalmemory_append_string(&buf, &buf_ptr, &buf_size, str);
-    icalmemory_append_string(&buf, &buf_ptr, &buf_size, "\n");    
+    icalmemory_append_string(&buf, &buf_ptr, &buf_size, newline);    
     icalmemory_append_string(&buf, &buf_ptr, &buf_size, "END:VCALENDAR\n");
 
     comp = icalparser_parse_string(buf);
@@ -286,7 +292,11 @@ icalproperty_as_ical_string (icalproperty* prop)
     icalvalue* value;
     char *out_buf;
 
+#ifdef ICAL_UNIX_NEWLINE
     char newline[] = "\n";
+#else
+    char newline[] = "\r\n";
+#endif
 
     struct icalproperty_impl *impl = (struct icalproperty_impl*)prop;
     
