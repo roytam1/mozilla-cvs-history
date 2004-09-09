24a4ad9215d6ae9a79a934a2698dd3b30a7824ee
despotdaemon <>
2004-09-09 20:05:48 +0000

    Pseudo-automatic update of changes made by steffen.wilberg@web.de.

--- CVSROOT/commitcheck.pl
+++ CVSROOT/commitcheck.pl
@@ -310,6 +310,14 @@ $fullname{'93'} = 'Zlib';
 sub GetT {
 ($b,$_) = (@_);
 if ($b eq 'HEAD') {
+if (m:^js/src/xpconnect$:) {return '118';}
+if (m:^xpcom/reflect/xptcall$:) {return '118';}
+if (m:^xpcom/reflect/xptinfo$:) {return '118';}
+if (m:^mozilla/mailnews/news/.*$:) {return '122';}
+if (m:^mozilla/mailnews/[^/]*$:) {return '123';}
+if (m:^mozilla/mailnews/mime/[^/]*$:) {return '124';}
+if (m:^mozilla/java/xpcom/.*$:) {return '128';}
+if (m:^mozilla/java/plugins/.*$:) {return '129';}
 if (m:^mozilla/java/webclient/.*$:) {return '127';}
 if (m:^mozilla/java/dom/.*$:) {return '130';}
 if (m:^CVSROOT/commitcheck\.pl$:) {return '3';}
@@ -394,12 +402,12 @@ if (m:^mozilla/widget/src/xpwidgets/.*$:) {return '177';}
 if (m:^mozilla/modules/libpref/.*$:) {return '82';}
 if (m:^mozilla/gfx/src/mac/.*$:) {return '178';}
 if (m:^mozilla/widget/src/mac/.*$:) {return '178';}
-if (m:^mozilla/modules/progress/.*$:) {return '83';}
 if (m:^mozilla/accessibility/.*$:) {return '180';}
-if (m:^mozilla/modules/libreg/.*$:) {return '84';}
+if (m:^mozilla/modules/progress/.*$:) {return '83';}
 if (m:^mozilla/extensions/typeaheadfind/.*$:) {return '181';}
-if (m:^mozilla/modules/security/.*$:) {return '86';}
+if (m:^mozilla/modules/libreg/.*$:) {return '84';}
 if (m:^mozilla/content/xbl/builtin/.*$:) {return '182';}
+if (m:^mozilla/modules/security/.*$:) {return '86';}
 if (m:^mozilla/modules/softupdt/.*$:) {return '87';}
 if (m:^mozilla/content/xml/.*$:) {return '88';}
 if (m:^mozilla/expat/.*$:) {return '88';}
@@ -443,14 +451,6 @@ if (m:^mozilla/layout/xul/.*$:) {return '106';}
 if (m:^mozilla/profile$:) {return '120';}
 if (m:^mozilla/tools/.*$:) {return '112';}
 if (m:^xpcom/typelib$:) {return '117';}
-if (m:^js/src/xpconnect$:) {return '118';}
-if (m:^xpcom/reflect/xptcall$:) {return '118';}
-if (m:^xpcom/reflect/xptinfo$:) {return '118';}
-if (m:^mozilla/mailnews/news/.*$:) {return '122';}
-if (m:^mozilla/mailnews/[^/]*$:) {return '123';}
-if (m:^mozilla/mailnews/mime/[^/]*$:) {return '124';}
-if (m:^mozilla/java/xpcom/.*$:) {return '128';}
-if (m:^mozilla/java/plugins/.*$:) {return '129';}
 }
 if ($b eq 'NSPRPUB_PRE_4_2_CLIENT_BRANCH') {
 if (m:^mozilla/nsprpub/.*$:) {return '146';}
