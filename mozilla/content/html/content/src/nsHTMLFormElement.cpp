b1ed3f7904fc0f5412ab9b8ff0dd641932ecf56c
jst%mozilla.jstenback.com <>
2004-09-10 17:25:18 +0000

    Fix crash reported in bug 235923. In odd cases when we get into situations where we're submitting a form while we're in the middle of submitting the same form (waiting for a modal dialog), the pending submission from the first call may get deleted. r+sr=shaver@mozilla.org, a=mkaply@us.ibm.com

--- mozilla/content/html/content/src/nsHTMLFormElement.cpp
+++ mozilla/content/html/content/src/nsHTMLFormElement.cpp
@@ -1211,6 +1211,8 @@ nsHTMLFormElement::OnSubmitClickEnd()
 NS_IMETHODIMP
 nsHTMLFormElement::FlushPendingSubmission()
 {
+  nsCOMPtr<nsIFormSubmission> kunkFuDeathGrip(mPendingSubmission);
+
   if (!mPendingSubmission) {
     return NS_OK;
   }
