f31afbc80e23a1fd698a718914a4aaafb76b12f4
bryner%brianryner.com <>
2004-09-10 08:15:12 +0000

    Hook up gtk2 native keybindings for editor. This gets rid of htmlBindings.xml and instead makes the platform-neutral base bindings be included via the preprocessor, allowing the editorBase bindings to not be applied on gtk2. Also, removed obsolete mac CFM package files. Bug 257405, r=bzbarsky, sr=ben, a=chofmann.

--- mozilla/layout/build/nsLayoutModule.cpp
+++ mozilla/layout/build/nsLayoutModule.cpp
@@ -132,6 +132,7 @@
 #include "nsStyleSet.h"
 #include "nsImageFrame.h"
 #include "nsTextControlFrame.h"
+#include "nsXBLWindowKeyHandler.h"
 
 // view stuff
 #include "nsViewsCID.h"
@@ -416,6 +417,7 @@ Shutdown(nsIModule* aSelf)
   GlobalWindowImpl::ShutDown();
   nsDOMClassInfo::ShutDown();
   nsTextControlFrame::ShutDown();
+  nsXBLWindowKeyHandler::ShutDown();
 }
 
 #ifdef NS_DEBUG
