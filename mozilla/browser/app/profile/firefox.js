00a23d33af7787ffd05d6d43baf642048db0fb74
ben%bengoodger.com <>
2004-09-14 01:07:24 +0000

    259192 - don't force location bar, force status instead to help webapp ue... r=dveditz a=chofmann

--- mozilla/browser/app/profile/firefox.js
+++ mozilla/browser/app/profile/firefox.js
@@ -201,8 +201,13 @@ pref("browser.bookmarks.sort.resource", "rdf:http://home.netscape.com/NC-rdf#Nam
 // Scripts & Windows prefs
 pref("dom.disable_open_during_load",              true);
 pref("javascript.options.showInConsole",          false);
-// Make the location bar reliably present and unaffected by pages
-pref("dom.disable_window_open_feature.location",  true);
+// Make the status bar reliably present and unaffected by pages
+pref("dom.disable_window_open_feature.status",    true);
+// This is the pref to control the location bar, change this to true to 
+// force this instead of or in addition to the status bar - this makes 
+// the origin of popup windows more obvious to avoid spoofing but we 
+// cannot do it by default because it affects UE for web applications.
+pref("dom.disable_window_open_feature.location",  false);
 pref("dom.disable_window_status_change",          true);
 
 pref("browser.trim_user_and_password",            true);
