cd518ddaebc75cfdadf66f1c4e4ca0a00b0f031e
bzbarsky%mit.edu <>
2004-09-25 14:27:51 +0000

    Fix problem with the external protocol channel that makes it return a null originalURI and cause crashes. Bug 241300, r=biesi, sr=darin, a=asa

--- mozilla/uriloader/exthandler/nsExternalProtocolHandler.cpp
+++ mozilla/uriloader/exthandler/nsExternalProtocolHandler.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * The contents of this file are subject to the Netscape Public License
  * Version 1.0 (the "NPL"); you may not use this file except in
@@ -68,6 +68,7 @@ public:
 
 private:
     nsCOMPtr<nsIURI> mUrl;
+    nsCOMPtr<nsIURI> mOriginalURI;
     nsresult mStatus;
 
     nsCOMPtr<nsIInterfaceRequestor> mCallbacks;
@@ -124,12 +125,14 @@ nsExtProtocolChannel::GetSecurityInfo(nsISupports * *aSecurityInfo)
 
 NS_IMETHODIMP nsExtProtocolChannel::GetOriginalURI(nsIURI* *aURI)
 {
-  *aURI = nsnull;
+  NS_IF_ADDREF(*aURI = mOriginalURI);
   return NS_OK; 
 }
  
 NS_IMETHODIMP nsExtProtocolChannel::SetOriginalURI(nsIURI* aURI)
 {
+  NS_ENSURE_ARG_POINTER(aURI);
+  mOriginalURI = aURI;
   return NS_OK;
 }
  
@@ -526,6 +529,7 @@ NS_IMETHODIMP nsExternalProtocolHandler::NewChannel(nsIURI *aURI, nsIChannel **_
     if (!channel) return NS_ERROR_OUT_OF_MEMORY;
 
     ((nsExtProtocolChannel*) channel.get())->SetURI(aURI);
+    channel->SetOriginalURI(aURI);
 
     if (_retval)
     {
