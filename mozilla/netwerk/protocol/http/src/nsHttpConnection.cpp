2ddf9d8bf30d3792af01c663fe02b5953009d350
cbiesinger%web.de <>
2004-10-04 14:18:07 +0000

    242393 r=darin sr=bz only write content to the pipe if the content started; this ensures that OnDataAvailable is only called if we have a response head. also, add some assertions.
    a=asa

--- mozilla/netwerk/protocol/http/src/nsHttpConnection.cpp
+++ mozilla/netwerk/protocol/http/src/nsHttpConnection.cpp
@@ -266,14 +266,10 @@ nsHttpConnection::OnHeadersAvailable(nsAHttpTransaction *trans,
 
     NS_ASSERTION(PR_GetCurrentThread() == gSocketThread, "wrong thread");
     NS_ENSURE_ARG_POINTER(trans);
+    NS_ASSERTION(responseHead, "No response head?");
 
     // we won't change our keep-alive policy unless the server has explicitly
     // told us to do so.
-
-    if (!trans || !responseHead) {
-        LOG(("nothing to do\n"));
-        return NS_OK;
-    }
 
     // inspect the connection headers for keep-alive info provided the
     // transaction completed successfully.
@@ -319,7 +315,7 @@ nsHttpConnection::OnHeadersAvailable(nsAHttpTransaction *trans,
         else
             mIdleTimeout = gHttpHandler->IdleTimeout();
         
-        LOG(("Connection can be reused [this=%x idle-timeout=%u\n", this, mIdleTimeout));
+        LOG(("Connection can be reused [this=%x idle-timeout=%u]\n", this, mIdleTimeout));
     }
 
     // if we're doing an SSL proxy connect, then we need to check whether or not
@@ -331,9 +327,11 @@ nsHttpConnection::OnHeadersAvailable(nsAHttpTransaction *trans,
         if (responseHead->Status() == 200) {
             LOG(("SSL proxy CONNECT succeeded!\n"));
             *reset = PR_TRUE;
-            ProxyStartSSL();
+            nsresult rv = ProxyStartSSL();
+            if (NS_FAILED(rv)) // XXX need to handle this for real
+                LOG(("ProxyStartSSL failed [rv=%x]\n", rv));
             mCompletedSSLConnect = PR_TRUE;
-            nsresult rv = mSocketOut->AsyncWait(this, 0, 0, nsnull);
+            rv = mSocketOut->AsyncWait(this, 0, 0, nsnull);
             // XXX what if this fails -- need to handle this error
             NS_ASSERTION(NS_SUCCEEDED(rv), "mSocketOut->AsyncWait failed");
         }
@@ -679,7 +677,7 @@ nsHttpConnection::SetupSSLProxyConnect()
         request.SetHeader(nsHttp::Proxy_Authorization, nsDependentCString(val));
     }
 
-    buf.Truncate(0);
+    buf.Truncate();
     request.Flatten(buf, PR_FALSE);
     buf.Append("\r\n");
 
