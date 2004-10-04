f51e8ae565356b5dba77d5cf4be25adb22020974
cbiesinger%web.de <>
2004-10-04 14:17:10 +0000

    242393 r=darin sr=bz only write content to the pipe if the content started; this ensures that OnDataAvailable is only called if we have a response head. also, add some assertions.
    a=asa

--- mozilla/netwerk/protocol/http/src/nsHttpTransaction.cpp
+++ mozilla/netwerk/protocol/http/src/nsHttpTransaction.cpp
@@ -240,8 +240,10 @@ nsHttpTransaction::Init(PRUint8 caps,
 nsHttpResponseHead *
 nsHttpTransaction::TakeResponseHead()
 {
-    if (!mHaveAllHeaders)
+    if (!mHaveAllHeaders) {
+        NS_WARNING("response headers not available or incomplete");
         return nsnull;
+    }
 
     nsHttpResponseHead *head = mResponseHead;
     mResponseHead = nsnull;
@@ -774,6 +776,9 @@ nsHttpTransaction::HandleContent(char *buf,
     if (!mDidContentStart) {
         rv = HandleContentStart();
         if (NS_FAILED(rv)) return rv;
+        // Do not write content to the pipe if we don't have any content yet
+        if (!mDidContentStart)
+          return NS_OK;
     }
 
     if (mChunkedDecoder) {
@@ -803,6 +808,7 @@ nsHttpTransaction::HandleContent(char *buf,
     }
     else {
         // when we are just waiting for the server to close the connection...
+        // (no explicit content-length given)
         *contentRead = count;
     }
 
@@ -815,7 +821,7 @@ nsHttpTransaction::HandleContent(char *buf,
         */
     }
 
-    LOG(("nsHttpTransaction [this=%x count=%u read=%u mContentRead=%u mContentLength=%d]\n",
+    LOG(("nsHttpTransaction::HandleContent [this=%x count=%u read=%u mContentRead=%u mContentLength=%d]\n",
         this, count, *contentRead, mContentRead, mContentLength));
 
     // check for end-of-file
