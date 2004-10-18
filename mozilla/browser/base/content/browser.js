f230b540c7d82a5dd8b5a44d134c092df2b69cae
jst%mozilla.jstenback.com <>
2004-10-18 18:08:18 +0000

    Fxing bug 249332. Don't use getters/setters for link.href in browser code. r=caillon@aillon.org, sr=dveditz@cruzio.com, a=asa@mozilla.org

--- mozilla/browser/base/content/browser.js
+++ mozilla/browser/base/content/browser.js
@@ -3183,12 +3183,14 @@ nsContextMenu.prototype = {
             if ( !link.protocol ) {
                 // We must resort to testing the URL string :-(.
                 var protocol;
-                if ( link.href ) {
-                    protocol = link.href.substr( 0, linktype.length );
+                var wrapper = new XPCNativeWrapper(link, "href",
+                                                   "getAttributeNS()");
+                if (wrapper.href) {
+                    protocol = wrapper.href.substr(0, linktype.length);
                 } else {
-                    protocol = link.getAttributeNS("http://www.w3.org/1999/xlink","href");
-                    if ( protocol ) {
-                        protocol = protocol.substr( 0, linktype.length );
+                    protocol = wrapper.getAttributeNS("http://www.w3.org/1999/xlink","href");
+                    if (protocol) {
+                        protocol = protocol.substr(0, linktype.length);
                     }
                 }
                 return protocol.toLowerCase() === linktype;        
@@ -3420,14 +3422,17 @@ nsContextMenu.prototype = {
     },
     // Generate fully-qualified URL for clicked-on link.
     linkURL : function () {
-        if (this.link.href) {
-          return this.link.href;
+        var wrapper = new XPCNativeWrapper(this.link, "href", "baseURI",
+                                           "getAttributeNS()");
+        if (wrapper.href) {
+          return wrapper.href;
         }
-        var href = this.link.getAttributeNS("http://www.w3.org/1999/xlink","href");
+        var href = wrapper.getAttributeNS("http://www.w3.org/1999/xlink",
+                                          "href");
         if (!href || !href.match(/\S/)) {
           throw "Empty href"; // Without this we try to save as the current doc, for example, HTML case also throws if empty
         }
-        href = makeURLAbsolute(this.link.baseURI,href);
+        href = makeURLAbsolute(wrapper.baseURI, href);
         return href;
     },
     // Get text of link.
@@ -3438,12 +3443,16 @@ nsContextMenu.prototype = {
           if (!text || !text.match(/\S/)) {
             text = this.link.getAttribute("alt");
             if (!text || !text.match(/\S/)) {
-              if (this.link.href) {                
-                text = this.link.href;
+              var wrapper = new XPCNativeWrapper(this.link, "href", "baseURI",
+                                                 "getAttributeNS()");
+
+              if (wrapper.href) {
+                text = wrapper.href;
               } else {
-                text = getAttributeNS("http://www.w3.org/1999/xlink", "href");
+                text = wrapper.getAttributeNS("http://www.w3.org/1999/xlink",
+                                              "href");
                 if (text && text.match(/\S/)) {
-                  text = makeURLAbsolute(this.link.baseURI, text);
+                  text = makeURLAbsolute(wrapper.baseURI, text);
                 }
               }
             }
@@ -3646,6 +3655,7 @@ function asyncOpenWebPanel(event)
        break;
    }
    if (linkNode) {
+     var wrapper = new XPCNativeWrapper(linkNode, "href", "getAttribute()");
      if (event.button == 0 && !event.ctrlKey && !event.shiftKey &&
          !event.altKey && !event.metaKey) {
        // A Web panel's links should target the main content area.  Do this
@@ -3658,12 +3668,12 @@ function asyncOpenWebPanel(event)
            (!target || target == "_content" || target  == "_main")) 
          // IE uses _main, SeaMonkey uses _content, we support both
        {
-         if (!linkNode.href) return true;
+         if (!wrapper.href) return true;
          if (linkNode.getAttribute("onclick")) return true;
-         var url = getShortcutOrURI(linkNode.href);
+         var url = getShortcutOrURI(wrapper.href);
          if (!url)
            return true;
-         markLinkVisited(linkNode.href, linkNode);
+         markLinkVisited(wrapper.href, linkNode);
          loadURI(url);
          event.preventDefault();
          return false;
@@ -3674,21 +3684,23 @@ function asyncOpenWebPanel(event)
          // title attribute contains the title that should be used for the sidebar panel.
          openDialog("chrome://browser/content/bookmarks/addBookmark2.xul", "",
                     "centerscreen,chrome,dialog,resizable,dependent",
-                    linkNode.getAttribute("title"), 
-                    linkNode.href, null, null, null, null, true);
+                    wrapper.getAttribute("title"), 
+                    wrapper.href, null, null, null, null, true);
          event.preventDefault();
          return false;
        }
        else if (target == "_search") {
          // Used in WinIE as a way of transiently loading pages in a sidebar.  We
          // mimic that WinIE functionality here and also load the page transiently.
-         openWebPanel(gNavigatorBundle.getString("webPanels"), linkNode.href);
+         openWebPanel(gNavigatorBundle.getString("webPanels"), wrapper.href);
          event.preventDefault();
          return false;
        }
      }
-     else
-       handleLinkClick(event, linkNode.href, linkNode);
+     else {
+       handleLinkClick(event, wrapper.href, linkNode);
+     }
+
      return true;
    } else {
      // Try simple XLink
@@ -3696,7 +3708,8 @@ function asyncOpenWebPanel(event)
      linkNode = target;
      while (linkNode) {
        if (linkNode.nodeType == Node.ELEMENT_NODE) {
-         href = linkNode.getAttributeNS("http://www.w3.org/1999/xlink", "href");
+         var wrapper = new XPCNativeWrapper(linkNode, "getAttributeNS()");
+         href = wrapper.getAttributeNS("http://www.w3.org/1999/xlink", "href");
          break;
        }
        linkNode = linkNode.parentNode;
