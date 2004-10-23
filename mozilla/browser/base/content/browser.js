dbff85b6f6695ccf07b15d2fa2dc1e69e1f18164
dveditz%cruzio.com <>
2004-10-23 00:32:59 +0000

    bug 249322: strip any newlines from urlbar on unix. r=jsr,sr=bryner,a=asa

--- mozilla/browser/base/content/browser.js
+++ mozilla/browser/base/content/browser.js
@@ -2405,9 +2405,16 @@ nsBrowserStatusHandler.prototype =
         // pages. 
         if (/^\s*wyciwyg:\/\/\d+\//.test(location))
           location = RegExp.rightContext;
-        
-        setTimeout(function(loc, aloc) { gURLBar.value = loc; SetPageProxyState("valid", aloc);}, 0, location, aLocation);
-        
+
+        setTimeout(function(loc, aloc) { 
+                     gURLBar.value = loc; 
+                     if (gURLBar.value != loc) { 
+                       gURLBar.value = ""; // hack for bug 249322
+                       gURLBar.value = loc;
+                     }
+                     SetPageProxyState("valid", aloc);
+                   }, 0, location, aLocation);
+
         // Setting the urlBar value in some cases causes userTypedValue to
         // become set because of oninput, so reset it to its old value.
         browser.userTypedValue = userTypedValue;
