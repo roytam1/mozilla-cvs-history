025e449384fbf9f0f7f61b82b737e95f0cb37914
scott%scott-macgregor.org <>
2004-11-30 01:27:02 +0000

    Bug #268503 --> space bar focused on blog message does next-unread, not page-down
    sr=bienvenu

--- mozilla/mail/base/content/mailWindowOverlay.js
+++ mozilla/mail/base/content/mailWindowOverlay.js
@@ -1777,6 +1777,11 @@ function MsgAddAllToAddressBook() {}
 function SpaceHit(event)
 {
   var contentWindow = window.top._content;
+  var rssiframe = contentWindow.document.getElementById('_mailrssiframe');
+
+  // if we are displaying an RSS article, we really want to scroll the nested iframe
+  if (rssiframe)
+    contentWindow = rssiframe.contentWindow;
 
   if (event && event.shiftKey) {
     // if at the start of the message, go to the previous one
