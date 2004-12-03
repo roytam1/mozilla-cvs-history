ad0ca15cd443245ec80ce4828326d3c3f83f8aa7
scott%scott-macgregor.org <>
2004-12-03 07:14:07 +0000

    Bug #269573 --> TB09 IMAP crash [@ nsImapServerResponseParser::msg_fetch_literal]
    patch by bienvenu
    
    sr=mscott

--- mozilla/mailnews/imap/src/nsImapServerResponseParser.cpp
+++ mozilla/mailnews/imap/src/nsImapServerResponseParser.cpp
@@ -2687,7 +2687,7 @@ PRBool nsImapServerResponseParser::msg_fetch_literal(PRBool chunk, PRInt32 origi
   // If we opened a tunnel, finish everything off here.  Otherwise, get everything here.
   // (just like before)
   
-  while (ContinueParse() && (charsReadSoFar < numberOfCharsInThisChunk))
+  while (ContinueParse() && !fServerConnection.DeathSignalReceived() && (charsReadSoFar < numberOfCharsInThisChunk))
   {
     AdvanceToNextLine();
     if (ContinueParse())
