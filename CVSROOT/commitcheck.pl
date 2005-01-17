113c0c359407cb65fcf4d7cfc7a49a30add1f751
despotdaemon <>
2005-01-17 19:25:41 +0000

    Pseudo-automatic update of changes made by wtchang@redhat.com.

--- CVSROOT/commitcheck.pl
+++ CVSROOT/commitcheck.pl
@@ -26,7 +26,7 @@ $fullname{'183'} = 'calendar';
 $mode{'146'} = 'Restricted';
 $branch{'146'} = 'NSPRPUB_PRE_4_2_CLIENT_BRANCH';
 $fullname{'146'} = 'Client NSPR';
-$blessed{'146'} = ['seawood%netscape.com','smfr%smfr.org','sdagley%mac.com','mkaply%us.ibm.com','lpham%netscape.com','jje%mac.com','granrose%netscape.com','dbaron%dbaron.org',];
+$blessed{'146'} = ['smfr%smfr.org','seawood%netscape.com','sdagley%mac.com','mkaply%us.ibm.com','lpham%netscape.com','jje%mac.com','granrose%netscape.com','dbaron%dbaron.org','gerv%gerv.net',];
 $super{'146'} = ['wtchang%redhat.com','darin%meer.net','cls%seawood.org','bryner%brianryner.com','leaf%mozilla.org',];
 $mode{'96'} = 'Open';
 $branch{'96'} = 'HEAD';
@@ -313,6 +313,29 @@ $fullname{'93'} = 'Zlib';
 sub GetT {
 ($b,$_) = (@_);
 if ($b eq 'HEAD') {
+if (m:^mozilla/build/.*$:) {return '55';}
+if (m:^mozilla/config/.*$:) {return '55';}
+if (m:^mozilla/l10n/.*$:) {return '55';}
+if (m:^mozilla/extensions/vixen/.*$:) {return '156';}
+if (m:^mozilla/editor/.*$:) {return '56';}
+if (m:^mozilla/mailnews/movemail/.*$:) {return '157';}
+if (m:^mozilla/directory/buildsdk\.txt$:) {return '59';}
+if (m:^mozilla/directory/c-sdk/.*$:) {return '59';}
+if (m:^mozilla/directory/ldapsdk\.mak$:) {return '59';}
+if (m:^mozilla/directory/ldapsdk\.mk$:) {return '59';}
+if (m:^mozilla/directory/Makefile$:) {return '59';}
+if (m:^mozilla/uriloader/.*$:) {return '158';}
+if (m:^gfx/src/ps/.*$:) {return '62';}
+if (m:^gfx/src/psshared/.*$:) {return '62';}
+if (m:^mozilla/modules/libpr0n/.*$:) {return '63';}
+if (m:^mozilla/string/.*$:) {return '160';}
+if (m:^jpeg$:) {return '64';}
+if (m:^mozilla/gfx/src/qt/.*$:) {return '161';}
+if (m:^mozilla/widget/src/qt/.*$:) {return '161';}
+if (m:^mozilla/modules/libimg/png/.*$:) {return '65';}
+if (m:^mozilla/extensions/p3p/.*$:) {return '162';}
+if (m:^mozilla/intl/.*$:) {return '67';}
+if (m:^mozilla/layout/mathml/.*$:) {return '165';}
 if (m:^mozilla/modules/oji/.*$:) {return '68';}
 if (m:^mozilla/nav-java/.*$:) {return '68';}
 if (m:^mozilla/sun-java/.*$:) {return '68';}
@@ -434,29 +457,6 @@ if (m:^mozilla/xpinstall/.*$:) {return '150';}
 if (m:^mozilla/rdf/.*$:) {return '52';}
 if (m:^mozilla/security/manager/.*$:) {return '151';}
 if (m:^mozilla/dbm/.*$:) {return '53';}
-if (m:^mozilla/build/.*$:) {return '55';}
-if (m:^mozilla/config/.*$:) {return '55';}
-if (m:^mozilla/l10n/.*$:) {return '55';}
-if (m:^mozilla/extensions/vixen/.*$:) {return '156';}
-if (m:^mozilla/editor/.*$:) {return '56';}
-if (m:^mozilla/mailnews/movemail/.*$:) {return '157';}
-if (m:^mozilla/directory/buildsdk\.txt$:) {return '59';}
-if (m:^mozilla/directory/c-sdk/.*$:) {return '59';}
-if (m:^mozilla/directory/ldapsdk\.mak$:) {return '59';}
-if (m:^mozilla/directory/ldapsdk\.mk$:) {return '59';}
-if (m:^mozilla/directory/Makefile$:) {return '59';}
-if (m:^mozilla/uriloader/.*$:) {return '158';}
-if (m:^gfx/src/ps/.*$:) {return '62';}
-if (m:^gfx/src/psshared/.*$:) {return '62';}
-if (m:^mozilla/modules/libpr0n/.*$:) {return '63';}
-if (m:^mozilla/string/.*$:) {return '160';}
-if (m:^jpeg$:) {return '64';}
-if (m:^mozilla/gfx/src/qt/.*$:) {return '161';}
-if (m:^mozilla/widget/src/qt/.*$:) {return '161';}
-if (m:^mozilla/modules/libimg/png/.*$:) {return '65';}
-if (m:^mozilla/extensions/p3p/.*$:) {return '162';}
-if (m:^mozilla/intl/.*$:) {return '67';}
-if (m:^mozilla/layout/mathml/.*$:) {return '165';}
 }
 if ($b eq 'NSPRPUB_PRE_4_2_CLIENT_BRANCH') {
 if (m:^mozilla/nsprpub/.*$:) {return '146';}
