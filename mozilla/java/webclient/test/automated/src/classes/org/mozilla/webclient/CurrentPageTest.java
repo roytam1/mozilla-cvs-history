cda6167d74359de546ded13d5214fe0f50d68ced
edburns%acm.org <>
2005-02-07 06:21:35 +0000

    This checkin re-implements the basic find functionality.  Next step is to provide a way to return result of the find, by defining methods on the CurrentPage2 interface.
    A test/automated/src/test/FindTest0.html
    
    - fodder for find
    
    M src_moz/CurrentPageImpl.cpp
    
    - copy content from CurrentPageActionEvents
    
    M test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
    
    - basic test.  Needs more robustness.

--- mozilla/java/webclient/test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
+++ mozilla/java/webclient/test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
@@ -214,5 +214,73 @@ public class CurrentPageTest extends WebclientTestCase implements ClipboardOwner
 	BrowserControlFactory.deleteBrowserControl(firstBrowserControl);
     }
 
+    public void testFind() throws Exception {
+	BrowserControl firstBrowserControl = null;
+	DocumentLoadListenerImpl listener = null;
+	Selection selection = null;
+	firstBrowserControl = BrowserControlFactory.newBrowserControl();
+	assertNotNull(firstBrowserControl);
+	BrowserControlCanvas canvas = (BrowserControlCanvas)
+	    firstBrowserControl.queryInterface(BrowserControl.BROWSER_CONTROL_CANVAS_NAME);
+	eventRegistration = (EventRegistration2)
+	    firstBrowserControl.queryInterface(BrowserControl.EVENT_REGISTRATION_NAME);
+
+	assertNotNull(canvas);
+	Frame frame = new Frame();
+	frame.setUndecorated(true);
+	frame.setBounds(0, 0, 640, 480);
+	frame.add(canvas, BorderLayout.CENTER);
+	frame.setVisible(true);
+	canvas.setVisible(true);
+	
+	Navigation2 nav = (Navigation2) 
+	    firstBrowserControl.queryInterface(BrowserControl.NAVIGATION_NAME);
+	assertNotNull(nav);
+	currentPage = (CurrentPage2) 
+	  firstBrowserControl.queryInterface(BrowserControl.CURRENT_PAGE_NAME);
+	
+	assertNotNull(currentPage);
+
+	eventRegistration.addDocumentLoadListener(listener = new DocumentLoadListenerImpl() {
+		public void doEndCheck() {
+		    CurrentPageTest.keepWaiting = false;
+		}
+	    });
+	
+	Thread.currentThread().sleep(3000);
+	
+
+	//
+	// load four files.
+	//
+	CurrentPageTest.keepWaiting = true;
+
+	nav.loadURL("http://localhost:5243/FindTest0.html");
+	
+	// keep waiting until the previous load completes
+	while (CurrentPageTest.keepWaiting) {
+	    Thread.currentThread().sleep(1000);
+	}
+
+	currentPage.resetFind();
+	currentPage.findInPage("one", true, false);
+
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("one"));
+
+	Thread.currentThread().sleep(1000);
+
+	currentPage.findNextInPage();
+		   
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("one"));
+	
+
+	Thread.currentThread().sleep(2000);
+
+	frame.setVisible(false);
+	BrowserControlFactory.deleteBrowserControl(firstBrowserControl);
+    }
+
 
 }
