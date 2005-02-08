0556d1ced0c9fab9748187c0edf29909df7c33b3
mostafah%oeone.com <>
2005-02-08 16:58:56 +0000

    Copied from trunk to make installer work for 0.2

--- mozilla/toolkit/mozapps/installer/windows/wizard/setup/dialogs.c
+++ mozilla/toolkit/mozapps/installer/windows/wizard/setup/dialogs.c
@@ -1,30 +1,43 @@
 /* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/*
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
- * The Original Code is Mozilla Communicator client code,
- * released March 31, 1998.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- * The Initial Developer of the Original Code is Netscape Communications
- * Corporation.  Portions created by Netscape are
- * Copyright (C) 1998 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
  *
- * Contributor(s): 
- *     Sean Su <ssu@netscape.com>
- *     Curt Patrick <curt@netscape.com>
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
  *
- *  Next Generation Apps Version:
- *     Ben Goodger <ben@mozilla.org>
- */
+ * Contributor(s):
+ *   Sean Su <ssu@netscape.com>
+ *   Curt Patrick <curt@netscape.com>
+ *   Ben Goodger <ben@mozilla.org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 // needed to build with mingw
 #ifndef _WIN32_IE
@@ -46,6 +59,20 @@
 
 // commdlg.h is needed to build with WIN32_LEAN_AND_MEAN
 #include <commdlg.h>
+
+// re-define PSH_WIZARD97 as per latest Platform SDK (below)
+#ifdef PSH_WIZARD97
+#undef PSH_WIZARD97
+#endif
+
+#if (_WIN32_IE >= 0x0400)
+//----- New flags for wizard97 -----------
+#if (_WIN32_IE < 0x0500)
+#define PSH_WIZARD97            0x00002000
+#else
+#define PSH_WIZARD97            0x01000000
+#endif
+#endif
 
 // Turn off default to safe upgrade for 0.8... until we can guarantee that we won't be deleting other files. 
 #define DEFAULT_SAFE_UPGRADE FALSE
@@ -215,7 +242,45 @@ void InitSequence(HINSTANCE hInstance)
 #endif
 
   // Start the Wizard.
-  PropertySheet(&psh);
+  if (psh.nPages > 0) {
+    PropertySheet(&psh);
+  } else {
+    // Silent install
+
+    InstallFiles(NULL);
+
+    gbIgnoreRunAppX = TRUE;
+    // Apply settings and close. 
+    if (sgProduct.bInstallFiles)
+      UpdateJSProxyInfo();
+
+    /* POST_SMARTUPDATE process file manipulation functions */
+    ProcessFileOpsForAll(T_POST_SMARTUPDATE);
+
+    if (sgProduct.bInstallFiles) {
+      /* PRE_LAUNCHAPP process file manipulation functions */
+      ProcessFileOpsForAll(T_PRE_LAUNCHAPP);
+
+      LaunchApps();
+
+      // Refresh system icons if necessary
+      if (gSystemInfo.bRefreshIcons)
+        RefreshIcons();
+
+      UnsetSetupState(); // clear setup state
+      ClearWinRegUninstallFileDeletion();
+      if (!gbIgnoreProgramFolderX)
+        ProcessProgramFolderShowCmd();
+
+      CleanupArgsRegistry();
+      CleanupPreviousVersionRegKeys();
+
+      /* POST_LAUNCHAPP process file manipulation functions */
+      ProcessFileOpsForAll(T_POST_LAUNCHAPP);
+      /* DEPEND_REBOOT process file manipulation functions */
+      ProcessFileOpsForAll(T_DEPEND_REBOOT);
+    }
+  }
 
   DeleteObject(sgInstallGui.welcomeTitleFont);
 }
@@ -2340,5 +2405,10 @@ void InitPathDisplay (HWND aWindow, char* aPath, int aFolderIcon, int aFolderFie
                  aPath, buf, sizeof(buf));
   SetDlgItemText(aWindow, aFolderField, buf);
 }
+
+
+
+
+
 
 
