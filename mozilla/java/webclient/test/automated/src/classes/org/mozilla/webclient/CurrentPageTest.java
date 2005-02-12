7088fede303ec7ed32afcb84f62b58d6796e96c4
edburns%acm.org <>
2005-02-12 21:29:47 +0000

    Complete test for find.  Next step is highlightSelection and clearAllSelection.
    test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
    
    - flesh out this test.

--- mozilla/java/webclient/test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
+++ mozilla/java/webclient/test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
@@ -268,16 +268,35 @@ public class CurrentPageTest extends WebclientTestCase implements ClipboardOwner
 	selection = currentPage.getSelection();
 	assertTrue(-1 != selection.toString().indexOf("one"));
 
-	Thread.currentThread().sleep(1000);
-
 	assertTrue(currentPage.findNext());
 		   
 	selection = currentPage.getSelection();
 	assertTrue(-1 != selection.toString().indexOf("one"));
+
+	assertTrue(currentPage.findNext());
+
+	assertTrue(!currentPage.findNext());
+
+	assertTrue(currentPage.find("one", false, false));
+	assertTrue(currentPage.findNext());
+
+	assertTrue(currentPage.find("three", true, false));
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("Three"));
+	assertTrue(currentPage.findNext());
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("Three"));
+	assertTrue(currentPage.findNext());
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("three"));
+
+	assertTrue(currentPage.find("Three", false, true));
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("Three"));
+	assertTrue(currentPage.findNext());
+	selection = currentPage.getSelection();
+	assertTrue(-1 != selection.toString().indexOf("Three"));
 	
-
-	Thread.currentThread().sleep(2000);
-
 	frame.setVisible(false);
 	BrowserControlFactory.deleteBrowserControl(firstBrowserControl);
     }
