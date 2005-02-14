78d6cfc7198b94540e1730575cd98c2933ec38ea
edburns%acm.org <>
2005-02-14 02:37:51 +0000

    This checkin re-enables printing.  Thanks to Kyle Yuan from Sun Microsystems.
    Next step is to take a step back and see what's best to do next!
    
    M classes_spec/org/mozilla/webclient/impl/wrapper_native/CurrentPageImpl.java
    
    - call through to print related methods
    
    M src_moz/CurrentPageImpl.cpp
    
    - Copy from CurrentPageActionEvents.cpp
    
    M test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
    
    - add test stub for print preview.

--- mozilla/java/webclient/test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
+++ mozilla/java/webclient/test/automated/src/classes/org/mozilla/webclient/CurrentPageTest.java
@@ -381,4 +381,120 @@ public class CurrentPageTest extends WebclientTestCase implements ClipboardOwner
 	BrowserControlFactory.deleteBrowserControl(firstBrowserControl);
     }
 
+    public void NoIdeaHowToTestPrintingUsingJunit() throws Exception {
+	BrowserControl firstBrowserControl = null;
+	DocumentLoadListenerImpl listener = null;
+	Selection selection = null;
+	firstBrowserControl = BrowserControlFactory.newBrowserControl();
+	assertNotNull(firstBrowserControl);
+	BrowserControlCanvas canvas = (BrowserControlCanvas)
+	    firstBrowserControl.queryInterface(BrowserControl.BROWSER_CONTROL_CANVAS_NAME);
+	eventRegistration = (EventRegistration2)
+	    firstBrowserControl.queryInterface(BrowserControl.EVENT_REGISTRATION_NAME);
+
+	assertNotNull(canvas);
+	Frame frame = new Frame();
+	frame.setUndecorated(true);
+	frame.setBounds(0, 0, 640, 480);
+	frame.add(canvas, BorderLayout.CENTER);
+	frame.setVisible(true);
+	canvas.setVisible(true);
+	
+	Navigation2 nav = (Navigation2) 
+	    firstBrowserControl.queryInterface(BrowserControl.NAVIGATION_NAME);
+	assertNotNull(nav);
+	currentPage = (CurrentPage2) 
+	  firstBrowserControl.queryInterface(BrowserControl.CURRENT_PAGE_NAME);
+	
+	assertNotNull(currentPage);
+
+	eventRegistration.addDocumentLoadListener(listener = new DocumentLoadListenerImpl() {
+		public void doEndCheck() {
+		    CurrentPageTest.keepWaiting = false;
+		}
+	    });
+	
+	Thread.currentThread().sleep(3000);
+	
+
+	//
+	// load four files.
+	//
+	CurrentPageTest.keepWaiting = true;
+
+	nav.loadURL("http://localhost:5243/HistoryTest0.html");
+	
+	// keep waiting until the previous load completes
+	while (CurrentPageTest.keepWaiting) {
+	    Thread.currentThread().sleep(1000);
+	}
+
+	currentPage.print();
+
+	Thread.currentThread().sleep(10000);
+	
+	
+	frame.setVisible(false);
+	BrowserControlFactory.deleteBrowserControl(firstBrowserControl);
+    }
+
+    public void testPrintPreview() throws Exception {
+	BrowserControl firstBrowserControl = null;
+	DocumentLoadListenerImpl listener = null;
+	Selection selection = null;
+	firstBrowserControl = BrowserControlFactory.newBrowserControl();
+	assertNotNull(firstBrowserControl);
+	BrowserControlCanvas canvas = (BrowserControlCanvas)
+	    firstBrowserControl.queryInterface(BrowserControl.BROWSER_CONTROL_CANVAS_NAME);
+	eventRegistration = (EventRegistration2)
+	    firstBrowserControl.queryInterface(BrowserControl.EVENT_REGISTRATION_NAME);
+
+	assertNotNull(canvas);
+	Frame frame = new Frame();
+	frame.setUndecorated(true);
+	frame.setBounds(0, 0, 640, 480);
+	frame.add(canvas, BorderLayout.CENTER);
+	frame.setVisible(true);
+	canvas.setVisible(true);
+	
+	Navigation2 nav = (Navigation2) 
+	    firstBrowserControl.queryInterface(BrowserControl.NAVIGATION_NAME);
+	assertNotNull(nav);
+	currentPage = (CurrentPage2) 
+	  firstBrowserControl.queryInterface(BrowserControl.CURRENT_PAGE_NAME);
+	
+	assertNotNull(currentPage);
+
+	eventRegistration.addDocumentLoadListener(listener = new DocumentLoadListenerImpl() {
+		public void doEndCheck() {
+		    CurrentPageTest.keepWaiting = false;
+		}
+	    });
+	
+	Thread.currentThread().sleep(3000);
+	
+
+	//
+	// load four files.
+	//
+	CurrentPageTest.keepWaiting = true;
+
+	nav.loadURL("http://localhost:5243/HistoryTest0.html");
+	
+	// keep waiting until the previous load completes
+	while (CurrentPageTest.keepWaiting) {
+	    Thread.currentThread().sleep(1000);
+	}
+
+	currentPage.printPreview(true);
+
+	Thread.currentThread().sleep(3000);
+
+	currentPage.printPreview(false);
+	
+	
+	frame.setVisible(false);
+	BrowserControlFactory.deleteBrowserControl(firstBrowserControl);
+    }
+
 }
