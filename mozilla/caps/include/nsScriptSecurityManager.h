50956bd5aa584be8d606f4150b9440869924264f
brendan%mozilla.org <>
2005-04-07 00:25:25 +0000

    Stop evals and Script object calls/execs that cross trust domains (289074, r=shaver, sr=jst, a=drivers).

--- mozilla/caps/include/nsScriptSecurityManager.h
+++ mozilla/caps/include/nsScriptSecurityManager.h
@@ -387,6 +387,12 @@ private:
                             const char* aClassName, jsval aProperty,
                             void** aCachedClassPolicy);
 
+    void
+    ThrowAccessDeniedException(JSContext *cx,
+                               PRUint32 aAction,
+                               const char *aClassName,
+                               jsval aProperty);
+
     nsresult
     CheckSameOriginPrincipalInternal(nsIPrincipal* aSubject,
                                      nsIPrincipal* aObject,
