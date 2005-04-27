f41ac8ed8a86a24ead317930187ce16b3827925f
pinkerton%aol.net <>
2005-04-27 13:09:04 +0000

    084 branding

--- mozilla/camino/resources/application/ChimeraVersion.r
+++ mozilla/camino/resources/application/ChimeraVersion.r
@@ -51,14 +51,14 @@
 // Version Numbers //
 
 #define     VERSION_MAJOR           0
-#define     VERSION_MINOR           0x83    // revision & fix in BCD
+#define     VERSION_MINOR           0x84    // revision & fix in BCD
 #define     VERSION_KIND            final    // alpha, beta, or final
 #define     VERSION_MICRO           0       // internal stage: alpha or beta number
 
 
 // Version Strings (Finder's Get Info dialog box) //
 
-#define     VERSION_STRING          "0.8.3+"
+#define     VERSION_STRING          "0.8.4"
 
 #define     COPYRIGHT_STRING        "© 1998-2005 The Mozilla Foundation"
 #define     GETINFO_VERSION         VERSION_STRING ", " COPYRIGHT_STRING
