667911f90833314c8970c37514360035df124652
ben%bengoodger.com <>
2005-04-28 23:50:48 +0000

    set branch tags

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -197,10 +197,10 @@ BOOTSTRAP_macbrowser := mozilla/camino/config/mozconfig
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-#MOZ_CO_TAG          = <tag>
-NSPR_CO_TAG          = NSPRPUB_PRE_4_2_CLIENT_BRANCH
-NSS_CO_TAG           = NSS_CLIENT_TAG
-LDAPCSDK_CO_TAG      = ldapcsdk_50_client_branch
+MOZ_CO_TAG           = SOFTWARE_UPDATE_20050428_BRANCH
+NSPR_CO_TAG          = SOFTWARE_UPDATE_20050428_BRANCH
+NSS_CO_TAG           = SOFTWARE_UPDATE_20050428_BRANCH
+LDAPCSDK_CO_TAG      = SOFTWARE_UPDATE_20050428_BRANCH
 LOCALES_CO_TAG       =
 
 BUILD_MODULES = all
