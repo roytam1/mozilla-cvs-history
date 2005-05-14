7dc323f1430ed1d3649072df89bd09bd18d3e28b
darin%meer.net <>
2005-05-14 00:56:06 +0000

    honor umask when creating files

--- mozilla/modules/libmar/src/mar_extract.c
+++ mozilla/modules/libmar/src/mar_extract.c
@@ -38,23 +38,46 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "mar.h"
 #include "mar_private.h"
 
+/* Ensure that the directory containing this file exists */
+static int mar_ensure_parent_dir(const char *path)
+{
+  char *slash = strrchr(path, '/');
+  if (slash)
+  {
+    *slash = '\0';
+    mar_ensure_parent_dir(path);
+#ifdef XP_WIN
+    _mkdir(path);
+#else
+    mkdir(path, 0755);
+#endif
+    *slash = '/';
+  }
+  return 0;
+}
+
 static int mar_test_callback(MarFile *mar, const MarItem *item, void *unused) {
   FILE *fp;
   char buf[BLOCKSIZE];
-  int len, offset = 0;
+  int fd, len, offset = 0;
 
-  fp = fopen(item->name, "w");
-  if (!fp)
+  if (mar_ensure_parent_dir(item->name))
     return -1;
 
-#ifndef XP_WIN
-  fchmod(fileno(fp), item->flags);
-#endif
+  fd = creat(item->name, item->flags);
+  if (fd == -1)
+    return -1;
+
+  fp = fdopen(fd, "w");
+  if (!fp)
+    return -1;
 
   while ((len = mar_read(mar, item, offset, buf, sizeof(buf))) > 0) {
     if (fwrite(buf, len, 1, fp) != 1)
@@ -68,13 +91,14 @@ static int mar_test_callback(MarFile *mar, const MarItem *item, void *unused) {
 
 int mar_extract(const char *path) {
   MarFile *mar;
+  int rv;
 
   mar = mar_open(path);
   if (!mar)
     return -1;
 
-  mar_enum_items(mar, mar_test_callback, NULL);
+  rv = mar_enum_items(mar, mar_test_callback, NULL);
 
   mar_close(mar);
-  return 0;
+  return rv;
 }
