f43a4d76e56a40ccbd7dd93b956bb8cfb4c24a70
cvs2git <>
2005-05-16 23:45:28 +0000

    This commit was manufactured by cvs2svn to create tag 'kiko_product_admin_20050517-BASE'.

--- mozilla/extensions/reporter/resources/content/prefs/reporter.js
+++
@@ -1,4 +0,0 @@
-// Make sure there are trailing slashes!
-pref("extensions.reporter.privacyURL", "http://reporter-test.mozilla.org/privacy/");
-pref("extensions.reporter.serviceURL", "http://reporter-test.mozilla.org/service/");
-
