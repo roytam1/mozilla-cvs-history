ddf47bb4fb48d358510950be8f3d4fe8d019f70b
darin%meer.net <>
2005-05-18 18:22:42 +0000

    landing tools to generate update packages

---
+++ mozilla/tools/make-update/make_full_update.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# This tool generates full update packages for the update system.
+# Author: Darin Fisher
+#
+
+# -----------------------------------------------------------------------------
+# By default just assume that these tools exist on our path
+MAR=${MAR:-mar}
+BZIP2=${BZIP2:-bzip2}
+
+# -----------------------------------------------------------------------------
+
+print_usage() {
+  echo "Usage: $(basename $0) [OPTIONS] ARCHIVE DIRECTORY"
+}
+
+if [ $# = 0 ]; then
+  print_usage
+  exit 1
+fi
+
+if [ $1 = -h ]; then
+  print_usage
+  echo ""
+  echo "The contents of DIRECTORY will be stored in ARCHIVE."
+  echo ""
+  echo "Options:"
+  echo "  -h  show this help text"
+  echo ""
+  exit 1
+fi
+
+# -----------------------------------------------------------------------------
+
+archive="$1"
+targetdir="$2"
+workdir="$targetdir.work"
+manifest="$workdir/update.manifest"
+
+# Generate a list of all files in the target directory.
+targetfiles=$(cd "$targetdir" && find . -type f | cut -d'/' -f2-)
+
+mkdir -p "$workdir"
+> $manifest
+
+for f in $targetfiles; do
+  echo "  processing $f"
+
+  echo "add $f" >> $manifest
+
+  dir=$(dirname $f)
+  mkdir -p "$workdir/$dir"
+  $BZIP2 -cz9 "$targetdir/$f" > "$workdir/$f"
+  chmod --reference="$targetdir/$f" "$workdir/$f"
+done
+
+(cd "$workdir" && $MAR -c output.mar update.manifest $targetfiles)
+mv -f "$workdir/output.mar" "$archive"
+
+# cleanup
+rm -fr "$workdir"
