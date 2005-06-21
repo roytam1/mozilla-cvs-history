26d1aab3fc5553cbf7725bdd60e242aa83d49496
jst%mozilla.jstenback.com <>
2005-06-21 21:42:07 +0000

    Fixing bug 295093. Check the window chrome flags when opening a new window before pushing the callee context onto the context stack. r=dveditz@cruzio.com, sr=brendan@mozilla.org, a=dveditz@curzio.com

--- mozilla/embedding/components/windowwatcher/src/nsWindowWatcher.cpp
+++ mozilla/embedding/components/windowwatcher/src/nsWindowWatcher.cpp
@@ -564,6 +564,14 @@ nsWindowWatcher::OpenWindowJS(nsIDOMWindow *aParent,
 
   nsCOMPtr<nsIDOMChromeWindow> chromeParent(do_QueryInterface(aParent));
 
+  // Make sure we call CalculateChromeFlags() *before* we push the
+  // callee context onto the context stack so that
+  // CalculateChromeFlags() sees the actual caller when doing it's
+  // security checks.
+  chromeFlags = CalculateChromeFlags(features.get(), featuresSpecified,
+                                     aDialog, uriToLoadIsChrome,
+                                     !aParent || chromeParent);
+
   PRBool isCallerChrome = PR_FALSE;
   nsCOMPtr<nsIScriptSecurityManager>
     sm(do_GetService(NS_SCRIPTSECURITYMANAGER_CONTRACTID));
@@ -580,10 +588,6 @@ nsWindowWatcher::OpenWindowJS(nsIDOMWindow *aParent,
 
     callerContextGuard.Push(cx);
   }
-
-  chromeFlags = CalculateChromeFlags(features.get(), featuresSpecified,
-                                     aDialog, uriToLoadIsChrome,
-                                     !aParent || chromeParent);
 
   // no extant window? make a new one.
   if (!newDocShellItem) {
