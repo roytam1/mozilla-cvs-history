fcae4b8a574b1afc93272e616fd6bfaf6a878b1a
mike.morgan%oregonstate.edu <>
2005-07-15 15:27:17 +0000

    updated header file to fix plugins links

--- mozilla/webtools/update/core/inc_header.php
+++ mozilla/webtools/update/core/inc_header.php
@@ -105,7 +105,7 @@ if (!empty($page_headers)) {
 	<div id="key-menu">	
 		<dl id="menu-firefox">
 		<dt>Firefox:</dt>
-		<dd><a href="<?php echo WEB_PATH; ?>/extensions/?application=firefox" title="Get Extensions for the Firefox Browser">Extensions</a>, <a href="<?php echo WEB_PATH; ?>/themes/?application=firefox" title="Get Themes for the Firefox Browser">Themes</a>, <a href="<?php echo WEB_PATH; ?>/plugins/" title="Get Plugins for Firefox">Plugins</a><?php /*, <a href="<?php echo WEB_PATH; ?>/searchengines/" title="Get New Search Engines for the Search Box in Firefox">Search Engines</a>--> */ ?></dd>
+		<dd><a href="<?php echo WEB_PATH; ?>/extensions/?application=firefox" title="Get Extensions for the Firefox Browser">Extensions</a>, <a href="<?php echo WEB_PATH; ?>/themes/?application=firefox" title="Get Themes for the Firefox Browser">Themes</a>, <a href="https://pfs.mozilla.org/plugins/" title="Get Plugins for Firefox">Plugins</a><?php /*, <a href="<?php echo WEB_PATH; ?>/searchengines/" title="Get New Search Engines for the Search Box in Firefox">Search Engines</a>--> */ ?></dd>
 		</dl>
 		<dl id="menu-thunderbird">
 		<dt>Thunderbird:</dt>
@@ -113,7 +113,7 @@ if (!empty($page_headers)) {
 		</dl>
 		<dl id="menu-mozillasuite">
 		<dt>Mozilla Suite:</dt>
-		<dd><a href="<?php echo WEB_PATH; ?>/extensions/?application=mozilla" title="Get Extensions for the Mozilla Suite">Extensions</a>, <a href="<?php echo WEB_PATH; ?>/themes/?application=mozilla" title="Get Themes for the Mozilla Suite">Themes</a>, <a href="<?php echo WEB_PATH; ?>/plugins/" title="Get Plugins for Mozilla Suite">Plugins</a></dd>
+		<dd><a href="<?php echo WEB_PATH; ?>/extensions/?application=mozilla" title="Get Extensions for the Mozilla Suite">Extensions</a>, <a href="<?php echo WEB_PATH; ?>/themes/?application=mozilla" title="Get Themes for the Mozilla Suite">Themes</a>, <a href="https://pfs.mozilla.org/plugins/" title="Get Plugins for Mozilla Suite">Plugins</a></dd>
 		</dl>
 		<div class="ie-clear-menu">&nbsp;</div>
 	</div>
