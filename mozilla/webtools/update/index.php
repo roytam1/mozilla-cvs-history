52e9c8cde1bed69efe62005539d6b664102219f3
mike.morgan%oregonstate.edu <>
2005-07-22 15:56:35 +0000

    removed php developer notice; help no longer needed.

--- mozilla/webtools/update/index.php
+++ mozilla/webtools/update/index.php
@@ -125,10 +125,14 @@ vulputate sapien et leo. Nullam euismod tortor id wisi.
 <!-- close security update -->
 <?php } ?>
 
+<?php
+/*
 <div class="key-point"><p><strong>Want to get involved?</strong><br>
 We are looking for volunteers to help us with UMO. We are in need of PHP developers to help with redesigning the site, and people to review extensions and themes that get submitted to UMO. We especially need Mac and Thunderbird users. If you are interested in being a part of this exciting project, please send information such as your full name, timezone and experience to <a href="mailto:umo-reviewer@mozilla.org?subject=Review%20Application%20for%20[Name Here]">umo-reviewer@mozilla.org</a>. Also, please join us in #umo on irc.mozilla.org to start getting a feeling for what's up or for a more informal chat.
 </p>
 </div>
+*/
+?>
 
 <div id="mBody">
 	<div id="mainContent" class="right">
