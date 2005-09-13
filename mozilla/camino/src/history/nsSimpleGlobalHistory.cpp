1e1fc94eb4699188d0db7414fea73399ecfb321a
smfr%smfr.org <>
2005-09-13 21:53:39 +0000

    Merge Camino changes to the CAMINO_1_0a1_MINIBRANCH.

--- mozilla/camino/src/history/nsSimpleGlobalHistory.cpp
+++ mozilla/camino/src/history/nsSimpleGlobalHistory.cpp
@@ -1606,47 +1606,6 @@ nsSimpleGlobalHistory::MarkPageAsTyped(nsIURI *aURI)
   return NS_OK;
 }
 
-NS_IMETHODIMP
-nsSimpleGlobalHistory::SetURIGeckoFlags(nsIURI *aURI, PRUint32 aFlags)
-{
-  nsCAutoString spec;
-  nsresult rv = aURI->GetSpec(spec);
-  if (NS_FAILED(rv)) return rv;
-
-  nsCOMPtr<nsIMdbRow> row;
-  rv = FindRow(kToken_URLColumn, spec.get(), getter_AddRefs(row));
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
-
-  rv = SetRowValue(row, kToken_GeckoFlagsColumn, (PRInt32)aFlags);
-  SetDirty();
-  return rv;
-}
-
-NS_IMETHODIMP
-nsSimpleGlobalHistory::GetURIGeckoFlags(nsIURI *aURI, PRUint32* aFlags)
-{
-  nsCAutoString spec;
-  nsresult rv = aURI->GetSpec(spec);
-  if (NS_FAILED(rv)) return rv;
-
-  nsCOMPtr<nsIMdbRow> row;
-  rv = FindRow(kToken_URLColumn, spec.get(), getter_AddRefs(row));
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
-
-  if (!HasCell(mEnv, row, kToken_GeckoFlagsColumn))
-    return NS_ERROR_FAILURE;
-
-  PRInt32 val;
-  mdb_err err = GetRowValue(row, kToken_GeckoFlagsColumn, &val);
-  NS_ENSURE_TRUE(err == 0, NS_ERROR_FAILURE);
-  *aFlags = val;
-  return NS_OK;
-}
-
 //----------------------------------------------------------------------
 //
 // nsGlobalHistory
@@ -2033,9 +1992,6 @@ nsSimpleGlobalHistory::CreateTokens()
   if (err != 0) return NS_ERROR_FAILURE;
 
   err = mStore->StringToToken(mEnv, "Typed", &kToken_TypedColumn);
-  if (err != 0) return NS_ERROR_FAILURE;
-
-  err = mStore->StringToToken(mEnv, "GeckoFlags", &kToken_GeckoFlagsColumn);
   if (err != 0) return NS_ERROR_FAILURE;
 
   // meta-data tokens
