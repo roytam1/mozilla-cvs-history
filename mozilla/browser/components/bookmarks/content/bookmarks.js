4fcbef4194f86c18fc2f634f97ca9949538de35d
vladimir%pobox.com <>
2005-09-20 18:10:57 +0000

    b=222883, get rid of bookmarks -> manage folder, r=mconnor,a=asa

--- mozilla/browser/components/bookmarks/content/bookmarks.js
+++ mozilla/browser/components/bookmarks/content/bookmarks.js
@@ -342,7 +342,7 @@ var BookmarksCommand = {
       break;
     case "Folder":
     case "PersonalToolbarFolder":
-      commands = ["bm_expandfolder", "bm_openfolder", "bm_managefolder", "bm_separator", 
+      commands = ["bm_expandfolder", "bm_openfolder", "bm_separator", 
                   "bm_newbookmark", "bm_newfolder", "bm_newseparator", "bm_separator",
                   "cut", "copy", "paste", "bm_separator",
                   "delete", "bm_separator",
