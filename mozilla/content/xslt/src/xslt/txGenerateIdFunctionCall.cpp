78bfe6ae2bd143d1fa7abc17d19a453878fffe3e
nisheeth%netscape.com <>
2005-11-02 07:34:49 +0000

    More changes to get Transformiix to build as a Mozilla component on windows.  Not part of the mozilla build.

--- mozilla/content/xslt/src/xslt/txGenerateIdFunctionCall.cpp
+++ mozilla/content/xslt/src/xslt/txGenerateIdFunctionCall.cpp
@@ -55,7 +55,7 @@ ExprResult* GenerateIdFunctionCall::evaluate(Node* context, ContextState* cs) {
 
     if (argc > 0) {
         ListIterator* iter = params.iterator();
-        Expr* param = iter->next();
+        Expr* param = (Expr*) iter->next();
         delete iter;
         ExprResult* exprResult = param->evaluate(context, cs);
         if (!exprResult) return new StringResult("");
