fdc4df70a2e1ce09242f128e6ce59adccd31e6d9
mvl%exedo.nl <>
2005-11-04 16:51:37 +0000

    create client.mk for sunbird 0.3a1

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -231,11 +231,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-#MOZ_CO_TAG          = <tag>
-NSPR_CO_TAG          = NSPRPUB_PRE_4_2_CLIENT_BRANCH
-NSS_CO_TAG           = NSS_CLIENT_TAG
-LDAPCSDK_CO_TAG      = ldapcsdk_50_client_branch
-LOCALES_CO_TAG       =
+MOZ_CO_TAG           = SUNBIRD_0_3a1_RELEASE
+NSPR_CO_TAG          = SUNBIRD_0_3a1_RELEASE
+NSS_CO_TAG           = SUNBIRD_0_3a1_RELEASE
+LDAPCSDK_CO_TAG      = SUNBIRD_0_3a1_RELEASE
+LOCALES_CO_TAG       = SUNBIRD_0_3a1_RELEASE
 
 BUILD_MODULES = all
 
