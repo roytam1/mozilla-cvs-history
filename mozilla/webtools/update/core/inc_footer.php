489516ae04ae17d7ca74f9b675bef13744fd49d9
mike.morgan%oregonstate.edu <>
2005-11-29 22:02:13 +0000

    core/inc_footer.php

--- mozilla/webtools/update/core/inc_footer.php
+++ mozilla/webtools/update/core/inc_footer.php
@@ -38,7 +38,7 @@
 ?>
 <hr class="hide">
 	<div id="footer">
-        <p><a href="<?=$_SERVER['PHP_SELF']?>/" class="switch-fx">Firefox Add-ons </a><a href="<?=$_SERVER['PHP_SELF']?>/?application=thunderbird" class="switch-tb">Thunderbird Add-ons </a><a href="<?=$_SERVER['PHP_SELF']?>/?application=mozilla" class="switch-suite">Mozilla Suite Add-ons </a></p>
+        <p><a href="<?=$_SERVER['PHP_SELF']?>?application=firefox" class="switch-fx">Firefox Add-ons </a><a href="<?=$_SERVER['PHP_SELF']?>?application=thunderbird" class="switch-tb">Thunderbird Add-ons </a><a href="<?=$_SERVER['PHP_SELF']?>?application=mozilla" class="switch-suite">Mozilla Suite Add-ons </a></p>
         <p><a href="http://www.mozilla.org/privacy-policy.html">Privacy Policy</a> <a href="<?=WEB_PATH?>/faq.php">FAQ</a> <a href="http://www.mozilla.org/foundation/donate.html">Donate to Mozilla</a> <a href="http://mozilla.org/">The Mozilla Organization</a></p>
         <p><span>Copyright &copy; 2004-2006</span>  <a href="http://www.xramp.com/">256-bit SSL Encryption provided by XRamp</a></p>
 	</div>
