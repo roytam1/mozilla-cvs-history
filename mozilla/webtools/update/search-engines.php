4dff1c594eb341ec1169685fd9a386dbeb20d3cc
mike.morgan%oregonstate.edu <>
2005-12-15 22:23:00 +0000

    Fixed alphabetical order.  :)

--- mozilla/webtools/update/search-engines.php
+++ mozilla/webtools/update/search-engines.php
@@ -101,20 +101,20 @@ require_once(HEADER);
 <dt><img src="./images/search-engines/bbcnews.png" alt=""/> <a href="javascript:addEngine('bbcnews','png','News','0')">BBC News</a></dt><dd>Search for the latest news on BBC.</dd>
 <dt><img src="./images/search-engines/bittorrent.png" alt=""/> <a href="javascript:addEngine('bittorrent','png','File Sharing','0')">BitTorrent</a></dt><dd>Find Torrent files</dd>
 <dt><img src="./images/search-engines/del.icio.us.png" alt=""/> <a href="javascript:addEngine('del.icio.us','png','General','1091')">del.icio.us</a></dt><dd>Search through most bookmarked sites.</dd>
+<dt><img src="./images/search-engines/espn.png" alt=""/> <a href="javascript:addEngine('espn','png','Recreation','0')">ESPN</a></dt><dd>Get the latest sports news, scores, and highlights.</dd>
 <dt><img src="./images/search-engines/freedict.png" alt=""/> <a href="javascript:addEngine('freedict','png','Language dictionary','0')">The Free Dictionary</a></dt><dd>English, Medical, Legal, and Financial Dictionaries, Thesaurus, Encyclopedia and more.</dd>
 </dl>
 </div>
 
 <div class="front-section">
 <dl>
-<dt><img src="./images/search-engines/espn.png" alt=""/> <a href="javascript:addEngine('espn','png','Recreation','0')">ESPN</a></dt><dd>Get the latest sports news, scores, and highlights.</dd>
 <dt><img src="./images/search-engines/flickr-tags.png" alt=""/> <a href="javascript:addEngine('flickr-tags','png','Arts','0')">Flickr Tags</a></dt><dd>Search for photos on Flickr.</dd>
 <dt><img src="./images/search-engines/foodnetwork.png" alt=""/> <a href="javascript:addEngine('foodtv','png','Undefined','4003')">Food Network Recipes</a></dt><dd>Find recipes from your favorite chefs.</dd>
 <dt><img src="./images/search-engines/cddball.png" alt=""/> <a href="javascript:addEngine('cddball','png','CDDB Music Search','0')">gracenote</a></dt><dd>Find album, artist, and song information.</dd>
 <dt><img src="./images/search-engines/IMDB.png" alt=""/> <a href="javascript:addEngine('IMDB','png','Arts','0')">IMDB</a></dt><dd>The Internet Movie Database.</dd>
+<dt><img src="./images/search-engines/linkedin.png" alt=""/> <a href="javascript:addEngine('linkedin','png','Web Services','0')">LinkedIn</a></dt><dd>Search your LinkedIn network for the people you need when you are on any Web site.</dd>
 <dt><img src="./images/search-engines/lonelyplanet.png" alt=""/> <a href="javascript:addEngine('lonelyplanet','png','Travel','0')">Lonely Planet Online</a></dt><dd>Search through Lonely Planet's travel guides.</dd>
 <dt><img src="./images/search-engines/marketwatch.gif" alt=""/> <a href="javascript:addEngine('marketwatch','gif','Business and Economy','4897')">MarketWatch</a></dt><dd>Stock quote look-up and financial information.</dd>
-<dt><img src="./images/search-engines/linkedin.png" alt=""/> <a href="javascript:addEngine('linkedin','png','Web Services','0')">LinkedIn</a></dt><dd>Search your LinkedIn network for the people you need when you are on any Web site.</dd>
 </dl>
 </div>
 
@@ -124,8 +124,8 @@ require_once(HEADER);
 <dt><img src="./images/search-engines/msn.png" alt=""/> <a href="javascript:addEngine('MSN','png','General','3796')">MSN</a></dt><dd>MSN search engine.</dd>
 <dt><img src="./images/search-engines/odeo.png" alt=""/> <a href="javascript:addEngine('odeo','png','Music','2069')">Odeo</a></dt><dd>Explore the world of podcasting.</dd>
 <dt><img src="./images/search-engines/technorati.gif" alt=""/> <a href="javascript:addEngine('technorati-new','gif','Weblogs','2662')">Technorati</a></dt><dd>A real-time search engine that keeps track of what is going on in the blogosphere</dd>
-<dt><img src="./images/search-engines/WebMD.png" alt=""/> <a href="javascript:addEngine('WebMD','png','Health','0')">WebMD</a></dt><dd>WebMD provides health information and tools for managing your health.</dd>
 <dt><img src="./images/search-engines/weatherchannel.png" alt=""/> <a href="javascript:addEngine('weather','png','Weather','0')">Weather Channel</a></dt><dd>Enter city, state or zip code to find your weather information.</dd>
+<dt><img src="./images/search-engines/WebMD.png" alt=""/> <a href="javascript:addEngine('WebMD','png','Health','0')">WebMD</a></dt><dd>WebMD provides health information and tools for managing your health.</dd>
 <dt><img src="./images/search-engines/wikipedia.png" alt=""/> <a href="javascript:addEngine('wikipedia','png','Reference','2286')">Wikipedia</a></dt><dd>The incredible free encyclopedia.</dd>
 <dt><img src="./images/search-engines/yahooligans.png" alt=""/> <a href="javascript:addEngine('yahooligans','png','Kids and Teens','0')">Yahooligans</a></dt><dd>Search engine for kids and teens.</dd>
 </dl>
