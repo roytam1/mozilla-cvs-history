aa6f92aca6e0f88a3927fd4f8f4c79b6cfd8fcaf
joshmoz%gmail.com <>
2006-01-04 01:23:34 +0000

    fix for downloads failing often on dual-cpu machines. b=208843 r=dougt r=wtc sr=darin a=dveditz

--- mozilla/xpcom/io/nsPipe3.cpp
+++ mozilla/xpcom/io/nsPipe3.cpp
@@ -662,14 +662,15 @@ nsPipeInputStream::OnInputException(nsresult reason, nsPipeEvents &events)
 NS_IMETHODIMP_(nsrefcnt)
 nsPipeInputStream::AddRef(void)
 {
-    ++mReaderRefCnt;
+    PR_AtomicIncrement((PRInt32*)&mReaderRefCnt);
     return mPipe->AddRef();
 }
 
 NS_IMETHODIMP_(nsrefcnt)
 nsPipeInputStream::Release(void)
 {
-    if (--mReaderRefCnt == 0)
+    nsrefcnt count = PR_AtomicDecrement((PRInt32 *)&mReaderRefCnt);
+    if (count == 0)
         Close();
     return mPipe->Release();
 }
@@ -1018,14 +1019,15 @@ nsPipeOutputStream::OnOutputException(nsresult reason, nsPipeEvents &events)
 NS_IMETHODIMP_(nsrefcnt)
 nsPipeOutputStream::AddRef()
 {
-    mWriterRefCnt++;
+    PR_AtomicIncrement((PRInt32*)&mWriterRefCnt);
     return mPipe->AddRef();
 }
 
 NS_IMETHODIMP_(nsrefcnt)
 nsPipeOutputStream::Release()
 {
-    if (--mWriterRefCnt == 0)
+    nsrefcnt count = PR_AtomicDecrement((PRInt32 *)&mWriterRefCnt);
+    if (count == 0)
         Close();
     return mPipe->Release();
 }
