d1ed63eee0b8e8234d133b90ac26811b92e7a7d0
bzbarsky%mit.edu <>
2006-01-05 03:49:50 +0000

    Fix wrapper reparenting to work with splitwindow... Bug 318678, r+sr=jst, a=schrep

--- mozilla/content/base/public/nsContentUtils.h
+++ mozilla/content/base/public/nsContentUtils.h
@@ -600,9 +600,8 @@ public:
   
 private:
   static nsresult doReparentContentWrapper(nsIContent *aChild,
-                                           nsIDocument *aNewDocument,
-                                           nsIDocument *aOldDocument,
                                            JSContext *cx,
+                                           JSObject *aOldGlobal,
                                            JSObject *parent_obj);
 
   static nsresult EnsureStringBundle(PropertiesFile aFile);
