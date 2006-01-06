946dac112fa47441bf07c9232829b492c65d75a0
mrbkap%gmail.com <>
2006-01-06 22:02:47 +0000

    bug 316049: Hold strong refs to the shell so that flushing doesn't cause it to go away too early. r+sr=jst a=dveditz

--- mozilla/content/html/document/src/nsHTMLDocument.cpp
+++ mozilla/content/html/document/src/nsHTMLDocument.cpp
@@ -2551,7 +2551,9 @@ nsHTMLDocument::GetWidth(PRInt32* aWidth)
 
   // We make the assumption that the first presentation shell
   // is the one for which we need information.
-  nsIPresShell *shell = GetShellAt(0);
+  // Since GetPixelDimensions flushes and flushing can destroy
+  // our shell, hold a strong ref to it.
+  nsCOMPtr<nsIPresShell> shell = GetShellAt(0);
   if (!shell) {
     return NS_OK;
   }
@@ -2571,7 +2573,9 @@ nsHTMLDocument::GetHeight(PRInt32* aHeight)
 
   // We make the assumption that the first presentation shell
   // is the one for which we need information.
-  nsIPresShell *shell = GetShellAt(0);
+  // Since GetPixelDimensions flushes and flushing can destroy
+  // our shell, hold a strong ref to it.
+  nsCOMPtr<nsIPresShell> shell = GetShellAt(0);
   if (!shell) {
     return NS_OK;
   }
