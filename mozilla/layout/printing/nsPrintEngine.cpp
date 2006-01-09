cf91fc49777f4c4d7e2d07e47afef0fb2649063f
bzbarsky%mit.edu <>
2006-01-09 21:33:00 +0000

    Don't paint selection shading on images when printing. Bug 312415 and bug 319357, r+sr=roc, a=dveditz

--- mozilla/layout/printing/nsPrintEngine.cpp
+++ mozilla/layout/printing/nsPrintEngine.cpp
@@ -2609,6 +2609,10 @@ nsPrintEngine::ReflowPrintObject(nsPrintObject * aPO, PRBool aDoCalcShrink)
     return rv;
   }
 
+  // Don't paint selection stuff for images while printing.
+  // XXXbz should we be painting it for text, even?
+  aPO->mPresShell->SetSelectionFlags(nsISelectionDisplay::DISPLAY_TEXT);
+
   aPO->mStyleSet->EndUpdate();
   
   // The pres shell now owns the style set object.
