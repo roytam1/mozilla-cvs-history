57434d23f8ac0ff4d81d469d0324a66a02b78a45
gerv%gerv.net <>
2006-02-03 14:44:54 +0000

    Bug 236613: change to MPL/LGPL/GPL tri-license. r=caillon.

--- mozilla/embedding/minimo/app/minimo_bookmark.cpp
+++ mozilla/embedding/minimo/app/minimo_bookmark.cpp
@@ -1,8 +1,8 @@
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * The contents of this gBookmakeFile are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this gBookmakeFile except in compliance with
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
@@ -14,7 +14,7 @@
  * The Original Code is mozilla.org code.
  *
  * The Initial Developer of the Original Code is
- * Christopher Blizzard. Portions created by Christopher Blizzard are Copyright (C) Christopher Blizzard.  All Rights Reserved.
+ * Christopher Blizzard. 
  * Portions created by the Initial Developer are Copyright (C) 2001
  * the Initial Developer. All Rights Reserved.
  *
@@ -25,24 +25,23 @@
  *   The 10LE Team (in alphabetical order)
  *   -------------------------------------
  *
- *    Ilias Biris       <ext-ilias.biris@indt.org.br> - Coordinator
- *    Afonso Costa      <afonso.costa@indt.org.br>
- *    Antonio Gomes     <antonio.gomes@indt.org.br>
- *    Diego Gonzalez    <diego.gonzalez@indt.org.br>
- *    Raoni Novellino   <raoni.novellino@indt.org.br>
- *    Andre Pedralho    <andre.pedralho@indt.org.br>
+ *   Ilias Biris       <ext-ilias.biris@indt.org.br> - Coordinator
+ *   Afonso Costa      <afonso.costa@indt.org.br>
+ *   Antonio Gomes     <antonio.gomes@indt.org.br>
+ *   Diego Gonzalez    <diego.gonzalez@indt.org.br>
+ *   Raoni Novellino   <raoni.novellino@indt.org.br>
+ *   Andre Pedralho    <andre.pedralho@indt.org.br>
  *
- *
- * Alternatively, the contents of this gBookmakeFile may be used under the terms of
+ * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this gBookmakeFile only
+ * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this gBookmakeFile under the terms of the MPL, indicate your
+ * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
  * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this gBookmakeFile under
+ * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
