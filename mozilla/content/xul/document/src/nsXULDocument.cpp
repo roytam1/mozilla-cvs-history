b7900cf5100abf50613aec0106c5bd375cba00b8
dveditz%cruzio.com <>
2006-02-07 08:10:32 +0000

    bug 319846 prevent over-long attributes that choke the parser, r=mrbkap, sr=bsmedberg, a=timr

--- mozilla/content/xul/document/src/nsXULDocument.cpp
+++ mozilla/content/xul/document/src/nsXULDocument.cpp
@@ -152,6 +152,7 @@ const nsForwardReference::Phase nsForwardReference::kPasses[] = {
     nsForwardReference::eDone
 };
 
+const PRUint32 kMaxAttributeLength = 4096;
 
 //----------------------------------------------------------------------
 //
@@ -1555,6 +1556,12 @@ nsXULDocument::Persist(nsIContent* aElement, PRInt32 aNameSpaceID,
     nsAutoString valuestr;
     rv = aElement->GetAttr(kNameSpaceID_None, aAttribute, valuestr);
     if (NS_FAILED(rv)) return rv;
+
+    // prevent over-long attributes that choke the parser (bug 319846)
+    // (can't simply Truncate without testing, it's implemented
+    // using SetLength and will grow a short string)
+    if (valuestr.Length() > kMaxAttributeLength)
+        valuestr.Truncate(kMaxAttributeLength);
 
     PRBool novalue = (rv != NS_CONTENT_ATTR_HAS_VALUE);
 
