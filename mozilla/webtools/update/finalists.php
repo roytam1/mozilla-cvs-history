70425012d51d08bca62871743324b2d1a6a0c1cf
mike.morgan%oregonstate.edu <>
2006-02-08 21:44:05 +0000

    Hopefully the last update for this page for a long time.

--- mozilla/webtools/update/finalists.php
+++ mozilla/webtools/update/finalists.php
@@ -100,7 +100,7 @@ You can also easily repeat your search on all engines included in toolbar.",
 '{bbc21d30-1cff-11da-8cd6-0800200c9a66}'=>"Allows sticky notes to be added to any web page, and viewed upon visiting the Web page again.  You can also share sticky notes.  Requires account.",
 '{37E4D8EA-8BDA-4831-8EA1-89053939A250}'=>"PDF Download Extension allows you to choose if you want to view a PDF file inside the browser (as PDF or HTML), if you want to view it outside Firefox with your default or custom PDF reader, or if you want to download it.",
 '{a089fffd-e0cb-431b-8d3a-ebb8afb26dcf}'=>"Platypus is a Firefox extension which lets you modify a Web page from your browser -- \"What You See Is What You Get\" -- and then save those changes as a GreaseMonkey script so that they'll be repeated the next time you visit the page.",
-'Reveal@sourmilk.net'=>"Reveal allows you to see thumbnails of pages in your history by mousing over the back and forward buttons.  With many tabs open, quickly find the page you want, by pressing F2. Reveal also has rectangular magnifying glass you can use to zoom in on areas of any web page. Comes  with a quick tour of all the features. ",
+'Reveal@sourmilk.net'=>"Reveal allows you to see thumbnails of pages in your history by mousing over the back and forward buttons.  With many tabs open, quickly find the page you want, by pressing F2. Reveal also has a rectangular magnifying glass you can use to zoom in on areas of any web page. Comes  with a quick tour of all the features. ",
 '{a6ca9b3b-5e52-4f47-85d8-cca35bb57596}'=>"A lightweight RSS and Atom feed aggregator.  Alt+S to open Sage in the Sidebar to start reading feed content.",
 '{53A03D43-5363-4669-8190-99061B2DEBA5}'=>"Highlight text, create sticky notes, and more to Web pages and Web sites that are saved to your desktop.  Scrapbook Includes full text search and quick filtering of saved pages.",
 'separe@m4ng0.lilik.it'=>"Manage tabs by creating a tab separator.  Right click on a Tab to add a new Tab separator.  Click on the Tab separator to view thumbnail images of web sites that are to the left and right of the Tab separator.",
@@ -144,7 +144,7 @@ $finalists = array();
 
 // Get data for GUIDs.
 $finalists_sql = "
-            SELECT DISTINCT
+            SELECT
                 m.guid,
                 m.id, 
                 m.name, 
@@ -161,12 +161,9 @@ $finalists_sql = "
             JOIN authorxref a ON m.id = a.id
             JOIN userprofiles u ON a.userid = u.userid
             WHERE
-                v.vid = (SELECT max(vid) FROM version WHERE id=m.id) AND
-                approved = 'YES' AND
+                v.vid = (SELECT max(vid) FROM version WHERE id=m.id AND approved='YES') AND
                 type = 'E' AND
                 m.guid IN({$guids_imploded})
-            GROUP BY
-                v.ID
             ORDER BY
                 LTRIM(m.name)
 ";
