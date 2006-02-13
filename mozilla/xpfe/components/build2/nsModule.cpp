2771145534757a63177a852b701b222b228c263c
scott%scott-macgregor.org <>
2006-02-13 20:28:43 +0000

    Bug #239136 --> Thunderbird needs to integrate with the new download manager in toolkit sr=bsmedberg

--- mozilla/xpfe/components/build2/nsModule.cpp
+++ mozilla/xpfe/components/build2/nsModule.cpp
@@ -47,8 +47,6 @@
 #include "nsWindowDataSource.h"
 #include "nsRDFCID.h"
 #include "nsAutoComplete.h"
-#include "nsDownloadManager.h"
-#include "nsDownloadProxy.h"
 
 #if defined(MOZ_LDAP_XPCOM)
 #include "nsLDAPAutoCompleteSession.h"
@@ -66,8 +64,6 @@ NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsWindowDataSource, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAutoCompleteItem)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAutoCompleteResults)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsFontPackageHandler)
-NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsDownloadManager, Init)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsDownloadProxy)
 
 #if defined(MOZ_LDAP_XPCOM)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsLDAPAutoCompleteSession)
@@ -95,9 +91,6 @@ RegisterWindowDS(nsIComponentManager *aCompMgr,
 }
 
 static const nsModuleComponentInfo components[] = {
-    { "Download Manager", NS_DOWNLOADMANAGER_CID, NS_DOWNLOADMANAGER_CONTRACTID, nsDownloadManagerConstructor },
-    { "Download", NS_DOWNLOAD_CID, NS_TRANSFER_CONTRACTID, nsDownloadProxyConstructor },
-
     { "AutoComplete Search Results", NS_AUTOCOMPLETERESULTS_CID, NS_AUTOCOMPLETERESULTS_CONTRACTID,
       nsAutoCompleteResultsConstructor},
     { "AutoComplete Search Item", NS_AUTOCOMPLETEITEM_CID, NS_AUTOCOMPLETEITEM_CONTRACTID,
@@ -122,4 +115,3 @@ static const nsModuleComponentInfo components[] = {
 };
 
 NS_IMPL_NSGETMODULE(application, components)
-
