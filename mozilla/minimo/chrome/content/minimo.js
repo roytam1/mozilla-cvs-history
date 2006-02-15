6f66ece777ec378215c8d79baa8ab9bb828fa5c1
dougt%meer.net <>
2006-02-15 03:04:12 +0000

    Adding in necko 2 for keyword and data support.  fixing crash when adding a space into the gGlobalHistory.  removing unused js components used in webservices.  updating for 013.

--- mozilla/minimo/chrome/content/minimo.js
+++ mozilla/minimo/chrome/content/minimo.js
@@ -1138,15 +1138,18 @@ function URLBarEntered()
       BrowserSetDeck(0,document.getElementById("command_ViewDeckDefault"));
       return;
     }
+
     /* Other normal cases */ 
     
-    var fixedUpURI = gURIFixup.createFixupURI(url, 2 /*fixup url*/ );
-    gGlobalHistory.markPageAsTyped(fixedUpURI);
+    if (gURLBar.value.indexOf(" ") == -1)
+    {
+      var fixedUpURI = gURIFixup.createFixupURI(url, 2 /*fixup url*/ );
+      gGlobalHistory.markPageAsTyped(fixedUpURI);
+      gURLBar.value = fixedUpURI.spec;
+    }
     
-    gURLBar.value = fixedUpURI.spec;
-    
-    loadURI(fixedUpURI.spec);
-    
+    loadURI(gURLBar.value);
+
     content.focus();
   }
   catch(ex) {alert(ex);}
@@ -1455,5 +1458,4 @@ function BrowserHomeBar()  {
 
     if(document.getElementById("homebarcontainer").style.display=="none") document.getElementById("homebarcontainer").style.display="block"; 
     else document.getElementById("homebarcontainer").style.display="none";
-
 }
