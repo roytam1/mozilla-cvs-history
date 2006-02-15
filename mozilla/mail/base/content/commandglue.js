b713fdc1ba21dd5e3e452b05649e0dac13227313
bienvenu%nventure.com <>
2006-02-15 01:09:56 +0000

    fix f6/cntrl tab navigation in 3 pane to skip search bar, patch by mcow, r=bienvenu,sr=mscott 311239

--- mozilla/mail/base/content/commandglue.js
+++ mozilla/mail/base/content/commandglue.js
@@ -633,7 +633,7 @@ function ConvertSortTypeToColumnID(sortKey)
 	    break;
     default:
       dump("unsupported sort key: " + sortKey + "\n");
-      columnID = null;
+      columnID = "dateCol";
       break;
   }
   return columnID;
@@ -910,8 +910,7 @@ function FolderPaneSelectionChange()
                 gPreQuickSearchView.close();
                 gPreQuickSearchView = null;  
               }
-              
-              clearQuickSearchAfterFolderChange();
+              ClearQSIfNecessary();
             }
             ClearMessagePane();
 
