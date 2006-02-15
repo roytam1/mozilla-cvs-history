574dae815c568966abe7d9d39b23e5c47adf4c19
timeless%mozdev.org <>
2006-02-15 09:52:04 +0000

    Bug 320498 Fix NewChannel impls to handle null in argument r=darin sr=darin a=dveditz

--- mozilla/netwerk/protocol/data/src/nsDataHandler.cpp
+++ mozilla/netwerk/protocol/data/src/nsDataHandler.cpp
@@ -117,7 +117,7 @@ nsDataHandler::NewURI(const nsACString &aSpec,
 NS_IMETHODIMP
 nsDataHandler::NewChannel(nsIURI* url, nsIChannel* *result)
 {
-    NS_ENSURE_ARG_POINTER(uri);
+    NS_ENSURE_ARG_POINTER(url);
     nsresult rv;
     
     nsDataChannel* channel;
