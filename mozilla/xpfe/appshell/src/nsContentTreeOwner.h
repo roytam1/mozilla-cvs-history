817af5691ac330d148d5a259b42e87d5057c4c73
bzbarsky%mit.edu <>
2006-02-24 04:58:50 +0000

    Make it possible to target non-primary content <browser>s; specifically ones that are type="content-targetable". Bug 326009, r=bsmedberg, sr=jst, branch181=jst

--- mozilla/xpfe/appshell/src/nsContentTreeOwner.h
+++ mozilla/xpfe/appshell/src/nsContentTreeOwner.h
@@ -55,7 +55,7 @@
 class nsXULWindow;
 class nsSiteWindow2;
 
-class nsContentTreeOwner : public nsIDocShellTreeOwner,
+class nsContentTreeOwner : public nsIDocShellTreeOwner_MOZILLA_1_8_BRANCH,
                                   public nsIBaseWindow,
                                   public nsIInterfaceRequestor,
                                   public nsIWebBrowserChrome,
@@ -72,6 +72,7 @@ public:
    NS_DECL_NSIINTERFACEREQUESTOR
    NS_DECL_NSIWEBBROWSERCHROME
    NS_DECL_NSIWINDOWPROVIDER
+   NS_DECL_NSIDOCSHELLTREEOWNER_MOZILLA_1_8_BRANCH
 
 protected:
    nsContentTreeOwner(PRBool fPrimary);
