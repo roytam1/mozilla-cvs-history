52f4bb9b3fb0a4ec5d0c01873d85cebfa49315ae
masayuki%d-toybox.com <>
2006-02-27 15:11:15 +0000

    fixed mingw bustage by b=179056. patch by Masatoshi Kimura (emk) <VYV03354@nifty.ne.jp> r+sr+branch181=roc

--- mozilla/widget/src/windows/nsWindow.cpp
+++ mozilla/widget/src/windows/nsWindow.cpp
@@ -106,6 +106,11 @@
 #include <imm.h>
 #include "aimm.h"
 
+#include <pbt.h>
+#ifndef PBT_APMRESUMEAUTOMATIC
+#define PBT_APMRESUMEAUTOMATIC 0x0012
+#endif
+
 #include "nsNativeDragTarget.h"
 #include "nsIRollupListener.h"
 #include "nsIMenuRollup.h"
