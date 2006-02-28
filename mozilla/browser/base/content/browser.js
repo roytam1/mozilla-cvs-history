02ea6919baa9f6a7dfeef687c25ed97f1f438d7b
gavin%gavinsharp.com <>
2006-02-28 04:15:36 +0000

    Bug 323537: add DOMLinkAdded event listener in Startup instead of delayedStartup, since delayedStartup is too late to catch some of the events, r=mconnor

--- mozilla/browser/base/content/browser.js
+++ mozilla/browser/base/content/browser.js
@@ -805,6 +805,9 @@ function prepareForStartup()
 
   // hook up UI through progress listener
   gBrowser.addProgressListener(window.XULBrowserWindow, Components.interfaces.nsIWebProgress.NOTIFY_ALL);
+
+  // Initialize the feedhandler
+  FeedHandler.init();
 }
 
 function delayedStartup()
@@ -964,8 +967,6 @@ function delayedStartup()
     document.getElementById("textfieldDirection-separator").hidden = false;
     document.getElementById("textfieldDirection-swap").hidden = false;
   }
-
-  FeedHandler.init();
 }
 
 function BrowserShutdown()
