a28707c213c83fef964bdf019226294d81727b90
dbaron%dbaron.org <>
2006-02-28 14:55:53 +0000

    Rename browser.display.screen_resolution pref, for which we used to have user interface, to layout.css.dpi, so old settings no longer take effect.  b=327406  r+sr=roc  a=roc

--- mozilla/modules/libpref/src/init/all.js
+++ mozilla/modules/libpref/src/init/all.js
@@ -1515,7 +1515,7 @@ pref("network.dns.disableIPv6", true);
 #if XP_OS2
 
 pref("ui.key.menuAccessKeyFocuses", true);
-pref("browser.display.screen_resolution", -1); // System setting
+pref("layout.css.dpi", -1); // max(96dpi, System setting)
 
 /* Fonts only needs lists if we have a default that might not be available. */
 /* Tms Rmn, Helv and Courier are ALWAYS available on OS/2 */
@@ -1788,7 +1788,7 @@ pref("browser.download.dir", "/boot/home/Downloads");
 pref("network.hosts.smtp_server", "localhost");
 pref("network.hosts.pop_server", "pop");
 pref("network.protocol-handler.warn-external.file", false);
-pref("browser.display.screen_resolution", -1); // System setting
+pref("layout.css.dpi", -1); // max(96dpi, System setting)
 pref("browser.drag_out_of_frame_style", 1);
 pref("editor.singleLine.pasteNewlines", 0);
 
