f577ba546b76d4f09bae2676fa0af54b44681de3
mozilla.mano%sent.com <>
2006-03-06 15:22:00 +0000

    Bug 262905 - Add a way to change the backspace key behavior in the content area  (hidden pref browser.backspace_action: 0 = back/foward; 1 = pgup/pgdn; 2 = nothing.). r+a=mconnor.

--- mozilla/browser/base/content/browser.js
+++ mozilla/browser/base/content/browser.js
@@ -1378,7 +1378,26 @@ function BrowserBack(aEvent, aIgnoreAlt)
 
 function BrowserHandleBackspace()
 {
-  BrowserBack();
+  switch (gPrefService.getIntPref("browser.backspace_action")) {
+  case 0:
+    BrowserBack();
+    break;
+  case 1:
+    goDoCommand("cmd_scrollPageUp");
+    break;
+  }
+}
+
+function BrowserHandleShiftBackspace()
+{
+  switch (gPrefService.getIntPref("browser.backspace_action")) {
+  case 0:
+    BrowserForward();
+    break;
+  case 1:
+    goDoCommand("cmd_scrollPageDown");
+    break;
+  }
 }
 
 function BrowserBackMenu(event)
