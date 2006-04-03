3af30d62e179a5be432423b684eccca08204b19f
mrbkap%gmail.com <>
2006-04-03 17:56:13 +0000

    Oops, SAVE_SP_AND_PC doesn't exist on this branch (bug 326281)

--- mozilla/js/src/jsinterp.c
+++ mozilla/js/src/jsinterp.c
@@ -4314,7 +4314,7 @@ js_Interpret(JSContext *cx, jsbytecode *pc, jsval *result)
 
             /* Lookup id in order to check for redeclaration problems. */
             id = ATOM_TO_JSID(atom);
-            SAVE_SP_AND_PC(fp);
+            SAVE_SP(fp);
             ok = js_CheckRedeclaration(cx, obj, id, attrs, &obj2, &prop);
             if (!ok)
                 goto out;
@@ -4440,7 +4440,7 @@ js_Interpret(JSContext *cx, jsbytecode *pc, jsval *result)
              * as well as multiple HTML script tags.
              */
             parent = fp->varobj;
-            SAVE_SP_AND_PC(fp);
+            SAVE_SP(fp);
             ok = js_CheckRedeclaration(cx, parent, id, attrs, NULL, NULL);
             if (ok) {
                 ok = OBJ_DEFINE_PROPERTY(cx, parent, id, rval,
