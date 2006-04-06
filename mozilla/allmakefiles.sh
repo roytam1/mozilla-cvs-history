493b67172f87d2f1914ef66ba23b9067690e7a4e
bent.mozilla%gmail.com <>
2006-04-06 16:28:13 +0000

    Bug 321650 - "error in bsdiff makefile results in compile error of mbsdiff". Adds bsdiff makefiles to allmakefiles.sh (uses the --enable-update-packaging mozconfig option), adds RTL_FLAGS to HOST_CFLAGS globally, and uses HOST_EXTRA_LIBS instead of EXTRA_LIBS in the bsdiff makefile.

--- mozilla/allmakefiles.sh
+++ mozilla/allmakefiles.sh
@@ -1216,7 +1216,10 @@ MAKEFILES_libmar="
 "
 
 if test -n "$MOZ_UPDATE_PACKAGING"; then
-    MAKEFILES_update_packaging="tools/update-packaging/Makefile"
+    MAKEFILES_update_packaging="
+        tools/update-packaging/Makefile
+        other-licenses/bsdiff/Makefile
+    "
 fi
 
 if [ ! "$SYSTEM_PNG" ]; then
