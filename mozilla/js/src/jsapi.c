6b08cdcfc985ed31b31ca816ba2e5c0fba7eb353
bclary%bclary.com <>
2006-04-07 16:07:45 +0000

    Update JS_GetImplementationVersion() date string (bug 67111, r,a=brendan)

--- mozilla/js/src/jsapi.c
+++ mozilla/js/src/jsapi.c
@@ -1064,7 +1064,7 @@ JS_ToggleOptions(JSContext *cx, uint32 options)
 JS_PUBLIC_API(const char *)
 JS_GetImplementationVersion(void)
 {
-    return "JavaScript-C 1.5 pre-release 6a 2004-06-09";
+    return "JavaScript-C 1.6 pre-release 1 2006-04-04";
 }
 
 
