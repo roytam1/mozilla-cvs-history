7aa93a9b28d211ddc88ae803500d2b17c69d8884
gavin%gavinsharp.com <>
2006-04-20 23:25:04 +0000

    Fix regression of bug 331222 caused by patch for bug 331522.

--- mozilla/browser/base/content/browser.js
+++ mozilla/browser/base/content/browser.js
@@ -2935,7 +2935,7 @@ const BrowserSearch = {
   
     if (useNewTab) {
       getBrowser().loadOneTab(submission.uri.spec, null, null,
-                              submission.postData, false);
+                              submission.postData, null, false);
     } else
       loadURI(submission.uri.spec, null, submission.postData, false);
   },
