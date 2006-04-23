0983cce63452c10ba7087fb4071a228b651fb588
brendan%mozilla.org <>
2006-04-23 06:28:02 +0000

    Fixes for 329530, a=dveditz.

--- mozilla/js/src/jsinterp.c
+++ mozilla/js/src/jsinterp.c
@@ -2937,7 +2937,7 @@ js_Interpret(JSContext *cx, jsbytecode *pc, jsval *result)
                 VALUE_TO_PRIMITIVE(cx, rval, JSTYPE_VOID, &sp[-1]);
                 rval = sp[-1];
                 if ((cond = JSVAL_IS_STRING(lval)) || JSVAL_IS_STRING(rval)) {
-                    SAVE_SP(fp);
+                    SAVE_SP_AND_PC(fp);
                     if (cond) {
                         str = JSVAL_TO_STRING(lval);
                         ok = (str2 = js_ValueToString(cx, rval)) != NULL;
