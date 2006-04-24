bdda3e419a0a3a9775e8425f32e6a7263da9786c
dougt%meer.net <>
2006-04-24 18:53:30 +0000

    Adding cmd line support.  minimo-only

--- mozilla/minimo/base/wince/runner.cpp
+++ mozilla/minimo/base/wince/runner.cpp
@@ -1,6 +1,13 @@
 #include <windows.h>
 
-
+void SentURLLoadRequest(const char* url)
+{
+  // This will change in the future -- this window is for
+  // the use of minimo_runner.exe only.
+  COPYDATASTRUCT cds = { 0, ::strlen( url ) + 1, (void*)url };
+  HWND a = FindWindowW(L"MINIMO_LISTENER", NULL);
+  SendMessage(a, WM_COPYDATA, NULL, (LPARAM)&cds); 
+}
 
 
 int main(int argc, char *argv[])
@@ -8,39 +15,36 @@ int main(int argc, char *argv[])
   SetCursor(LoadCursor(NULL,IDC_WAIT));
 
   HWND h = FindWindowW(NULL, L"Minimo");
-  if (h)
+  if (!h)
   {
-    ShowWindow(h, SW_SHOWNORMAL);
+    char *cp;
+    char exe[MAX_PATH];
+    GetModuleFileName(GetModuleHandle(NULL), exe, sizeof(exe));
+    cp = strrchr(exe,'\\');
+    if (cp != NULL)
+    {
+      cp++; // pass the \ char.
+      *cp = 0;
+    }
+    strcat(exe, "minimo.exe");
+    
+    PROCESS_INFORMATION pi;
+    CreateProcess(exe, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, &pi);
+  }
+  else
+  {
     SetForegroundWindow(h);
-    return 0;
+    ShowWindow(h, SW_SHOWNORMAL);
   }
 
-  char *cp;
-  char exe[MAX_PATH];
-  GetModuleFileName(GetModuleHandle(NULL), exe, sizeof(exe));
-  cp = strrchr(exe,'\\');
-  if (cp != NULL)
+  if (argc == 3)
   {
-    cp++; // pass the \ char.
-    *cp = 0;
+    if (!strcmp("-url", argv[1]))
+      SentURLLoadRequest(argv[2]);
   }
-  
-  strcat(exe, "minimo.exe");
-
-  PROCESS_INFORMATION pi;
-
-  BOOL b = CreateProcess(exe,
-                         NULL,
-                         NULL,
-                         NULL,
-                         NULL,
-                         0,
-                         NULL,
-                         NULL,
-                         NULL,
-                         &pi);
 
   SetCursor(NULL);
   return 0;
 }
 
+ 
