006ed99d6c78e1bfe717304210a8134838dc80eb
gijskruitbosch%gmail.com <>
2006-04-25 14:40:48 +0000

    Bug 153232 - Missing break; for BAD_SIGNATURE case r+sr = Kai Engert

--- mozilla/security/manager/ssl/src/nsNSSIOLayer.cpp
+++ mozilla/security/manager/ssl/src/nsNSSIOLayer.cpp
@@ -725,6 +725,7 @@ nsHandleSSLError(nsNSSSocketInfo *socketInfo, PRInt32 err)
     params[0] = hostNameU.get();
     nssComponent->PIPBundleFormatStringFromName("PeersCertBadSignature", 
                                                 params, 1, formattedString);
+    break;
 
   //A generic error handler for peer cert
   case SEC_ERROR_UNKNOWN_CERT:
