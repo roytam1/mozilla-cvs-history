8bcd10fba92bd44215de606288833bd63c27a884
cls%seawood.org <>
2006-05-15 17:15:57 +0000

    Fix mingw cross-compile & static build bustage Bug #333941 r,a=bsmedberg

--- mozilla/config/static-config.mk
+++ mozilla/config/static-config.mk
@@ -125,7 +125,12 @@ STATIC_EXTRA_LIBS	+= $(MOZ_XPCOM_OBSOLETE_LIBS)
 endif
 
 ifeq ($(OS_ARCH),WINNT)
-STATIC_EXTRA_LIBS += $(call EXPAND_LIBNAME,comctl32 comdlg32 uuid shell32 ole32 oleaut32 Urlmon version winspool)
+STATIC_EXTRA_LIBS += $(call EXPAND_LIBNAME,comctl32 comdlg32 uuid shell32 ole32 oleaut32 version winspool)
+ifdef GNU_CXX
+STATIC_EXTRA_LIBS += -lgdi32
+else
+STATIC_EXTRA_LIBS += urlmon
+endif
 endif
 
 ifeq ($(OS_ARCH),AIX)
