dbaf92eca9467c89800fe9764ba0d64e75d3d711
cltbld <>
2006-05-29 05:13:30 +0000

    For the Firefox 2.0alpha3 release, redirect client.mk onto the FIREFOX_2_0a3_RELEASE tag (take 2)

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -248,11 +248,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = MOZILLA_1_8_BRANCH
-NSPR_CO_TAG          = MOZILLA_1_8_BRANCH
-NSS_CO_TAG           = MOZILLA_1_8_BRANCH
-LDAPCSDK_CO_TAG      = MOZILLA_1_8_BRANCH
-LOCALES_CO_TAG       = MOZILLA_1_8_BRANCH
+MOZ_CO_TAG           = FIREFOX_2_0a3_RELEASE
+NSPR_CO_TAG          = FIREFOX_2_0a3_RELEASE
+NSS_CO_TAG           = FIREFOX_2_0a3_RELEASE
+LDAPCSDK_CO_TAG      = FIREFOX_2_0a3_RELEASE
+LOCALES_CO_TAG       = FIREFOX_2_0a3_RELEASE
 
 BUILD_MODULES = all
 
