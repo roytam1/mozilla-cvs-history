aa3a62457487a26399614e42f5a43b0a6fb11e90
mark%moxienet.com <>
2006-06-02 20:38:52 +0000

    Running numbers for 1.0.2

--- mozilla/camino/resources/application/ChimeraVersion.r
+++ mozilla/camino/resources/application/ChimeraVersion.r
@@ -51,14 +51,14 @@
 // Version Numbers //
 
 #define     VERSION_MAJOR           1
-#define     VERSION_MINOR           0x01    // revision & fix in BCD
-#define     VERSION_KIND            alpha   // alpha, beta, or final
+#define     VERSION_MINOR           0x02    // revision & fix in BCD
+#define     VERSION_KIND            final   // alpha, beta, or final
 #define     VERSION_MICRO           0       // internal stage: alpha or beta number
 
 
 // Version Strings (Finder's Get Info dialog box) //
 
-#define     VERSION_STRING          "1.0.0+"
+#define     VERSION_STRING          "1.0.2"
 
 #define     COPYRIGHT_STRING        "© 1998-2006 Contributors"
 #define     GETINFO_VERSION         VERSION_STRING ", " COPYRIGHT_STRING
