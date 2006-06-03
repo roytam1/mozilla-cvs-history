c53e2aadfda9cb22eb935c6e2faf1c6181bc4e7e
cst%andrew.cmu.edu <>
2006-06-03 18:37:38 +0000

    Bug 339728 Sync xpfe/tookit config.xul and config.js - add an id to an hbox and explicitly save the pref file after each change r+sr=neil a=KaiRo

--- mozilla/xpfe/global/resources/content/config.js
+++ mozilla/xpfe/global/resources/content/config.js
@@ -554,6 +554,8 @@ function ModifyPref(entry)
     }
   }
 
+  gPrefService.savePrefFile(null);
+
   // Fire event for accessibility
   var event = document.createEvent('Events');
   event.initEvent('NameChange', false, true);
