ca0ba5409510ab36d8ea7941f8da18a5c6745e6d
Olli.Pettay%helsinki.fi <>
2006-06-07 07:36:09 +0000

    Bug 336978, Crash when window gets destroyed on pagehide event, r+sr=bryner, a=dveditz

--- mozilla/layout/base/nsDocumentViewer.cpp
+++ mozilla/layout/base/nsDocumentViewer.cpp
@@ -1184,6 +1184,9 @@ DocumentViewerImpl::PageHide(PRBool aIsUnload)
   if (!aIsUnload)
     return NS_OK;
 
+  // if Destroy() was called during OnPageHide(), mDocument is nsnull.
+  NS_ENSURE_STATE(mDocument);
+
   // First, get the script global object from the document...
   nsIScriptGlobalObject *global = mDocument->GetScriptGlobalObject();
 
