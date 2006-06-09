ee546c7856e278d09009494ce79036c2b45b5b9d
ginn.chen%sun.com <>
2006-06-09 09:13:57 +0000

    Bug 338234 Name of preference tab can not be reported by screen readers patch by evan.yan at sun.com r=aaronleventhal sr=roc a=aaronleventhal

--- mozilla/accessible/src/base/nsRootAccessible.cpp
+++ mozilla/accessible/src/base/nsRootAccessible.cpp
@@ -782,17 +782,12 @@ NS_IMETHODIMP nsRootAccessible::HandleEvent(nsIDOMEvent* aEvent)
                               accessible, nsnull);
   }
   else if (eventType.LowerCaseEqualsLiteral("radiostatechange") ) {
-    // first the XUL radio buttons
-    if (targetNode &&
-        NS_SUCCEEDED(mAccService->GetAccessibleInShell(targetNode, eventShell,
-                                                       getter_AddRefs(accessible)))) {
-      privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_STATE_CHANGE, 
-                                accessible, nsnull);
+    privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_STATE_CHANGE, 
+                              accessible, nsnull);
+    PRUint32 finalState;
+    accessible->GetFinalState(&finalState);
+    if (finalState & (STATE_CHECKED | STATE_SELECTED)) {
       FireAccessibleFocusEvent(accessible, targetNode, aEvent);
-    }
-    else { // for the html radio buttons -- apparently the focus code just works. :-)
-      privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_STATE_CHANGE, 
-                                accessible, nsnull);
     }
   }
   else if (eventType.LowerCaseEqualsLiteral("dommenubaractive"))
@@ -856,6 +851,24 @@ NS_IMETHODIMP nsRootAccessible::HandleEvent(nsIDOMEvent* aEvent)
         privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_ATK_LINK_SELECTED, accessible, &selectedLink);
       }
     }
+    else if (localName.EqualsIgnoreCase("radiogroup")) {
+      // fire focus event for checked radio instead of radiogroup
+      PRInt32 childCount = 0;
+      accessible->GetChildCount(&childCount);
+      nsCOMPtr<nsIAccessible> radioAcc;
+      for (PRInt32 index = 0; index < childCount; index++) {
+        accessible->GetChildAt(index, getter_AddRefs(radioAcc));
+        if (radioAcc) {
+          radioAcc->GetFinalState(&stateData.state);
+          if (stateData.state & (STATE_CHECKED | STATE_SELECTED)) {
+            break;
+          }
+        }
+      }
+      if (radioAcc) {
+        privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_FOCUS, radioAcc, nsnull);
+      }
+    }
     else
       FireAccessibleFocusEvent(accessible, targetNode, aEvent);
   }
@@ -886,10 +899,13 @@ NS_IMETHODIMP nsRootAccessible::HandleEvent(nsIDOMEvent* aEvent)
   else if (eventType.LowerCaseEqualsLiteral("checkboxstatechange") || // it's a XUL <checkbox>
            eventType.LowerCaseEqualsLiteral("radiostatechange")) { // it's a XUL <radio>
     accessible->GetFinalState(&stateData.state);
-    stateData.enable = (stateData.state & STATE_CHECKED) != 0;
+    // prefPane tab is implemented as list items in A11y, so we need to check STATE_SELECTED also
+    stateData.enable = (stateData.state & (STATE_CHECKED | STATE_SELECTED)) != 0;
     stateData.state = STATE_CHECKED;
     privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_STATE_CHANGE, accessible, &stateData);
-    if (eventType.LowerCaseEqualsLiteral("radiostatechange")) {
+    // only fire focus event for checked radio
+    if (eventType.LowerCaseEqualsLiteral("radiostatechange") &&
+        stateData.enable) {
       FireAccessibleFocusEvent(accessible, targetNode, aEvent);
     }
   }
@@ -919,7 +935,12 @@ NS_IMETHODIMP nsRootAccessible::HandleEvent(nsIDOMEvent* aEvent)
     FireAccessibleFocusEvent(accessible, targetNode, aEvent);
   }
   else if (eventType.EqualsLiteral("DOMMenuInactive")) {
-    //FireAccessibleFocusEvent(accessible, targetNode);  // Not yet used in ATK
+    nsCOMPtr<nsIDOMXULPopupElement> popup(do_QueryInterface(targetNode));
+    if (popup) {
+      privAcc->FireToolkitEvent(nsIAccessibleEvent::EVENT_MENUPOPUPEND,
+                                accessible, nsnull);
+    }
+
   }
 #endif
   return NS_OK;
