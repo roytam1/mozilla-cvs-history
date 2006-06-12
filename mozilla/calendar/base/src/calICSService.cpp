0167d98474b957aa329884f5b1327d88c9b856ff
michael.buettner%sun.com <>
2006-06-12 14:35:04 +0000

    Bug 330573 Added TZID information to EXDATE attribute. r=dmose

--- mozilla/calendar/base/src/calICSService.cpp
+++ mozilla/calendar/base/src/calICSService.cpp
@@ -67,6 +67,12 @@ calIcalProperty::GetIcalProperty()
     return mProperty;
 }
 
+icalcomponent*
+calIcalProperty::GetIcalComponent()
+{
+    return mParent->GetIcalComponent();
+}
+
 NS_IMETHODIMP
 calIcalProperty::GetValue(nsACString &str)
 {
@@ -1186,7 +1192,7 @@ calICSService::GetTimezone(const nsACString& tzid,
     // not found, we need to locate it
     const ical_timezone_data_struct *tzdata = get_timezone_data_struct_for_tzid(nsPromiseFlatCString(tzid).get());
     if (!tzdata)
-        return NS_ERROR_FAILURE;
+        return calIErrors::INVALID_TIMEZONE;
 
     // found it
     nsresult rv;
