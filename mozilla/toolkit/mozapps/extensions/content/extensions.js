c1e6717ef35ef85987666f91cf47ec41c2876da1
rob_strong%exchangecode.com <>
2006-06-13 18:09:54 +0000

    Bug 337866 - Update not found notification should have regular close button. r=bsmedberg, approval-branch-1.8.1=bsmedberg

--- mozilla/toolkit/mozapps/extensions/content/extensions.js
+++ mozilla/toolkit/mozapps/extensions/content/extensions.js
@@ -774,9 +774,7 @@ UpdateCheckListener.prototype = {
       var addonsMsg = document.getElementById("addonsMsg");
       addonsMsg.showMessage("chrome://mozapps/skin/extensions/question.png",
                             getExtensionString("noUpdatesMsg"),
-                            getExtensionString("closeButtonLabel"),
-                            getExtensionString("closeButtonAccesskey"),
-                            false, "addons-no-updates");
+                            null, null, true, "addons-no-updates");
     }
   },
   
