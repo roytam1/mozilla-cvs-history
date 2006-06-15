1a89bc3e3065048582a33dcdc7646c26da2387df
bienvenu%nventure.com <>
2006-06-15 21:44:35 +0000

    fix 265472 support for expunging after every imap delete

--- mozilla/mailnews/imap/src/nsImapProtocol.cpp
+++ mozilla/mailnews/imap/src/nsImapProtocol.cpp
@@ -319,6 +319,7 @@ static PRBool gHideUnusedNamespaces = PR_TRUE;
 static PRBool gHideOtherUsersFromList = PR_FALSE;
 static PRBool gUseEnvelopeCmd = PR_FALSE;
 static PRBool gUseLiteralPlus = PR_TRUE;
+static PRBool gExpungeAfterDelete = PR_FALSE;
 static PRBool gCheckDeletedBeforeExpunge = PR_FALSE; //bug 235004
 static PRInt32 gResponseTimeout = 60;
 
@@ -343,6 +344,7 @@ nsresult nsImapProtocol::GlobalInitialization()
     prefBranch->GetBoolPref("mail.imap.use_envelope_cmd",
                             &gUseEnvelopeCmd);
     prefBranch->GetBoolPref("mail.imap.use_literal_plus", &gUseLiteralPlus);
+    prefBranch->GetBoolPref("mail.imap.expunge_after_delete", &gExpungeAfterDelete);
     prefBranch->GetBoolPref("mail.imap.check_deleted_before_expunge", &gCheckDeletedBeforeExpunge);
     prefBranch->GetIntPref("mailnews.tcptimeout", &gResponseTimeout);
     return NS_OK;
@@ -2433,7 +2435,10 @@ void nsImapProtocol::ProcessSelectedStateURL()
               Store(messageIdString, "+FLAGS (\\Deleted \\Seen)",
                 bMessageIdsAreUids); 
               PRBool storeSuccessful = GetServerStateParser().LastCommandSuccessful();
-              
+
+              if (gExpungeAfterDelete && storeSuccessful)
+                Expunge();
+
               if (m_imapMailFolderSink)
               {
                 copyState = storeSuccessful ? (ImapOnlineCopyState) ImapOnlineCopyStateType::kSuccessfulDelete 
@@ -2475,7 +2480,11 @@ void nsImapProtocol::ProcessSelectedStateURL()
               {
                 Store(messageIdString, "+FLAGS (\\Deleted \\Seen)",bMessageIdsAreUids); 
                 if (GetServerStateParser().LastCommandSuccessful())
+                {
                   copyStatus = ImapOnlineCopyStateType::kSuccessfulDelete;
+                  if (gExpungeAfterDelete)
+                    Expunge();
+                }
                 else
                   copyStatus = ImapOnlineCopyStateType::kFailedDelete;
                 
