2a79cd3f16d2434eab9dc78238ca594bab7f8fa6
bugzilla%standard8.demon.co.uk <>
2006-07-01 15:46:42 +0000

    Bug 341142 Search Addresses dialog provides incorrect "Match all messages" option. r/sr=Neil,a=CTho for seamonkey only checkin

--- mozilla/mailnews/base/search/resources/content/ABSearchDialog.js
+++ mozilla/mailnews/base/search/resources/content/ABSearchDialog.js
@@ -114,6 +114,8 @@ function initializeSearchWindowWidgets()
   gPropertiesButton = document.getElementById("propertiesButton");
   gComposeButton = document.getElementById("composeButton");
   gStatusText = document.getElementById('statusText');
+  // matchAll doesn't make sense for address book search
+  hideMatchAllItem();
 }
 
 function onSearchStop() 
