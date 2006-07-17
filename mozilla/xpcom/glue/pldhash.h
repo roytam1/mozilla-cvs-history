f91d0282a92e949bcd80b59601ac2d1973dd8b7a
daniel.boelzle%sun.com <>
2006-07-17 17:14:42 +0000

    Bug 344105: no msvc __fastcall on MOZILLA_1_8_BRANCH r=bsmedberg

--- mozilla/xpcom/glue/pldhash.h
+++ mozilla/xpcom/glue/pldhash.h
@@ -48,8 +48,6 @@ PR_BEGIN_EXTERN_C
 
 #if defined(__GNUC__) && defined(__i386__) && (__GNUC__ >= 3) && !defined(XP_OS2)
 #define PL_DHASH_FASTCALL __attribute__ ((regparm (3),stdcall))
-#elif defined(XP_WIN)
-#define PL_DHASH_FASTCALL __fastcall
 #else
 #define PL_DHASH_FASTCALL
 #endif
