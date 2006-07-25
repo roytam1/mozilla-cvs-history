b7198fcf8e39ec64b4701f1ef1027c749e737a57
dveditz%cruzio.com <>
2006-07-25 01:04:13 +0000

    Bug 343473: UniversalXPConnect privs should allow access to chrome DOM nodes (regression from bug 340107). r=bzbarsky, sr=sicking, a=mconnor

--- mozilla/content/base/src/nsContentUtils.cpp
+++ mozilla/content/base/src/nsContentUtils.cpp
@@ -693,16 +693,17 @@ nsContentUtils::CanCallerAccess(nsIDOMNode *aNode)
     return PR_TRUE;
   }
 
+  PRBool enabled = PR_FALSE;
   if (principal == systemPrincipal) {
-      // we already know subjectPrincipal isn't the systemPrincipal so if
-      // the object principal is they cannot match. Bail out now to
-      // avoid wasting time in CheckSameOriginPrincipal
+    // we know subjectPrincipal != systemPrincipal so we can only
+    // access the object if UniversalXPConnect is enabled. We can
+    // avoid wasting time in CheckSameOriginPrincipal
 
-      return PR_FALSE;
+    rv = sSecurityManager->IsCapabilityEnabled("UniversalXPConnect", &enabled);
+    return NS_SUCCEEDED(rv) && enabled;
   }
 
-  rv = sSecurityManager->CheckSameOriginPrincipal(subjectPrincipal,
-                                                  principal);
+  rv = sSecurityManager->CheckSameOriginPrincipal(subjectPrincipal, principal);
   if (NS_SUCCEEDED(rv)) {
     return PR_TRUE;
   }
@@ -710,11 +711,8 @@ nsContentUtils::CanCallerAccess(nsIDOMNode *aNode)
   // see if the caller has otherwise been given the ability to touch
   // input args to DOM methods
 
-  PRBool enabled = PR_FALSE;
-  rv = sSecurityManager->IsCapabilityEnabled("UniversalBrowserRead",
-                                             &enabled);
-  NS_ENSURE_SUCCESS(rv, PR_FALSE);
-  return enabled;
+  rv = sSecurityManager->IsCapabilityEnabled("UniversalBrowserRead", &enabled);
+  return NS_SUCCEEDED(rv) && enabled;
 }
 
 //static
