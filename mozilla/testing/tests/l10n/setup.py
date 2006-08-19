9d5d83b59fbc5717c951cbd98120255bee69cfdb
cvs2git <>
2006-08-19 00:34:11 +0000

    This commit was manufactured by cvs2svn to create tag 'AUS2_RTM_200608081512'.

--- mozilla/testing/tests/l10n/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name="l10n-tools",
-      version="0.1",
-      author="Axel Hecht",
-      author_email="l10n@mozilla.com",
-      scripts=['scripts/compare-locales'],
-      package_dir={'': 'lib'},
-      packages=['Mozilla']
-      )
