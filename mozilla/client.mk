bb0e41cfaba700b0ad4c54d95f88c7ee11e55307
dbaron%dbaron.org <>
2006-08-23 04:57:53 +0000

    Reverting previous change.

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -350,7 +350,10 @@ MOZCONFIG_FINDER := mozilla/build/autoconf/mozconfig-find
 MOZCONFIG_MODULES := mozilla/build/unix/modules.mk mozilla/build/unix/uniq.pl
 run_for_side_effects := \
   $(shell cd $(ROOTDIR); \
-     $(CVSCO) $(MOZCONFIG_FINDER) $(MOZCONFIG_LOADER) $(MOZCONFIG_MODULES); \
+     if test "$(_IS_FIRST_CHECKOUT)"; then \
+        $(CVSCO) $(MOZCONFIG_FINDER) $(MOZCONFIG_LOADER) $(MOZCONFIG_MODULES); \
+     else true; \
+     fi; \
      $(MOZCONFIG_LOADER) $(TOPSRCDIR) mozilla/.mozconfig.mk > mozilla/.mozconfig.out)
 include $(TOPSRCDIR)/.mozconfig.mk
 include $(TOPSRCDIR)/build/unix/modules.mk
