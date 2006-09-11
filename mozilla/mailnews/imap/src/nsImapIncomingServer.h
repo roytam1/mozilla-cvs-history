8151da8f690b23d8076cb582853b05f3ae7dc7a0
bienvenu%nventure.com <>
2006-09-11 18:47:13 +0000

    close cached imap connections when socket type for a server changes, sr=mscott 338243

--- mozilla/mailnews/imap/src/nsImapIncomingServer.h
+++ mozilla/mailnews/imap/src/nsImapIncomingServer.h
@@ -98,6 +98,7 @@ public:
   NS_IMETHOD GetNumIdleConnections(PRInt32 *aNumIdleConnections);
   NS_IMETHOD ForgetSessionPassword();
   NS_IMETHOD GetMsgFolderFromURI(nsIMsgFolder *aFolderResource, const char *aURI, nsIMsgFolder **aFolder);
+  NS_IMETHOD SetSocketType(PRInt32 aSocketType);
 
 protected:
 	nsresult GetFolder(const char* name, nsIMsgFolder** pFolder);
