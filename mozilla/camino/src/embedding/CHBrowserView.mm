4eb0e227fd5c6b2fb672efa1f9815b40deb55f1f
stridey%gmail.com <>
2006-09-12 03:55:12 +0000

    Camino only: Bug 181712 - Menu item for default font size.  Also bug 345657 - Sync validation for cmd-+ and cmd-=.  r=smorgan sr=pink

--- mozilla/camino/src/embedding/CHBrowserView.mm
+++ mozilla/camino/src/embedding/CHBrowserView.mm
@@ -124,6 +124,7 @@ typedef unsigned int DragReference;
 const char kPersistContractID[] = "@mozilla.org/embedding/browser/nsWebBrowserPersist;1";
 const char kDirServiceContractID[] = "@mozilla.org/file/directory_service;1";
 
+#define DEFAULT_TEXT_ZOOM 1.0f
 #define MIN_TEXT_ZOOM 0.01f
 #define MAX_TEXT_ZOOM 20.0f
 
@@ -1108,14 +1109,24 @@ const long NSFindPanelActionSetFindString = 7;
   return [self isCommandEnabled: "cmd_redo"];
 }
 
-- (void)biggerTextSize
+- (void)makeTextBigger
 {
   [self incrementTextZoom:0.25 min:MIN_TEXT_ZOOM max:MAX_TEXT_ZOOM];
 }
 
-- (void)smallerTextSize
+- (void)makeTextSmaller
 {
   [self incrementTextZoom:-0.25 min:MIN_TEXT_ZOOM max:MAX_TEXT_ZOOM];
+}
+
+- (void)makeTextDefaultSize
+{
+  nsCOMPtr<nsIContentViewer> contentViewer = dont_AddRef([self getContentViewer]);
+  nsCOMPtr<nsIMarkupDocumentViewer> markupViewer(do_QueryInterface(contentViewer));
+  if (!markupViewer)
+    return;
+
+  markupViewer->SetTextZoom(DEFAULT_TEXT_ZOOM);
 }
 
 - (BOOL)canMakeTextBigger
@@ -1128,6 +1139,11 @@ const long NSFindPanelActionSetFindString = 7;
 {
   float zoom = [self getTextZoom];
   return zoom > MIN_TEXT_ZOOM;
+}
+
+- (BOOL)isTextDefaultSize
+{
+  return (fabsf([self getTextZoom] - DEFAULT_TEXT_ZOOM) < .001);
 }
 
 - (void)moveToBeginningOfDocument:(id)sender
@@ -1254,10 +1270,10 @@ const long NSFindPanelActionSetFindString = 7;
 
 - (float)getTextZoom
 {
-  nsCOMPtr<nsIContentViewer> contentViewer = getter_AddRefs([self getContentViewer]);
+  nsCOMPtr<nsIContentViewer> contentViewer = dont_AddRef([self getContentViewer]);
   nsCOMPtr<nsIMarkupDocumentViewer> markupViewer(do_QueryInterface(contentViewer));
   if (!markupViewer)
-    return 1.0;
+    return DEFAULT_TEXT_ZOOM;
 
   float zoom;
   markupViewer->GetTextZoom(&zoom);
@@ -1266,7 +1282,7 @@ const long NSFindPanelActionSetFindString = 7;
 
 - (void)incrementTextZoom:(float)increment min:(float)min max:(float)max
 {
-  nsCOMPtr<nsIContentViewer> contentViewer = getter_AddRefs([self getContentViewer]);
+  nsCOMPtr<nsIContentViewer> contentViewer = dont_AddRef([self getContentViewer]);
   nsCOMPtr<nsIMarkupDocumentViewer> markupViewer(do_QueryInterface(contentViewer));
   if (!markupViewer)
     return;
