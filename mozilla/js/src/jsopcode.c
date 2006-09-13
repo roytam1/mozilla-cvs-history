cdae2021da9df8b590b5e122e185ca6c7ab2b2be
brendan%mozilla.org <>
2006-09-13 05:44:07 +0000

    Fix 352269, a=schrep.

--- mozilla/js/src/jsopcode.c
+++ mozilla/js/src/jsopcode.c
@@ -1784,6 +1784,7 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb)
 
 #if JS_HAS_GENERATORS
               case JSOP_YIELD:
+                op = JSOP_SETNAME;      /* turn off most parens */
                 rval = POP_STR();
                 todo = (*rval != '\0')
                        ? Sprint(&ss->sprinter,
