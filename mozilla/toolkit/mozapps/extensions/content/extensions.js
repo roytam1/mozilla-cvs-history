286dd217a0a6513a87b4339cf395306652da1da2
gavin%gavinsharp.com <>
2006-09-18 16:46:51 +0000

    Bug 353157: Extension descriptions aren't restored after dismissing notification, patch by Michael Wu <flamingice@sourmilk.net>, r+a=mconnor

--- mozilla/toolkit/mozapps/extensions/content/extensions.js
+++ mozilla/toolkit/mozapps/extensions/content/extensions.js
@@ -499,7 +499,7 @@ function flushDataSource()
 function noUpdatesDismiss(aEvent)
 {
   window.removeEventListener("command", noUpdatesDismiss, true);
-  if (aEvent.target.localName == "notification")
+  if (aEvent.target.localName != "notification")
     return;
 
   var children = gExtensionsView.children;
@@ -508,7 +508,6 @@ function noUpdatesDismiss(aEvent)
     if (child.hasAttribute("updateStatus"))
       child.removeAttribute("updateStatus");
   }
-  document.getElementById("addonsMsg").removeCurrentNotification();
 }
 
 function setRestartMessage(aItem)
