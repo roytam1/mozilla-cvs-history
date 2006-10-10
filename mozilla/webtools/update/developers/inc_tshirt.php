41ad63d786e4d1e423d0adb2737f36a7eac89681
mike.morgan%oregonstate.edu <>
2006-10-10 22:57:10 +0000

    Patch v4 for bug 241858 to add support for storing tshirt information.

--- mozilla/webtools/update/developers/inc_tshirt.php
+++ mozilla/webtools/update/developers/inc_tshirt.php
@@ -1,24 +1,60 @@
 <?php
 require_once('../core/inc_version_comparison.php');
 
-function tshirtEligible() {
-    $userid = $_SESSION['uid'];
-    
-    if ($_SESSION['level'] == "admin" || $_SESSION['level'] == "editor") {
-        return true;
+/**
+ * Returns the eligibility of the current user to get a tshirt
+ * If $showDetails is false, the function will return true or false
+ * If $showDetails is true, the function will return an array
+ * @param boolean $showDetails
+ */
+function tshirtEligible($showDetails = false) {
+    $userid = mysql_real_escape_string($_SESSION['uid']);
+    $details = array();
+
+    if ($_SESSION['level'] == "admin") {
+        if ($showDetails == true) {
+            $details[] = 'AMO Administrator';
+        }
+        else {
+            return true;
+        }
     }
 
-    $addons = mysql_query("SELECT * FROM authorxref WHERE UserID='{$userid}'");
+    if ($_SESSION['level'] == "editor") {
+        $checkqry = mysql_query("SELECT COUNT(*) AS `num` FROM `approvallog` WHERE `approvallog`.`action`!='Approval?' AND `approvallog`.`date` > '2006-07-09 00:00:00' AND `approvallog`.`UserID`='{$userid}'"); 
+        $check = mysql_fetch_array($checkqry);
+
+        if ($check['num'] >= 20) {
+            if ($showDetails == true) {
+                $details[] = 'Active AMO Reviewer ('.$check['num'].')';
+            }
+            else {
+                return true;
+            }
+        }
+    }
+
+/*    $addons = mysql_query("SELECT * FROM `main` LEFT JOIN `authorxref` ON `main`.`ID`=`authorxref`.`ID` WHERE `authorxref`.`UserID`='{$userid}'");
     while($row = mysql_fetch_array($addons)) {
         $latestversionqry = mysql_query("SELECT * FROM version WHERE ID='".$row['ID']."' AND AppID=1 AND DateAdded<'2006-10-01 00:00:00' AND MaxAppVer!='' ORDER BY DateAdded DESC LIMIT 1");
         $latestversion = mysql_fetch_array($latestversionqry);
 
         if (mysql_num_rows($latestversionqry) > 0 && NS_CompareVersions($latestversion['MaxAppVer'], '2.0') == 1) {
-            return true;
+            if ($showDetails == false) {
+                return true;
+            }
+            else {
+                $details[] = $row['Name'].' ('.$latestversion['MaxAppVer'].' - '.$latestversion['DateAdded'].')';
+            }
         }
-    }
+    }*/
     
-    return false;
+    if ($showDetails == true) {
+        return $details;
+    }
+    else {
+        return false;
+    }
 }
 
 ?>
--- mozilla/webtools/update/developers/tshirt.php
+++ mozilla/webtools/update/developers/tshirt.php
@@ -17,18 +17,21 @@ $userid = mysql_real_escape_string($_SESSION['uid']);
 
 if (isset($_POST['submit'])) {
     $name = mysql_real_escape_string($_POST['name']);
+    $phone = mysql_real_escape_string($_POST['phone']);
     $address1 = mysql_real_escape_string($_POST['address1']);
     $address2 = mysql_real_escape_string($_POST['address2']);
     $address3 = mysql_real_escape_string($_POST['address3']);
     $size = mysql_real_escape_string($_POST['size']);
+
+    $eligibility = implode(', ', tshirtEligible(true));
     
-    if (!empty($address1) && !empty($size)) {
-        mysql_query("UPDATE userprofiles SET UserName='{$name}', UserAddress1='{$address1}', UserAddress2='{$address2}', UserAddress3='{$address3}', UserTShirtSize='{$size}' WHERE UserID='{$userid}'");
+    if (!empty($address1) && !empty($size) && !empty($phone) && !empty($name)) {
+        mysql_query("UPDATE userprofiles SET UserRealName='{$name}', UserAddress1='{$address1}', UserAddress2='{$address2}', UserAddress3='{$address3}', UserTShirtSize='{$size}', UserPhone='{$phone}', UserTShirtDetails='{$eligibility}' WHERE UserID='{$userid}'");
     
         $message = 'Your request has been submitted! Please be patient for delivery.';
     }
     else {
-        $message = 'Error: Please fill in your address and t-shirt size.';
+        $message = 'Error: Please fill in all of the fields.';
     }
 }
 
@@ -37,16 +40,28 @@ $user = mysql_fetch_array($userqry);
 ?>
 <h1>T-Shirt Request Form</h1>
 <?=!empty($message) ? '<div style="font-weight: bold;">'.$message.'</div><br>' : ''?>
-<div style="font-size: 12px;">You are eligible to receive a free Bon Echo Extension Team t-shirt for your contributions to Firefox Add-ons. Please complete the following form to receive your t-shirt, making sure to include your full address and country if not in the United States. Please be patient, as requests will not be processed immediately.</div><br>
-<div>Current Request Status: <strong><?=!empty($user['UserTShirtSize']) ? 'Submitted' : 'Not Submitted'?></strong></div><br>
-<form method="post">
-<table cellpadding=2 cellspacing=0 align="center">
+<div style="font-size: 12px;">You are eligible to receive a free Firefox 2 Extension Team t-shirt for your contributions to Firefox Add-ons. Please complete the following form to receive your t-shirt. Please be patient, as requests will not be processed immediately. For questions related to this offer, please e-mail <a href="mailto:tshirts@mozilla.org">tshirts@mozilla.org</a>.</div><br>
+<div>Current Request Status: <strong><?=!empty($user['UserTShirtSize']) ? 'Submitted' : 'Not Submitted'?></strong></div>
+<table cellpadding=2 cellspacing=0>
     <tr>
-        <td>Name</td>
-        <td><input type="text" name="name" size="30" value="<?=$user['UserName']?>"></td>
+        <td valign="top">Current Eligibility:</td>
+        <td valign="top" style="font-weight: bold;"><?=implode('<br>', tshirtEligible(true))?></td>
+    </tr>
+</table>
+<hr>
+<div>All fields are required.</div><br>
+<form method="post">
+<table cellpadding=2 cellspacing=0>
+    <tr>
+        <td><span class="tooltip" title="Real name for t-shirt delivery only; will not be displayed publicly">Full Name</span></td>
+        <td><input type="text" name="name" size="30" value="<?=$user['UserRealName']?>"></td>
     </tr>
     <tr>
-        <td>Address</td>
+        <td><span class="tooltip" title="May be required by delivery service (Please include country and area code)">Telephone</span></td>
+        <td><input type="text" name="phone" value="<?=$user['UserPhone']?>"></td>
+    </tr>
+    <tr>
+        <td><span class="tooltip" title="Full shipping address, including country">Address</span></td>
         <td><input type="text" name="address1" size="40" value="<?=$user['UserAddress1']?>"></td>
     </tr>
     <tr>
@@ -68,6 +83,9 @@ $user = mysql_fetch_array($userqry);
                 <option value="2X"<?=($user['UserTShirtSize'] == '2X') ? ' selected' : ''?>>Extra Large</option>
             </select>
         </td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center">The information above is collected in accordance with Mozilla's <a href="http://www.mozilla.org/privacy-policy.html">Privacy Policy</a>.</td>
     </tr>
     <tr>
         <td colspan="2" align="center"><input type="submit" name="submit" value="Submit Request"></td>
