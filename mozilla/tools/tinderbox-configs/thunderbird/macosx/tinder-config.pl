5ddc378d688cbb74b171c408839b12452d6120c3
ccooper%deadsquid.com <>
2006-11-06 21:48:38 +0000

    - move l10n configs for Fx and Tb into public CVS

--- mozilla/tools/tinderbox-configs/thunderbird/macosx/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/macosx/tinder-config.pl
@@ -1,38 +1,17 @@
 #
-## hostname: xserve03.build.mozilla.org
-## uname: Darwin xserve03.build.mozilla.org 8.1.0 Darwin Kernel Version 8.1.0: Tue May 10 18:16:08 PDT 2005; root:xnu-792.1.5.obj~4/RELEASE_PPC Power Macintosh powerpc
+## hostname: xserve05
+## uname: Darwin xserve05.build.mozilla.org 8.7.0 Darwin Kernel Version 8.7.0: Fri May 26 15:20:53 PDT 2006; root:xnu-792.6.76.obj~1/RELEASE_PPC Power Macintosh powerpc
 #
 
 #- tinder-config.pl - Tinderbox configuration file.
 #-    Uncomment the variables you need to set.
 #-    The default values are the same as the commented variables.
 
-# $ENV{NO_EM_RESTART} = "1";
-# $ENV{DYLD_NO_FIX_PREBINDING} = "1";
-# $ENV{LD_PREBIND_ALLOW_OVERLAP} = "1";
-
-$MacUniversalBinary = 1;
-
-# $ENV{MOZ_PACKAGE_MSI}
-#-----------------------------------------------------------------------------
-#  Default: 0
-#   Values: 0 | 1
-#  Purpose: Controls whether a MSI package is made.
-# Requires: Windows and a local MakeMSI installation.
-#$ENV{MOZ_PACKAGE_MSI} = 0;
-
-# $ENV{MOZ_SYMBOLS_TRANSFER_TYPE}
-#-----------------------------------------------------------------------------
-#  Default: scp
-#   Values: scp | rsync
-#  Purpose: Use scp or rsync to transfer symbols to the Talkback server.
-# Requires: The selected type requires the command be available both locally
-#           and on the Talkback server.
-#$ENV{MOZ_SYMBOLS_TRANSFER_TYPE} = "scp";
+$ENV{CVS_RSH} = "ssh";
+$ENV{MOZ_SYMBOLS_TRANSFER_TYPE} = "rsync";
 
 #- PLEASE FILL THIS IN WITH YOUR PROPER EMAIL ADDRESS
 $BuildAdministrator = "chase\@mozilla.org";
-#$BuildAdministrator = "$ENV{USER}\@$ENV{HOST}";
 #$BuildAdministrator = ($ENV{USER} || "cltbld") . "\@" . ($ENV{HOST} || "dhcp");
 
 #- You'll need to change these to suit your machine's needs
@@ -40,16 +19,16 @@ $BuildAdministrator = "chase\@mozilla.org";
 
 #- Default values of command-line opts
 #-
-#$BuildDepend       = 1;      # Depend or Clobber
+$BuildDepend       = 0;      # Depend or Clobber
 #$BuildDebug        = 0;      # Debug or Opt (Darwin)
 #$ReportStatus      = 1;      # Send results to server, or not
 #$ReportFinalStatus = 1;      # Finer control over $ReportStatus.
-#$UseTimeStamp      = 1;      # Use the CVS 'pull-by-timestamp' option, or not
+$UseTimeStamp      = 0;      # Use the CVS 'pull-by-timestamp' option, or not
 #$BuildOnce         = 0;      # Build once, don't send results to server
 #$TestOnly          = 0;      # Only run tests, don't pull/build
 #$BuildEmbed        = 0;      # After building seamonkey, go build embed app.
-#$SkipMozilla       = 0;      # Use to debug post-mozilla.pl scripts.
-#$BuildLocales      = 0;      # Do l10n packaging?
+#$SkipMozilla       = 1;      # Use to debug post-mozilla.pl scripts.
+$BuildLocales      = 1;
 
 # Tests
 $CleanProfile             = 1;
@@ -57,9 +36,9 @@ $CleanProfile             = 1;
 $ProductName              = "Thunderbird";
 $VendorName               = 'Mozilla';
 
-$RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
-$RegxpcomTest             = 1;
-$AliveTest                = 1;
+#$RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
+#$RegxpcomTest             = 1;
+#$AliveTest                = 1;
 #$JavaTest                 = 0;
 #$ViewerTest               = 0;
 #$BloatTest                = 0;  # warren memory bloat test
@@ -75,17 +54,21 @@ $AliveTest                = 1;
 #$QATest                   = 0;  
 #$XULWindowOpenTest        = 0;  # Txul
 #$StartupPerformanceTest   = 0;  # Ts
+#@CompareLocaleDirs        = (); # Run compare-locales test on these directories
+@CompareLocaleDirs = (
+  "netwerk",
+  "dom",
+  "toolkit",
+  "security/manager",
+  "other-licenses/branding/thunderbird",
+  "editor/ui",
+  "mail",
+);
+#$CompareLocalesAviary     = 0;  # Should the compare-locales commands use the
+#                                # aviary directory structure?
 
 #$TestsPhoneHome           = 0;  # Should test report back to server?
-
-# $results_server
-#----------------------------------------------------------------------------
-# Server on which test results will be accessible.  This was originally tegu,
-# then became axolotl.  Once we moved services from axolotl, it was time
-# to give this service its own hostname to make future transitions easier.
-# - cmp@mozilla.org
-#$results_server           = "build-graphs.mozilla.org";
-
+#$results_server           = "axolotl.mozilla.org"; # was tegu
 #$pageload_server          = "spider";  # localhost
 
 #
@@ -109,7 +92,7 @@ $AliveTest                = 1;
 #$DHTMLPerformanceTestTimeout      = 1200;  # entire test, seconds
 #$QATestTimeout                    = 1200;   # entire test, seconds
 #$LayoutPerformanceTestPageTimeout = 30000; # each page, ms
-#$StartupPerformanceTestTimeout    = 15;    # seconds
+#$StartupPerformanceTestTimeout    = 60;    # seconds
 #$XULWindowOpenTestTimeout	      = 150;   # seconds
 
 
@@ -136,7 +119,6 @@ $AliveTest                = 1;
 # Note that win32 may not need \@, depends on ' or ".
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
-#$moz_cvsroot   = $ENV{CVSROOT};
 $moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
 
 #- Set these proper values for your tinderbox server
@@ -146,14 +128,23 @@ $moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
 #$moz_client_mk = 'client.mk';
 
 #- Set if you want to build in a separate object tree
-$ObjDir = '../build/universal';
+#$ObjDir = '';
 
 # Extra build name, if needed.
-$BuildNameExtra = 'Universal Nightly';
+$BuildNameExtra = 'Tb Trunk l10n';
 
 # User comment, eg. ip address for dhcp builds.
 # ex: $UserComment = "ip = 208.12.36.108";
 #$UserComment = 0;
+
+# Configure only, don't build.
+$ConfigureOnly = 1;
+%WGetFiles = (
+	     "http://stage.mozilla.org/pub/mozilla.org/thunderbird/tinderbox-builds/triton-trunk/thunderbird-3.0a1.en-US.mac.dmg" =>
+	     "/builds/tinderbox/Tb-Trunk-l10n/Darwin_8.7.0_Clobber/thunderbird.dmg"
+	     );
+$BuildLocalesArgs = "ZIP_IN=/builds/tinderbox/Tb-Trunk-l10n/Darwin_8.7.0_Clobber/thunderbird.dmg";
+
 
 #-
 #- The rest should not need to be changed
@@ -164,11 +155,12 @@ $BuildNameExtra = 'Universal Nightly';
 
 #- Until you get the script working. When it works,
 #- change to the tree you're actually building
-#$BuildTree  = 'MozillaTest';
-$BuildTree  = 'Thunderbird';
+$BuildTree  = 'Mozilla-l10n';
 
 #$BuildName = '';
+$BuildTag = '';
 #$BuildTag = 'AVIARY_1_0_1_20050124_BRANCH';
+#$BuildTag = 'FIREFOX_1_0_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
@@ -187,37 +179,31 @@ $BinaryName = 'thunderbird-bin';
 #$NSPRArgs = '';
 #$ShellOverride = '';
 
+# allow override of timezone value (for win32 POSIX::strftime)
+#$Timezone = '';
+
 # Release build options
 $ReleaseBuild  = 1;
-$shiptalkback  = 1;
-#$ReleaseToLatest = 1; # Push the release to latest-<milestone>?
-#$ReleaseToDated = 1; # Push the release to YYYY-MM-DD-HH-<milestone>?
-$build_hour    = "4";
+$LocaleProduct = "mail";
+$shiptalkback  = 0;
+$ReleaseToLatest = 1; # Push the release to latest-<milestone>?
+$ReleaseToDated = 0; # Push the release to YYYY-MM-DD-HH-<milestone>?
+$build_hour    = "9";
 $package_creation_path = "/mail/installer";
 # needs setting for mac + talkback: $mac_bundle_path = "/browser/app";
 $mac_bundle_path = "/mail/app";
-$ssh_version   = "2";
 $ssh_user      = "cltbld";
 $ssh_server    = "stage.mozilla.org";
 $ftp_path      = "/home/ftp/pub/thunderbird/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly";
-$tbox_ftp_path = "/home/ftp/pub/thunderbird/tinderbox-builds";
-$tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/tinderbox-builds";
-$milestone     = "trunk";
+$tbox_ftp_path = "/home/ftp/pub/thunderbird/tinderbox";
+$tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/tinderbox";
+$milestone     = "trunk-l10n";
 $notify_list   = "build-announce\@mozilla.org";
 $stub_installer = 0;
 $sea_installer = 0;
 $archive       = 1;
-$push_raw_xpis = 0;
-$update_package = 1;
-$update_product = "Thunderbird";
-$update_version = "trunk";
-$update_platform = "Darwin_Universal-gcc3";
-$update_hash = "md5";
-$update_filehost = "ftp.mozilla.org";
-$update_appv = "3.0a1";
-$update_extv = "3.0a1";
-$update_pushinfo = 1;
+#$push_raw_xpis = 1;
 
 # Reboot the OS at the end of build-and-test cycle. This is primarily
 # intended for Win9x, which can't last more than a few cycles before
