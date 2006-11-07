79185a7c4a5b8b1e0d8323d14e2d0b43a5839ee3
ccooper%deadsquid.com <>
2006-11-07 23:01:48 +0000

    - adding Tb linux release configs

--- mozilla/tools/tinderbox-configs/thunderbird/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/linux/tinder-config.pl
@@ -1,26 +1,28 @@
 #
 ## hostname: crazyhorse
-## uname: Linux crazyhorse 2.4.18-14 #1 Wed Sep 4 13:35:50 EDT 2002 i686 i686 i386 GNU/Linux
+## uname: Linux crazyhorse 2.4.18-14 #1 Wed Sep 4 13:35:50 EDT 2002 i686 athlon i386 GNU/Linux
 #
 
 #- tinder-config.pl - Tinderbox configuration file.
 #-    Uncomment the variables you need to set.
 #-    The default values are the same as the commented variables.
 
+$ENV{'MOZILLA_OFFICIAL'} = 1;
+
 #- PLEASE FILL THIS IN WITH YOUR PROPER EMAIL ADDRESS
 #$BuildAdministrator = "$ENV{USER}\@$ENV{HOST}";
 #$BuildAdministrator = ($ENV{USER} || "cltbld") . "\@" . ($ENV{HOST} || "dhcp");
 
 #- You'll need to change these to suit your machine's needs
-#$DisplayServer = ':0.0';
+$DisplayServer = ':1.0';
 
 #- Default values of command-line opts
 #-
-$BuildDepend       = 0;      # Depend or Clobber
+$BuildDepend       = 1;      # Depend or Clobber
 #$BuildDebug        = 0;      # Debug or Opt (Darwin)
 #$ReportStatus      = 1;      # Send results to server, or not
 #$ReportFinalStatus = 1;      # Finer control over $ReportStatus.
-#$UseTimeStamp      = 1;      # Use the CVS 'pull-by-timestamp' option, or not
+$UseTimeStamp      = 0;      # Use the CVS 'pull-by-timestamp' option, or not
 #$BuildOnce         = 0;      # Build once, don't send results to server
 #$TestOnly          = 0;      # Only run tests, don't pull/build
 #$BuildEmbed        = 0;      # After building seamonkey, go build embed app.
@@ -32,7 +34,7 @@ $CleanProfile             = 1;
 $ProductName              = "Thunderbird";
 #$VendorName               = "";
 
-$RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
+$RunMozillaTests          = 0;  # Allow turning off of all tests if needed.
 #$RegxpcomTest             = 1;
 #$AliveTest                = 1;
 #$JavaTest                 = 0;
@@ -113,7 +115,7 @@ $moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
 #$ObjDir = '';
 
 # Extra build name, if needed.
-$BuildNameExtra = 'release';
+$BuildNameExtra = 'Tb-Release-Clobber';
 
 # User comment, eg. ip address for dhcp builds.
 # ex: $UserComment = "ip = 208.12.36.108";
@@ -128,10 +130,11 @@ $BuildNameExtra = 'release';
 
 #- Until you get the script working. When it works,
 #- change to the tree you're actually building
-$BuildTree  = 'Thunderbird';
+$BuildTree  = 'Mozilla1.8.0';
+#$BuildTree  = 'MozillaTest';
 
 #$BuildName = '';
-$BuildTag = '';
+$BuildTag = 'THUNDERBIRD_1_5_0_8_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
@@ -156,6 +159,8 @@ $BinaryName = 'thunderbird-bin';
 # Release build options
 $ReleaseBuild  = 1;
 #$shiptalkback  = 1;
+$ReleaseToLatest = 0; # Push the release to latest-<milestone>?
+$ReleaseToDated = 1; # Push the release to YYYY-MM-DD-HH-<milestone>?
 $build_hour    = "3";
 $package_creation_path = "/mail/installer";
 $ssh_version   = "2";
@@ -165,20 +170,13 @@ $ftp_path      = "/home/ftp/pub/thunderbird/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly";
 $tbox_ftp_path = "/home/ftp/pub/thunderbird/tinderbox-builds";
 $tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/tinderbox-builds";
-$milestone     = "trunk";
-$notify_list   = "build-announce\@mozilla.org";
+$milestone     = "thunderbird1.5.0.8";
+$notify_list   = 'build-announce@mozilla.org';
 $stub_installer = 0;
 $sea_installer = 0;
 $archive       = 1;
+
 $update_package = 1;
-$update_product = "Thunderbird";
-$update_version = "trunk";
-$update_platform = "Linux_x86-gcc3";
-$update_hash = "sha1";
-$update_filehost = "ftp.mozilla.org";
-$update_appv = "3.0a1";
-$update_extv = "3.0a1";
-$update_pushinfo = 1;
 
 # Reboot the OS at the end of build-and-test cycle. This is primarily
 # intended for Win9x, which can't last more than a few cycles before
@@ -202,3 +200,10 @@ $update_pushinfo = 1;
 #$LogEncoding = 'base64';
 
 $ENV{CVS_RSH} = "ssh";
+
+$update_product  = "Thunderbird";
+$update_version  = "1.5.0.x";
+$update_platform = "Linux_x86-gcc3";
+$update_ver_file = 'mail/config/version.txt';
+$update_pushinfo = 0;
+
