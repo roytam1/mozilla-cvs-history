259340f8386be185a12f757bcb6aa379232f5072
dbaron%dbaron.org <>
2006-11-08 20:05:03 +0000

    Set mFace to null after calling FT_Done_Face.  b=355059  r+sr=roc  a=dveditz

--- mozilla/gfx/src/ps/nsFontMetricsPS.cpp
+++ mozilla/gfx/src/ps/nsFontMetricsPS.cpp
@@ -1037,8 +1037,10 @@ nsFontPSXft::Init(nsXftEntry* aEntry,
 
 nsFontPSXft::~nsFontPSXft()
 {
-  if (mEntry->mFace) 
+  if (mEntry->mFace) {
     FT_Done_Face(mEntry->mFace);
+    mEntry->mFace = nsnull;
+  }
 
   if (FT_Done_FreeType(mFreeTypeLibrary))
     return;
@@ -2327,8 +2329,10 @@ nsXftType1Generator::Init(nsXftEntry* aEntry)
 
 nsXftType1Generator::~nsXftType1Generator()
 {
-  if (mEntry->mFace) 
+  if (mEntry->mFace) {
     FT_Done_Face(mEntry->mFace);
+    mEntry->mFace = nsnull;
+  }
 
   if (FT_Done_FreeType(mFreeTypeLibrary))
     return;
