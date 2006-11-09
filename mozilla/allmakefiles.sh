8ced561eda593bbf2edb12836df6afcb49bdb06f
alex%croczilla.com <>
2006-11-09 10:05:25 +0000

    Merge trunk changes.

--- mozilla/allmakefiles.sh
+++ mozilla/allmakefiles.sh
@@ -1358,10 +1358,6 @@ if [ "$MOZ_XTF" ]; then
 	content/xtf/public/Makefile
 	content/xtf/src/Makefile
 "
-    MAKEFILES_layout="$MAKEFILES_layout
-	layout/xtf/Makefile
-	layout/xtf/src/Makefile
-"
 fi
 
 if [ "$MOZ_XMLEXTRAS" ]; then
