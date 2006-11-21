859091d7118aca1f4878c28624766632bbe2303c
wtchang%redhat.com <>
2006-11-21 00:01:28 +0000

    Bugzilla Bug 357333: switched to official NSPR and NSS release tags on the MOZILLA_1_8_BRANCH.  We upgraded to NSPR 4.6.4 and NSS 3.11.4. r=dveditz approved for 1.8 branch, a=dveditz for drivers

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -258,8 +258,8 @@ MODULES_all :=                                  \
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
 MOZ_CO_TAG           = MOZILLA_1_8_BRANCH
-NSPR_CO_TAG          = MOZILLA_1_8_BRANCH
-NSS_CO_TAG           = MOZILLA_1_8_BRANCH
+NSPR_CO_TAG          = NSPR_4_6_4_RTM
+NSS_CO_TAG           = NSS_3_11_4_RTM
 LDAPCSDK_CO_TAG      = MOZILLA_1_8_BRANCH
 LOCALES_CO_TAG       = MOZILLA_1_8_BRANCH
 
