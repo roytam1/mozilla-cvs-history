8b3d4f2f00da4d80e984eca9f17f9b51d5baa05b
scott%scott-macgregor.org <>
2006-11-29 22:18:26 +0000

    Bug #362225 --> backout Bug #332160 (new mail alert notification for unix) which is causing a crash on the branch only.

--- mozilla/mailnews/build/nsMailModule.cpp
+++ mozilla/mailnews/build/nsMailModule.cpp
@@ -115,9 +115,6 @@
 #ifdef XP_MACOSX
 #include "nsMessengerOSXIntegration.h"
 #endif
-#if defined(MOZ_WIDGET_GTK) || defined(MOZ_WIDGET_GTK2)
-#include "nsMessengerUnixIntegration.h"
-#endif
 #include "nsCURILoader.h"
 #include "nsMessengerContentHandler.h"
 
@@ -343,9 +340,6 @@ NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsMessengerOS2Integration, Init)
 #endif
 #ifdef XP_MACOSX
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsMessengerOSXIntegration, Init)
-#endif
-#if defined(MOZ_WIDGET_GTK) || defined(MOZ_WIDGET_GTK2)
-NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsMessengerUnixIntegration, Init)
 #endif
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsMessengerContentHandler)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsMsgContentPolicy, Init)
@@ -855,12 +849,6 @@ static const nsModuleComponentInfo gComponents[] = {
     { "OSX OS Integration", NS_MESSENGEROSXINTEGRATION_CID,
       NS_MESSENGEROSINTEGRATION_CONTRACTID,
       nsMessengerOSXIntegrationConstructor,
-    },
-#endif
-#if defined(MOZ_WIDGET_GTK) || defined(MOZ_WIDGET_GTK2)
-    { "Unix OS Integration", NS_MESSENGERUNIXINTEGRATION_CID,
-      NS_MESSENGEROSINTEGRATION_CONTRACTID,
-      nsMessengerUnixIntegrationConstructor,
     },
 #endif
     { "application/x-message-display content handler",
