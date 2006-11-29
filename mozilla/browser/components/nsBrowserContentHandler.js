0b59c67f59f010adc99588e9b749891c36221d4b
gavin%gavinsharp.com <>
2006-11-29 19:47:52 +0000

    Bug 357853: if no browser window exists, '-new-tab <url>' commandline argument does not open <url>, r=bsmedberg, a=dveditz

--- mozilla/browser/components/nsBrowserContentHandler.js
+++ mozilla/browser/components/nsBrowserContentHandler.js
@@ -307,7 +307,7 @@ var nsBrowserContentHandler = {
           else if (/new-tab/.test(remoteParams[1]))
             location = nsIBrowserDOMWindow.OPEN_NEWTAB;
 
-          handURIToExistingBrowser(uri, location);
+          handURIToExistingBrowser(uri, location, cmdLine);
           break;
 
         case "xfedocommand":
@@ -355,7 +355,7 @@ var nsBrowserContentHandler = {
     try {
       while ((uriparam = cmdLine.handleFlagWithParam("new-tab", false))) {
         var uri = resolveURIInternal(cmdLine, uriparam);
-        handURIToExistingBrowser(uri, nsIBrowserDOMWindow.OPEN_NEWTAB);
+        handURIToExistingBrowser(uri, nsIBrowserDOMWindow.OPEN_NEWTAB, cmdLine);
         cmdLine.preventDefault = true;
       }
     }
@@ -548,7 +548,7 @@ const bch_contractID = "@mozilla.org/browser/clh;1";
 const bch_CID = Components.ID("{5d0ce354-df01-421a-83fb-7ead0990c24e}");
 const CONTRACTID_PREFIX = "@mozilla.org/uriloader/content-handler;1?type=";
 
-function handURIToExistingBrowser(uri, location)
+function handURIToExistingBrowser(uri, location, cmdLine)
 {
   if (!shouldLoadURI(uri))
     return;
@@ -623,7 +623,7 @@ var nsDefaultCommandLineHandler = {
         // Try to find an existing window and load our URI into the
         // current tab, new tab, or new window as prefs determine.
         try {
-          handURIToExistingBrowser(urilist[0], nsIBrowserDOMWindow.OPEN_DEFAULTWINDOW);
+          handURIToExistingBrowser(urilist[0], nsIBrowserDOMWindow.OPEN_DEFAULTWINDOW, cmdLine);
           return;
         }
         catch (e) {
