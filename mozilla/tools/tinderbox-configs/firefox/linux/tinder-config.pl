4bcdead4ae6814d39b14dea921bfc019dc387061
preed%mozilla.com <>
2006-12-08 23:27:35 +0000

    Bug 360585: bump l10n configs to pick up rc2 respin (bug 363054) on Linux only. patch=tfullhart, r=preed

--- mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
@@ -139,7 +139,7 @@ $BuildNameExtra = 'Fx-Release';
 # Configure only, don't build.
 $ConfigureOnly = 1;
 %WGetFiles = (
-	      "http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/2.0.0.1-candidates/rc1/firefox-2.0.0.1.en-US.linux-i686.tar.gz" => 
+	      "http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/2.0.0.1-candidates/rc2/firefox-2.0.0.1.en-US.linux-i686.tar.gz" => 
 	      "/builds/tinderbox/Fx-Mozilla1.8-l10n-release/Linux_2.4.20-28.8_Clobber/firefox.tar.gz"
 	      );
 
