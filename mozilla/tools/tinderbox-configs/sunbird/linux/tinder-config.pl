84d384849a181119d317b9a701c2af0afd3c1170
mattwillis%gmail.com <>
2006-12-19 13:20:39 +0000

    Fixes lt18-linux-tbox 'can't connect to display' error

--- mozilla/tools/tinderbox-configs/sunbird/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/sunbird/linux/tinder-config.pl
@@ -41,7 +41,7 @@ $BuildAdministrator = 'build@mozilla.org';
 #$BuildAdministrator = ($ENV{USER} || "cltbld") . "\@" . ($ENV{HOST} || "dhcp");
 
 #- You'll need to change these to suit your machine's needs
-$DisplayServer = ':1.0';
+#$DisplayServer = ':0.0';
 
 #- Default values of command-line opts
 #-
