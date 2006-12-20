5bc14bfd20e86b2ed1a8b06b61d52a4536ff13ff
crowder%fiverocks.com <>
2006-12-20 23:42:52 +0000

    Bug 364350: free of uninitialized memory in OOM condition, patch by Gavin Reaney (gavin@picsel.com), r=crowder@fiverocks.com, a=jay

--- mozilla/js/src/jsregexp.c
+++ mozilla/js/src/jsregexp.c
@@ -1969,6 +1969,8 @@ js_NewRegExp(JSContext *cx, JSTokenStream *ts,
             re = NULL;
             goto out;
         }
+        for (i = 0; i < re->classCount; i++)
+            re->classList[i].converted = JS_FALSE;
     } else {
         re->classList = NULL;
     }
