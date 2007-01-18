a376c33b68e459cb5ced06eaba84b4a4a9616917
wtchang%redhat.com <>
2007-01-18 23:41:18 +0000

    Set NSPR version to 4.6.6 Beta. Modified files: configure configure.in repackage.sh prinit.h vercheck.c  Tag: NSPR_4_6_BRANCH

--- mozilla/nsprpub/admin/repackage.sh
+++ mozilla/nsprpub/admin/repackage.sh
@@ -64,10 +64,10 @@
 # 
 # ------------------------------------------------------------------
 
-FROMTOP=/share/builds/components/nspr20/v4.6.5
-TOTOP=./v4.6.5
-NSPRDIR=nspr-4.6.5
-SOURCETAG=NSPR_4_6_5_RTM
+FROMTOP=/share/builds/components/nspr20/v4.6.6
+TOTOP=./v4.6.6
+NSPRDIR=nspr-4.6.6
+SOURCETAG=NSPR_4_6_6_RTM
 
 #
 # enumerate Unix object directories on /s/b/c
