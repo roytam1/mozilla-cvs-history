dc419c62fbd5bab27e72dab5b5e817a345c1f69c
gavin%gavinsharp.com <>
2007-01-25 15:03:31 +0000

    Bug 273496: pressing 'get new messages' shouldn't ask to send unsent messages when going online, patch by Magnus Melin <mkmelin+mozilla@iki.fi>, r+a=mscott

--- mozilla/mail/base/content/msgMail3PaneWindow.js
+++ mozilla/mail/base/content/msgMail3PaneWindow.js
@@ -1056,7 +1056,7 @@ function loadStartFolder(initialUri)
       MsgGetMessagesForAllServers(defaultServer);
 
     // if appropriate, send unsent messages. This may end up prompting the user
-    if (MailOfflineMgr.shouldSendUnsentMessages())
+    if (MailOfflineMgr.isOnline() && MailOfflineMgr.shouldSendUnsentMessages())
       SendUnsentMessages();
 }
 
