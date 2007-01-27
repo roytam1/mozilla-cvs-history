54bfdeabed844dabb1acde20c9e308de72bda09e
stuart.morgan%alumni.case.edu <>
2007-01-27 23:13:21 +0000

    Bug 368444: Disable Camino's crash-restoration dialog in tinderbox tests. r=rhelmer

--- mozilla/tools/tinderbox/build-seamonkey-util.pl
+++ mozilla/tools/tinderbox/build-seamonkey-util.pl
@@ -2061,7 +2061,11 @@ sub run_all_tests {
                 set_pref($pref_file, 'browser.sessionstore.resume_from_crash', 'false');
             }
             elsif ($Settings::BinaryName eq 'Camino') {
+                # Suppress default browser dialog
                 set_pref($pref_file, 'camino.check_default_browser', 'false');
+
+                # Suppress session restore dialog
+                set_pref($pref_file, 'browser.sessionstore.resume_from_crash', 'false');
             }
 
             # Suppress security warnings for QA test.
