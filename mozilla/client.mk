114ae89d2a9fe4332b18550cf5865cce3164a45e
mattwillis%gmail.com <>
2007-02-01 01:42:33 +0000

    bug 368851 - Use more recent tags for NSS and NSPR. r1=dveditz, r2=dmose

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -382,8 +382,8 @@ MODULES_all :=                                  \
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
 MOZ_CO_TAG           = SUNBIRD_0_3_BRANCH
-NSPR_CO_TAG          = SUNBIRD_0_3_BRANCH
-NSS_CO_TAG           = SUNBIRD_0_3_BRANCH
+NSPR_CO_TAG          = NSPRPUB_PRE_4_2_CLIENT_BRANCH
+NSS_CO_TAG           = NSS_3_11_5_RTM
 LDAPCSDK_CO_TAG      = SUNBIRD_0_3_BRANCH
 LOCALES_CO_TAG       = SUNBIRD_0_3_BRANCH
 
