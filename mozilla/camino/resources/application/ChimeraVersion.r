9d382b568ffbac9d542b6ef5a68d0d6db2f398c2
mark%moxienet.com <>
2007-02-23 16:01:12 +0000

    Numerals for Camino 1.1b

--- mozilla/camino/resources/application/ChimeraVersion.r
+++ mozilla/camino/resources/application/ChimeraVersion.r
@@ -52,13 +52,13 @@
 
 #define     VERSION_MAJOR           1
 #define     VERSION_MINOR           0x10    // revision & fix in BCD
-#define     VERSION_KIND            alpha   // alpha, beta, or final
-#define     VERSION_MICRO           2       // internal stage: alpha or beta number
+#define     VERSION_KIND            beta    // alpha, beta, or final
+#define     VERSION_MICRO           1       // internal stage: alpha or beta number
 
 
 // Version Strings (Finder's Get Info dialog box) //
 
-#define     VERSION_STRING          "1.1a2+"
+#define     VERSION_STRING          "1.1b"
 
 #define     COPYRIGHT_STRING        "© 1998-2007 Contributors"
 #define     GETINFO_VERSION         VERSION_STRING ", " COPYRIGHT_STRING
