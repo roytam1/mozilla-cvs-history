2f76b59bfaa9b880b04e4e4443fbce7daf745dbe
mark%moxienet.com <>
2007-02-23 21:55:58 +0000

    Bumping to 1.1b+ after 1.1b release.

--- mozilla/camino/resources/application/ChimeraVersion.r
+++ mozilla/camino/resources/application/ChimeraVersion.r
@@ -58,7 +58,7 @@
 
 // Version Strings (Finder's Get Info dialog box) //
 
-#define     VERSION_STRING          "1.1b"
+#define     VERSION_STRING          "1.1b+"
 
 #define     COPYRIGHT_STRING        "© 1998-2007 Contributors"
 #define     GETINFO_VERSION         VERSION_STRING ", " COPYRIGHT_STRING
