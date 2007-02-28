a848aa8e9ac664fa6580e9205fb7837c14bf18f4
mark%moxienet.com <>
2007-02-28 21:23:55 +0000

    1.0.4-final, not 1.0.0-final-4

--- mozilla/camino/resources/application/ChimeraVersion.r
+++ mozilla/camino/resources/application/ChimeraVersion.r
@@ -51,9 +51,9 @@
 // Version Numbers //
 
 #define     VERSION_MAJOR           1
-#define     VERSION_MINOR           0x00    // revision & fix in BCD
+#define     VERSION_MINOR           0x04    // revision & fix in BCD
 #define     VERSION_KIND            final   // alpha, beta, or final
-#define     VERSION_MICRO           4       // internal stage: alpha or beta number
+#define     VERSION_MICRO           0       // internal stage: alpha or beta number
 
 
 // Version Strings (Finder's Get Info dialog box) //
