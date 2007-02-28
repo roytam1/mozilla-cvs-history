03a9b3bb5aa3bee9624e75bef1d9da8edadeb543
mark%moxienet.com <>
2007-02-28 21:26:42 +0000

    Updating version number on this branch to 1.0.4+.

--- mozilla/camino/resources/application/ChimeraVersion.r
+++ mozilla/camino/resources/application/ChimeraVersion.r
@@ -51,14 +51,14 @@
 // Version Numbers //
 
 #define     VERSION_MAJOR           1
-#define     VERSION_MINOR           0x01    // revision & fix in BCD
+#define     VERSION_MINOR           0x05    // revision & fix in BCD
 #define     VERSION_KIND            alpha   // alpha, beta, or final
 #define     VERSION_MICRO           0       // internal stage: alpha or beta number
 
 
 // Version Strings (Finder's Get Info dialog box) //
 
-#define     VERSION_STRING          "1.0.0+"
+#define     VERSION_STRING          "1.0.4+"
 
 #define     COPYRIGHT_STRING        "© 1998-2007 Contributors"
 #define     GETINFO_VERSION         VERSION_STRING ", " COPYRIGHT_STRING
