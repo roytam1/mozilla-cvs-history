96089b0d5c5d13b13da0422096165a64e42f48df
mattwillis%gmail.com <>
2007-03-20 11:36:22 +0000

    bug 374042 - Read-only access to l10n for calendar tinderbox

--- mozilla/tools/tinderbox-configs/sunbird/macosx/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/sunbird/macosx/tinder-config.pl
@@ -50,10 +50,10 @@ $BuildLocales      = 1;      # Do l10n packaging?
 # Tests
 $CleanProfile             = 1;
 #$ResetHomeDirForTests     = 1;
-$ProductName              = 'Sunbird';
-$MacOSProductName         = 'Sunbird';
-#$ProductName              = 'Calendar';
-#$MacOSProductName         = 'Calendar';
+#$ProductName              = 'Sunbird';
+#$MacOSProductName         = 'Sunbird';
+$ProductName              = 'Calendar';
+$MacOSProductName         = 'Calendar';
 $VendorName               = 'Mozilla';
 
 #$RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
@@ -145,7 +145,8 @@ $VendorName               = 'Mozilla';
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 #$moz_cvsroot   = $ENV{CVSROOT};
-$moz_cvsroot   = ':ext:calbld@cvs.mozilla.org:/cvsroot';
+#$moz_cvsroot   = ':ext:calbld@cvs.mozilla.org:/cvsroot';
+$moz_cvsroot   = ':pserver:anonymous@cvs.mozilla.org:/cvsroot';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -226,8 +227,8 @@ $ssh_user      = "calbld";
 $ssh_server    = "stage.mozilla.org";
 $ftp_path      = "/home/ftp/pub/calendar/sunbird/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/calendar/sunbird/nightly";
-$tbox_ftp_path = "/home/ftp/pub/calendar/sunbird/tinderbox";
-$tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/calendar/sunbird/tinderbox";
+$tbox_ftp_path = "/home/ftp/pub/calendar/sunbird/tinderbox-builds";
+$tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/calendar/sunbird/tinderbox-builds";
 $milestone     = "trunk-l10n";
 $notify_list   = 'build-announce@mozilla.org';
 $stub_installer = 0;
