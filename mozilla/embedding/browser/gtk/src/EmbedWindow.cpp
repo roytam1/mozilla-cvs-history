ca667189ee6854a4a5c72fb8108a970eae274695
caillon%redhat.com <>
2007-03-25 20:49:28 +0000

    Bug 312998 fix EmbedWindow::GetVisibility Patch from chpe@gnome.org r=caillon sr=roc a=beltzner

--- mozilla/embedding/browser/gtk/src/EmbedWindow.cpp
+++ mozilla/embedding/browser/gtk/src/EmbedWindow.cpp
@@ -359,7 +359,13 @@ EmbedWindow::GetSiteWindow(void **aSiteWindow)
 NS_IMETHODIMP
 EmbedWindow::GetVisibility(PRBool *aVisibility)
 {
-  *aVisibility = mVisibility;
+  // Work around the problem that sometimes the window
+  // is already visible even though mVisibility isn't true
+  // yet.
+  *aVisibility = mVisibility ||
+                 (!mOwner->mIsChrome &&
+                  mOwner->mOwningWidget &&
+                  GTK_WIDGET_MAPPED(mOwner->mOwningWidget));
   return NS_OK;
 }
 
