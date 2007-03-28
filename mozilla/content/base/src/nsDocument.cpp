dc14525fcd54ca750029e6fa79823c47a5e3d705
bzbarsky%mit.edu <>
2007-03-28 20:41:43 +0000

    Trust not the caller. Bug 369216, r+sr=jst, a=dveditz

--- mozilla/content/base/src/nsDocument.cpp
+++ mozilla/content/base/src/nsDocument.cpp
@@ -2930,7 +2930,11 @@ nsDocument::AddBinding(nsIDOMElement* aContent, const nsAString& aURI)
   nsCOMPtr<nsIContent> content(do_QueryInterface(aContent));
 
   nsCOMPtr<nsIURI> uri;
-  NS_NewURI(getter_AddRefs(uri), aURI);
+  rv = NS_NewURI(getter_AddRefs(uri), aURI);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  
   return mBindingManager->AddLayeredBinding(content, uri);
 }
 
