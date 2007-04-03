dc92950ea1166b6c262ef06f9df4e044e11b3a45
benjamin%smedbergs.us <>
2007-04-03 14:00:57 +0000

    bug 367428 - Tighten resource protocol parsing, r=biesi a=dveditz

--- mozilla/netwerk/protocol/res/src/nsResProtocolHandler.cpp
+++ mozilla/netwerk/protocol/res/src/nsResProtocolHandler.cpp
@@ -51,6 +51,7 @@
 #include "nsDirectoryServiceDefs.h"
 #include "nsNetUtil.h"
 #include "nsURLHelper.h"
+#include "nsEscape.h"
 
 static NS_DEFINE_CID(kResURLCID, NS_RESURL_CID);
 
@@ -330,6 +331,10 @@ nsResProtocolHandler::ResolveURI(nsIURI *uri, nsACString &result)
 
     // Don't misinterpret the filepath as an absolute URI.
     if (filepath.FindChar(':') != -1)
+        return NS_ERROR_MALFORMED_URI;
+
+    NS_UnescapeURL(filepath);
+    if (filepath.FindChar('\\') != -1)
         return NS_ERROR_MALFORMED_URI;
 
     const char *p = path.get() + 1; // path always starts with a slash
