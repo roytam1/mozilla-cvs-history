77b4704b3b18de84532f957d573713ba8400e228
rhelmer%mozilla.com <>
2007-04-24 05:43:12 +0000

    add annotations for Config::Bump b=371325 r=preed r=cf

--- mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
@@ -119,6 +119,7 @@ $VendorName               = 'Mozilla';
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 #$moz_cvsroot   = $ENV{CVSROOT};
+# CONFIG: $moz_cvsroot   = '%mozillaCvsroot%';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -139,10 +140,13 @@ $BuildNameExtra = 'Fx-Release';
 # Configure only, don't build.
 $ConfigureOnly = 1;
 %WGetFiles = (
+# CONFIG: 	      "http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/%version%-candidates/rc%rc%/firefox-%version%.en-US.linux-i686.tar.gz" => 
 	      "http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/2.0.0.3-candidates/rc1/firefox-2.0.0.3.en-US.linux-i686.tar.gz" => 
+# CONFIG:	      "/builds/tinderbox/Fx-Mozilla1.8-l10n-release/%l10n_buildPlatform%/firefox.tar.gz"
 	      "/builds/tinderbox/Fx-Mozilla1.8-l10n-release/Linux_2.4.20-28.8_Depend/firefox.tar.gz"
 	      );
 
+# CONFIG: $BuildLocalesArgs = "ZIP_IN=/builds/tinderbox/Fx-Mozilla1.8-l10n-release/%l10n_buildPlatform%/firefox.tar.gz";
 $BuildLocalesArgs = "ZIP_IN=/builds/tinderbox/Fx-Mozilla1.8-l10n-release/Linux_2.4.20-28.8_Depend/firefox.tar.gz";
 
 #-
@@ -154,9 +158,11 @@ $BuildLocalesArgs = "ZIP_IN=/builds/tinderbox/Fx-Mozilla1.8-l10n-release/Linux_2
 
 #- Until you get the script working. When it works,
 #- change to the tree you're actually building
+# CONFIG: $BuildTree  = '%buildTree%';
 $BuildTree  = 'Mozilla1.8-l10n';
 
 #$BuildName = '';
+# CONFIG: $BuildTag = '%productTag%_RELEASE';
 $BuildTag = 'FIREFOX_2_0_0_3_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
@@ -188,12 +194,15 @@ $build_hour    = "9";
 $package_creation_path = "/browser/installer";
 # needs setting for mac + talkback: $mac_bundle_path = "/browser/app";
 $ssh_version   = "2";
+# CONFIG: $ssh_user      = "%sshUser%";
 $ssh_user      = "cltbld";
+# CONFIG: $ssh_server    = "%sshServer%";
 $ssh_server    = "stage.mozilla.org";
 $ftp_path      = "/home/ftp/pub/firefox/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly";
 $tbox_ftp_path = "/home/ftp/pub/firefox/tinderbox";
 $tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox";
+# CONFIG: $milestone     = "firefox%version%-l10n";
 $milestone     = "firefox2.0.0.3-l10n";
 $notify_list   = 'build-announce@mozilla.org';
 $stub_installer = 0;
