02e812875a10118db2ff2f131da4ad3599fbe228
cvs2git <>
2007-04-26 12:23:13 +0000

    This commit was manufactured by cvs2svn to create tag 'THUNDERBIRD_2_0_0_0_RC2'.

--- mozilla/layout/reftests/xul-document-load/test006-007.js
+++
@@ -1,8 +0,0 @@
-function checkDOM(target, data) {
-  // assuming whitespace and the XML declaration are not in the DOM.
-  var piNode = document.firstChild;
-  if (!piNode || piNode.nodeType != Node.PROCESSING_INSTRUCTION_NODE ||
-      piNode.target != target || piNode.data != data) {
-    document.documentElement.style.backgroundColor = "red";
-  }
-}
