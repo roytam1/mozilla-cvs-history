abaa169c0e898a027438f386f3b40588cf01b49a
kaie%kuix.de <>
2007-04-27 13:38:05 +0000

    Bug 332771, delayed S/Mime verification broken when message opened in separate window r/sr=mscott, a=dveditz

--- mozilla/mail/base/content/msgMail3PaneWindow.js
+++ mozilla/mail/base/content/msgMail3PaneWindow.js
@@ -1739,11 +1739,6 @@ function SelectMessage(messageUri)
     gDBView.selectMsgByKey(msgHdr.messageKey);
 }
 
-function ReloadWithAllParts()
-{
-  gDBView.reloadMessageWithAllParts();
-}
-
 function ReloadMessage()
 {
   gDBView.reloadMessage();
--- mozilla/mailnews/base/resources/content/msgMail3PaneWindow.js
+++ mozilla/mailnews/base/resources/content/msgMail3PaneWindow.js
@@ -1519,11 +1519,6 @@ function SelectMessage(messageUri)
     gDBView.selectMsgByKey(msgHdr.messageKey);
 }
 
-function ReloadWithAllParts()
-{
-  gDBView.reloadMessageWithAllParts();
-}
-
 function ReloadMessage()
 {
   gDBView.reloadMessage();
