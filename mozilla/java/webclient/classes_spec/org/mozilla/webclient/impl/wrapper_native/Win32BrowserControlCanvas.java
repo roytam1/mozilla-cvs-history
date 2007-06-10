c1f6c96ffb9cfbb50a37be527743589587eb69ca
edburns%acm.org <>
2007-06-10 03:32:25 +0000

    M webclient/classes_spec/org/mozilla/webclient/impl/wrapper_native/Win32BrowserControlCanvas.java
    - Added some logging
    
    - Added informal protocol: newNativeEventThread.  This will be used
      to allow the Mac OS X Version to provide a special NativeEventThread
      extension that forwards all things normally run on the
      NativeEventThread to run on the AppKit thread instead.
    
    M webclient/classes_spec/org/mozilla/webclient/impl/wrapper_native/WrapperFactoryImpl.java
    
    - Use new informal protocol to instantiate NativeEventThread
    
    M util/classes/org/mozilla/util/Utilities.java
    
    - Instead of doing Class.forName use the proper ClassLoader pattern.
      This will help when webclient becomes a server-side mashup
      facilitator.

--- mozilla/java/webclient/classes_spec/org/mozilla/webclient/impl/wrapper_native/Win32BrowserControlCanvas.java
+++ mozilla/java/webclient/classes_spec/org/mozilla/webclient/impl/wrapper_native/Win32BrowserControlCanvas.java
@@ -21,14 +21,8 @@
 
 package org.mozilla.webclient.impl.wrapper_native;
 
-// Win32BrowserControlCanvas.java
-
-import org.mozilla.util.Assert;
-import org.mozilla.util.Log;
-import org.mozilla.util.ParameterCheck;
 
 import org.mozilla.util.ReturnRunnable;
-import org.mozilla.webclient.impl.wrapper_native.NativeEventThread;
 
 /**
 
@@ -50,6 +44,7 @@ import sun.awt.*;
 import sun.awt.windows.*;
 
 import org.mozilla.webclient.BrowserControlCanvas;
+import org.mozilla.webclient.impl.WrapperFactory;
 
 /**
  * Win32BrowserControlCanvas provides a concrete realization
@@ -81,4 +76,12 @@ public class Win32BrowserControlCanvas extends BrowserControlCanvas {
 		});
 	return result.intValue();
     }
+
+    public static NativeEventThread newNativeEventThread(WrapperFactory owner) {
+        NativeEventThread result = new NativeEventThread("WebclientEventThread",
+                owner);
+        return result;
+    }
+    
+    
 }
