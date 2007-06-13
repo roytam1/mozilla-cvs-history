b34754049da7dca6718846dfe8b1b8664cd38543
rhelmer%mozilla.com <>
2007-06-13 04:39:24 +0000

    use more explicit graph name

--- mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
@@ -88,7 +88,7 @@ $DownloadBuildDir = 'firefox';
 # parseable format
 $TinderboxServerURL = 'http://tinderbox.mozilla.org/showbuilds.cgi?tree=Firefox&quickparse=1';
 $MatchBuildname = 'Linux fx-linux-tbox Depend Nightly';
-#$GraphNameOverride        = $MatchBuildname."-test1"; # Override name built from ::hostname() and $BuildTag
+$GraphNameOverride        = "bl-bldlnx01_fx-linux-tbox-HEAD"; # Override name built from ::hostname() and $BuildTag
 
 # $results_server
 #----------------------------------------------------------------------------
