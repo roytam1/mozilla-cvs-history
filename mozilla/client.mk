4948912ffefb915fafa4af20e18a690399217305
mark%moxienet.com <>
2007-06-18 18:12:54 +0000

    client.mk on CAMINO_1_0_5_MINIBRANCH should use CAMINO_1_0_5_RELEASE

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -238,11 +238,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_1_5_0_12_RELEASE
+MOZ_CO_TAG           = CAMINO_1_0_5_RELEASE
 NSPR_CO_TAG          = NSPR_4_6_7_RTM
 NSS_CO_TAG           = NSS_3_11_5_RTM
-LDAPCSDK_CO_TAG      = FIREFOX_1_5_0_12_RELEASE
-LOCALES_CO_TAG       = FIREFOX_1_5_0_12_RELEASE
+LDAPCSDK_CO_TAG      = CAMINO_1_0_5_RELEASE
+LOCALES_CO_TAG       = CAMINO_1_0_5_RELEASE
 
 BUILD_MODULES = all
 
