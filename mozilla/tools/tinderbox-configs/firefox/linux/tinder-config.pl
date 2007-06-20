d6ad3cadbdc0edd79bc36272f687916751f06889
reed%reedloden.com <>
2007-06-20 02:00:51 +0000

    Re-land patches in bug 363499 now that bug 384032 has been fixed. [p=reed r=preed]

--- mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
@@ -94,7 +94,7 @@ $RunMozillaTests          = 0;  # Allow turning off of all tests if needed.
 #$results_server           = "build-graphs.mozilla.org";
 
 #$pageload_server          = "spider";  # localhost
-$pageload_server          = "axolotl.mozilla.org";  # localhost
+$pageload_server          = "pageload.build.mozilla.org";  # localhost
 
 #
 # Timeouts, values are in seconds.
