36cc89f98dc8384293ab939b6daf243a4e244bde
cvs2git <>
2007-06-29 17:25:43 +0000

    This commit was manufactured by cvs2svn to create tag 'MOZILLA_1_9a6_RELEASE'.

--- mozilla/security/nss/cmd/p7env/p7env.c
+++ mozilla/security/nss/cmd/p7env/p7env.c
@@ -209,7 +209,7 @@ main(int argc, char **argv)
 	    break;
 
 	  case 'o':
-	    outFile = fopen(optstate->value, "wb");
+	    outFile = fopen(optstate->value, "w");
 	    if (!outFile) {
 		fprintf(stderr, "%s: unable to open \"%s\" for writing\n",
 			progName, optstate->value);
