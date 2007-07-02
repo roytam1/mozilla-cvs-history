e91e98a2cace939391db1d2ab4b20ad454a3529f
Olli.Pettay%helsinki.fi <>
2007-07-02 20:21:16 +0000

    Bug 351236, r+sr=neil, a=dveditz

--- mozilla/editor/composer/src/nsComposerCommandsUpdater.cpp
+++ mozilla/editor/composer/src/nsComposerCommandsUpdater.cpp
@@ -50,14 +50,12 @@
 #include "nsString.h"
 
 #include "nsICommandManager.h"
-#include "nsPICommandUpdater.h"
 
 #include "nsIDocShell.h"
 #include "nsITransactionManager.h"
 
 nsComposerCommandsUpdater::nsComposerCommandsUpdater()
 :  mDOMWindow(nsnull)
-,  mDocShell(nsnull)
 ,  mDirtyState(eStateUninitialized)
 ,  mSelectionCollapsed(eStateUninitialized)
 ,  mFirstDoOfFirstUndo(PR_TRUE)
@@ -249,7 +247,7 @@ nsComposerCommandsUpdater::Init(nsIDOMWindow* aDOMWindow)
   nsCOMPtr<nsIScriptGlobalObject> scriptObject(do_QueryInterface(aDOMWindow));
   if (scriptObject)
   {
-    mDocShell = scriptObject->GetDocShell();
+    mDocShell = do_GetWeakReference(scriptObject->GetDocShell());
   }
   return NS_OK;
 }
@@ -302,10 +300,7 @@ nsComposerCommandsUpdater::UpdateDirtyState(PRBool aNowDirty)
 nsresult
 nsComposerCommandsUpdater::UpdateCommandGroup(const nsAString& aCommandGroup)
 {
-  if (!mDocShell) return NS_ERROR_FAILURE;
-
-  nsCOMPtr<nsICommandManager> commandManager = do_GetInterface(mDocShell);
-  nsCOMPtr<nsPICommandUpdater> commandUpdater = do_QueryInterface(commandManager);
+  nsCOMPtr<nsPICommandUpdater> commandUpdater = GetCommandUpdater();
   if (!commandUpdater) return NS_ERROR_FAILURE;
 
   
@@ -359,10 +354,7 @@ nsComposerCommandsUpdater::UpdateCommandGroup(const nsAString& aCommandGroup)
 nsresult
 nsComposerCommandsUpdater::UpdateOneCommand(const char *aCommand)
 {
-  if (!mDocShell) return NS_ERROR_FAILURE;
-  
-  nsCOMPtr<nsICommandManager>  	commandManager = do_GetInterface(mDocShell);
-  nsCOMPtr<nsPICommandUpdater>	commandUpdater = do_QueryInterface(commandManager);
+  nsCOMPtr<nsPICommandUpdater> commandUpdater = GetCommandUpdater();
   if (!commandUpdater) return NS_ERROR_FAILURE;
 
   commandUpdater->CommandStatusChanged(aCommand);
@@ -386,6 +378,18 @@ nsComposerCommandsUpdater::SelectionIsCollapsed()
   NS_WARNING("nsComposerCommandsUpdater::SelectionIsCollapsed - no domSelection");
 
   return PR_FALSE;
+}
+
+already_AddRefed<nsPICommandUpdater>
+nsComposerCommandsUpdater::GetCommandUpdater()
+{
+  nsCOMPtr<nsIDocShell> docShell = do_QueryReferent(mDocShell);
+  NS_ENSURE_TRUE(docShell, nsnull);
+  nsCOMPtr<nsICommandManager> manager = do_GetInterface(docShell);
+  nsCOMPtr<nsPICommandUpdater> updater = do_QueryInterface(manager);
+  nsPICommandUpdater* retVal = nsnull;
+  updater.swap(retVal);
+  return retVal;
 }
 
 #if 0
