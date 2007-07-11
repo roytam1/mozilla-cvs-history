59f099b225a60b4a56590b4dd79cf410ed24443f
mats.palmgren%bredband.net <>
2007-07-11 20:43:36 +0000

    OpenBSD: ld libldap50.a error.  Patch by Martynas Venckus <martynas@openbsd.org>. r=mcs@pearlcrescent.com a=dveditz

--- mozilla/directory/c-sdk/config/OpenBSD.mk
+++ mozilla/directory/c-sdk/config/OpenBSD.mk
@@ -39,6 +39,7 @@ include $(MOD_DEPTH)/config/UNIX.mk
 
 CC                     = gcc
 CCC                    = g++
+LD                     = ${CC}
 RANLIB                 = ranlib
 
 OS_REL_CFLAGS          =
@@ -62,17 +63,6 @@ ARCH                   = openbsd
 DLL_SUFFIX             = so.1.0
 
 DSO_CFLAGS             = -fPIC
-DSO_LDOPTS             = -Bshareable
-ifeq ($(OS_TEST),alpha)
 DSO_LDOPTS             = -shared
-endif
-ifeq ($(OS_TEST),mips)
-DSO_LDOPTS             = -shared
-endif
-ifeq ($(OS_TEST),pmax)  
-DSO_LDOPTS             = -shared
-endif
 
 MKSHLIB                        = $(LD) $(DSO_LDOPTS)
-
-G++INCLUDES            = -I/usr/include/g++
