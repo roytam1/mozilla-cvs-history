56cae348a8df775cdf2f7d6e0da30d72abd228c6
mats.palmgren%bredband.net <>
2007-07-11 20:19:22 +0000

    Merge rev. 1.1353 and 1.1355 from trunk to disable floats within XUL. Original fixes by roc@ocallahan.org. b=384344 r+sr=bzbarsky a=dveditz

--- mozilla/layout/base/nsCSSFrameConstructor.cpp
+++ mozilla/layout/base/nsCSSFrameConstructor.cpp
@@ -6022,6 +6022,8 @@ nsCSSFrameConstructor::ConstructXULFrame(nsFrameConstructorState& aState,
                aTag == nsXULAtoms::description) {
         if ((aTag == nsHTMLAtoms::label || aTag == nsXULAtoms::description) && 
             (! aContent->HasAttr(kNameSpaceID_None, nsHTMLAtoms::value))) {
+          // XXX we should probably be calling ConstructBlock here to handle
+          // things like columns etc
           rv = NS_NewAreaFrame(mPresShell, &newFrame,
                                NS_BLOCK_SPACE_MGR | NS_BLOCK_SHRINK_WRAP | NS_BLOCK_MARGIN_ROOT);
         }
@@ -6384,6 +6386,15 @@ nsCSSFrameConstructor::ConstructXULFrame(nsFrameConstructorState& aState,
 #ifdef MOZ_XUL
     }
 #endif
+
+    // If the new frame isn't a float containing block, then push a null
+    // float containing block to disable floats. This is needed to disable
+    // floats within XUL frames.
+    nsFrameConstructorSaveState floatSaveState;
+    PRBool isFloatContainingBlock =
+      newFrame->GetContentInsertionFrame()->IsFloatContainingBlock();
+    aState.PushFloatContainingBlock(isFloatContainingBlock ? newFrame : nsnull,
+                                    floatSaveState, PR_FALSE, PR_FALSE);
 
     // Process the child content if requested
     nsFrameItems childItems;
@@ -8136,7 +8147,8 @@ nsCSSFrameConstructor::GetFloatContainingBlock(nsIFrame* aFrame)
   // IF we hit a mathml frame, bail out; we don't allow floating out of mathml
   // frames, because they don't seem to be able to deal.
   for (nsIFrame* containingBlock = aFrame;
-       containingBlock && !containingBlock->IsFrameOfType(nsIFrame::eMathML);
+       containingBlock && !containingBlock->IsFrameOfType(nsIFrame::eMathML) &&
+       !containingBlock->IsBoxFrame();
        containingBlock = containingBlock->GetParent()) {
     if (containingBlock->IsFloatContainingBlock()) {
       return containingBlock;
