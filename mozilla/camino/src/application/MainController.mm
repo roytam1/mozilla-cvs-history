bbedded931785a4a009100a722c9067d05660bf3
stuart.morgan%alumni.case.edu <>
2007-07-11 15:07:38 +0000

    Camino only - Bug 383867: Remove bookmark bar from tab chain when hidden. Patch by Sean Murphy <murph@seanmurph.com>. r=smorgan sr=pink

--- mozilla/camino/src/application/MainController.mm
+++ mozilla/camino/src/application/MainController.mm
@@ -1315,9 +1315,7 @@ NSString* const kPreviousSessionTerminatedNormallyKey = @"PreviousSessionTermina
   if (!browserController)
     return;
 
-  float height = [[browserController bookmarkToolbar] frame].size.height;
-  BOOL showToolbar = (BOOL)(!(height > 0));
-
+  BOOL showToolbar = ![[browserController bookmarkToolbar] isVisible];
   [[browserController bookmarkToolbar] setVisible:showToolbar];
 
   // save prefs here
