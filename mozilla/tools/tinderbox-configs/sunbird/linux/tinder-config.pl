6dea7fef47d3c99f429f6a97985ad835bc2031a6
ccooper%deadsquid.com <>
2007-07-11 04:27:18 +0000

    b=381996 - use calbld user

--- mozilla/tools/tinderbox-configs/sunbird/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/sunbird/linux/tinder-config.pl
@@ -38,7 +38,7 @@ $ENV{LANG} = "en_US.iso885915";
 #- PLEASE FILL THIS IN WITH YOUR PROPER EMAIL ADDRESS
 $BuildAdministrator = 'build@mozilla.org';
 #$BuildAdministrator = "$ENV{USER}\@$ENV{HOST}";
-#$BuildAdministrator = ($ENV{USER} || "cltbld") . "\@" . ($ENV{HOST} || "dhcp");
+#$BuildAdministrator = ($ENV{USER} || "calbld") . "\@" . ($ENV{HOST} || "dhcp");
 
 #- You'll need to change these to suit your machine's needs
 #$DisplayServer = ':0.0';
@@ -151,7 +151,7 @@ $RunMozillaTests          = 0;  # Allow turning off of all tests if needed.
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 #$moz_cvsroot   = $ENV{CVSROOT};
-$moz_cvsroot   = ':ext:cltbld@cvs.mozilla.org:/cvsroot';
+$moz_cvsroot   = ':ext:calbld@cvs.mozilla.org:/cvsroot';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -222,7 +222,7 @@ $build_hour    = "3";
 $package_creation_path = "/calendar/installer";
 # needs setting for mac + talkback: $mac_bundle_path = "/browser/app";
 $ssh_version   = "2";
-#$ssh_user      = "cltbld";
+$ssh_user      = "calbld";
 #$ssh_server    = "stage.mozilla.org";
 $ftp_path      = "/home/ftp/pub/calendar/sunbird/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/calendar/sunbird/nightly";
