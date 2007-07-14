3792778ee2466146d0f72fac3e637c8916bfb629
cltbld <>
2007-07-14 08:07:01 +0000

    "Automated configuration bump, release for firefox 2.0.0.5"

--- mozilla/tools/tinderbox-configs/firefox/macosx/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/macosx/tinder-config.pl
@@ -141,7 +141,7 @@ $BuildNameExtra = 'Fx-Release';
 $ConfigureOnly = 1;
 %WGetFiles = (
 # CONFIG: 	      "http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/%version%-candidates/rc%rc%/firefox-%version%.en-US.mac.dmg" => 
-"http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/2.0.0.5-candidates/rc1/firefox-2.0.0.5.en-US.mac.dmg" => 
+"http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/2.0.0.5-candidates/rc2/firefox-2.0.0.5.en-US.mac.dmg" => 
 # CONFIG:	      "%l10n_buildDir%/%l10n_buildPlatform%/firefox.dmg"
 "/builds/tinderbox/Fx-Mozilla1.8-l10n-release/Darwin_8.7.0_Depend/firefox.dmg"
 	      );
