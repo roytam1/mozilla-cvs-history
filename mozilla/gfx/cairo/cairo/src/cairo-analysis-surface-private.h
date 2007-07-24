ed421225cb7c433e53e4427a8f68dda96c11f671
vladimir%pobox.com <>
2007-07-24 17:29:35 +0000

    b=383960,  Cairo 1.5.x: Cairo

--- mozilla/gfx/cairo/cairo/src/cairo-analysis-surface-private.h
+++ mozilla/gfx/cairo/cairo/src/cairo-analysis-surface-private.h
@@ -1,5 +1,4 @@
-/* $Id$
- *
+/*
  * Copyright © 2005 Keith Packard
  *
  * This library is free software; you can redistribute it and/or
@@ -43,10 +42,10 @@ _cairo_analysis_surface_create (cairo_surface_t		*target,
 				int			 width,
 				int			 height);
 
-cairo_private pixman_region16_t *
+cairo_private cairo_region_t *
 _cairo_analysis_surface_get_supported (cairo_surface_t *surface);
 
-cairo_private pixman_region16_t *
+cairo_private cairo_region_t *
 _cairo_analysis_surface_get_unsupported (cairo_surface_t *unsupported);
 
 cairo_private cairo_bool_t
