b82647f8261c86270a6a9eb713fd2173f64a65b0
cltbld <>
2007-08-02 03:58:50 +0000

    bug #390514, for the 1.9a7 release, redirect client.mk onto the _RELEASE tag, me=joduinn, r=preed

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -406,10 +406,10 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-#MOZ_CO_TAG          = <tag>
-NSPR_CO_TAG          = NSPR_HEAD_20070713
-NSS_CO_TAG           = NSS_3_12_ALPHA1A
-LDAPCSDK_CO_TAG      = LDAPCSDK_6_0_3_CLIENT_BRANCH
+MOZ_CO_TAG           = MOZILLA_1_9a7_RELEASE
+NSPR_CO_TAG          = MOZILLA_1_9a7_RELEASE
+NSS_CO_TAG           = MOZILLA_1_9a7_RELEASE
+LDAPCSDK_CO_TAG      = MOZILLA_1_9a7_RELEASE
 LOCALES_CO_TAG       =
 
 #######################################################################
