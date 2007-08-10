34532588d84f537cdbe952eb5b7d855f67e25a57
cltbld <>
2007-08-10 00:14:22 +0000

    "Automated configuration bump, release for thunderbird 1.5.0.13"

--- mozilla/tools/tinderbox-configs/thunderbird/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/linux/tinder-config.pl
@@ -149,9 +149,9 @@ $ConfigureOnly = 1;
 
 %WGetFiles = (
 # CONFIG:	      'http://stage.mozilla.org/pub/mozilla.org/thunderbird/nightly/%version%-candidates/rc%rc%/thunderbird-%version%.en-US.linux-i686.tar.gz' =>
-             'http://stage.mozilla.org/pub/mozilla.org/thunderbird/nightly/1.5.0.12-candidates/rc2/thunderbird-1.5.0.12.en-US.linux-i686.tar.gz' =>
+'http://stage.mozilla.org/pub/mozilla.org/thunderbird/nightly/1.5.0.13-candidates/rc1/thunderbird-1.5.0.13.en-US.linux-i686.tar.gz' =>
 # CONFIG:	      '%l10n_buildDir%/%l10n_buildPlatform%/thunderbird.tar.gz'
-             '/builds/tinderbox/Tb-Mozilla1.8.0-l10n-Release/Linux_2.4.18-14_Depend/thunderbird.tar.gz'
+'/builds/tinderbox/Tb-Mozilla1.8.0-l10n-Release/Linux_2.4.18-14_Depend/thunderbird.tar.gz'
 	      );
 
 # CONFIG: $BuildLocalesArgs = 'ZIP_IN=%l10n_buildDir%/%l10n_buildPlatform%/thunderbird.tar.gz';
@@ -170,7 +170,7 @@ $BuildTree  = 'Mozilla1.8.0-l10n';
 
 #$BuildName = '';
 # CONFIG: $BuildTag = '%productTag%_RELEASE';
-$BuildTag = 'THUNDERBIRD_1_5_0_12_RELEASE';
+$BuildTag = 'THUNDERBIRD_1_5_0_13_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
@@ -211,7 +211,7 @@ $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly";
 $tbox_ftp_path = "/home/ftp/pub/thunderbird/tinderbox-builds";
 $tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/tinderbox-builds";
 # CONFIG: $milestone     = 'thunderbird%version%-l10n';
-$milestone     = 'thunderbird1.5.0.12-l10n';
+$milestone     = 'thunderbird1.5.0.13-l10n';
 $notify_list   = 'build-announce@mozilla.org';
 $stub_installer = 0;
 $sea_installer = 0;
