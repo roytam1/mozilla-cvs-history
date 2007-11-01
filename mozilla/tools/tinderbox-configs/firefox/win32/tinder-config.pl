829f149ad2ca745e9261781e6211627365142585
ccooper%deadsquid.com <>
2007-11-01 22:04:37 +0000

    b=388654 - add some config to allow to start publishing builds as experimental/

--- mozilla/tools/tinderbox-configs/firefox/win32/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/win32/tinder-config.pl
@@ -37,3 +37,25 @@ $blat = '/d/mozilla-build/blat261/full/blat.exe';
 $use_blat = 1;
 
 $Make = 'make';
+
+$ENV{MOZ_INSTALLER_USE_7ZIP} = '1';
+$ENV{NO_EM_RESTART} = '1';
+$ENV{MOZ_PACKAGE_NSIS} = '1';
+$ENV{MOZ_CRASHREPORTER_NO_REPORT} = '1';
+
+# Release build options
+$ReleaseBuild  = 1;
+$shiptalkback  = 0;
+$ReleaseToLatest = 0; # Push the release to latest-<milestone>?
+$ReleaseToDated = 1; # Push the release to YYYY-MM-DD-HH-<milestone>?
+$build_hour    = "3";
+$package_creation_path = "/browser/installer";
+$ftp_path      = "/home/ftp/pub/firefox/nightly/experimental/ff-xr";
+$url_path      = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/experimental/ff-xr";
+$tbox_ftp_path = "/home/ftp/pub/firefox/tinderbox-builds/experimental";
+$tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/experimental";
+$milestone     = "trunk";
+#$notify_list   = 'build-announce@mozilla.org';
+$stub_installer = 0;
+$sea_installer = 1;
+$archive       = 1;
