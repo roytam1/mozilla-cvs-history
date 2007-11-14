ff56d740821d4d746a59d78ea44be631b790b5df
christophe.ravel.bugs%sun.com <>
2007-11-14 22:23:40 +0000

    Set NSPR version to 4.6.9 Beta.

--- mozilla/nsprpub/admin/repackage.sh
+++ mozilla/nsprpub/admin/repackage.sh
@@ -64,10 +64,10 @@
 # 
 # ------------------------------------------------------------------
 
-FROMTOP=/share/builds/components/nspr20/v4.6.8
-TOTOP=./v4.6.8
-NSPRDIR=nspr-4.6.8
-SOURCETAG=NSPR_4_6_8_RTM
+FROMTOP=/share/builds/components/nspr20/v4.6.9
+TOTOP=./v4.6.9
+NSPRDIR=nspr-4.6.9
+SOURCETAG=NSPR_4_6_9_RTM
 
 #
 # enumerate Unix object directories on /s/b/c
