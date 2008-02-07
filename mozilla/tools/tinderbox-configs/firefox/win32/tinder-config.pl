440b663aee3f5ef937bdc7142c2f417deea1560e
nrthomas%gmail.com <>
2008-02-07 16:43:13 +0000

    Config bump for Fx2.0.0.12 FS builds

--- mozilla/tools/tinderbox-configs/firefox/win32/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/win32/tinder-config.pl
@@ -167,7 +167,7 @@ $BuildTree  = 'MozillaRelease';
 #$BuildTree  = 'Mozilla1.8';
 
 #$BuildName = '';
-$BuildTag = 'FIREFOX_2_0_0_11_RELEASE';
+$BuildTag = 'FIREFOX_2_0_0_12_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
