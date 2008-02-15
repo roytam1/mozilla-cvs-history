e15e4207b65899f692970cf58e2fd444f4214e6a
despotdaemon <>
2008-02-15 22:58:43 +0000

    Pseudo-automatic update of changes made by rcampbell@mozilla.com.

--- CVSROOT/commitcheck.pl
+++ CVSROOT/commitcheck.pl
@@ -311,6 +311,10 @@ if ($b eq 'BUGZILLA-3_0-BRANCH') {
 if (m:^mozilla/webtools/bugzilla/.*$:) {return '212';}
 }
 if ($b eq 'HEAD') {
+if (m:^mozilla/testing/.*$:) {return '202';}
+if (m:^mozilla/extensions/irc/.*$:) {return '211';}
+if (m:^mozilla/profile/.*$:) {return '120';}
+if (m:^mozilla/tools/.*$:) {return '112';}
 if (m:^mozilla/xpcom/typelib/.*$:) {return '117';}
 if (m:^mozilla/js/src/xpconnect/.*$:) {return '118';}
 if (m:^mozilla/js/jsd/.*$:) {return '207';}
@@ -486,10 +490,6 @@ if (m:^mozilla/gfx/src/photon/.*$:) {return '113';}
 if (m:^mozilla/widget/src/photon/.*$:) {return '113';}
 if (m:^mozilla/content/xul/.*$:) {return '106';}
 if (m:^mozilla/layout/xul/.*$:) {return '106';}
-if (m:^mozilla/testing/.*$:) {return '202';}
-if (m:^mozilla/extensions/irc/.*$:) {return '211';}
-if (m:^mozilla/profile/.*$:) {return '120';}
-if (m:^mozilla/tools/.*$:) {return '112';}
 }
 if ($b eq 'NSPRPUB_PRE_4_2_CLIENT_BRANCH') {
 if (m:^mozilla/nsprpub/.*$:) {return '146';}
