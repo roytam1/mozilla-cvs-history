2debb6db9229f88b34cf40d1a4482315e81cdaf6
kaie%kuix.de <>
2008-02-18 10:10:26 +0000

    Bug 416202, 1.8 branch only: Upgrade to NSS_3_11_5_WITH_CKBI_1_65_RTM r=rrelyea, approval1.8.1.13+=dveditz

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -259,7 +259,7 @@ MODULES_all :=                                  \
 # and commit this file on that tag.
 MOZ_CO_TAG           = MOZILLA_1_8_BRANCH
 NSPR_CO_TAG          = NSPR_4_6_8_RTM
-NSS_CO_TAG           = NSS_3_11_5_WITH_CKBI_1_64_RTM
+NSS_CO_TAG           = NSS_3_11_5_WITH_CKBI_1_65_RTM
 LDAPCSDK_CO_TAG      = MOZILLA_1_8_BRANCH
 LOCALES_CO_TAG       = MOZILLA_1_8_BRANCH
 
