de0a1ea83647fb6aafc09d70cff4df6ad775ad20
nrthomas%gmail.com <>
2008-02-21 13:29:51 +0000

    Bug 399848, convert moz180-win32-tbox to ffxbld

--- mozilla/tools/tinderbox-configs/firefox/win32/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/win32/tinder-config.pl
@@ -112,7 +112,7 @@ $use_blat       = 1;
 # Note that win32 may not need \@, depends on ' or ".
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
-$moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
+$moz_cvsroot   = ":ext:ffxbld\@cvs.mozilla.org:/cvsroot";
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -187,7 +187,8 @@ $build_hour    = "9";
 $package_creation_path = "/browser/installer";
 # needs setting for mac + talkback: $mac_bundle_path = "/browser/app";
 $ssh_version   = "2";
-$ssh_user      = "cltbld";
+$ssh_user      = "ffxbld";
+$ssh_key       = "'$ENV{HOME}/.ssh/ffxbld_dsa'";
 $ssh_server    = "stage.mozilla.org";
 $ftp_path      = "/home/ftp/pub/firefox/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly";
