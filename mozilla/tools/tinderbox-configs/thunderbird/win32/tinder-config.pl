765d257d0ac4338d37600f3a13c9e24cdc9c65a0
nrthomas%gmail.com <>
2008-02-21 12:38:09 +0000

    Bug 399848, convert patrocles to tbirdbld key

--- mozilla/tools/tinderbox-configs/thunderbird/win32/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/win32/tinder-config.pl
@@ -112,7 +112,7 @@ $use_blat       = 1;
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 # CONFIG: $moz_cvsroot   = '%mozillaCvsroot%';
-$moz_cvsroot   = ':ext:cltbld@cvs.mozilla.org:/cvsroot';
+$moz_cvsroot   = ':ext:tbirdbld@cvs.mozilla.org:/cvsroot';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -174,7 +174,8 @@ $package_creation_path = "/mail/installer";
 # needs setting for mac + talkback: $mac_bundle_path = "/browser/app";
 $ssh_version   = "2";
 # CONFIG: $ssh_user      = "%sshUser%";
-$ssh_user      = "cltbld";
+$ssh_user      = "tbirdbld";
+$ssh_key       = "'$ENV{HOME}/.ssh/tbirdbld_dsa'";
 # CONFIG: $ssh_server    = "%sshServer%";
 $ssh_server    = "production-1.8-master.build.mozilla.org";
 $ftp_path      = "/home/ftp/pub/thunderbird/nightly";
