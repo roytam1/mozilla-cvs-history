070c5c1c4bcaa1b0b0c16a7aa6b10f2a68bccbdf
ause%sun.com <>
2008-03-05 08:47:18 +0000

    bug 420840 - switch to SUNBIRD_0_8_BRANCH

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -257,11 +257,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = MOZILLA_1_8_BRANCH
+MOZ_CO_TAG           = SUNBIRD_0_8_BRANCH
 NSPR_CO_TAG          = NSPR_4_6_8_RTM
 NSS_CO_TAG           = NSS_3_11_5_WITH_CKBI_1_65_RTM
-LDAPCSDK_CO_TAG      = MOZILLA_1_8_BRANCH
-LOCALES_CO_TAG       = MOZILLA_1_8_BRANCH
+LDAPCSDK_CO_TAG      = SUNBIRD_0_8_BRANCH
+LOCALES_CO_TAG       = SUNBIRD_0_8_BRANCH
 
 BUILD_MODULES = all
 
