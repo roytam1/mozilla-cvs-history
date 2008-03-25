d5000a7fa10b4190cb3c4611364eef6437b1bf7d
mark%moxienet.com <>
2008-03-25 02:54:58 +0000

    Numbers for Camino 1.6b3

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -257,11 +257,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_2_0_0_13_RELEASE
-NSPR_CO_TAG          = FIREFOX_2_0_0_13_RELEASE
-NSS_CO_TAG           = FIREFOX_2_0_0_13_RELEASE
-LDAPCSDK_CO_TAG      = FIREFOX_2_0_0_13_RELEASE
-LOCALES_CO_TAG       = FIREFOX_2_0_0_13_RELEASE
+MOZ_CO_TAG           = CAMINO_1_6_B3_RELEASE
+NSPR_CO_TAG          = CAMINO_1_6_B3_RELEASE
+NSS_CO_TAG           = CAMINO_1_6_B3_RELEASE
+LDAPCSDK_CO_TAG      = CAMINO_1_6_B3_RELEASE
+LOCALES_CO_TAG       = CAMINO_1_6_B3_RELEASE
 
 BUILD_MODULES = all
 
