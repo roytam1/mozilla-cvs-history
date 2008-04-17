71708e4dceef9f187c9d57e35cd3cf81689bd201
cltbld <>
2008-04-17 20:26:35 +0000

    Automated checkin: version bump, remove pre tag for thunderbird 2.0.0.14 release on GECKO181_20080311_RELBRANCH

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -257,11 +257,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = MOZILLA_1_8_BRANCH
-NSPR_CO_TAG          = MOZILLA_1_8_BRANCH
-NSS_CO_TAG           = MOZILLA_1_8_BRANCH
-LDAPCSDK_CO_TAG      = MOZILLA_1_8_BRANCH
-LOCALES_CO_TAG       = MOZILLA_1_8_BRANCH
+MOZ_CO_TAG           = THUNDERBIRD_2_0_0_14_RELEASE
+NSPR_CO_TAG          = THUNDERBIRD_2_0_0_14_RELEASE
+NSS_CO_TAG           = THUNDERBIRD_2_0_0_14_RELEASE
+LDAPCSDK_CO_TAG      = THUNDERBIRD_2_0_0_14_RELEASE
+LOCALES_CO_TAG       = THUNDERBIRD_2_0_0_14_RELEASE
 
 BUILD_MODULES = all
 
