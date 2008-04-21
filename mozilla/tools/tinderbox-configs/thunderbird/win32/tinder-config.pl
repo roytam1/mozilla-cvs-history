56fe47c0fdc96c595a22146412bbecc1165dafa4
cltbld <>
2008-04-21 10:15:33 +0000

    "Automated configuration bump, release for thunderbird 2.0.0.14rc1"

--- mozilla/tools/tinderbox-configs/thunderbird/win32/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/win32/tinder-config.pl
@@ -143,11 +143,11 @@ $ConfigureOnly = 1;
 $LocalizationVersionFile = 'mail/config/version.txt';
 %WGetFiles = (
 # CONFIG:          "http://%stagingServer%/pub/mozilla.org/thunderbird/nightly/%version%-candidates/rc%rc%/unsigned/thunderbird-%version%.en-US.win32.installer.exe" =>
-"http://production-1.8-master.build.mozilla.org/pub/mozilla.org/thunderbird/nightly/2.0.0.13-candidates/rc1/unsigned/thunderbird-2.0.0.13.en-US.win32.installer.exe" =>
+"http://production-1.8-master.build.mozilla.org/pub/mozilla.org/thunderbird/nightly/2.0.0.14-candidates/rc1/unsigned/thunderbird-2.0.0.14.en-US.win32.installer.exe" =>
 # CONFIG:            "%l10n_buildDir%/%l10n_buildPlatform%/thunderbird-installer.exe",
 "/cygdrive/c/builds/tinderbox/Tb-Mozilla1.8-l10n-Release/WINNT_5.2_Depend/thunderbird-installer.exe",
 # CONFIG:         "http://%stagingServer%/pub/mozilla.org/thunderbird/nightly/%version%-candidates/rc%rc%/unsigned/thunderbird-%version%.en-US.win32.zip" =>
-"http://production-1.8-master.build.mozilla.org/pub/mozilla.org/thunderbird/nightly/2.0.0.13-candidates/rc1/unsigned/thunderbird-2.0.0.13.en-US.win32.zip" =>
+"http://production-1.8-master.build.mozilla.org/pub/mozilla.org/thunderbird/nightly/2.0.0.14-candidates/rc1/unsigned/thunderbird-2.0.0.14.en-US.win32.zip" =>
 # CONFIG:            "%l10n_buildDir%/%l10n_buildPlatform%/thunderbird.zip"
 "/cygdrive/c/builds/tinderbox/Tb-Mozilla1.8-l10n-Release/WINNT_5.2_Depend/thunderbird.zip"
              );
@@ -169,7 +169,7 @@ $BuildTree  = 'MozillaRelease';
 
 #$BuildName = '';
 # CONFIG: $BuildTag = '%productTag%_RELEASE';
-$BuildTag = 'THUNDERBIRD_2_0_0_13_RELEASE';
+$BuildTag = 'THUNDERBIRD_2_0_0_14_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
@@ -211,7 +211,7 @@ $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly";
 $tbox_ftp_path = "/home/ftp/pub/thunderbird/tinderbox-builds";
 $tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/tinderbox-builds";
 # CONFIG: $milestone     = "thunderbird%version%-l10n";
-$milestone     = "thunderbird2.0.0.13-l10n";
+$milestone     = "thunderbird2.0.0.14-l10n";
 $notify_list   = "build-announce\@mozilla.org";
 $stub_installer = 0;
 $sea_installer = 1;
