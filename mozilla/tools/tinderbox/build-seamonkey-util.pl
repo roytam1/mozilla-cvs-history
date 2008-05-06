73fbe388705362294540113fe1ebc1ccc9fe6aff
gavin%gavinsharp.com <>
2008-05-06 17:25:17 +0000

    Bug 431713: add EULA override pref to tinderbox script to prevent the EULA dialog from causing trouble, r=nthomas

--- mozilla/tools/tinderbox/build-seamonkey-util.pl
+++ mozilla/tools/tinderbox/build-seamonkey-util.pl
@@ -2193,6 +2193,9 @@ sub run_all_tests {
 
                 # Do not run frecency recalculation during tests.
                 set_pref($pref_file, 'places.frecency.updateIdleTime', 0);
+
+                # Override any EULA dialogs.
+                set_pref($pref_file, 'browser.EULA.override', 'true');
             }
             elsif ($Settings::BinaryName eq 'Camino') {
                 # Suppress default browser dialog
