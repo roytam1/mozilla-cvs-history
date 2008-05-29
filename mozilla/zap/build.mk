8b3ed39592920fe6cfa2e15ccb9a06ea16484de9
alex%croczilla.com <>
2008-05-29 16:56:36 +0000

    Build zapi libs in toolkit tier, so that they are available when xulrunner packages libs.

--- mozilla/zap/build.mk
+++ mozilla/zap/build.mk
@@ -5,10 +5,20 @@
 
 include $(topsrcdir)/xulrunner/build.mk
 
+######################################################################
 # zap api ('zapi') buid:
-tier_app_dirs += zap/base zap/netutils zap/zmk zap/sdp zap/sip 
+
+# We put this in the toolkit tier, so that it gets built before
+# xulrunner. E.g. on OS X, xulrunner will package dist/bin/components/
+# into dist/XUL.framework as part of the 'libs' build phase, so zapi
+# libs must be built by then.
+
+tier_toolkit_dirs += zap/base zap/netutils zap/zmk zap/sdp zap/sip 
+
 # xxx might want to split zap/base, zap/netutils and zap/zmk into
 # tier_gecko_dirs, if we end up using them in mozilla code.
 
+######################################################################
 # zap sip client build:
+
 tier_app_dirs += zap/client
