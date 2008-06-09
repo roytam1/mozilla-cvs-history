eaa5cb680dc906934309245e0f6645fa8b190941
axel%pike.org <>
2008-06-09 18:52:32 +0000

    bug 435004, extend filter.py on the 1.8 branch to catch common no-bugs, r=ted.mielczarek, a=dveditz

--- mozilla/browser/locales/filter.py
+++ mozilla/browser/locales/filter.py
@@ -5,8 +5,10 @@ def test(mod, path, entity = None):
                  "browser", "extensions/reporter", "extensions/spellcheck",
                  "other-licenses/branding/firefox"):
     return False
-  if mod != "browser" and mod != "extensions/spellcheck":
+  if mod not in ["browser", "extensions/spellcheck", 
+                 "other-licenses/branding/firefox"]:
     # we only have exceptions for browser and extensions/spellcheck
+    # and other-licenses/branding/firefox
     return True
   if not entity:
     if mod == "extensions/spellcheck":
@@ -20,9 +22,15 @@ def test(mod, path, entity = None):
   if path == "defines.inc":
     return entity != "MOZ_LANGPACK_CONTRIBUTORS"
 
-  if path != "chrome/browser-region/region.properties":
-    # only region.properties exceptions remain, compare all others
-    return True
+  if mod == "browser":
+    if path == "chrome/browser-region/region.properties":
   
-  return not (re.match(r"browser\.search\.order.[1-9]", entity) or
-              re.match(r"browser\.contentHandlers\.types.[0-5]", entity))
+      return not (re.match(r"browser\.search\.order.[1-9]", entity) or
+                  re.match(r"browser\.contentHandlers\.types.[0-5]", entity))
+    return not path == "chrome/browser/preferences/tabs.dtd" or \
+        not entity == "brandDTD"
+
+  if path == "brand.properties":
+    return not (re.match(r"homePageMultipleStart[123](Label|URL)", entity) or
+                re.match(r"homePageMultipleStartMain", entity) or
+                re.match(r"homePageOptionCount", entity))
