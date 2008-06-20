28091d8a0c065296906905b5d7d49114788c5c6e
cvs2git <>
2008-06-20 21:30:37 +0000

    This commit was manufactured by cvs2svn to create tag 'SEAMONKEY_1_1_10_RELEASE'.

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -257,11 +257,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_2_0_0_15_RELEASE
-NSPR_CO_TAG          = FIREFOX_2_0_0_15_RELEASE
-NSS_CO_TAG           = FIREFOX_2_0_0_15_RELEASE
-LDAPCSDK_CO_TAG      = FIREFOX_2_0_0_15_RELEASE
-LOCALES_CO_TAG       = FIREFOX_2_0_0_15_RELEASE
+MOZ_CO_TAG           = SEAMONKEY_1_1_10_RELEASE
+NSPR_CO_TAG          = SEAMONKEY_1_1_10_RELEASE
+NSS_CO_TAG           = SEAMONKEY_1_1_10_RELEASE
+LDAPCSDK_CO_TAG      = SEAMONKEY_1_1_10_RELEASE
+LOCALES_CO_TAG       = SEAMONKEY_1_1_10_RELEASE
 
 NSS_FIPS_CO_TAG      = NSS_3_11_4_RTM
 NSS_FIPS_CO_DIR      = nss-fips
