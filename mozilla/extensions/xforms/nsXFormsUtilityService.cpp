62130419726968ca0ed0a6aeaf812f68741a2284
aaronr%us.ibm.com <>
2008-07-27 01:40:28 +0000

    [XForms] NPOTDB now(), local-date-time() have issues on mac 427874, r=olli+doron

--- mozilla/extensions/xforms/nsXFormsUtilityService.cpp
+++ mozilla/extensions/xforms/nsXFormsUtilityService.cpp
@@ -476,7 +476,7 @@ nsXFormsUtilityService::GetTime(nsAString & aResult, PRBool aUTC)
 
     PR_FormatTime(ctime, sizeof(ctime), "%Y-%m-%dT%H:%M:%S\0", &time);
 
-    aResult.AssignLiteral(ctime);
+    aResult.Assign(NS_ConvertASCIItoUTF16(ctime));
 
     if (aUTC) {
       aResult.AppendLiteral("Z");
