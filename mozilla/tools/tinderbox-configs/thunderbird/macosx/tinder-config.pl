3272145a4e6ad06830b6edb33074ee0f1dbb5b91
nrthomas%gmail.com <>
2008-07-29 04:58:25 +0000

    Bug 445991, use Thunderbird.app on mac since not all the branding changes made the cutoff

--- mozilla/tools/tinderbox-configs/thunderbird/macosx/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/macosx/tinder-config.pl
@@ -55,7 +55,7 @@ $UseTimeStamp      = 0;      # Use the CVS 'pull-by-timestamp' option, or not
 # Tests
 $CleanProfile             = 1;
 #$ResetHomeDirForTests     = 1;
-$ProductName              = "Shredder";
+$ProductName              = "Thunderbird";
 #$VendorName               = 'Mozilla';
 
 $RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
