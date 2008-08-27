1bf7424299b816bcd06dba0aa8a39a1df3fe1a0c
kairo%kairo.at <>
2008-08-27 23:48:12 +0000

    edit tags to pull from SEAMONKEY_1_1_12_RELEASE

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -257,11 +257,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_2_0_0_17_RELEASE
-NSPR_CO_TAG          = FIREFOX_2_0_0_17_RELEASE
-NSS_CO_TAG           = FIREFOX_2_0_0_17_RELEASE
-LDAPCSDK_CO_TAG      = FIREFOX_2_0_0_17_RELEASE
-LOCALES_CO_TAG       = FIREFOX_2_0_0_17_RELEASE
+MOZ_CO_TAG           = SEAMONKEY_1_1_12_RELEASE
+NSPR_CO_TAG          = SEAMONKEY_1_1_12_RELEASE
+NSS_CO_TAG           = SEAMONKEY_1_1_12_RELEASE
+LDAPCSDK_CO_TAG      = SEAMONKEY_1_1_12_RELEASE
+LOCALES_CO_TAG       = SEAMONKEY_1_1_12_RELEASE
 
 NSS_FIPS_CO_TAG      = NSS_3_11_4_RTM
 NSS_FIPS_CO_DIR      = nss-fips
