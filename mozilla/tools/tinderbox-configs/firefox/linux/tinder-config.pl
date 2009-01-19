a2992e36b596707bc0f2e742a8a8431f519e7324
cltbld <>
2009-01-19 20:12:33 +0000

    "Automated configuration bump, release for firefox 3.0.6build1"

--- mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/linux/tinder-config.pl
@@ -177,7 +177,7 @@ $BuildTree  = 'MozillaRelease';
 
 #$BuildName = '';
 # CONFIG: $BuildTag = '%productTag%_RELEASE';
-$BuildTag = 'FIREFOX_3_0_5_RELEASE';
+$BuildTag = 'FIREFOX_3_0_6_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
@@ -214,7 +214,7 @@ $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly";
 $tbox_ftp_path = "/home/ftp/pub/firefox/tinderbox-builds";
 $tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds";
 # CONFIG: $milestone     = "firefox%version%";
-$milestone     = "firefox3.0.5";
+$milestone     = "firefox3.0.6";
 $notify_list   = 'build-announce@mozilla.org';
 $stub_installer = 0;
 $sea_installer = 0;
