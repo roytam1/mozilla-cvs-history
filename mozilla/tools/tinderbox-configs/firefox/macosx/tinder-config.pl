2a5365622e5a379d746c103b3957ab76d86b82c5
nrthomas%gmail.com <>
2009-04-11 09:05:10 +0000

    Bug 433595, stop doing old-school tinderbox tests on bm-xserve05 so that dm-pageload01 can be decomissioned, p=joduin, r=me

--- mozilla/tools/tinderbox-configs/firefox/macosx/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/firefox/macosx/tinder-config.pl
@@ -57,7 +57,7 @@ $ProductName              = 'BonEcho';
 $MacOSProductName         = 'BonEcho';
 $VendorName               = "";
 
-$RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
+$RunMozillaTests          = 0;  # Allow turning off of all tests if needed.
 $RegxpcomTest             = 1;
 $AliveTest                = 1;
 #$JavaTest                 = 0;
