3920486a1dc8133d1ecc8979d800c5d9ce183166
cltbld <>
2009-06-25 05:41:13 +0000

    "Automated configuration bump, release for thunderbird nightlybuild1"

--- mozilla/tools/tinderbox-configs/thunderbird/linux/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/thunderbird/linux/tinder-config.pl
@@ -102,7 +102,7 @@ $RunMozillaTests          = 1;  # Allow turning off of all tests if needed.
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 # CONFIG: $moz_cvsroot   = '%mozillaCvsroot%';
-$moz_cvsroot   = ":ext:tbirdbld\@cvs.mozilla.org:/cvsroot";
+$moz_cvsroot   = ':ext:cltbld@cvs.mozilla.org:/cvsroot';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -167,7 +167,7 @@ $ssh_user      = "tbirdbld";
 # CONFIG: $ssh_key       = "'$ENV{HOME}/.ssh/%sshUser%_dsa'";
 $ssh_key       = "'$ENV{HOME}/.ssh/tbirdbld_dsa'";
 # CONFIG: $ssh_server    = "%sshServer%";
-$ssh_server    = "stage.mozilla.org";
+$ssh_server    = "stage-old.mozilla.org";
 $ftp_path      = "/home/ftp/pub/thunderbird/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly";
 $tbox_ftp_path = "/home/ftp/pub/thunderbird/tinderbox-builds";
