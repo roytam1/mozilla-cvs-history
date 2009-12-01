3f59f2e30f3a55fc3aa2beb649f7d221326ab02a
cltbld <>
2009-12-01 23:04:56 +0000

    "Automated configuration bump, release for xulrunner 1.9.0.16build2"

--- mozilla/tools/tinderbox-configs/xulrunner/win32/tinder-config.pl
+++ mozilla/tools/tinderbox-configs/xulrunner/win32/tinder-config.pl
@@ -169,7 +169,7 @@ $BuildTree  = 'MozillaRelease';
 
 #$BuildName = '';
 # CONFIG: $BuildTag = '%productTag%_RELEASE';
-$BuildTag = 'FIREFOX_3_0_15_RELEASE';
+$BuildTag = 'FIREFOX_3_0_16_RELEASE';
 #$BuildConfigDir = 'mozilla/config';
 #$Topsrcdir = 'mozilla';
 
@@ -208,7 +208,7 @@ $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/xulrunner/nightly";
 $tbox_ftp_path = "/home/ftp/pub/xulrunner/tinderbox-builds";
 $tbox_url_path = "http://ftp.mozilla.org/pub/mozilla.org/xulrunner/tinderbox-builds";
 # CONFIG: $milestone     = 'xulrunner%version%';
-$milestone     = 'xulrunner1.9.0.15';
+$milestone     = 'xulrunner1.9.0.16';
 $notify_list   = 'build-announce@mozilla.org';
 $stub_installer = 0;
 $sea_installer = 0;
