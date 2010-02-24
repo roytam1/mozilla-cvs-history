efe651d98055406b53a5b4ca53e259c02a0a2231
dveditz%cruzio.com <>
2010-02-24 23:59:16 +0000

    bug 284876 fix topcrash null deref. patch by Kent James, r/sr=bienvenu, a=dveditz

--- mozilla/mailnews/local/src/nsLocalMailFolder.cpp
+++ mozilla/mailnews/local/src/nsLocalMailFolder.cpp
@@ -2903,6 +2903,8 @@ NS_IMETHODIMP nsMsgLocalMailFolder::EndMove(PRBool moveSucceeded)
 // this is the beginning of the next message copied
 NS_IMETHODIMP nsMsgLocalMailFolder::StartMessage()
 {
+  // We get crashes that we don't understand (bug 284876), so stupidly prevent that.
+  NS_ENSURE_ARG_POINTER(mCopyState);
   return WriteStartOfNewMessage();
 }
 
