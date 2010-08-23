1b7c990067582c6819e63878891570159760323a
bhearsum%mozilla.com <>
2010-08-23 17:34:34 +0000

    Bump UPDATE_PACKAGING tag on _R12 minibranch

--- mozilla/client.mk
+++ mozilla/client.mk
@@ -403,9 +403,9 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = UPDATE_PACKAGING_R11
-NSPR_CO_TAG          = UPDATE_PACKAGING_R11
-NSS_CO_TAG           = UPDATE_PACKAGING_R11
+MOZ_CO_TAG           = UPDATE_PACKAGING_R12
+NSPR_CO_TAG          = UPDATE_PACKAGING_R12
+NSS_CO_TAG           = UPDATE_PACKAGING_R12
 LDAPCSDK_CO_TAG      = ldapcsdk_5_17_client_branch
 LOCALES_CO_TAG       =
 
