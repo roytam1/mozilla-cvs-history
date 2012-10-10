102c5088ebb73c057247943fca768950e037dfc8
bhearsum%mozilla.com <>
2012-10-10 16:21:24 +0000

    bug 799999: Please throttle updates to TB 16. r=catlee

--- mozilla/webtools/aus/xml/inc/config-dist.php
+++ mozilla/webtools/aus/xml/inc/config-dist.php
@@ -103,6 +103,18 @@ $productThrottling = array(
          '15.0.1' => 0,
      ),
      'Thunderbird' => array(
+         '10.0' => 0,
+         '10.0.1' => 0,
+         '10.0.2' => 0,
+         '11.0' => 0,
+         '11.0.1', => 0,
+         '12.0' => 0,
+         '12.0.1' => 0,
+         '13.0' => 0,
+         '13.0.1' => 0,
+         '14.0' => 0,
+         '15.0' => 0,
+         '15.0.1' => 0,
      )
 );
 
@@ -184,6 +196,17 @@ $throttleExceptions = array(
           'beta',
       ),
       '15.0.1' => array(
+          'betatest',
+          'releasetest',
+          'beta',
+      ),
+      # Thunderbird only
+      '11.0.1' => array(
+          'betatest',
+          'releasetest',
+          'beta',
+      ),
+      '12.0.1' => array(
           'betatest',
           'releasetest',
           'beta',
